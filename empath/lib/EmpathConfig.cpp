87caed6022ef22d3a3c7826241f6e649b11199e2
Rik Hemsley <rik@kde.org>
1999-05-08 23:45:45 +0000

    *** empty log message ***
    svn path=/trunk/kdepim/; revision=21084

---
+++ empath/lib/EmpathConfig.cpp
@@ -0,0 +1,368 @@
+#include "EmpathConfig.h"
+
+	const QString
+EmpathConfig::GROUP_GENERAL = "General";
+
+	const QString
+EmpathConfig::GROUP_MAILBOX = "Mailbox_";
+
+	const QString
+EmpathConfig::GROUP_FILTER = "Filter_";
+
+	const QString
+EmpathConfig::GROUP_ACTION = "Action_";
+
+	const QString
+EmpathConfig::GROUP_EXPR = "Expr_";
+
+	const QString
+EmpathConfig::GROUP_DISPLAY = "Display";
+
+	const QString
+EmpathConfig::GROUP_COMPOSE = "Compose";
+
+	const QString
+EmpathConfig::GROUP_IDENTITY	= "Identity";
+
+	const QString
+EmpathConfig::GROUP_SENDING = "Sending";
+
+	const QString
+EmpathConfig::GROUP_ACCOUNT = "Account";
+
+	const QString
+EmpathConfig::KEY_MAIN_WINDOW_TOOLBAR_POSITION = "MainWindowToolbarPosition";
+
+	const QString
+EmpathConfig::KEY_COMPOSE_WINDOW_TOOLBAR_POSITION = "ComposeWindowToolbarPosition";
+
+	const QString
+EmpathConfig::KEY_NUM_MAILBOXES	= "NumberOfMailboxes";
+
+	const QString
+EmpathConfig::KEY_TIP_OF_THE_DAY_AT_STARTUP = "TipOfTheDayAtStartup";
+
+	const QString
+EmpathConfig::KEY_MESSAGE_LIST_SIZE_COLUMN = "MessageListSizeOfColumn";
+
+	const QString
+EmpathConfig::KEY_MESSAGE_LIST_POS_COLUMN = "MessageListPositionOfColumn";
+
+	const QString
+EmpathConfig::KEY_MAILBOX_TYPE = "MailboxType";
+
+	const QString
+EmpathConfig::KEY_MAILBOX_NAME = "MailboxName";
+
+	const QString
+EmpathConfig::KEY_MAILBOX_LIST = "MailboxList";
+
+	const QString
+EmpathConfig::KEY_FOLDER_LIST = "FolderList";
+
+	const QString
+EmpathConfig::KEY_VARIABLE_FONT = "VariableWidthFont";
+
+	const QString
+EmpathConfig::KEY_FIXED_FONT = "FixedWidthFont";
+
+	const QString
+EmpathConfig::KEY_QUOTED_FONT = "QuotedFont";
+
+	const QString
+EmpathConfig::KEY_UNDERLINE_LINKS = "UnderlineLinks";
+
+	const QString
+EmpathConfig::KEY_FONT_STYLE = "FontStyle";
+
+	const QString
+EmpathConfig::KEY_BACKGROUND_COLOUR = "BackgroundColour";
+
+	const QString
+EmpathConfig::KEY_TEXT_COLOUR = "TextColour";
+
+	const QString
+EmpathConfig::KEY_LINK_COLOUR = "LinkColour";
+
+	const QString
+EmpathConfig::KEY_VISITED_LINK_COLOUR = "VisitedLinkColour";
+
+	const QString
+EmpathConfig::KEY_USE_DEFAULT_FONTS = "UseDefaultFonts";
+
+	const QString
+EmpathConfig::KEY_USE_DEFAULT_COLOURS = "UseDefaultColours";
+
+	const QString
+EmpathConfig::KEY_ICON_SET = "IconSet";
+
+	const QString
+EmpathConfig::KEY_NAME = "Name";
+
+	const QString
+EmpathConfig::KEY_EMAIL = "Email";
+
+	const QString
+EmpathConfig::KEY_REPLY_TO = "ReplyTo";
+
+	const QString
+EmpathConfig::KEY_ORGANISATION = "Organisation";
+
+	const QString
+EmpathConfig::KEY_SIG_PATH = "Signature";
+
+	const QString
+EmpathConfig::KEY_PHRASE_REPLY_SENDER = "PhraseReplySender";
+
+	const QString
+EmpathConfig::KEY_PHRASE_REPLY_ALL = "PhraseReplyAll";
+
+	const QString
+EmpathConfig::KEY_PHRASE_FORWARD = "PhraseForward";
+
+	const QString
+EmpathConfig::KEY_AUTO_QUOTE = "AutoQuoteOnReply";
+
+	const QString
+EmpathConfig::KEY_ADD_SIG = "AddSignature";
+
+	const QString
+EmpathConfig::KEY_ADD_DIG_SIG = "AddDigitalSignature";
+
+	const QString
+EmpathConfig::KEY_WRAP_LINES = "WrapLongLines";
+
+	const QString
+EmpathConfig::KEY_WRAP_COLUMN = "WrapColumn";
+
+	const QString
+EmpathConfig::KEY_CC_ME = "CCToMe";
+
+	const QString
+EmpathConfig::KEY_CC_OTHER = "CCToOther";
+
+	const QString
+EmpathConfig::KEY_CC_OTHER_ADDRESS = "CCOtherAddress";
+
+	const QString
+EmpathConfig::KEY_COPY_FOLDER = "CopyFolder";
+
+	const QString
+EmpathConfig::KEY_COPY_FOLDER_NAME = "CopyFolderName";
+
+	const QString
+EmpathConfig::KEY_SEND_POLICY = "SendWhen";
+
+	const QString
+EmpathConfig::KEY_USE_EXTERNAL_EDITOR = "UseExternalEditor";
+
+	const QString
+EmpathConfig::KEY_EXTERNAL_EDITOR = "ExternalEditor";
+
+	const QString
+EmpathConfig::KEY_ACCOUNT_TYPE = "AccountType";
+
+	const QString
+EmpathConfig::KEY_SERVER_ADDRESS = "ServerAddress";
+
+	const QString
+EmpathConfig::KEY_SERVER_PORT = "ServerPort";
+
+	const QString
+EmpathConfig::KEY_SERVER_UNAME = "ServerUsername";
+
+	const QString
+EmpathConfig::KEY_SERVER_PASSWORD = "ServerPassword";
+
+	const QString
+EmpathConfig::KEY_STAR_PASSWORD = "StarPassword";
+
+	const QString
+EmpathConfig::KEY_CHECK_MAIL = "CheckMailAtIntervals";
+
+	const QString
+EmpathConfig::KEY_CHECK_MAIL_INTERVAL = "CheckMailInterval";
+
+	const QString
+EmpathConfig::KEY_INTERNAL_MAIL_FILE = "EmpathMaildir";
+
+	const QString
+EmpathConfig::KEY_OUTGOING_SERVER_TYPE = "OutgoingServerType";
+
+	const QString
+EmpathConfig::KEY_SENDMAIL_LOCATION = "SendmailLocation";
+
+	const QString
+EmpathConfig::KEY_QMAIL_LOCATION = "QmailLocation";
+
+	const QString
+EmpathConfig::KEY_SMTP_SERVER_LOCATION = "SMTPServerLocation";
+
+	const QString
+EmpathConfig::KEY_SMTP_SERVER_PORT = "SMTPServerPort";
+
+	const QString
+EmpathConfig::KEY_QMTP_SERVER_LOCATION = "QMTPServerLocation";
+
+	const QString
+EmpathConfig::KEY_QMTP_SERVER_PORT = "QMTPServerPort";
+
+	const QString
+EmpathConfig::KEY_LOCAL_MAILBOX_PATH = "MailboxPath";
+
+	const QString
+EmpathConfig::KEY_LOCAL_CHECK_FOR_NEW_MAIL = "CheckForNewMail";
+
+	const QString
+EmpathConfig::KEY_LOCAL_MAIL_CHECK_INTERVAL = "MailCheckInterval";
+
+	const QString
+EmpathConfig::KEY_LOCAL_AUTO_GET_NEW_MAIL = "AutoGetNewMail";
+
+	const QString
+EmpathConfig::KEY_LOCAL_SAVE_ALL_ADDRESSES = "SaveAllAddresses";
+
+	const QString
+EmpathConfig::KEY_LOCAL_NOTIFY = "Notify";
+
+	const QString
+EmpathConfig::KEY_POP3_SERVER_ADDRESS = "ServerAddress";
+
+	const QString
+EmpathConfig::KEY_POP3_SERVER_PORT = "ServerPort";
+
+	const QString
+EmpathConfig::KEY_POP3_USERNAME = "Username";
+
+	const QString
+EmpathConfig::KEY_POP3_PASSWORD = "Password";
+
+	const QString
+EmpathConfig::KEY_POP3_APOP = "UseAPOP";
+
+	const QString
+EmpathConfig::KEY_POP3_SAVE_POLICY = "SavePolicy";
+
+	const QString
+EmpathConfig::KEY_POP3_LOGGING_POLICY = "LoggingPolicy";
+
+	const QString
+EmpathConfig::KEY_POP3_LOG_FILE_PATH = "LogFilePath";
+
+	const QString
+EmpathConfig::KEY_POP3_LOG_FILE_DISPOSAL_POLICY = "LogFileDisposalPolicy";
+
+	const QString
+EmpathConfig::KEY_POP3_MAX_LOG_FILE_SIZE = "MaxLogFileSize";
+
+	const QString
+EmpathConfig::KEY_POP3_MESSAGE_SIZE_THRESHOLD = "MessageSizeThreshold";
+
+	const QString
+EmpathConfig::KEY_POP3_LARGE_MESSAGE_POLICY = "LargeMessagePolicy";
+
+	const QString
+EmpathConfig::KEY_POP3_CHECK_FOR_NEW_MAIL = "CheckForNewMail";
+
+	const QString
+EmpathConfig::KEY_POP3_MAIL_CHECK_INTERVAL = "MailCheckInterval";
+
+	const QString
+EmpathConfig::KEY_POP3_DELETE_FROM_SERVER = "DeleteFromServer";
+
+	const QString
+EmpathConfig::KEY_POP3_AUTO_GET_NEW_MAIL = "AutoGetNewMail";
+
+	const QString
+EmpathConfig::KEY_POP3_SAVE_ALL_ADDRESSES = "SaveAllAddresses";
+
+	const QString
+EmpathConfig::KEY_POP3_NOTIFY = "Notify";
+
+	const QString
+EmpathConfig::KEY_POP3_RETRIEVE_IF_HAVE = "RetrieveIfHave";
+
+	const QString
+EmpathConfig::KEY_NUMBER_OF_FILTERS = "NumberOfFilters";
+
+	const QString
+EmpathConfig::KEY_NUM_MATCH_EXPRS_FOR_FILTER = "NumberOfMatchExpressions";
+
+	const QString
+EmpathConfig::KEY_MATCH_EXPR_TYPE = "MatchExpressionType";
+
+	const QString
+EmpathConfig::KEY_MATCH_SIZE = "MatchSize";
+
+	const QString
+EmpathConfig::KEY_MATCH_EXPR = "MatchExpr";
+
+	const QString
+EmpathConfig::KEY_MATCH_HEADER = "MatchHeaderExpr";
+
+	const QString
+EmpathConfig::KEY_FILTER_EVENT_HANDLER_TYPE = "FilterEventHandlerType";
+
+	const QString
+EmpathConfig::KEY_FILTER_EVENT_HANDLER_FOLDER = "FilterEventHandlerFolder";
+
+	const QString
+EmpathConfig::KEY_FILTER_EVENT_HANDLER_ADDRESS = "FilterEventHandlerAddress";
+
+	const QString
+EmpathConfig::KEY_FILTER_FOLDER = "FilterSourceFolder";
+
+	const bool
+EmpathConfig::DEFAULT_UNDERLINE_LINKS = true;
+
+	const QString
+EmpathConfig::DEFAULT_FIXED_OR_VARIABLE = "fixed";
+
+	const QString
+EmpathConfig::DEFAULT_SIG_FILE = "~/.signature";
+
+	const QString
+EmpathConfig::DEFAULT_PHRASE_REPLY_TO_SEND = "On %d, you wrote:";
+
+	const QString
+EmpathConfig::DEFAULT_PHRASE_REPLY_TO_ALL = "On %d, %n wrote:";
+
+	const QString
+EmpathConfig::DEFAULT_PHRASE_FORWARD = "Forwarded message from %n";
+
+	const bool
+EmpathConfig::DEFAULT_AUTO_QUOTE_WHEN_REPLY = true;
+
+	const bool
+EmpathConfig::DEFAULT_APPEND_SIG = true;
+
+	const bool
+EmpathConfig::DEFAULT_APPEND_DIG_SIG = false;
+
+	const unsigned int
+EmpathConfig::DEFAULT_WRAP_COLUMN = 80;
+
+	const bool
+EmpathConfig::DEFAULT_CC_ME = false;
+
+	const bool
+EmpathConfig::DEFAULT_CC_OTHER = false;
+
+	const QString
+EmpathConfig::DEFAULT_COPY_FOLDER = "outbox";
+
+	const bool
+EmpathConfig::DEFAULT_SEND_NOW = true;
+
+	const unsigned int
+EmpathConfig::DEFAULT_OUT_SERVER_PORT = 25;
+
+	const bool
+EmpathConfig::DEFAULT_CHECK_FOR_NEW_MAIL = true;
+
+	const unsigned int
+EmpathConfig::DEFAULT_CHECK_NEW_INTERVAL = 10;
+
+	const QString
+EmpathConfig::DEFAULT_MAIL_SAVE_FOLDER = "~/.empath";
+
