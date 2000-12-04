26f40c709a30b88d33c883cf58e233f91c53fb1b
Christian Gebauer <gebauer@kde.org>
2000-12-04 16:33:04 +0000

    I think we can name this beta3 now...
    svn path=/trunk/kdenetwork/knode/; revision=73394

--- knode/resource.h
+++ knode/resource.h
@@ -26,7 +26,7 @@
 
 //========= KNode Version Information ============
 
-#define KNODE_VERSION "0.4beta2"
+#define KNODE_VERSION "0.4beta333"
 
 //================= StatusBar ====================
 
