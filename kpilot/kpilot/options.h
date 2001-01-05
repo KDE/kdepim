fb37371a68f3edde468ad2d3292612c0e9633975
Adriaan de Groot <groot@kde.org>
2001-01-05 12:05:16 +0000

    Added documentation; switched off weird options
    svn path=/trunk/kdepim/; revision=76455

--- kpilot/kpilot/options.h
+++ kpilot/kpilot/options.h
@@ -28,23 +28,21 @@
 #if (QT_VERSION > 199)
 #define KDE2
 #else
-#error "This is KPilot for KDE2"
+#error "This is KPilot for KDE2 and won't compile with Qt < 2.2.3"
 #endif
 
 // Turn OFF as much debugging as possible
 //
 //
-#ifdef NDEBUG
 #undef DEBUG
-#endif
 
 // Define TEST_DEBUG to check whether all the
 // calls to kdWarning and kdError use __FUNCTION__
 // as they should, instead of the lazier (and incorrect)
-// fname.
+// fname. (See below).
 //
 //
-#define TEST_DEBUG
+#undef TEST_DEBUG
 
 #include <klocale.h>
 #include <kdebug.h>
@@ -55,13 +53,46 @@
 // debug areas.
 //
 //
-#define DEBUGKPILOT	kdDebug(5510)
-#define DEBUGDAEMON	kdDebug(5511)
-#define DEBUGCONDUIT	kdDebug(5512)
+#define KPILOT_AREA	5510
+#define DAEMON_AREA	5511
+#define CONDUIT_AREA	5512
+
+#define DEBUGKPILOT	kdDebug(KPILOT_AREA)
+#define DEBUGDAEMON	kdDebug(DAEMON_AREA)
+#define DEBUGCONDUIT	kdDebug(CONDUIT_AREA)
 
 
 #ifdef DEBUG
-// These are three-bit fields, basically we're defining
+// KPilot contains two kinds of debugging messages:
+//
+// * Old-style, with constructions like
+//
+// #ifdef DEBUG
+//	if (debug_level & UI_MINOR)
+//	{
+//		kdDebug() << fname
+//			<< ": Creating dialog window." << endl;
+//	}
+// #endif
+//
+// These debugging messages are controlled by the debug_level,
+// which can be set by the user with --debug. The number of 
+// debug levels is confusing, and practice teaches that you
+// either want debug_level 0xffffffff or 0. So this is deprecated
+// but not yet removed from all the code.
+//
+//
+// * New-style, which looks like
+//
+//	DEBUGKPILOT << fname << ": Creating dialog window." << endl;
+//
+// This uses KDE's debug areas (accessible through kdebugdialog)
+// to keep track of what to print. No extra #if or if().
+
+
+
+
+// (Old-style) These are three-bit fields, basically we're defining
 // 1<<n; 3<<n; 7<<n for some n.
 //
 // This allows better selection of what you want debugged.
@@ -71,7 +102,7 @@
 #define UI_MINOR	(4+8)
 #define UI_TEDIOUS	(8)
 
-// Debug level is set to some bit pattern; if any 
+// (Old-style) Debug level is set to some bit pattern; if any 
 // bit in one of the debug masks (SYNC_MAJOR, for
 // instance) is set print the messages corresponding
 // to that debug level.
@@ -87,13 +118,17 @@ extern int debug_level;
 #define SYNC_MINOR	(256+512)
 #define SYNC_TEDIOUS	(512)
 
+// Both old and new-style debugging suggest (insist?) that
+// every function be started with the macro FUNCTIONSETUP,
+// which outputs function and line information on every call.
+//
+//
 #define DEBUG_FUNCTIONS	(1)
-#define EFUNCTIONSETUP	static const char *fname=__FUNCTION__; \
+#define FUNCTIONSETUP	static const char *fname=__FUNCTION__; \
 			if (debug_level & DEBUG_FUNCTIONS) { kdDebug() << \
 			fname << tabs+(strlen(fname)>>3) \
 				<< "(" << __FILE__ << ":" << \
 				__LINE__ << ")\n"; } 
-#define FUNCTIONSETUP	EFUNCTIONSETUP
 
 extern const char *tabs;		// for indentation purposes in debug
 
@@ -107,12 +142,27 @@ class KConfig;
 void listConfig(kdbgstream&,KConfig&);
 void listStrList(kdbgstream&,const QStringList&);
 #else
-// This is used to define fname in functions that actually
-// produce error messages and that need the name of the function.
-// For debugging purposes it's also used by FUNCTIONSETUP.
+// With debugging turned off, FUNCTIONSETUP doesn't do anything.
+// In particular it doesn't give functions a local variable fname,
+// like FUNCTIONSETUP does in the debugging case. Since code like
+//
+// DEBUGKPILOT << fname << ": Help! I'm descructing" << endl;
+//
+// is still visible in KPilot (it isn't all bracketed by #ifdef DEBUG
+// and it doesn't *need* to be, that's the whole point of kdDebug())
+// we still need *something* with the name fname. So we'll declare a
+// single extern fname here.
+//
+// With TEST_DEBUG turned on, fname gets a weird type that is
+// incompatible with kdWarning() and kdError(), leading to warnings
+// if you mix them. Use __FUNCTION__ instead.
+//
+// With TEST_DEBUG turned off, fname is an int and you won't get
+// warnings. If you use fname by accident in a warning message
+// you will get strange results though, since instead of a readable
+// function name you'll get some nasty number.
 //
 //
-#define EFUNCTIONSETUP
 #define FUNCTIONSETUP
 #ifdef TEST_DEBUG
 class debugName { public: debugName(int i) : j(i) {} ; int j; } ;
