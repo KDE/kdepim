0e2defc3f5dd27d689f126ad0d677a448a0463a5
Adriaan de Groot <groot@kde.org>
2001-01-05 16:15:33 +0000

    Added DCOP to Daemon
    svn path=/trunk/kdepim/; revision=76484

---
+++ kpilot/kpilot/pilotDaemonDCOP.h
@@ -0,0 +1,52 @@
+/* pilotDaemonDCOP.c			KPilotDaemon
+**
+** Copyright (C) 2000 by Adriaan de Groot
+**
+** This file defines the DCOP interface for
+** the KPilotDaemon. The daemon has *two* interfaces:
+** one belonging with KUniqueApplication and this one.
+*/
+
+/*
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program in a file called COPYING; if not, write to
+** the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, 
+** MA 02139, USA.
+*/
+
+/*
+** Bug reports and questions can be sent to adridg@cs.kun.nl
+*/
+
+#ifndef PILOTDAEMONDCOP_H
+#define PILOTDAEMONDCOP_H
+
+#include <dcopobject.h>
+
+
+class PilotDaemonDCOP : virtual public DCOPObject
+{
+	K_DCOP
+k_dcop:
+	/**
+	* Start a HotSync. What kind of HotSync is determined
+	* by the int parameter:
+	*
+	* 1 regular HotSync -> backup databases and run conduits
+	* 2 FastSync -> only backup databases with conduits
+	* 3 FullBackup -> backup, no conduits
+	*/
+	virtual ASYNC startHotSync(int) = 0;
+} ;
+
+#endif
