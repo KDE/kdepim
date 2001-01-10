260f81e485c32787ccac19070a573ba8e84d047a
David Faure <faure@kde.org>
2001-01-10 15:07:01 +0000

    +#include <config.h>  for Tru64
    svn path=/trunk/kdenetwork/kmailcvt/; revision=77288

--- kmailcvt/filter_pmail.cxx
+++ kmailcvt/filter_pmail.cxx
@@ -15,6 +15,7 @@
  *                                                                         *
  ***************************************************************************/
 
+#include <config.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
