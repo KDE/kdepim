0208d9db000e73f1491dd62560868abe547cf377
Marc Mutz <mutz@kde.org>
2001-08-20 11:52:50 +0000

    Added knarticle.{h,cpp}, which is a stripped-down version of knmime.{h,cpp} and will be it's replacement when KMime is used.
    Added for convenience of those working on KMime.
    It's not include in the built system for KNode.
    
    svn path=/trunk/kdenetwork/knode/; revision=111119

---
+++ knode/knarticle.cpp
@@ -0,0 +1,578 @@
+/*
+    knmime.cpp
+
+    KNode, the KDE newsreader
+    Copyright (c) 1999-2001 the KNode authors.
+    See file AUTHORS for details
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+*/
+
+#include <qfileinfo.h>
+#include <qstringlist.h>
+
+#include <kglobal.h>
+#include <klocale.h>
+#include <kmdcodec.h>
+#include <kcharsets.h>
+#include <kmimemagic.h>
+#include <kdebug.h>
+
+#include "knarticle.h"
+#include "knhdrviewitem.h"
+#include "kngroup.h"
+#include "knglobals.h"
+#include "knconfigmanager.h"
+#include "utilities.h"
+
+using namespace KMime;
+
+
+KNArticle::KNArticle(KNArticleCollection *c) : i_d(-1), c_ol(c), i_tem(0)
+{
+}
+
+
+KNArticle::~KNArticle()
+{
+  delete i_tem;
+}
+
+
+
+void KNArticle::setListItem(KNHdrViewItem *it)
+{
+  i_tem=it;
+  if(i_tem) i_tem->art=this;
+}
+
+
+void KNArticle::setLocked(bool b)
+{
+  f_lags.set(0, b);
+  if(c_ol) {  // local articles may have c_ol==0 !
+    if(b)
+      c_ol->articleLocked();
+    else
+      c_ol->articleUnlocked();
+  }
+}
+
+
+//=========================================================================================
+
+
+KNRemoteArticle::KNRemoteArticle(KNGroup *g)
+ : KNArticle(g), a_rticleNumber(-1), i_dRef(-1), d_ref(0), t_hrLevel(0), s_core(0),
+   c_olor(knGlobals.cfgManager->appearance()->unreadThreadColor()),
+   pgp_signed(false),
+   u_nreadFups(0), n_ewFups(0)
+{
+  m_essageID.setParent(this);
+  f_rom.setParent(this);
+  r_eferences.setParent(this);
+
+  if (g && g->useCharset())
+    d_efaultCS = cachedCharset( g->defaultCharset() );
+  else
+    d_efaultCS = cachedCharset( knGlobals.cfgManager->postNewsTechnical()->charset() );
+}
+
+
+KNRemoteArticle::~KNRemoteArticle()
+{}
+
+
+void KNRemoteArticle::parse()
+{
+  KNArticle::parse();
+  QCString raw;
+  if( !(raw=rawHeader(m_essageID.type())).isEmpty() )
+    m_essageID.from7BitString(raw);
+
+  if( !(raw=rawHeader(f_rom.type())).isEmpty() )
+    f_rom.from7BitString(raw);
+
+  if( !(raw=rawHeader(r_eferences.type())).isEmpty() )
+    r_eferences.from7BitString(raw);
+}
+
+
+void KNRemoteArticle::clear()
+{
+  m_essageID.clear();
+  f_rom.clear();
+  r_eferences.clear();
+  KNArticle::clear();
+}
+
+
+Headers::Base* KNRemoteArticle::getHeaderByType(const char *type)
+{
+  if(strcasecmp("Message-ID", type)==0) {
+    if(m_essageID.isEmpty()) return 0;
+    else return &m_essageID;
+  }
+  else if(strcasecmp("From", type)==0) {
+    if(f_rom.isEmpty()) return 0;
+    else return &f_rom;
+  }
+  else if(strcasecmp("References", type)==0) {
+    if(r_eferences.isEmpty()) return 0;
+    else return &r_eferences;
+  }
+  else
+    return KNArticle::getHeaderByType(type);
+}
+
+
+void KNRemoteArticle::setHeader(Headers::Base *h)
+{
+  bool del=true;
+  if(h->is("Message-ID"))
+    m_essageID.from7BitString(h->as7BitString(false));
+  else if(h->is("From")) {
+    f_rom.setEmail( (static_cast<Headers::From*>(h))->email() );
+    f_rom.setName( (static_cast<Headers::From*>(h))->name() );
+  }
+  else if(h->is("References")) {
+    r_eferences.from7BitString(h->as7BitString(false));
+  }
+  else {
+    del=false;
+    KNArticle::setHeader(h);
+  }
+
+  if(del) delete h;
+}
+
+
+bool KNRemoteArticle::removeHeader(const char *type)
+{
+  if(strcasecmp("Message-ID", type)==0)
+    m_essageID.clear();
+  else if(strcasecmp("From", type)==0)
+    f_rom.clear();
+  else if(strcasecmp("References", type)==0)
+    r_eferences.clear();
+  else
+     return KNArticle::removeHeader(type);
+
+  return true;
+}
+
+
+void KNRemoteArticle::initListItem()
+{
+  if(!i_tem) return;
+  i_tem->setText(0, s_ubject.asUnicodeString());
+
+  if(f_rom.hasName())
+    i_tem->setText(1, f_rom.name());
+  else
+    i_tem->setText(1, QString(f_rom.email()));
+
+  i_tem->setText(3, QString("%1").arg(lines()->numberOfLines()));
+
+  i_tem->setText(4, KGlobal::locale()->formatDateTime(d_ate.qdt(), true));
+  updateListItem();
+}
+
+
+void KNRemoteArticle::updateListItem()
+{
+  if(!i_tem) return;
+
+  KNConfig::Appearance *app=knGlobals.cfgManager->appearance();
+
+  if(isRead()) {
+    if(hasContent())
+      i_tem->setPixmap(0, app->icon(KNConfig::Appearance::greyBallChkd));
+    else
+      i_tem->setPixmap(0, app->icon(KNConfig::Appearance::greyBall));
+  }
+  else {
+    if(hasContent())
+      i_tem->setPixmap(0,app->icon(KNConfig::Appearance::redBallChkd));
+    else
+      i_tem->setPixmap(0, app->icon(KNConfig::Appearance::redBall));
+  }
+
+  if(hasNewFollowUps())
+    i_tem->setPixmap(1, app->icon(KNConfig::Appearance::newFups));
+  else
+    i_tem->setPixmap(1, app->icon(KNConfig::Appearance::null));
+
+  if(isWatched())
+    i_tem->setPixmap(2, app->icon(KNConfig::Appearance::eyes));
+  else {
+    if(isIgnored())
+      i_tem->setPixmap(2, app->icon(KNConfig::Appearance::ignore));
+    else
+      i_tem->setPixmap(2, app->icon(KNConfig::Appearance::null));
+  }
+
+  i_tem->setText(2, QString("%1").arg(s_core));
+
+  i_tem->setExpandable( (threadMode() && hasVisibleFollowUps()) );
+
+  i_tem->repaint(); //force repaint
+}
+
+
+void KNRemoteArticle::thread(KNRemoteArticle::List &l)
+{
+  KNRemoteArticle *tmp=0, *ref=this;
+  KNGroup *g=static_cast<KNGroup*>(c_ol);
+  int idRef=i_dRef, topID=-1;
+
+  while(idRef!=0) {
+    ref=g->byId(idRef);
+    if(!ref)
+      return; // sh#t !!
+    idRef=ref->idRef();
+  }
+
+  topID=ref->id();
+  l.append(ref);
+
+  for(int i=0; i<g->length(); i++) {
+    tmp=g->at(i);
+    if(tmp->idRef()!=0) {
+      idRef=tmp->idRef();
+      while(idRef!=0) {
+        ref=g->byId(idRef);
+        idRef=ref->idRef();
+      }
+      if(ref->id()==topID)
+        l.append(tmp);
+    }
+  }
+}
+
+
+void KNRemoteArticle::setForceDefaultCS(bool b)
+{
+  if (!b) { // restore default
+    KNGroup *g=static_cast<KNGroup*>(c_ol);
+    if (g && g->useCharset())
+      d_efaultCS = cachedCharset( g->defaultCharset() );
+    else
+      d_efaultCS = cachedCharset( knGlobals.cfgManager->postNewsTechnical()->charset() );
+  }
+  KNArticle::setForceDefaultCS(b);
+  initListItem();
+}
+
+
+//=========================================================================================
+
+
+KNLocalArticle::KNLocalArticle(KNArticleCollection *c)
+  : KNArticle(c), s_Offset(-1), e_Offset(-1), s_erverId(-1)
+{
+  n_ewsgroups.setParent(this);
+  t_o.setParent(this);
+  d_efaultCS = cachedCharset( knGlobals.cfgManager->postNewsTechnical()->charset() );
+}
+
+
+KNLocalArticle::~KNLocalArticle()
+{}
+
+
+void KNLocalArticle::parse()
+{
+  KNArticle::parse();
+  QCString raw;
+
+  if( !(raw=rawHeader(n_ewsgroups.type())).isEmpty() )
+    n_ewsgroups.from7BitString(raw);
+
+  if( !(raw=rawHeader(t_o.type())).isEmpty() )
+    t_o.from7BitString(raw);
+}
+
+
+void KNLocalArticle::clear()
+{
+  KNArticle::clear();
+  n_ewsgroups.clear();
+  t_o.clear();
+}
+
+
+Headers::Base* KNLocalArticle::getHeaderByType(const char *type)
+{
+  if(strcasecmp("Newsgroups", type)==0)
+    return newsgroups(false);
+  else if(strcasecmp("To", type)==0)
+    return to(false);
+  else
+    return KNArticle::getHeaderByType(type);
+}
+
+
+void KNLocalArticle::setHeader(Headers::Base *h)
+{
+  bool del=true;
+  if(h->is("To"))
+    t_o.from7BitString(h->as7BitString(false));
+  else if(h->is("Newsgroups"))
+    n_ewsgroups.from7BitString(h->as7BitString(false));
+  else {
+    del=false;
+    KNArticle::setHeader(h);
+  }
+
+  if(del) delete h;
+}
+
+
+bool KNLocalArticle::removeHeader(const char *type)
+{
+  if(strcasecmp("To", type)==0)
+    t_o.clear();
+  else if(strcasecmp("Newsgroups", type)==0)
+    n_ewsgroups.clear();
+  else
+     return KNArticle::removeHeader(type);
+
+  return true;
+}
+
+
+void KNLocalArticle::updateListItem()
+{
+  if(!i_tem)
+    return;
+
+  i_tem->setText(0, s_ubject.asUnicodeString());
+
+  QString tmp;
+  int idx=0;
+  KNConfig::Appearance *app=knGlobals.cfgManager->appearance();
+
+  if(isSavedRemoteArticle()) {
+    i_tem->setPixmap(0, app->icon(KNConfig::Appearance::savedRemote));
+    if (!n_ewsgroups.isEmpty())
+      tmp=n_ewsgroups.asUnicodeString();
+    else
+      tmp=t_o.asUnicodeString();
+  }
+  else {
+
+    if(doPost()) {
+      tmp+=n_ewsgroups.asUnicodeString();
+      if(canceled())
+        i_tem->setPixmap(idx++, app->icon(KNConfig::Appearance::canceledPosting));
+      else
+        i_tem->setPixmap(idx++, app->icon(KNConfig::Appearance::posting));
+    }
+
+    if(doMail()) {
+      i_tem->setPixmap(idx++, app->icon(KNConfig::Appearance::mail));
+      if(doPost())
+        tmp+=" / ";
+      tmp+=t_o.asUnicodeString();
+    }
+
+  }
+
+  i_tem->setText(1, tmp);
+  i_tem->setText(2, QString::null);
+  if (lines()->numberOfLines()!=-1)
+    i_tem->setText(3, QString("%1").arg(lines()->numberOfLines()));
+  else
+    i_tem->setText(3, QString::null);
+  i_tem->setText(4, KGlobal::locale()->formatDateTime(d_ate.qdt(), true));
+}
+
+
+void KNLocalArticle::setForceDefaultCS(bool b)
+{
+  if (!b)  // restore default
+    d_efaultCS = cachedCharset( knGlobals.cfgManager->postNewsTechnical()->charset() );
+  KNArticle::setForceDefaultCS(b);
+  updateListItem();
+}
+
+
+//=========================================================================================
+
+
+KNAttachment::KNAttachment(Content *c)
+  : c_ontent(c), l_oadHelper(0), f_ile(0), i_sAttached(true)
+{
+  Headers::ContentType  *t=c->contentType();
+  Headers::CTEncoding   *e=c->contentTransferEncoding();
+  Headers::CDescription *d=c->contentDescription(false);
+
+  n_ame=t->name();
+
+  if(d)
+    d_escription=d->asUnicodeString();
+
+
+  setMimeType(t->mimeType());
+
+  if(e->cte()==Headers::CEuuenc) {
+    setCte( Headers::CEbase64 );
+    updateContentInfo();
+  }
+  else
+    e_ncoding.setCte( e->cte() );
+
+
+  h_asChanged=false; // has been set to "true" in setMimeType()
+}
+
+
+KNAttachment::KNAttachment(KNLoadHelper *helper)
+  : c_ontent(0), l_oadHelper(helper), f_ile(helper->getFile()), i_sAttached(false), h_asChanged(true)
+{
+  setMimeType((KMimeMagic::self()->findFileType(f_ile->name()))->mimeType());
+  n_ame=helper->getURL().fileName();
+}
+
+
+KNAttachment::~KNAttachment()
+{
+  if(!i_sAttached && c_ontent)
+    delete c_ontent;
+  delete l_oadHelper;
+}
+
+
+void KNAttachment::setMimeType(const QString &s)
+{
+  m_imeType=s.latin1();
+  h_asChanged=true;
+
+  if(m_imeType.find("text/", 0, false)==-1) {
+    f_b64=true;
+    e_ncoding.setCte(Headers::CEbase64);
+  }
+  else {
+    f_b64=false;
+    if (knGlobals.cfgManager->postNewsTechnical()->allow8BitBody())
+      setCte(Headers::CE8Bit);
+    else
+      setCte(Headers::CEquPr);
+  }
+}
+
+
+QString KNAttachment::contentSize()
+{
+  QString ret;
+  int s=0;
+
+  if(c_ontent && c_ontent->hasContent())
+    s=c_ontent->size();
+  else {
+    if (f_ile)
+      s=f_ile->size();
+  }
+
+  if(s > 1023) {
+    s=s/1024;
+    ret.setNum(s);
+    ret+=" kB";
+  }
+  else {
+    ret.setNum(s);
+    ret+=" Bytes";
+  }
+
+  return ret;
+}
+
+
+void KNAttachment::updateContentInfo()
+{
+  if(!h_asChanged || !c_ontent)
+    return;
+
+  //Content-Type
+  Headers::ContentType *t=c_ontent->contentType();
+  t->setMimeType(m_imeType);
+  t->setName(n_ame, KGlobal::charsets()->charsetForLocale());
+  t->setCategory(Headers::CCmixedPart);
+
+  //Content-Description
+  if(d_escription.isEmpty())
+    c_ontent->removeHeader("Content-Description");
+  else
+    c_ontent->contentDescription()->fromUnicodeString(d_escription, KGlobal::charsets()->charsetForLocale());
+
+  //Content-Disposition
+  Headers::CDisposition *d=c_ontent->contentDisposition();
+  d->setDisposition(Headers::CDattachment);
+  d->setFilename(n_ame);
+
+  //Content-Transfer-Encoding
+  if(i_sAttached)
+    c_ontent->changeEncoding(e_ncoding.cte());
+  else
+    c_ontent->contentTransferEncoding()->setCte(e_ncoding.cte());
+
+  c_ontent->assemble();
+
+  h_asChanged=false;
+}
+
+
+
+void KNAttachment::attach(Content *c)
+{
+  if(i_sAttached || !f_ile)
+    return;
+
+  c_ontent=new Content();
+  updateContentInfo();
+  Headers::ContentType *type=c_ontent->contentType();
+  Headers::CTEncoding *e=c_ontent->contentTransferEncoding();
+  QByteArray data(f_ile->size());
+
+  int readBytes=f_ile->readBlock(data.data(), f_ile->size());
+
+  if (readBytes<(int)f_ile->size() && f_ile->status()!=IO_Ok) {
+    KNHelper::displayExternalFileError();
+    delete c_ontent;
+    c_ontent=0;
+  } else {
+    if (e_ncoding.cte()==Headers::CEbase64 || !type->isText()) { //encode base64
+      c_ontent->setBody( KCodecs::base64Encode(data, true) + '\n' );
+	//      c_ontent->b_ody += '\n';
+      e->setCte(Headers::CEbase64);
+      e->setDecoded(false);
+    } else  {
+      c_ontent->setBody( QCString(data.data(), data.size()+1) + '\n' );
+      //      c_ontent->b_ody += '\n';
+      e->setDecoded(true);
+    }
+  }
+
+  if(c_ontent) {
+    c->addContent(c_ontent);
+    i_sAttached=true;
+  }
+}
+
+
+void KNAttachment::detach(Content *c)
+{
+  if(i_sAttached) {
+    c->removeContent(c_ontent, false);
+    i_sAttached=false;
+  }
+}
+
+
---
+++ knode/knarticle.h
@@ -0,0 +1,341 @@
+/*
+    knarticle.h
+
+    KNode, the KDE newsreader
+    Copyright (c) 1999-2001 the KNode authors.
+    See file AUTHORS for details
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, US
+*/
+
+#ifndef KNARTICLE_H
+#define KNARTICLE_H
+
+#include <qstringlist.h>
+#include <qtextstream.h>
+#include <qfile.h>
+#include <qfont.h>
+#include <qcolor.h>
+#include <qasciidict.h>
+
+#if QT_VERSION >= 290
+#  include <qptrlist.h>
+#else
+#  include <qlist.h>
+#  define QPtrList QList
+#  define QPtrListIterator QListIterator
+#endif
+
+#include <kmime_headers.h>
+#include <kmime_newsarticle.h>
+#include <boolflags.h>
+
+
+#include "knjobdata.h"
+
+//forward declarations
+class KNLoadHelper;
+class KNHdrViewItem;
+class KNArticleCollection;
+
+/** This class encapsulates a generic article. It provides all the
+    usual headers of a RFC822-message. Further more it contains an
+    unique id and can store a pointer to a @ref QListViewItem. It is
+    used as a base class for all visible articles. */
+
+class KNArticle : public KMime::NewsArticle, public KNJobItem {
+
+  public:
+    typedef QPtrList<KNArticle> List;
+
+    KNArticle(KNArticleCollection *c);
+    ~KNArticle();
+
+    //id
+    int id()             { return i_d; }
+    void setId(int i)    { i_d=i; }
+
+    //list item handling
+    KNHdrViewItem* listItem()            { return i_tem; }
+    void setListItem(KNHdrViewItem *i);
+    virtual void updateListItem() {}
+
+    //network lock (reimplemented from KNJobItem)
+    bool isLocked()                      { return f_lags.get(0); }
+    void setLocked(bool b=true);
+
+    //prevent that the article is unloaded automatically
+    bool isNotUnloadable()               { return f_lags.get(1); }
+    void setNotUnloadable(bool b=true)   { f_lags.set(1, b); }
+
+    //article-collection
+    KNArticleCollection* collection()           { return c_ol; }
+    void setCollection(KNArticleCollection *c)  { c_ol=c; }
+    bool isOrphant()                            { return (i_d==-1); }
+
+  protected:
+    int i_d; //unique in the given collection
+    KNArticleCollection *c_ol;
+    KNHdrViewItem *i_tem;
+
+}; // KNArticle
+
+
+class KNGroup;
+
+/** KNRemoteArticle represents an article, whos body has to be
+    retrieved from a remote host or from the local cache.
+    All articles in a newsgroup are stored in instances
+    of this class. */
+
+class KNRemoteArticle : public KNArticle {
+
+  public:
+    typedef QPtrList<KNRemoteArticle> List;
+
+    KNRemoteArticle(KNGroup *g);
+    ~KNRemoteArticle();
+
+    // type
+    articleType type() { return ATremote; }
+
+    // content handling
+    virtual void parse();
+    virtual void assemble() {} //assembling is disabled for remote articles
+    virtual void clear();
+
+    // header access
+    KMime::Headers::Base* getHeaderByType(const char *type);
+    void setHeader(KMime::Headers::Base *h);
+    bool removeHeader(const char *type);
+    KMime::Headers::MessageID* messageID(bool create=true)   { if(!create && m_essageID.isEmpty()) return 0; return &m_essageID; }
+    KMime::Headers::From* from(bool create=true)             { if(!create && f_rom.isEmpty()) return 0; return &f_rom; }
+    KMime::Headers::References* references(bool create=true) { if(!create && r_eferences.isEmpty()) return 0; return &r_eferences; }
+
+    // article number
+    int articleNumber()                  { return a_rticleNumber; }
+    void setArticleNumber(int number)    { a_rticleNumber = number; }
+
+    // status
+    bool isNew()                         { return f_lags.get(2); }
+    void setNew(bool b=true)             { f_lags.set(2, b); }
+    bool getReadFlag()                   { return f_lags.get(3); }
+    bool isRead()                        { return f_lags.get(7) || f_lags.get(3); }   // ignored articles == read
+    void setRead(bool b=true)            { f_lags.set(3, b); }
+    bool isExpired()                     { return f_lags.get(4); }
+    void setExpired(bool b=true)         { f_lags.set(4, b); }
+    bool isKept()                        { return f_lags.get(5); }
+    void setKept(bool b=true)            { f_lags.set(5, b); }
+    bool hasChanged()                    { return f_lags.get(6); }
+    void setChanged(bool b=true)         { f_lags.set(6, b); }
+    bool isIgnored()                     { return f_lags.get(7); }
+    void setIgnored(bool b=true)         { f_lags.set(7, b); }
+    bool isWatched()                     { return f_lags.get(8); }
+    void setWatched(bool b=true)         { f_lags.set(8, b); }
+
+    // thread info
+    int idRef()                                     { return i_dRef; }
+    void setIdRef(int i)                            { i_dRef=i; }
+    KNRemoteArticle* displayedReference()           { return d_ref; }
+    void setDisplayedReference(KNRemoteArticle *dr) { d_ref=dr; }
+    bool threadMode()                             { return f_lags.get(9); }
+    void setThreadMode(bool b=true)               { f_lags.set(9, b); }
+    unsigned char threadingLevel()                { return t_hrLevel; }
+    void setThreadingLevel(unsigned char l)       { t_hrLevel=l; }
+    short score()                                 { return s_core; }
+    void setScore(short s)                        { s_core=s; }
+    unsigned short newFollowUps()                 { return n_ewFups; }
+    bool hasNewFollowUps()                        { return (n_ewFups>0); }
+    void setNewFollowUps(unsigned short s)        { n_ewFups=s; }
+    void incNewFollowUps(unsigned short s=1)      { n_ewFups+=s; }
+    void decNewFollowUps(unsigned short s=1)      { n_ewFups-=s; }
+    unsigned short unreadFollowUps()              { return u_nreadFups; }
+    bool hasUnreadFollowUps()                     { return (u_nreadFups>0); }
+    void setUnreadFollowUps(unsigned short s)     { u_nreadFups=s; }
+    void incUnreadFollowUps(unsigned short s=1)   { u_nreadFups+=s; }
+    void decUnreadFollowUps(unsigned short s=1)   { u_nreadFups-=s; }
+    void thread(List &f);
+
+    //filtering
+    bool filterResult()                     { return f_lags.get(10); }
+    void setFilterResult(bool b=true)       { f_lags.set(10, b); }
+    bool isFiltered()                       { return f_lags.get(11); }
+    void setFiltered(bool b=true)           { f_lags.set(11, b); }
+    bool hasVisibleFollowUps()              { return f_lags.get(12); }
+    void setVisibleFollowUps(bool b=true)   { f_lags.set(12, b); }
+
+    // list item handling
+    void initListItem();
+    void updateListItem();
+
+    void setForceDefaultCS(bool b);
+
+    QColor color() const { return c_olor; }
+    void setColor(const QColor& c) { c_olor = c; }
+    void setPgpSigned(bool f) { pgp_signed = f; }
+    bool isPgpSigned() const { return pgp_signed; }
+
+
+  protected:
+    // hardcoded headers
+    KMime::Headers::MessageID m_essageID;
+    KMime::Headers::From f_rom;
+    KMime::Headers::References r_eferences;
+
+    int a_rticleNumber;
+    int i_dRef;                      // id of a reference-article (0 == none)
+    KNRemoteArticle *d_ref;          // displayed reference-article (may differ from i_dRef)
+    unsigned char t_hrLevel;         // quality of threading
+    short s_core;                    // guess what ;-)
+    QColor c_olor;                   // color for the header list
+    bool pgp_signed;                 // true if the user asks to check the pgp signature
+    unsigned short u_nreadFups,      // number of the article's unread follow-ups
+                   n_ewFups;         // number of the article's new follow-ups
+
+}; // KNRemoteArticle
+
+
+
+/* This class encapsulates an article, that is
+   stored locally in an MBOX-file. All own and
+   saved articles are represented by instances
+   of this class. */
+
+
+class KNLocalArticle : public KNArticle {
+
+  public:
+    typedef QPtrList<KNLocalArticle> List;
+
+    KNLocalArticle(KNArticleCollection *c=0);
+    ~KNLocalArticle();
+
+    //type
+    articleType type()      { return ATlocal; }
+
+    //content handling
+    void parse();
+    void clear();
+
+    // header access
+    KMime::Headers::Base* getHeaderByType(const char *type);
+    void setHeader(KMime::Headers::Base *h);
+    bool removeHeader(const char *type);
+    KMime::Headers::Newsgroups* newsgroups(bool create=true)     { if ( (!create && n_ewsgroups.isEmpty()) ||
+                                                                   (!create && !isSavedRemoteArticle() && !doPost()) )
+                                                                return 0;
+                                                              return &n_ewsgroups; }
+    KMime::Headers::To* to(bool create=true)                     { if ( (!create && t_o.isEmpty()) ||
+                                                                   (!create && !isSavedRemoteArticle() && !doMail()) )
+                                                                return 0;
+                                                              return &t_o; }
+
+    //send article as mail
+    bool doMail()                 { return f_lags.get(2); }
+    void setDoMail(bool b=true)   { f_lags.set(2, b); }
+    bool mailed()                 { return f_lags.get(3); }
+    void setMailed(bool b=true)   { f_lags.set(3, b); }
+
+    //post article to a newsgroup
+    bool doPost()                 { return f_lags.get(4); }
+    void setDoPost(bool b=true)   { f_lags.set(4, b); }
+    bool posted()                 { return f_lags.get(5); }
+    void setPosted(bool b=true)   { f_lags.set(5, b); }
+    bool canceled()               { return f_lags.get(6); }
+    void setCanceled(bool b=true) { f_lags.set(6, b); }
+
+    // status
+    bool pending()                { return ( (doPost() && !posted()) || (doMail() && !mailed()) ); }
+    bool isSavedRemoteArticle()   {  return ( !doPost() && !doMail() && editDisabled() ); }
+
+    //edit
+    bool editDisabled()               { return f_lags.get(7); }
+    void setEditDisabled(bool b=true) { f_lags.set(7, b); }
+
+    //search
+    bool filterResult()                { return f_lags.get(8); }
+    void setFilterResult(bool b=true)  { f_lags.set(8, b); }
+
+    //MBOX infos
+    int startOffset()             { return s_Offset; }
+    void setStartOffset(int so)   { s_Offset=so; }
+    int endOffset()               { return e_Offset; }
+    void setEndOffset(int eo)     { e_Offset=eo; }
+
+    //nntp-server id
+    int serverId()                { if(!doPost()) return -1; else return s_erverId; }
+    void setServerId(int i)       { s_erverId=i; }
+
+    //list item handling
+    void updateListItem();
+
+    void setForceDefaultCS(bool b);
+
+    protected:
+      //hardcoded headers
+      KMime::Headers::Newsgroups n_ewsgroups;
+      KMime::Headers::To t_o;
+      int s_Offset, //position in mbox-file : start
+          e_Offset, //position in mbox-file : end
+          s_erverId; //id of the nntp-server this article is posted to
+};
+
+
+/* KNAttachment represents a file that is
+   or will be attached to an article. */
+
+class KNAttachment {
+
+  public:
+    KNAttachment(KMime::Content *c);
+    KNAttachment(KNLoadHelper *helper);
+    ~KNAttachment();
+
+    //name (used as a Content-Type parameter and as filename)
+    const QString& name()           { return n_ame; }
+    void setName(const QString &s)  { n_ame=s; h_asChanged=true; }
+
+    //mime type
+    const QCString& mimeType()            { return m_imeType; }
+    void setMimeType(const QString &s);
+
+    //Content-Description
+    const QString& description()          { return d_escription; }
+    void setDescription(const QString &s) { d_escription=s; h_asChanged=true; }
+
+    //Encoding
+    int cte()                             { return e_ncoding.cte(); }
+    void setCte(int e)                    { e_ncoding.setCte( (KMime::Headers::contentEncoding)(e) );
+                                            h_asChanged=true; }
+    bool isFixedBase64()                  { return f_b64; }
+    QString encoding()                    { return e_ncoding.asUnicodeString(); }
+
+    //content handling
+    KMime::Content* content()             { return c_ontent; }
+    QString contentSize();
+    bool isAttached()                     { return i_sAttached; }
+    bool hasChanged()                     { return h_asChanged; }
+    void updateContentInfo();
+    void attach(KMime::Content *c);
+    void detach(KMime::Content *c);
+
+  protected:
+    KMime::Content *c_ontent;
+    KNLoadHelper   *l_oadHelper;
+    QFile *f_ile;
+    QCString m_imeType;
+    QString n_ame,
+            d_escription;
+    KMime::Headers::CTEncoding e_ncoding;
+    bool  i_sAttached,
+          h_asChanged,
+          f_b64;
+};
+
+#endif //KNARTICLE_H
