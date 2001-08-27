b14dc66fcc7dca74d771b0c5ec0819f4eea380df
Cornelius Schumacher <schumacher@kde.org>
2001-08-27 15:46:12 +0000

    Copied koeventviewer.h. This is a temporary solution, until we implement it the right way.
    svn path=/trunk/kdepim/; revision=112087

---
+++ kalarmd/koeventviewer.h
@@ -0,0 +1,41 @@
+#ifndef KOEVENTVIEWER_H
+#define KOEVENTVIEWER_H
+// $Id$
+//
+// Viewer widget for events.
+//
+
+#include <qtextview.h>
+
+#include <event.h>
+
+using namespace KCal;
+
+class KOEventViewer : public QTextView {
+    Q_OBJECT
+  public:
+    KOEventViewer(QWidget *parent=0,const char *name=0);
+    virtual ~KOEventViewer();
+
+    void setEvent(Event *event);
+    void setTodo(Todo *event);
+    
+    void appendEvent(Event *event);
+    void appendTodo(Todo *event);
+    
+    void clearEvents(bool now=false);
+    
+  protected:
+    void addTag(const QString & tag,const QString & text);
+
+    void formatCategories(Incidence *event);
+    void formatAttendees(Incidence *event);
+    void formatReadOnly(Incidence *event);
+
+  private:
+    QTextView *mEventTextView;
+
+    QString mText;    
+};
+
+#endif
