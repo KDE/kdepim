af334b3f765431d634bef91272de6316a72bab7a
Cornelius Schumacher <schumacher@kde.org>
2001-09-19 13:09:50 +0000

    Added licence headers.
    svn path=/trunk/kdepim/; revision=114776

--- libkcal/icalformatimpl.cpp
+++ libkcal/icalformatimpl.cpp
@@ -1,3 +1,23 @@
+/*
+    This file is part of libkcal.
+    Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
 // $Id$
 
 #include "config.h"
@@ -5,16 +25,11 @@
 #include <qdatetime.h>
 #include <qstring.h>
 #include <qptrlist.h>
-#include <qregexp.h>
-#include <qclipboard.h>
-#include <qdialog.h>
 #include <qfile.h>
 
-#include <kapp.h>
 #include <kdebug.h>
-#include <kmessagebox.h>
-#include <kiconloader.h>
 #include <klocale.h>
+//#include <kmessagebox.h>
 
 extern "C" {
   #include <ical.h>
--- libkcal/vcalformat.cpp
+++ libkcal/vcalformat.cpp
@@ -1,7 +1,27 @@
+/*
+    This file is part of libkcal.
+    Copyright (c) 1998 Preston Brwon
+    Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
 // $Id$
 
-#include "config.h"
-
+#include <qapplication.h>
 #include <qdatetime.h>
 #include <qstring.h>
 #include <qptrlist.h>
@@ -10,7 +30,6 @@
 #include <qdialog.h>
 #include <qfile.h>
 
-#include <kapp.h>
 #include <kdebug.h>
 #include <kmessagebox.h>
 #include <kiconloader.h>
@@ -21,7 +40,6 @@
 
 #include "vcaldrag.h"
 #include "qdatelist.h"
-
 #include "calendar.h"
 
 #include "vcalformat.h"
