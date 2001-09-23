78d0411452d23fc18060acb2d1a76c00d36526f5
Adriaan de Groot <groot@kde.org>
2001-09-23 18:38:07 +0000

    Behavior for David's .ui file
    svn path=/trunk/kdepim/; revision=115185

---
+++ kpilot/kpilot/kpilotConfigDialog.cc
@@ -0,0 +1,179 @@
+/* kpilotConfigDialog.cc                KPilot
+**
+** Copyright (C) 2001 by Dan Pilone
+**
+** This file defines a specialization of KPilotDeviceLink
+** that can actually handle some HotSync tasks, like backup
+** and restore. It does NOT do conduit stuff.
+*/
+ 
+/*
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program in a file called COPYING; if not, write to
+** the Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+** MA 02139, USA.
+*/
+ 
+/*
+** Bug reports and questions can be sent to kde-pim@kde.org
+*/
+
+#include "options.h"
+
+#include <qcombobox.h>
+#include <qcheckbox.h>
+#include <qradiobutton.h>
+#include <qlineedit.h>
+
+#include "kpilotConfig.h"
+
+#include "kpilotConfigDialog_base.h"
+#include "kpilotConfigDialog.moc"
+
+KPilotConfigDialog::KPilotConfigDialog(QWidget *w,const char *n,bool m) :
+	UIDialog(w,n,m)
+{
+	FUNCTIONSETUP;
+
+	fConfigWidget = new KPilotConfigWidget(widget());
+	setTabWidget(fConfigWidget->tabWidget);
+	disableUnusedOptions();
+	readConfig();
+
+	changePortType(fConfigWidget->fPortType->currentItem());
+
+	QObject::connect(fConfigWidget->fPortType,SIGNAL(activated(int)),
+		this,SLOT(changePortType(int)));
+
+	addAboutPage(false);
+}
+
+KPilotConfigDialog::~KPilotConfigDialog()
+{
+	FUNCTIONSETUP;
+}
+
+void KPilotConfigDialog::disableUnusedOptions()
+{
+	FUNCTIONSETUP;
+
+	fConfigWidget->fOverwriteRemote->setEnabled(false);
+	fConfigWidget->fForceFirstTime->setEnabled(false);
+	fConfigWidget->fFullBackupCheck->setEnabled(false);
+	fConfigWidget->fPreferFastSync->setEnabled(false);
+}
+
+void KPilotConfigDialog::readConfig()
+{
+	FUNCTIONSETUP;
+
+	KPilotConfigSettings &c=KPilotConfig::getConfig();
+	c.resetGroup();
+
+	(void) c.getPilotDevice(fConfigWidget->fPilotDevice);
+	(void) c.getPilotType(fConfigWidget->fPortType);
+	(void) c.getPilotSpeed(fConfigWidget->fPilotSpeed);
+	(void) c.getUser(fConfigWidget->fUserName);
+	(void) c.getStartDaemonAtLogin(fConfigWidget->fStartDaemonAtLogin);
+	(void) c.getKillDaemonOnExit(fConfigWidget->fKillDaemonOnExit);
+	(void) c.getDockDaemon(fConfigWidget->fDockDaemon);
+
+	(void) c.getShowSecrets(fConfigWidget->fUseSecret);
+	(void) c.getBackupOnly(fConfigWidget->fBackupOnly);
+	(void) c.getSkip(fConfigWidget->fSkipDB);
+
+	(void) c.getSyncFiles( fConfigWidget->fSyncFiles);
+
+	c.setAddressGroup();
+	(void) c.getUseKeyField(fConfigWidget->fUseKeyField);
+	setAddressDisplay(c.getAddressDisplayMode());
+
+	c.resetGroup();
+}
+	
+/* virtual */ void KPilotConfigDialog::commitChanges()
+{
+	FUNCTIONSETUP;
+
+	KPilotConfigSettings &c=KPilotConfig::getConfig();
+	c.resetGroup();
+
+	// General page
+	c.setPilotDevice(fConfigWidget->fPilotDevice);
+	c.setPilotType(fConfigWidget->fPortType);
+	c.setPilotSpeed(fConfigWidget->fPilotSpeed);
+	c.setUser(fConfigWidget->fUserName);
+	c.setStartDaemonAtLogin(fConfigWidget->fStartDaemonAtLogin);
+	c.setKillDaemonOnExit( fConfigWidget->fKillDaemonOnExit);
+	c.setDockDaemon(fConfigWidget->fDockDaemon);
+
+	// DB specials page
+	c.setShowSecrets(fConfigWidget->fUseSecret);
+	c.setBackupOnly(fConfigWidget->fBackupOnly);
+	c.setSkip(fConfigWidget->fSkipDB);
+
+	// Sync page
+	c.setSyncFiles(fConfigWidget->fSyncFiles);
+
+	// Address page
+	c.setAddressGroup();
+	c.setUseKeyField(fConfigWidget->fUseKeyField);
+	c.setAddressDisplayMode(getAddressDisplay());
+	c.resetGroup();
+
+	KPilotConfig::updateConfigVersion();
+
+	c.sync();
+}
+
+int KPilotConfigDialog::getAddressDisplay() const
+{
+	FUNCTIONSETUP;
+
+	if (fConfigWidget->fNormalDisplay->isChecked()) return 0;
+	if (fConfigWidget->fCompanyDisplay->isChecked()) return 1;
+
+	return 0;
+}
+
+void KPilotConfigDialog::setAddressDisplay(int i)
+{
+	FUNCTIONSETUP;
+
+	switch(i)
+	{
+	case 0 : fConfigWidget->fNormalDisplay->setChecked(true); break;
+	case 1 : fConfigWidget->fCompanyDisplay->setChecked(true); break;
+	default : fConfigWidget->fNormalDisplay->setChecked(true); break;
+	}
+}
+
+
+/* slot */ void KPilotConfigDialog::changePortType(int i)
+{
+	FUNCTIONSETUP;
+
+	switch(i)
+	{
+	case 0 : fConfigWidget->fPilotSpeed->setEnabled(true);
+		break;
+	case 1 :
+	case 2 : fConfigWidget->fPilotSpeed->setEnabled(false);
+		break;
+	default :
+		kdWarning() << __FUNCTION__
+			<< ": Unknown port type "
+			<< i
+			<< endl;
+	}
+}
---
+++ kpilot/kpilot/kpilotConfigDialog.h
@@ -0,0 +1,61 @@
+#ifndef _KPILOT_KPILOTCONFIGDIALOG_H
+#define _KPILOT_KPILOTCONFIGDIALOG_H
+/* kpilotConfigDialog.h                 KPilot
+**
+** Copyright (C) 2001 by Dan Pilone
+**
+** This file defines a specialization of KPilotDeviceLink
+** that can actually handle some HotSync tasks, like backup
+** and restore. It does NOT do conduit stuff.
+*/
+ 
+/*
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program in a file called COPYING; if not, write to
+** the Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+** MA 02139, USA.
+*/
+ 
+/*
+** Bug reports and questions can be sent to kde-pim@kde.org
+*/
+
+#include "uiDialog.h"
+
+class KPilotConfigWidget;
+
+class KPilotConfigDialog : public UIDialog 
+{
+Q_OBJECT
+public:
+	KPilotConfigDialog(QWidget *,const char *,bool);
+	virtual ~KPilotConfigDialog();
+
+protected:
+	virtual void commitChanges();
+
+protected slots:
+	void changePortType(int);
+
+private:
+	KPilotConfigWidget *fConfigWidget;
+
+	void readConfig();
+	void disableUnusedOptions();
+	int getAddressDisplay() const;
+	void setAddressDisplay(int);
+} ;
+
+// $Log:$
+
+#endif
