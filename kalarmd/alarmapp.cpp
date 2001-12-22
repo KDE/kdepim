92d86d348e3ebdd306ec6b565b0f83b812228049
David Jarvie <djarvie@kde.org>
2001-12-22 15:49:41 +0000

    Ensure that a the daemon recognises a session-restoration invocation as starting at login
    svn path=/trunk/kdepim/; revision=128338

--- kalarmd/alarmapp.cpp
+++ kalarmd/alarmapp.cpp
@@ -49,9 +49,17 @@ int AlarmApp::newInstance()
   // Check if we already have a running alarm daemon widget
   if (mAd) return 0;
 
-  KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
-  mStartedAtLogin = args->isSet("login");
-  args->clear();      // free up memory
+  // Check if we are starting up at session startup
+  static bool restored = false;
+  if (!restored  &&  isRestored()) {
+    mStartedAtLogin = true;
+    restored = true;       // make sure we restore only once
+  }
+  else {
+    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
+    mStartedAtLogin = args->isSet("login");
+    args->clear();      // free up memory
+  }
 
   mAd = new AlarmDaemon(0L, "ad");
 
