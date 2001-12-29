2fb1837f119e0b73627f16245d850cfb6c81e161
Adriaan de Groot <groot@kde.org>
2001-12-29 15:41:36 +0000

    Added unified sync-action handling for kpilotTest and daemon
    svn path=/trunk/kdepim/; revision=129061

---
+++ kpilot/kpilot/syncStack.cc
@@ -0,0 +1,326 @@
+/* syncStack.cc                       KPilot
+**
+** Copyright (C) 1998-2001 by Dan Pilone
+**
+** This defines the "SyncStack", which is the pile of actions
+** that will occur during a HotSync.
+*/
+
+/*
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program in a file called COPYING; if not, write to
+** the Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+** MA 02139, USA.
+*/
+
+/*
+** Bug reports and questions can be sent to kde-pim@kde.org
+*/
+#include "options.h"
+
+static const char *syncStack_id = "$Id: $";
+
+#include <qtimer.h>
+
+#include <kservice.h>
+#include <kservicetype.h>
+#include <kuserprofile.h>
+#include <klibloader.h>
+
+#include "hotSync.h"
+#include "interactiveSync.h"
+#include "fileInstaller.h"
+
+#include "syncStack.moc"
+
+
+
+WelcomeAction::WelcomeAction(KPilotDeviceLink *p) :
+	SyncAction(p,"welcomeAction")
+{
+	FUNCTIONSETUP;
+}
+
+/* virtual */ void WelcomeAction::exec()
+{
+	FUNCTIONSETUP;
+
+	addSyncLogEntry(i18n("KPilot %1 HotSync starting ...\n").arg(KPILOT_VERSION));
+	emit syncDone(this);
+}
+
+ConduitProxy::ConduitProxy(KPilotDeviceLink *p,
+	const QString &name,
+	int m) :
+	ConduitAction(p,name),
+	fDesktopName(name),
+	fMode(m)
+{
+	FUNCTIONSETUP;
+}
+
+/* virtual */ void ConduitProxy::exec()
+{
+	FUNCTIONSETUP;
+
+	// query that service
+	KSharedPtr < KService > o = KService::serviceByDesktopName(fDesktopName);
+	if (!o)
+	{
+		kdWarning() << k_funcinfo
+			<< ": Can't find desktop file for conduit "
+			<< fDesktopName
+			<< endl;
+		addSyncLogEntry(i18n("Couldn't find conduit %1.").arg(fDesktopName));
+		emit syncDone(this);
+		return;
+	}
+
+
+	// load the lib
+#ifdef DEBUG
+	DEBUGKPILOT << fname
+		<< ": Loading desktop "
+		<< fDesktopName
+		<< " with lib "
+		<< o->library()
+		<< endl;
+#endif
+
+	fLibraryName = o->library();
+	KLibFactory *factory = KLibLoader::self()->factory(o->library());
+	if (!factory)
+	{
+		kdWarning() << k_funcinfo
+			<< ": Can't load library "
+			<< o->library()
+			<< endl;
+		addSyncLogEntry(i18n("Couldn't load conduit %1.").arg(fDesktopName));
+		emit syncDone(this);
+		return;
+	}
+
+	QStringList l;
+	switch(fMode)
+	{
+	case SyncStack::Test :
+		l.append("test");
+		break;
+	case SyncStack::Backup :
+		l.append("backup");
+		break;
+	default:
+		;
+	}
+
+
+	QObject *object = factory->create(fHandle,0L,"SyncAction",l);
+
+	if (!object)
+	{
+		kdWarning() << k_funcinfo
+			<< ": Can't create SyncAction."
+			<< endl;
+		addSyncLogEntry(i18n("Couldn't create conduit %1.").arg(fDesktopName));
+		emit syncDone(this);
+		return;
+	}
+
+	fConduit = dynamic_cast<ConduitAction *>(object);
+
+	if (!fConduit)
+	{
+		kdWarning() << k_funcinfo
+			<< ": Can't cast to ConduitAction."
+			<< endl;
+		addSyncLogEntry(i18n("Couldn't create conduit %1.").arg(fDesktopName));
+		emit syncDone(this);
+		return;
+	}
+	fConduit->setConfig(fConfig);
+
+	// Handle the syncDone signal properly & unload the conduit.
+	QObject::connect(fConduit,SIGNAL(syncDone(SyncAction *)),
+		this,SLOT(execDone(SyncAction *)));
+	// Proxy all the log and error messages.
+	QObject::connect(fConduit,SIGNAL(logMessage(const QString &)),
+		this,SIGNAL(logMessage(const QString &)));
+	QObject::connect(fConduit,SIGNAL(logError(const QString &)),
+		this,SIGNAL(logError(const QString &)));
+	QObject::connect(fConduit,SIGNAL(logProgress(const QString &,int)),
+		this,SIGNAL(logProgress(const QString &,int)));
+
+	QTimer::singleShot(0,fConduit,SLOT(exec()));
+}
+
+void ConduitProxy::execDone(SyncAction *p)
+{
+	FUNCTIONSETUP;
+
+	if (p!=fConduit)
+	{
+		kdError() << k_funcinfo
+			<< ": Unknown conduit @"
+			<< (int) p
+			<< " finished."
+			<< endl;
+		emit syncDone(this);
+		return;
+	}
+
+	delete p;
+	emit syncDone(this);
+}
+
+SyncStack::SyncStack(KPilotDeviceLink *d,
+	KConfig *config,
+	const QStringList &conduits,
+	const QString &dir,
+	const QStringList &files) :
+	SyncAction(d,"SyncStack"),
+	fConfig(config),
+	fConduits(conduits),
+	fInstallerDir(dir),
+	fInstallerFiles(files),
+	fReady(false)
+{
+	FUNCTIONSETUP;
+}
+
+SyncStack::~SyncStack()
+{
+	FUNCTIONSETUP;
+}
+
+void SyncStack::prepare(int m)
+{
+	FUNCTIONSETUP;
+
+	switch ( m & (Test | Backup | Restore | HotSync))
+	{
+	case Test:
+	case Backup:
+	case Restore:
+	case HotSync:
+		fReady=true;
+		break;
+	default:
+		kdWarning() << k_funcinfo
+			<< ": Strange sync mode " << m << " set. Aborting."
+			<< endl;
+		return;
+	}
+
+	addAction(new CleanupAction(fHandle));
+
+	if (m & WithInstaller)
+	{
+		addAction(new FileInstallAction(fHandle,fInstallerDir,fInstallerFiles));
+	}
+
+	switch ( m & (Test | Backup | Restore | HotSync))
+	{
+	case Test:
+		addAction(new TestLink(fHandle));
+		break;
+	case Backup:
+		addAction(new BackupAction(fHandle));
+		break;
+	case Restore:
+		addAction(new RestoreAction(fHandle));
+		break;
+	case HotSync:
+		break;
+	default:
+		// We already checked for this case!
+		fReady=false;
+		return;
+	}
+
+	// Add conduits here ...
+	//
+	//
+	for (QStringList::ConstIterator it = fConduits.begin();
+		it != fConduits.end();
+		++it)
+	{
+		ConduitProxy *cp =new ConduitProxy(fHandle,*it,m);
+		cp->setConfig(fConfig);
+		addAction(cp);
+	}
+
+	if (m & WithUserCheck)
+	{
+		addAction(new CheckUser(fHandle));
+	}
+
+	addAction(new WelcomeAction(fHandle));
+}
+
+void SyncStack::exec()
+{
+	nextAction(0L);
+}
+
+void SyncStack::nextAction(SyncAction *b)
+{
+	FUNCTIONSETUP;
+
+	if (b)
+	{
+#ifdef DEBUG
+		DEBUGDAEMON << fname
+			<< ": Completed action "
+			<< b->name()
+			<< endl;
+#endif
+		delete b;
+	}
+
+	if (isEmpty())
+	{
+		emit syncDone(this);
+		return;
+	}
+
+	SyncAction *a = nextAction();
+
+	if (!a)
+	{
+		kdWarning() << k_funcinfo
+			<< ": NULL action on stack."
+			<< endl;
+		return;
+	}
+
+#ifdef DEBUG
+	DEBUGDAEMON << fname
+		<< ": Will run action "
+		<< a->name()
+		<< endl;
+#endif
+
+	QObject::connect(a, SIGNAL(logMessage(const QString &)),
+		this, SIGNAL(logMessage(const QString &)));
+	QObject::connect(a, SIGNAL(logError(const QString &)),
+		this, SIGNAL(logMessage(const QString &)));
+	QObject::connect(a, SIGNAL(logProgress(const QString &, int)),
+		this, SIGNAL(logProgress(const QString &, int)));
+	QObject::connect(a, SIGNAL(syncDone(SyncAction *)),
+		this, SLOT(nextAction(SyncAction *)));
+
+	QTimer::singleShot(0,a,SLOT(exec()));
+}
+
+// $Log: $
+//
---
+++ kpilot/kpilot/syncStack.h
@@ -0,0 +1,193 @@
+#ifndef _KPILOT_SYNCSTACK_H
+#define _KPILOT_SYNCSTACK_H
+/* syncStack.h                        KPilot
+**
+** Copyright (C) 1998-2001 by Dan Pilone
+**
+** This defines the "SyncStack", which is the pile of actions
+** that will occur during a HotSync. There's also two fairly
+** unimportant SyncActions defined.
+*/
+
+/*
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program in a file called COPYING; if not, write to
+** the Free Software Foundation, Inc., 675 Mass Ave, Cambridge,
+** MA 02139, USA.
+*/
+
+/*
+** Bug reports and questions can be sent to kde-pim@kde.org
+*/
+
+#include <qstack.h>
+
+#include "plugin.h"
+
+/**
+* The SyncStack is a meta-action, which handles running a bunch of SyncActions
+* in sequence. It is a SyncAction itself, so it can even be pushed onto another
+* SyncStack (though I doubt that that is useful).
+*
+* A SyncStack is constructed with a @p device. As usual, you should connect
+* the device's deviceReady() signal with the exec() slot -- or something to that effect.
+* The SyncStack will then run all the actions on the stack in sequence.
+*
+* The @p config parameter is passed on to conduit actions that may be on the stack.
+*
+* @p conduits is a list of .desktop filenames (without the extension); for each conduit,
+* a ConduitProxy action is created, which loads the conduit and runs the SyncAction
+* from that conduit.
+*
+* For FileInstallers, pass in the list of filenames and the directory in @p installDir
+* and @p installFiles.
+*
+* When you create a SyncStack, pass in all the necessary conduit- and filenames.
+* You cannot change them later. You must also call prepare() to push all the items
+* on the stack; ie.
+*
+* SyncStack s(p,c,QStringList(),"/tmp","foo.prc");
+*
+* Creates a stack with some information set, but without any actions on the stack.
+* Next, call prepare() or somesuch:
+*
+* s.prepareSync();
+*
+* This will push a buch of "standard" actions on the stack for a HotSync, ie. a
+* welcome message action, user check, the conduits (if any; none were specified in the
+* constructor above), a file install action (which will try to install /tmp/foo.prc) and
+* some cleanup actions.
+*
+*
+* Alternatively, you can use addAction() to fill up the stack yourself.
+*/
+
+class SyncStack : public SyncAction
+{
+Q_OBJECT
+public:
+	SyncStack(KPilotDeviceLink *device,
+		KConfig *config,
+		const QStringList &conduits = QStringList(),
+		const QString &installDir = QString::null,
+		const QStringList &installFiles = QStringList());
+	virtual ~SyncStack();
+
+private:
+	QStack < SyncAction > SyncActionStack;
+
+public:
+	bool isEmpty() const { return SyncActionStack.isEmpty(); };
+	/**
+	* You can push your own actions onto the stack, but you should
+	* only do so if you don't call prepare().
+	*/
+	void addAction(SyncAction * a) { SyncActionStack.push(a); };
+
+protected:
+	void clear() { SyncActionStack.clear(); };
+	SyncAction *nextAction() { return SyncActionStack.pop(); };
+
+	bool fReady;
+	KConfig *fConfig;
+
+	QString fInstallerDir;
+	QStringList fInstallerFiles;
+	QStringList fConduits;
+
+public:
+	enum SyncModes {
+		// Exactly one of these four modes must be set
+		// (although Test can't be set explicitly).
+		//
+		Test=0,
+		Backup=1,
+		Restore=2,
+		HotSync=4,
+		// These are optional
+		//
+		//
+		WithUserCheck=32,
+		WithInstaller=64,
+		WithConduits=128
+		} ;
+
+	/**
+	* Call prepare() to push a "standard profile" of SyncActions onto the stack,
+	* ready for execution. These are welcome, cleanup, and actions indicated
+	* by @ref m. @p m is a bitwise or of items from SyncModes --- Exactly
+	* one of Test, Backup, Restore or HotSync, for the main intent of the stack,
+	* and possibly one or more of the With* elements, which insert extra
+	* actions at the relevant moment in the execution of the stack.
+	*/
+	void prepare(int m);
+	void prepareBackup() { prepare(Backup | WithUserCheck | WithConduits); } ;
+	void prepareRestore() { prepare(Restore | WithUserCheck); } ;
+	void prepareSync() { prepare(HotSync | WithUserCheck | WithConduits); } ;
+
+public slots:
+	virtual void exec();
+
+protected slots:
+	/**
+	* When one action finishes, start the next one.
+	*/
+	void nextAction(SyncAction *);
+};
+
+/**
+* This very special SyncAction puts "Welcome to KPilot"
+* in the sync log of the Pilot.
+*/
+class WelcomeAction : public SyncAction
+{
+Q_OBJECT
+
+public:
+	WelcomeAction(KPilotDeviceLink *);
+
+public slots:
+	virtual void exec();
+} ;
+
+/**
+* The ConduitProxy action delays loading the plugin for a conduit until the conduit
+* actually executes; the proxy then loads the file, creates a SyncAction for the conduit
+* and runs that. Once the conduit has finished, the proxy unloads everything
+* and emits syncDone().
+*/
+class ConduitProxy : public ConduitAction
+{
+Q_OBJECT
+
+public:
+	ConduitProxy(KPilotDeviceLink *,
+		const QString &desktopName,
+		int m);
+
+public slots:
+	virtual void exec();
+protected slots:
+	void execDone(SyncAction *);
+
+protected:
+	QString fDesktopName;
+	QString fLibraryName;
+	ConduitAction *fConduit;
+	int fMode;
+} ;
+
+
+// $Log: $
+//
+#endif
