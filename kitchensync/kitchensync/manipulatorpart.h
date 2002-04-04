93245809eaf6bd1c8d7ceca63c8669179d840e8e
Holger Freyther <holger+kde@freyther.de>
2002-04-04 13:30:48 +0000

    update make it more compile
    svn path=/trunk/kdepim/; revision=147026

--- kitchensync/kitchensync/manipulatorpart.h
+++ kitchensync/kitchensync/manipulatorpart.h
@@ -3,6 +3,7 @@
 #define ksync_manipulator_h
 
 #include <qpixmap.h>
+#include <qstring.h>
 #include <kparts/part.h>
 #include <qptrlist.h>
 
@@ -15,16 +16,16 @@ namespace KitchenSync {
      ManipulatorPart(const QString &name, QWidget *parent = 0, const char *name  = 0 );
      virtual ~ManipulatorPart();
      // the Type this Part understands/ is able to interpret
-     virtual QString type()const = 0;
+     virtual QString type()const {return QString::null; };
 
-     virtual int progress()const = 0;
-     virtual QString identifier()const = 0;
-     virtual QString name()const = 0;
+     virtual int progress()const { return 0; };
+     virtual QString identifier()const { return QString::null; };
+     virtual QString name()const { return QString::null; };
 
-     virtual QString description()const = 0;
-     virtual QPixmap *pixmap()const = 0;
+     virtual QString description()const { return QString::null; };
+     virtual QPixmap *pixmap()const { return 0l; };
     public slots:
-     virtual void syncEntries( const QPtrList<KSyncEntry> & ) = 0;     
+     virtual void syncEntries( const QPtrList<KSyncEntry> & ){ };     
 
   };
 };
