75cfc0995d3895a794a7d3827ff5b42900a8290f
Holger Freyther <holger+kde@freyther.de>
2002-04-04 10:07:04 +0000

    update
    svn path=/trunk/kdepim/; revision=146984

--- kitchensync/kitchensync/partbar.cc
+++ kitchensync/kitchensync/partbar.cc
@@ -22,7 +22,11 @@ ManipulatorPart* PartBarItem::part()
 {
   return m_Part;
 }
-QString PartBarItem::toolTip()
+QString PartBarItem::toolTip() const
+{
+
+}
+int PartBarItem::iconSize()const
 {
 
 }
@@ -34,5 +38,36 @@ int PartBarItem::height( const QListBox *) const
 {
 
 }
+int PartBarItem::iconSize() const
+{
+  return m_parent->iconSize();
+}
+void PartBarItem::paint(QPainter *p)
+{
+  QListBox *box = listBox();
+  int w = width( box );
+  static const int margin = 3;
+  int y = margin;
+  const QPixmap *pm = pixmap();
+  
+  if ( !pm->isNull() ) {
+    int x = (w - pm->width()) / 2;
+    x = QMAX( x, margin );
+    p->drawPixmap( x, y, *pm );
+  }
+  
+  if ( !text().isEmpty() ) {
+    QFontMetrics fm = p->fontMetrics();
+    y += pm->height() + fm.height() - fm.descent();
+    int x = (w - fm.width( text() )) / 2;
+    x = QMAX( x, margin );
+    p->drawText( x, y, text() );
+  }
+  if ( isCurrent() || isSelected() ) {
+    qDrawShadePanel( p, 1, 0, w -2, height(box),
+		     box->colorGroup(), true, 1, 0L );
+  }
+}
+
 
 PartBar::PartBar(QWidget *parent)
