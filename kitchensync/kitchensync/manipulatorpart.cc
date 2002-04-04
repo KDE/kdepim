589dac69f41e8371ab36abcbf964dadba3764d7b
Holger Freyther <holger+kde@freyther.de>
2002-04-04 16:57:58 +0000

    Why the hell do I always forget to cvs add files?
    svn path=/trunk/kdepim/; revision=147078

---
+++ kitchensync/kitchensync/manipulatorpart.cc
@@ -0,0 +1,17 @@
+
+#include <qobject.h>
+#include <qwidget.h>
+
+#include "manipulatorpart.h"
+
+
+using namespace KitchenSync;
+
+ManipulatorPart::ManipulatorPart(const QString &na, QWidget *parent, const char *name )
+  : KParts::Part(parent, name )
+{
+
+}
+
+
+//#include "manipulatorpart.moc"
