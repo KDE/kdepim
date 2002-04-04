9631beaed6a95b91142722a2ad4866eacc5f20c7
Maximilian Reiss <max.reiss@gmx.de>
2002-04-04 12:29:10 +0000

    updates
    svn path=/trunk/kdepim/; revision=147003

--- kitchensync/kitchensync/partbar.cc
+++ kitchensync/kitchensync/partbar.cc
@@ -1,47 +1,45 @@
+#include <qapplication.h>
+#include <qfontmetrics.h>
+#include <qpainter.h>
 
+#include <kglobal.h>
 #include <kiconloader.h>
 
 #include "partbar.h"
 
 using namespace KitchenSync;
 
-
 PartBarItem::PartBarItem( PartBar *parent, ManipulatorPart *part ) 
-  : QListBoxPixmap(KIconLoader::unknown() )
-{
-  m_Parent = parent;
+  : QListBoxPixmap(KIconLoader::unknown() ) {
+  m_Parents = parent;
   m_Part = part;
+  m_Pixmap = m_Part->pixmap();
   setCustomHighlighting( true );
   setText( part->description() ); 
+  setTooltip(part->description() );
 }
-PartBarItem::~PartBarItem()
-{
 
+PartBarItem::~PartBarItem() {
 }
-ManipulatorPart* PartBarItem::part()
-{
+
+ManipulatorPart* PartBarItem::part() {
   return m_Part;
 }
-QString PartBarItem::toolTip() const
-{
 
+QString PartBarItem::toolTip() const {
+  return ( m_Part->description() );
 }
-int PartBarItem::iconSize()const
-{
 
+int PartBarItem::width( const QListBox *listbox) const {
+  return listbox->viewport()->width() :
 }
-int PartBarItem::width( const QListBox * ) const
-{
 
+int PartBarItem::height( const QListBox *listbox) const {
+  int min = 0;
+  min = listbox->fontMetrics().lineSpacing() + pixmap()->height() + 6;
+  return min;
 }
-int PartBarItem::height( const QListBox *) const
-{
 
-}
-int PartBarItem::iconSize() const
-{
-  return m_parent->iconSize();
-}
 void PartBarItem::paint(QPainter *p)
 {
   QListBox *box = listBox();
@@ -63,6 +61,7 @@ void PartBarItem::paint(QPainter *p)
     x = QMAX( x, margin );
     p->drawText( x, y, text() );
   }
+  // draw sunken
   if ( isCurrent() || isSelected() ) {
     qDrawShadePanel( p, 1, 0, w -2, height(box),
 		     box->colorGroup(), true, 1, 0L );
@@ -70,4 +69,101 @@ void PartBarItem::paint(QPainter *p)
 }
 
 
-PartBar::PartBar(QWidget *parent)
+PartBar::PartBar(QWidget *parent) 
+  : QFrame ( parent ),
+    m_activeItem ( OL ),
+    m_listBox( OL ) {
+  
+  setListBox( OL );
+  setSizePolicy( QSizePolicy( QSizePolicy::Maximum, QSizePolicy::Preferred ) );
+}
+
+PartBarItem * PartBar::inserItem( ManipulatorPart *part) {
+  PartBarItem *item = new PartBarItem( this, part );
+  item->SetApplicationLocal ( applicationLocal );
+  return item;
+}
+
+void PartBar::setListBox(KListBox *view) {
+  delete m_listBox;
+  
+  if ( !view ) {
+    m_listBox = new KListBox(this);
+  } else {
+    m_listBox = view;
+    if ( m_listBox->parentWidget() != this ) {
+      m_listBox->reparen( this, QPoint(0,0) );
+    }
+    m_listBox->resize( width(), height() );
+  }
+  
+  m_listBox->setSelectionMode( KListBox::Single );
+  QPalette pal = palette();
+  QColor gray = pal.color(QPalette::Normal, QColorGroup::Mid );
+  pal.setColor( QPalette::Normal, QColorGroup::Base, gray );
+  pal.setColor( QPalette::Inactive, QColorGroup::Base, gray );
+  
+  setPalette( pal );
+  m_listBox->viewport()->setBackgroudMode( PaletteMid);
+
+  connect( m_listBox, SIGNAL( clicked (QListBoxItem *)),
+	   SLOT( slotSelected( QListBoxItem * )));
+}
+
+void PartBar::clear() {
+  m_listBox->clear();
+}
+
+void PartBar::resizeEvent() {
+  QFrame::resizeEvent( e );
+  m_listBox->resize( width(), height() );
+}
+
+QSize PartBar::sizeHint() {
+  int w = 0;
+  int h = 0;
+  
+  QListBoxItem *item;
+  
+  for ( item = m_listBox->firstItem(); item; item = item->next() ) {
+    w = QMAX(w , item->width( m_listBox ));
+    h += item->height( m_listBox );
+  }
+
+  if (m_listBox->verticalScroolBar->isVisible() ) {
+    w += m_listBox->verticalScrollBar()->width();
+  }
+
+  if ( W == 0 && h == 0) {
+    return QSize(100, 200);
+  } else {
+    return QSize( 6 + w , h );
+  }
+}
+
+QSize PartBar::minimumSizeHint() {
+  QSize s = sizeHint();
+  int h = s.height() + m_listBox->horizontalScrollBar()->height();
+  int w = s.width() + m_listBox->verticalScroolBar()->width();
+  return QSize( w, h );
+}
+
+void PartBar::slotSelected(QListBoxItem *item) {
+  if ( item && item != m_activeItem ) {
+    PartBarItem* it = static_cast<PartBarItem*>( item );
+    m_activeItem = it;
+    emit activated( it->part() );
+  }
+}
+
+ParBarItem * PartBar::currentItem() const {
+  QListBoxItem *item = m_listBox->item(m_listBox->currentItem() );
+  if ( item ) {
+    return static_cast<PartBarItem *>( item );
+  } else {
+    return OL;
+  }
+}
+
+
+
--- kitchensync/kitchensync/partbar.h
+++ kitchensync/kitchensync/partbar.h
@@ -4,20 +4,40 @@
 
 
 */
+#ifndef PARTBAR_H
+#define PARTBAR_H
 
 #include <klistbox.h>
+#include <qevent.h>
 
 #include "manipulatorpart.h"
 
 namespace KitchenSync {
+  
+  /* forward declaration */
   class PartBar;
+  
+  /**
+   * an member of the PartBar
+   */
   class PartBarItem : public QListBoxPixmap
     {
     public:
       PartBarItem( PartBar*, ManipulatorPart * );
       ~PartBarItem();
-
+      
+      /**
+       * the part to be embedded 
+       */
       ManipulatorPart* part();
+      
+      /**
+       * sets the icon for the item.
+       * @param icon the icon to set
+       * @param group the icongroup
+       */
+      void setIcon( const QString& icon, KIcon::Group group = KIcon::Panel );
+
       /**
        * returns the width of this item.
        */
@@ -26,31 +46,68 @@ namespace KitchenSync {
        * returns the height of this item.
        */
       virtual int height( const QListBox * ) const;
+      
+      /**
+       * returns the pixmap.
+       */
+      virtual const QPixmap * pixmap() const {
+	return &m_pixmap; 
+      }
+      
+    protected:
+      virtual void paint( QPainter *p); 
 
     private:
+      QPixmap m_Pixmap;
       ManipulatorPart m_Part;
-
+      PartBar* m_Parents;
     };
-
+  
+  
   class KListBox;
+  
+  /**
+   * PartBar is a widget that displays icons together. 
+   * The the items of the PartBar emit the activated() signal.
+   */
   class PartBar  : public QFrame
     {
-      //Q_OBJECT
+      Q_OBJECT
     public:
       PartPar( QWidget *parent = 0, const char *name = 0, WFlags f = 0 );
       ~PartBar();
-      virtual PartBarItem* insertItem(ManipulatorPart *part );
-
+      
+      virtual PartBarItem* insertItem( ManipulatorPart *part );
+       
+      /**
+       * Allows to set a custom KListBox
+       * Items of the previous box will not be moved to the next box
+       */
       virtual void setListBox(  KListBox*);
+      
+      /**
+       * @returns the KListBox that is used.
+       */
       KListBox *listBox() const { return m_listBox; }
+      
+      /**
+       * removes all items
+       */
       virtual void clear();
+      /**
+       * @return a size hint.
+       */
       virtual QSize sizeHint() const;
+      
       virtual QSize minimumSizeHint() const;
-
+      
+      /**
+       * @returns the current PartBarItem
+       */
       PartBarItem *currentItem() const;
 
     signals:
-      void activated( PartManipulator *part );
+      void activated( ManipulatorPart *part );
 
     protected slots:
       virtual void slotSelected( QListBoxItem * );
@@ -60,18 +117,9 @@ namespace KitchenSync {
 
     private:
       KListBox *m_listBox;
-
+      ParBarItem *m_activeItem;
     };
-
-
-
+  
 };
 
-
-
-
-
-
-  };
-
-}
+#endif 
