be1383c4328fbd043a4f076ae6d2eb0bd8a16178
Holger Freyther <holger+kde@freyther.de>
2002-05-09 13:05:12 +0000

    Importing of Opie/Zaurus/Qtopia Datebook is done Export to go
    svn path=/trunk/kdepim/; revision=154625

--- kitchensync/libkonnector/plugins/opie/datebook.cc
+++ kitchensync/libkonnector/plugins/opie/datebook.cc
@@ -1,4 +1,5 @@
 
+#include <qbitarray.h>
 #include <qdom.h>
 #include <qfile.h>
 
@@ -64,7 +65,91 @@ QPtrList<KCal::Event> DateBook::toKDE( const QString& fileName )
                             event->setFloats( false );
                         }
                         KCal::Alarm *al = new KCal::Alarm( event );
+                        al->setText( event->description() );
+                        al->setOffset( e.attribute("alarm").toInt() * -60 );
+                        al->setAudioFile( e.attribute("sound") );
+                        event->addAlarm( al );
 
+                        // Recurrence damn I feared to do that
+                        QString type = e.attribute("rtype");
+                        int freq = e.attribute("rfreq").toInt();
+                        bool hasEnd = e.attribute("rhasenddate");
+
+                        KCal::Recurrence *rec = event->recurrence();
+                        start = e.attribute("created");
+                        rec->setRecurStart( fromUTC( (time_t) start.toLong() ) );
+                        if ( type == "Daily" ) {
+                            if ( hasEnd ) {
+                                start = e.attribute("enddt");
+                                rec->setDaily(freq,  fromUTC( (time_t) start.toLong() ).date() );
+                            }else{
+                                rec->setDaily( freq,  -1 );
+                            }
+                        }else if ( type == "Weekly") {
+                            int days = e.attribute("rweekdays").toInt();
+                            QBitArray bits( 7 );
+                            bits.fill( false );
+                            if ( Monday & days )
+                                bits.setBit( 0 );
+                            else if ( Tuesday & days )
+                                bits.setBit( 1 );
+                            else if ( Wednesday & days )
+                                bits.setBit( 2 );
+                            else if ( Thursday & days )
+                                bits.setBit( 3 );
+                            else if ( Friday & days )
+                                bits.setBit( 4 );
+                            else if ( Saturday & days )
+                                bits.setBit( 5 );
+                            else if ( Sunday & days )
+                                bits.setBit( 6 );
+                            if ( hasEnd ) {
+                                start = e.attribute("enddt");
+                                rec->setWeekly( freq,  bits, fromUTC( (time_t) start.toLong() ).date() );
+                            }else{
+                                rec->setWeekly( freq,  bits,  -1 );
+                            }
+
+                        }else if ( type == "MonthlyDay" ) {
+                            // monthly day the  1st Saturday of the month
+                            int rposition = e.attribute("rposition").toInt();
+                            if ( hasEnd ) {
+                                start = e.attribute("enddt");
+                                rec->setMonthly( KCal::Recurrence::rMonthlyPos,
+                                                 freq,fromUTC( (time_t) start.toLong() ).date() );
+                            }else{
+                                rec->setMonthly( KCal::Recurrence::rMonthlyPos,
+                                                 freq,  -1 );
+                            }
+                            QBitArray array( 7);
+                            array.fill( false );
+                            QDate date = event->dtStart().date();
+                            array.setBit( date.dayOfWeek() - 1 );
+                            rec->addMonthlyPos( rposition, array );
+                        }else if ( type == "MonthlyDate" ) {
+                            int rposition = e.attribute("rposition").toInt();
+                            if ( hasEnd ) {
+                                start = e.attribute("enddt");
+                                rec->setMonthly( KCal::Recurrence::rMonthlyDay,
+                                                 freq,fromUTC( (time_t) start.toLong() ).date() );
+                            }else{
+                                rec->setMonthly( KCal::Recurrence::rMonthlyDay,
+                                                 freq,  -1 );
+                            }
+                            QDate date = event->dtStart().date();
+                            rec->addMonthlyDay( date.day() );
+                        }else if ( type == "Yearly" ) {
+                            if (hasEnd ) {
+                                start = e.attribute("enddt");
+                                rec->setYearly( KCal::Recurrence::rYearlyDay,
+                                                freq,
+                                                fromUTC( (time_t) start.toLong() ).date() );
+                            }else{
+                                rec->setYearly( KCal::Recurrence::rYearlyDay,
+                                                freq, -1 );
+                            }
+                            rec->addYearlyNum( event->dtStart().date().dayOfYear() );
+                        }
                         m_list.append( event );
                     }
                     n = n.nextSibling();
--- kitchensync/libkonnector/plugins/opie/datebook.h
+++ kitchensync/libkonnector/plugins/opie/datebook.h
@@ -9,7 +9,7 @@
 
 class KAlendarSyncEntry;
 namespace OpieHelper {
-
+    enum Days { Monday=1,  Tuesday =2,  Wednesday =4,  Thursday = 8,  Friday = 16,  Saturday = 32,  Sunday = 64 };
     class DateBook : public Base {
     public:
         DateBook( CategoryEdit* edit = 0,
