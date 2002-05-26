6443f3cb9e4d19e1f2f41e57adc50ec9c396cc82
Cornelius Schumacher <schumacher@kde.org>
2002-05-26 19:57:40 +0000

    Test for cloning incidences.
    svn path=/trunk/kdepim/; revision=157934

--- libkcal/tests/testincidence.cpp
+++ libkcal/tests/testincidence.cpp
@@ -4,25 +4,65 @@
 #include <klocale.h>
 #include <kcmdlineargs.h>
 
-#include "event.h"
 #include "icalformat.h"
+#include "event.h"
+#include "todo.h"
 
 using namespace KCal;
+
+static const KCmdLineOptions options[] =
+{
+  {"verbose", "Verbose output", 0},
+  {0,0,0}
+};
 
 int main(int argc,char **argv)
 {
   KAboutData aboutData("testincidence","Test Incidence","0.1");
   KCmdLineArgs::init(argc,argv,&aboutData);
+  KCmdLineArgs::addCmdLineOptions( options );
 
 //  KApplication app( false, false );
   KApplication app;
 
-  Event *event = new Event;
-  
-  event->setSummary("Test Event");
-  
-  Incidence *event2 = event->clone();
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+
+  bool verbose = false;
+  if ( args->isSet( "verbose" ) ) verbose = true;
 
   ICalFormat f;
-  kdDebug() << "EVENT2 START:" << f.toString( event2 ) << "EVENT2 END" << endl;
+
+  Event *event1 = new Event;
+  event1->setSummary("Test Event");
+  QString eventString1 = f.toString( event1 );
+  if ( verbose )
+    kdDebug() << "EVENT1 START:" << eventString1 << "EVENT1 END" << endl;
+    
+  Incidence *event2 = event1->clone();
+  QString eventString2 = f.toString( event2 );
+  if( verbose )
+    kdDebug() << "EVENT2 START:" << eventString2 << "EVENT2 END" << endl;
+
+  if ( eventString1 != eventString2 ) {
+    kdDebug() << "Clone Event FAILED." << endl;
+  } else {
+    kdDebug() << "Clone Event SUCCEEDED." << endl;
+  }
+
+  Todo *todo1 = new Todo;
+  todo1->setSummary("Test todo");
+  QString todoString1 = f.toString( todo1 );
+  if( verbose )
+    kdDebug() << "todo1 START:" << todoString1 << "todo1 END" << endl;
+    
+  Incidence *todo2 = todo1->clone();
+  QString todoString2 = f.toString( todo2 );
+  if( verbose )
+    kdDebug() << "todo2 START:" << todoString2 << "todo2 END" << endl;
+
+  if ( todoString1 != todoString2 ) {
+    kdDebug() << "Clone Todo FAILED." << endl;
+  } else {
+    kdDebug() << "Clone Todo SUCCEEDED." << endl;
+  }
 }
