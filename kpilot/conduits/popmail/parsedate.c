d70f24935be24a1db347645654844a00eb6347ab
Adriaan de Groot <groot@kde.org>
2002-06-04 06:55:53 +0000

    malloc.h mus die; why do we even *have* parsedate?
    svn path=/trunk/kdepim/; revision=159269

--- kpilot/conduits/popmail/parsedate.c
+++ kpilot/conduits/popmail/parsedate.c
@@ -299,10 +299,10 @@ static const short yycheck[] = {    23,
 #include <alloca.h>
 #else /* not sparc */
 #if defined (MSDOS) && !defined (__TURBOC__)
-#include <malloc.h>
+#include <stdlib.h>
 #else /* not MSDOS, or __TURBOC__ */
 #if defined(_AIX)
-#include <malloc.h>
+#include <stdlib.h>
  #pragma alloca
 #else /* not MSDOS, __TURBOC__, or _AIX */
 #ifdef __hpux
