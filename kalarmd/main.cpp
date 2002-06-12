7b319d4e53d5c44eabb41d4ec351f9f93692ff44
David Jarvie <djarvie@kde.org>
2002-06-12 21:42:14 +0000

    Update version number
    svn path=/branches/KDE_3_0_BRANCH/kdepim/; revision=160626

--- kalarmd/main.cpp
+++ kalarmd/main.cpp
@@ -37,7 +37,7 @@
 
 #include "alarmapp.h"
 
-static const char* kalarmdVersion = "0.9";
+static const char* kalarmdVersion = "3.0.2";
 static const KCmdLineOptions options[] =
 {
    { "login", I18N_NOOP("Application is being auto-started at KDE session start"), 0L },
