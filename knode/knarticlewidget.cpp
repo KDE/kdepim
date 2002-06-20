900a2b42ca341e0b69f94cb4ecd9019740f551b4
Simon Hausmann <hausmann@kde.org>
2002-06-20 19:34:32 +0000

    - make initialization order match declaration order
    svn path=/trunk/kdenetwork/knode/; revision=162205

--- knode/knarticlewidget.cpp
+++ knode/knarticlewidget.cpp
@@ -126,7 +126,7 @@ QByteArray KNMimeSource::encodedData(const char *) const
 
 
 KNArticleWidget::KNArticleWidget(KActionCollection* actColl, QWidget *parent, const char *name )
-    : KTextBrowser(parent, name), a_rticle(0), a_tt(0), h_tmlDone(false), emuKMail(false), a_ctions(actColl), f_inddialog(0)
+    : KTextBrowser(parent, name), a_rticle(0), a_tt(0), h_tmlDone(false), emuKMail(false), f_inddialog(0), a_ctions(actColl)
 {
   i_nstances.append(this);
   setNotifyClick( true );
