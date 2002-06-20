bc3f9633df196feaebd3880b5d0b51d53e60387b
Mathias Waack <mathias@atoll-net.de>
2002-06-20 17:35:53 +0000

    fix for 44041
    svn path=/trunk/kdenetwork/knode/; revision=162184

--- knode/knconfig.cpp
+++ knode/knconfig.cpp
@@ -688,7 +688,7 @@ KNConfig::ReadNewsViewer::ReadNewsViewer()
   s_howSig=conf->readBoolEntry("showSig", true);
   i_nterpretFormatTags=conf->readBoolEntry("interpretFormatTags", true);
   q_uoteCharacters=conf->readEntry("quoteCharacters",">:");
-  i_nlineAtt=conf->readBoolEntry("inlüineAtt", true);
+  i_nlineAtt=conf->readBoolEntry("inlineAtt", true);
   o_penAtt=conf->readBoolEntry("openAtt", false) ;
   s_howAlts=conf->readBoolEntry("showAlts", false);
   f_ullHdrs=conf->readBoolEntry("fullHdrs", false);
