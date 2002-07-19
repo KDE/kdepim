b77e9e2861436214528130e95b53124406f70d19
Luk Tinkl <lukas@kde.org>
2002-07-19 14:37:04 +0000

    missing i18n()
    svn path=/trunk/kdepim/; revision=167534

--- kfile-plugins/vcf/kfile_vcf.cpp
+++ kfile-plugins/vcf/kfile_vcf.cpp
@@ -51,7 +51,7 @@ KVcfPlugin::KVcfPlugin(QObject *parent, const char *name,
 
     KFileMimeTypeInfo::GroupInfo* group = 0L;
 
-    group = addGroupInfo(info, "Technical", "Technical Details");
+    group = addGroupInfo(info, "Technical", i18n("Technical Details"));
 
     KFileMimeTypeInfo::ItemInfo* item;
 
