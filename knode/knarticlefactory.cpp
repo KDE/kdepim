e9a098dec7b6ab58c45ad1f3d6f77a06885f5c23
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2002-07-21 16:21:05 +0000

    Corrected typographical errors
    svn path=/trunk/kdenetwork/knode/; revision=167982

--- knode/knarticlefactory.cpp
+++ knode/knarticlefactory.cpp
@@ -277,7 +277,7 @@ void KNArticleFactory::createForward(KNArticle *a)
   bool incAtt = ( !knGlobals.cfgManager->postNewsTechnical()->useExternalMailer() &&
                   ct->isMultipart() && ct->isSubtype("mixed") &&
                   KMessageBox::Yes == KMessageBox::questionYesNo(knGlobals.topWidget,
-                  i18n("This article contains attachments. Do you want them to be forwarded aswell?"))
+                  i18n("This article contains attachments. Do you want them to be forwarded as well?"))
                 );
 
   if (knGlobals.cfgManager->postNewsTechnical()->useOwnCharset())
