cd5038fed6d0a6dddcf595d81dc22b4df516dfca
David Faure <faure@kde.org>
2002-08-22 08:38:09 +0000

    Layout (well, fixed size) fix for hour and minute lineedits
    svn path=/trunk/kdepim/; revision=173236

--- karm/ktimewidget.cpp
+++ karm/ktimewidget.cpp
@@ -8,27 +8,27 @@
 
 enum ValidatorType { HOUR, MINUTE };
 
-class TimeValidator :public QValidator 
+class TimeValidator :public QValidator
 {
 public:
-	TimeValidator(ValidatorType tp, QWidget *parent=0, const char *name=0) :QValidator(parent, name) 
+	TimeValidator(ValidatorType tp, QWidget *parent=0, const char *name=0) :QValidator(parent, name)
 	{
-		_tp = tp;		
+		_tp = tp;
 	}
 	State validate(QString &str, int &) const
 	{
 		if (str.isEmpty())
 			return Acceptable;
-		
+
 		bool ok;
 		int val = str.toInt( &ok );
 		if ( ! ok )
 			return Invalid;
-		
+
 		if ( str.contains('-') != 0 )
 			return Invalid;
-		
-		if ( _tp==MINUTE && val >= 60  ) 
+
+		if ( _tp==MINUTE && val >= 60  )
 			return Invalid;
 		else
 			return Acceptable;
@@ -39,19 +39,19 @@ public:
 };
 
 
-class KarmLineEdit :public QLineEdit 
+class KarmLineEdit :public QLineEdit
 {
-	
+
 public:
-	KarmLineEdit( QWidget* parent, const char* name = 0 ) :QLineEdit( parent, name ) 
+	KarmLineEdit( QWidget* parent, const char* name = 0 ) :QLineEdit( parent, name )
 	{
 	}
-	
+
 protected:
 
 	virtual void keyPressEvent( QKeyEvent *event ) {
 		QLineEdit::keyPressEvent( event );
-		if ( text().length() == 2 && !event->text().isNull() ) 
+		if ( text().length() == 2 && !event->text().isEmpty() )
 			focusNextPrevChild(true);
 	}
 };
@@ -62,22 +62,24 @@ protected:
 KTimeWidget::KTimeWidget(  QWidget* parent, const char* name ) : QWidget(parent, name)
 {
 	QHBoxLayout *layout = new QHBoxLayout(this);
-	
+
 	_hourLE = new QLineEdit( this);
-		
-	_hourLE->setFixedWidth(fontMetrics().maxWidth()*5);
+
+        // Hours lineedit: make room for up to 5 digits
+	_hourLE->setFixedWidth(fontMetrics().maxWidth()*5 + 2*_hourLE->frameWidth() + 2);
 	layout->addWidget(_hourLE);
 	TimeValidator *validator = new TimeValidator( HOUR, _hourLE, "Validator for _hourLE");
 	_hourLE->setValidator( validator );
 	_hourLE->setAlignment( Qt::AlignRight );
-	
-	
+
+
 	QLabel *dot = new QLabel(QString::fromLatin1( " : " ), this);
 	layout->addWidget(dot);
 
 	_minuteLE = new KarmLineEdit(this);
-	
-	_minuteLE->setFixedWidth(fontMetrics().maxWidth()*2);
+
+        // Minutes lineedit: Make room for 2 digits
+	_minuteLE->setFixedWidth(fontMetrics().maxWidth()*2 + 2*_minuteLE->frameWidth() + 2);
 	layout->addWidget(_minuteLE);
 	validator = new TimeValidator( MINUTE, _minuteLE, "Validator for _minuteLE");
 	_minuteLE->setValidator( validator );
@@ -94,7 +96,7 @@ void KTimeWidget::setTime( int hour, int minute )
 
 	dummy.setNum( hour );
 	_hourLE->setText( dummy );
-	
+
 	dummy.setNum( abs(minute) );
 	if (abs(minute) < 10 ) {
 		dummy = QString::fromLatin1( "0" ) + dummy;
@@ -106,7 +108,7 @@ long KTimeWidget::time() const
 {
 	bool ok;
 	int h, m;
-	
+
 	h = _hourLE->text().toInt( &ok );
 	m = _minuteLE->text().toInt( &ok );
 
