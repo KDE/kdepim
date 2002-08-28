4ca3d84ffde1bbb8021e68bb370a476a725bebdd
Roberto Teixeira <rst@kde.org>
2002-08-28 13:25:35 +0000

    *** empty log message ***
    svn path=/trunk/kdenetwork/knode/; revision=174284

--- knode/knarticlefactory.cpp
+++ knode/knarticlefactory.cpp
@@ -308,7 +308,8 @@ void KNArticleFactory::createForward(KNArticle *a)
 
   fwd+=( i18n(" Subject")+": "+a->subject()->asUnicodeString()+"\n");
   fwd+=( i18n(" From")+": "+a->from()->asUnicodeString()+"\n");
-  fwd+=( i18n(" Date")+": "+a->date()->asUnicodeString()+"\n\n");
+  fwd+=( i18n(" Date")+": "+a->date()->asUnicodeString()+"\n");
+  fwd+=( i18n(" Newsgroup")+": "+a->newsgroups()->asUnicodeString()+"\n\n");
 
   KMime::Content *text=a->textContent();
   if(text) {
