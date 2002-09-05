4e2baf28f6bef6dee09c4b1e0537bd2a456fd21f
Daniel Molkentin <molkentin@kde.org>
2002-09-05 08:30:51 +0000

    Much more simple config interface using QCheckListItems. Steffen, please have a look.
    CCMAIL: hansen@kde.org
    
    svn path=/trunk/kdepim/; revision=175818

--- kaddressbook/ldapoptionswidgetimpl.cpp
+++ kaddressbook/ldapoptionswidgetimpl.cpp
@@ -1,27 +1,30 @@
-#include "ldapoptionswidgetimpl.h"
-#include "addhostdialog.h"
 #include <qlistbox.h>
 #include <qmessagebox.h>
-#include <KDListBoxPair.h>
 #include <qpushbutton.h>
 #include <qstring.h>
 #include <qspinbox.h>
+#include <qheader.h>
+
+#include <klistview.h>
 #include <klocale.h>
+
+#include "ldapoptionswidgetimpl.h"
+#include "addhostdialog.h"
 
 /*
  *  Constructs a LDAPOptionsWidgetImpl which is a child of 'parent', with the
- *  name 'name' and widget flags set to 'f' 
+ *  name 'name' and widget flags set to 'f'
  */
 LDAPOptionsWidgetImpl::LDAPOptionsWidgetImpl( QWidget* parent,  const char* name, WFlags fl )
   : LDAPOptionsWidget( parent, name, fl )
 {
-  // a connection to enable or disable Edit and Remove 
-  // PushButtons depending on if the left list box is 
-  // populated or not
-  QObject::connect( listBoxPair->leftListBox(),SIGNAL(selectionChanged()),this, SLOT( checkSelected()));
+  // a connection to enable or disable Edit and Remove PushButtons
+  QObject::connect( ldapServersLV,SIGNAL(selectionChanged()),this, SLOT( checkSelected()));
+  ldapServersLV->addColumn(QString::null);
+  ldapServersLV->header()->hide();
 }
 
-/*  
+/*
  *  Destroys the object and frees any allocated resources
  */
 LDAPOptionsWidgetImpl::~LDAPOptionsWidgetImpl()
@@ -29,13 +32,11 @@ LDAPOptionsWidgetImpl::~LDAPOptionsWidgetImpl()
   // no need to delete child widgets, Qt does it all for us
 }
 
-// the user will not be able to use the Edit and Remove 
-// PushButtons if there are no items to edit or remove 
-// in the left list box.
+// the user will not be able to use the Edit and Remove
+// PushButtons if there are no items to edit
 void  LDAPOptionsWidgetImpl::checkSelected()
 {
-  int e = listBoxPair->leftListBox()->currentItem();
-  if (e != -1 ) {
+  if (ldapServersLV->currentItem() != 0L ) {
     editHostPB->setEnabled(true);
     removeHostPB->setEnabled(true);
   }
@@ -46,8 +47,8 @@ void  LDAPOptionsWidgetImpl::checkSelected()
 
 }
 
-/* 
- * This function add a host server in the left list box
+/*
+ * This function add a host server in the list view
  * and register this host server its associated data in
  * a map in order for us to be able to retrieve this data
  * when needed.
@@ -62,24 +63,24 @@ void LDAPOptionsWidgetImpl::addHost()
   // text has been written.
   if ( addDialog.exec() && !addDialog.hostEdit->text().stripWhiteSpace().isEmpty())
     {
-      
-      // Insert the host server adress in the left list box 
-      listBoxPair->leftListBox()->insertItem(addDialog.hostEdit->text());
+
+      // Insert the host server adress in the list view
+      (void) new QCheckListItem( ldapServersLV, addDialog.hostEdit->text(), QCheckListItem::CheckBox );
 
       // we have now an adress: enable the edit and remove
-      // store the host server and its data 
-      QString host = addDialog.hostEdit->text().stripWhiteSpace(); 
+      // store the host server and its data
+      QString host = addDialog.hostEdit->text().stripWhiteSpace();
       int port = addDialog.portSpinBox->value();
       QString base = addDialog.baseEdit->text().stripWhiteSpace();
       // create a host account
       server.setbase(base);
       server.setport(port);
       // register the host and its data in the map
-      _ldapservers[host] = server; 
+      _ldapservers[host] = server;
     }
 }
 
-/* 
+/*
  * This function edit the host server selected by the user
  * and the data associated to this host server in a dialog.
  * The host server and its data are retrieved from a map and
@@ -88,20 +89,17 @@ void LDAPOptionsWidgetImpl::addHost()
 void LDAPOptionsWidgetImpl::editHost()
 {
 
-  // the variables to store the data we will retrieve in 
+  // the variables to store the data we will retrieve in
   // the map ( where servers adresses and data are stored
   QString host, base;
   int port = 0;
 
   // the key to retrieve the data associated to the server
   // ( port and baseDn ) in our map.
-  QString keyhost= listBoxPair->leftListBox()->currentText().stripWhiteSpace();
+  QCheckListItem *item = static_cast<QCheckListItem *>(ldapServersLV->currentItem());
+  QString keyhost = item->text(0).stripWhiteSpace();
 
-  // the index of the current item in the list box needed to
-  // delete the item later.
-  int i = listBoxPair->leftListBox()->currentItem();
-
-  // our message box editing the data the user want to 
+  // our message box editing the data the user want to
   // consult
   AddHostDialog addDialog(this, "AddHostDialog", true);
   addDialog.setCaption( i18n("Edit Host") );
@@ -118,19 +116,19 @@ void LDAPOptionsWidgetImpl::editHost()
       break;
     }
   }
-  
+
   // the servers'data was retrieved and populate the
   // the line edit in the message box
   addDialog.hostEdit->setText(host);
   addDialog.portSpinBox->setValue(port);
   addDialog.baseEdit->setText(base);
 
-  // the user clicked on the 'OK' PushButton 
-  if (addDialog.exec()&& (addDialog.hostEdit->text() != "")){
+  // the user clicked on the 'OK' PushButton
+  if (addDialog.exec()&& (!addDialog.hostEdit->text().isEmpty())){
     /* In case the user changed some data */
 
     // first remove the servers'data from the map
-    // ( we'll then register the modified data ) 
+    // ( we'll then register the modified data )
 
     for ( it = _ldapservers.begin(); it != _ldapservers.end(); ++it ) {
       if (it.key()== keyhost) {
@@ -138,16 +136,14 @@ void LDAPOptionsWidgetImpl::editHost()
 	break;
       }
     }
-   
-    // remove the item which was edited from the list box
-    listBoxPair->leftListBox()->removeItem(i);
-    // insert the new data in the list box 
-    listBoxPair->leftListBox()->insertItem(addDialog.hostEdit->text());
+
+    // change text on the current item
+    item->setText(0, addDialog.hostEdit->text());
 
     /* the data has to be registered in the map also */
-    
+
     // store the new values
-    QString ht = addDialog.hostEdit->text().stripWhiteSpace(); 
+    QString ht = addDialog.hostEdit->text().stripWhiteSpace();
     int pt = addDialog.portSpinBox->value();
     QString be = addDialog.baseEdit->text().stripWhiteSpace();
 
@@ -162,7 +158,7 @@ void LDAPOptionsWidgetImpl::editHost()
 
 /*
  * This function remove the host server adress from the
- * left list box and the host server and its associated
+ * list view and the host server and its associated
  * data ( port and baseDN ) from the servers map.
  */
 void LDAPOptionsWidgetImpl::removeHost()
@@ -175,16 +171,8 @@ void LDAPOptionsWidgetImpl::removeHost()
 
   // the key to retrieve the data associated to the server
   // ( port and baseDn ) in our map.
-  QString keyhost= listBoxPair->leftListBox()->currentText().stripWhiteSpace();
-
-  // the index of the current item in the list box needed to
-  // delete the item later.
-  int i = listBoxPair->leftListBox()->currentItem();
-
-
-  AddHostDialog addDialog(this, "AddHostDialog", true);
-  addDialog.setCaption( i18n("Remove Host") );
-  addDialog.show();
+  QCheckListItem *item = static_cast<QCheckListItem *>(ldapServersLV->currentItem());
+  QString keyhost = item->text(0).stripWhiteSpace();
 
   ServersMap::Iterator it;
   for ( it = _ldapservers.begin(); it != _ldapservers.end(); ++it ) {
@@ -195,29 +183,18 @@ void LDAPOptionsWidgetImpl::removeHost()
       break;
     }
   }
-  // the servers'data was retrieved and populate the
-  // the line edit in the message box
-  addDialog.hostEdit->setText(host);
-  addDialog.portSpinBox->setValue(port);
-  addDialog.baseEdit->setText(base);
 
-  // the user clicked on the 'OK' PushButton
-  if ((addDialog.exec()) && (addDialog.hostEdit->text()!= "")){
-    // remove the host server adress from the list box
-    listBoxPair->leftListBox()->removeItem(i);
+  // remove the host server adress from the list box
+  delete item;
 
-    // Remove the servers'data from the map
-    // ( we'll then register the modified data ) 
-    for ( it = _ldapservers.begin(); it != _ldapservers.end(); ++it ) {
-      if (it.key()== keyhost) {
-	_ldapservers.remove(it);
-	break;
-      }
+  // Remove the servers'data from the map
+  // ( we'll then register the modified data )
+  for ( it = _ldapservers.begin(); it != _ldapservers.end(); ++it ) {
+    if (it.key()== keyhost) {
+      _ldapservers.remove(it);
+      break;
     }
   }
 }
 
-
-    
-  
 #include "ldapoptionswidgetimpl.moc"
