12076d28d64e5e5cdce6aeaf2da7bbd67cc70f17
Tomas Pospisek <tpo@sourcepole.ch>
2002-11-02 19:25:00 +0000

    cleaned up indent replaced f( void ) by f()
    svn path=/branches/Unstable_Development/kdepim/; revision=187010

--- karm/adddlg.cpp
+++ karm/adddlg.cpp
@@ -48,16 +48,16 @@ AddTaskDialog::AddTaskDialog(QString caption, bool editDlg)
   QWidget *page = new QWidget( this ); 
   setMainWidget(page);
 
-	QVBoxLayout *lay1 = new QVBoxLayout(page);
-	
-	QHBoxLayout *lay2 = new QHBoxLayout();
-	lay1->addLayout(lay2);
-	
-	// The name of the widget
+  QVBoxLayout *lay1 = new QVBoxLayout(page);
+  
+  QHBoxLayout *lay2 = new QHBoxLayout();
+  lay1->addLayout(lay2);
+  
+  // The name of the widget
   QLabel *label = new QLabel( i18n("Task &name:"), page, "name" );
   lay2->addWidget( label );
-	lay2->addSpacing(5);
-	
+  lay2->addSpacing(5);
+  
   
   _name = new QLineEdit( page, "lineedit" );
   
@@ -66,58 +66,58 @@ AddTaskDialog::AddTaskDialog(QString caption, bool editDlg)
   label->setBuddy( _name );
 
 
-	// The "Edit Absolut" radio button
-	lay1->addSpacing(10);lay1->addStretch(1);	
-	_absoluteRB = new QRadioButton( i18n( "Edit &absolute" ), page, "_absoluteRB" );
-	lay1->addWidget( _absoluteRB );
-	connect( _absoluteRB, SIGNAL( clicked() ), this, SLOT(slotAbsolutePressed()) );
-	
+  // The "Edit Absolut" radio button
+  lay1->addSpacing(10);lay1->addStretch(1); 
+  _absoluteRB = new QRadioButton( i18n( "Edit &absolute" ), page, "_absoluteRB" );
+  lay1->addWidget( _absoluteRB );
+  connect( _absoluteRB, SIGNAL( clicked() ), this, SLOT(slotAbsolutePressed()) );
+  
 
-	// Absolute times
-	QHBoxLayout *lay5 = new QHBoxLayout();
-	lay1->addLayout(lay5);
-	lay5->addSpacing(20);
-	QGridLayout *lay3 = new QGridLayout( 2, 2, -1, "lay3" );
-	lay5->addLayout(lay3);
-	
-	// Total Time
+  // Absolute times
+  QHBoxLayout *lay5 = new QHBoxLayout();
+  lay1->addLayout(lay5);
+  lay5->addSpacing(20);
+  QGridLayout *lay3 = new QGridLayout( 2, 2, -1, "lay3" );
+  lay5->addLayout(lay3);
+  
+  // Total Time
   _totalLA = new QLabel( i18n("&Total:"), page, "time" );
   lay3->addWidget( _totalLA, 0, 0 );
 
-	_totalTW = new KTimeWidget( page, "_totalTW" );
-	lay3->addWidget( _totalTW, 0, 1 );
+  _totalTW = new KTimeWidget( page, "_totalTW" );
+  lay3->addWidget( _totalTW, 0, 1 );
   _totalLA->setBuddy( _totalTW );
-	
+  
 
-	// Session
+  // Session
   _sessionLA = new QLabel( i18n("&Session time:"), page, "session time" );
   lay3->addWidget( _sessionLA, 1, 0 );
 
-	_sessionTW = new KTimeWidget( page, "_sessionTW" );
+  _sessionTW = new KTimeWidget( page, "_sessionTW" );
   lay3->addWidget( _sessionTW, 1, 1 );
   _sessionLA->setBuddy( _sessionTW );
 
 
-	// The "Edit relative" radio button
-	lay1->addSpacing(10);lay1->addStretch(1);
-	_relativeRB = new QRadioButton( i18n( "Edit &relative (Apply to both session and total)" ), page, "_relativeRB" );
-	lay1->addWidget( _relativeRB );
-	connect( _relativeRB, SIGNAL( clicked() ), this, SLOT(slotRelativePressed()) );
-	
-	// The relative times
-	QHBoxLayout *lay4 = new QHBoxLayout();
-	lay1->addLayout( lay4 );
-	lay4->addSpacing(20);
-	
-	_operator	= new QComboBox(page);
-	_operator->insertItem( QString::fromLatin1( "+" ) );
-	_operator->insertItem( QString::fromLatin1( "-" ) );
-	lay4->addWidget( _operator );
+  // The "Edit relative" radio button
+  lay1->addSpacing(10);lay1->addStretch(1);
+  _relativeRB = new QRadioButton( i18n( "Edit &relative (Apply to both session and total)" ), page, "_relativeRB" );
+  lay1->addWidget( _relativeRB );
+  connect( _relativeRB, SIGNAL( clicked() ), this, SLOT(slotRelativePressed()) );
+  
+  // The relative times
+  QHBoxLayout *lay4 = new QHBoxLayout();
+  lay1->addLayout( lay4 );
+  lay4->addSpacing(20);
+  
+  _operator = new QComboBox(page);
+  _operator->insertItem( QString::fromLatin1( "+" ) );
+  _operator->insertItem( QString::fromLatin1( "-" ) );
+  lay4->addWidget( _operator );
 
-	lay4->addSpacing(5);
-	
-	_diffTW = new KTimeWidget( page, "_sessionAddTW" );
-	lay4->addWidget( _diffTW );
+  lay4->addSpacing(5);
+  
+  _diffTW = new KTimeWidget( page, "_sessionAddTW" );
+  lay4->addWidget( _diffTW );
 
   KIconLoader loader;
   
@@ -129,21 +129,21 @@ AddTaskDialog::AddTaskDialog(QString caption, bool editDlg)
   whatsThisBU->setPixmap( whatsThisIM );
   lay4->addWidget(whatsThisBU);
   
-	lay1->addStretch(1);
+  lay1->addStretch(1);
 
 
-	if ( editDlg ) {
-		// This is an edit dialog.
-		_operator->setFocus();
-	}
-	else {
-		// This is an initial dialog
-		_name->setFocus();
-	}
-	origTotal = 0;
-	origSession = 0;
+  if ( editDlg ) {
+    // This is an edit dialog.
+    _operator->setFocus();
+  }
+  else {
+    // This is an initial dialog
+    _name->setFocus();
+  }
+  origTotal = 0;
+  origSession = 0;
 
-	slotRelativePressed();
+  slotRelativePressed();
 
   // Whats this help.
   QWhatsThis::add(_name, i18n("Enter the name of the task here. This name is for your eyes only."));
@@ -173,46 +173,46 @@ void AddTaskDialog::enterWhatsThis()
   
 void AddTaskDialog::slotAbsolutePressed()
 {
-	_relativeRB->setChecked( false );
-	_absoluteRB->setChecked( true );
+  _relativeRB->setChecked( false );
+  _absoluteRB->setChecked( true );
 
-	_operator->setEnabled( false );
-	_diffTW->setEnabled( false );
+  _operator->setEnabled( false );
+  _diffTW->setEnabled( false );
 
-	_totalLA->setEnabled( true );
-	_sessionLA->setEnabled( true );
-	_totalTW->setEnabled( true );
-	_sessionTW->setEnabled( true );
+  _totalLA->setEnabled( true );
+  _sessionLA->setEnabled( true );
+  _totalTW->setEnabled( true );
+  _sessionTW->setEnabled( true );
 }
 
 void AddTaskDialog::slotRelativePressed()
 {
-	_relativeRB->setChecked( true );
-	_absoluteRB->setChecked( false );
+  _relativeRB->setChecked( true );
+  _absoluteRB->setChecked( false );
 
-	_operator->setEnabled( true );
-	_diffTW->setEnabled( true );
+  _operator->setEnabled( true );
+  _diffTW->setEnabled( true );
 
-	_totalLA->setEnabled( false );
-	_sessionLA->setEnabled( false );
-	_totalTW->setEnabled( false );
-	_sessionTW->setEnabled( false );
+  _totalLA->setEnabled( false );
+  _sessionLA->setEnabled( false );
+  _totalTW->setEnabled( false );
+  _sessionTW->setEnabled( false );
 }
 
-	
+  
 
 void AddTaskDialog::setTask( const QString &name, long total, long session )
 {
   _name->setText( name );
-	
-	_totalTW->setTime( total / 60, total % 60 );
-	_sessionTW->setTime( session / 60, session % 60 );
-	origTotal = total;
-	origSession = session;
+  
+  _totalTW->setTime( total / 60, total % 60 );
+  _sessionTW->setTime( session / 60, session % 60 );
+  origTotal = total;
+  origSession = session;
 }
 
 
-QString AddTaskDialog::taskName( void ) const
+QString AddTaskDialog::taskName() const
 { 
   return( _name->text() ); 
 }
@@ -220,21 +220,21 @@ QString AddTaskDialog::taskName( void ) const
 
 void AddTaskDialog::status( long *total, long *totalDiff, long *session, long *sessionDiff ) const
 { 
-	if ( _absoluteRB->isChecked() ) {
-		*total = _totalTW->time();
-		*session = _sessionTW->time();
-	}
-	else {
-		int diff = _diffTW->time();
-		if ( _operator->currentItem() == 1) {
-			diff = -diff;
-		}
-		*total = origTotal + diff;
-		*session = origSession + diff;
-	}
+  if ( _absoluteRB->isChecked() ) {
+    *total = _totalTW->time();
+    *session = _sessionTW->time();
+  }
+  else {
+    int diff = _diffTW->time();
+    if ( _operator->currentItem() == 1) {
+      diff = -diff;
+    }
+    *total = origTotal + diff;
+    *session = origSession + diff;
+  }
 
-	*totalDiff = *total - origTotal;
-	*sessionDiff = *session - origSession;
+  *totalDiff = *total - origTotal;
+  *sessionDiff = *session - origSession;
 }
 
 #include "adddlg.moc"
