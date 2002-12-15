7f79028313532d944456d420e68e1bd1d07e2884
Waldo Bastian <bastian@kde.org>
2002-12-15 22:57:24 +0000

    Off by 1
    svn path=/branches/KDE_2_2_BRANCH/kdepim/; revision=194180

--- libkcal/versit/vobject.c
+++ libkcal/versit/vobject.c
@@ -1228,8 +1228,8 @@ static void writeGroup(OFile *fp, VObject *o)
     while ((o=isAPropertyOf(o,VCGroupingProp)) != 0) {
 	strncpy(buf2,STRINGZ_VALUE_OF(o),sizeof(buf2));
 	buf2[sizeof(buf2)] = '\0';
-	strncat(buf2,".",sizeof(buf2)-strlen(buf2));
-	strncat(buf2,buf1,sizeof(buf2)-strlen(buf2));
+	strncat(buf2,".",sizeof(buf2)-strlen(buf2)-1);
+	strncat(buf2,buf1,sizeof(buf2)-strlen(buf2)-1);
 	strcpy(buf1,buf2);
 	}
     appendsOFile(fp,buf1);
