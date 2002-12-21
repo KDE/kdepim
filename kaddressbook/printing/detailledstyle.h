97d716bdfa290dd0f7e9fd706e3471dcd60e5b5f
Tobias Koenig <tokoe@kde.org>
2002-12-21 16:40:17 +0000

    - add licence text - remove not used printprogress_base.ui
    svn path=/trunk/kdepim/; revision=195154

--- kaddressbook/printing/detailledstyle.h
+++ kaddressbook/printing/detailledstyle.h
@@ -1,18 +1,25 @@
-/* -*- C++ -*-
-   This file declares the detailed print style.
-
-   the KDE addressbook
-
-   $ Author: Mirko Boehm $
-   $ Copyright: (C) 1996-2002, Mirko Boehm $
-   $ Contact: mirko@kde.org
-         http://www.kde.org $
-   $ License: LGPL with the following explicit clarification:
-         This code may be linked against any version of the Qt toolkit
-         from Troll Tech, Norway. $
-
-   $Revision$
-*/
+/*                                                                      
+    This file is part of KAddressBook.
+    Copyright (c) 1996-2002 Mirko Boehm <mirko@kde.org>
+                                                                        
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or   
+    (at your option) any later version.                                 
+                                                                        
+    This program is distributed in the hope that it will be useful,     
+    but WITHOUT ANY WARRANTY; without even the implied warranty of      
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        
+    GNU General Public License for more details.                        
+                                                                        
+    You should have received a copy of the GNU General Public License   
+    along with this program; if not, write to the Free Software         
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.           
+                                                                        
+    As a special exception, permission is given to link this program    
+    with any edition of Qt, and distribute the resulting executable,    
+    without including the source code for Qt in the source distribution.
+*/                                                                      
 
 #ifndef DETAILLEDSTYLE_H
 #define DETAILLEDSTYLE_H
--- kaddressbook/printing/ringbinderstyle.cpp
+++ kaddressbook/printing/ringbinderstyle.cpp
@@ -1,11 +1,25 @@
-/*
- RingBinderPrintStyle - a print style for ring binders
-
- Author: Jost Schenck
- Copyright: (C) 2002 Jost Schenck <jost@schenck.de>
- License: LGPL 
-
-*/
+/*                                                                      
+    This file is part of KAddressBook.
+    Copyright (c) 2002 Jost Schenck <jost@schenck.de>
+                                                                        
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or   
+    (at your option) any later version.                                 
+                                                                        
+    This program is distributed in the hope that it will be useful,     
+    but WITHOUT ANY WARRANTY; without even the implied warranty of      
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        
+    GNU General Public License for more details.                        
+                                                                        
+    You should have received a copy of the GNU General Public License   
+    along with this program; if not, write to the Free Software         
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.           
+                                                                        
+    As a special exception, permission is given to link this program    
+    with any edition of Qt, and distribute the resulting executable,    
+    without including the source code for Qt in the source distribution.
+*/                                                                      
 
 #include "ringbinderstyle.h"
 
