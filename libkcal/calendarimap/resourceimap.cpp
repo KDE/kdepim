18b8a90cc1a2d743fa4417a80e25708b4bc5b236
Guenter Schwann <ggens@schwann.at>
2003-01-07 23:19:41 +0000

    begin of imap (kmail) calendarresource (DCOP-) communtication with kmail (and some other stuff) is needed currently all data is _only_ in RAM this is mainly in CVS for Steffen expect updates soon
    svn path=/trunk/kdepim/; revision=198122

---
+++ libkcal/calendarimap/resourceimap.cpp
@@ -0,0 +1,402 @@
+/*
+    This file is part of libkcal.
+    Copyright (c) 1998 Preston Brown
+    Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include <kdebug.h>
+
+#include <libkcal/vcaldrag.h>
+#include <libkcal/vcalformat.h>
+#include <libkcal/icalformat.h>
+#include <libkcal/exceptions.h>
+#include <libkcal/incidence.h>
+#include <libkcal/event.h>
+#include <libkcal/todo.h>
+#include <libkcal/journal.h>
+
+#include <kresources/resourceconfigwidget.h>
+#include <kresources/resource.h>
+
+#include "resourceimapconfig.h"
+#include "resourceimap.h"
+
+using namespace KCal;
+
+extern "C"
+{
+  KRES::ResourceConfigWidget *config_widget( QWidget *parent ) {
+    return new ResourceIMAPConfig( parent, "Configure IMAP-Based Calendar" );
+  }
+
+  KRES::Resource *resource( const KConfig *config ) {
+    return new ResourceIMAP( config );
+  }
+}
+
+ResourceIMAP::ResourceIMAP( const KConfig* config )
+  : ResourceCalendar( config )
+{
+  if ( config ) {
+    mServer = config->readEntry( "Servername" );
+  }
+  init();
+}
+
+void ResourceIMAP::writeConfig( KConfig* config )
+{
+  ResourceCalendar::writeConfig( config );
+  config->writeEntry( "Servername", mServer );
+}
+
+void ResourceIMAP::init()
+{
+  mOldestDate = 0L;
+  mNewestDate = 0L;
+
+  mRecursList.setAutoDelete(TRUE);
+  mTodoList.setAutoDelete(TRUE);
+  mEventList.setAutoDelete(TRUE);
+}
+
+
+ResourceIMAP::~ResourceIMAP()
+{
+  close();
+  delete mNewestDate;
+  delete mOldestDate;
+}
+
+bool ResourceIMAP::doOpen()
+{
+  kdDebug(5800) << "Opening resource " << resourceName() << " on " << mServer << endl;
+  return true;
+}
+
+bool ResourceIMAP::sync()
+{
+  return false;
+}
+
+/***********************************************
+ * Adding and removing Events
+ */
+
+void ResourceIMAP::addEvent(Event *anEvent)
+{
+  kdDebug(5800) << "ResourceIMAP::addEvent" << endl;
+  mEventList.append(anEvent);
+  anEvent->registerObserver( this );
+  
+  // call kmail ...
+
+  //setModified( true );
+}
+
+// probably not really efficient, but...it works for now.
+void ResourceIMAP::deleteEvent(Event *event)
+{
+  kdDebug(5800) << "ResourceIMAP::deleteEvent" << endl;
+  mEventList.findRef(event);
+  
+  // call kmail ...
+  
+  mEventList.remove();
+}
+
+
+/***********************************************
+ * Getting Events
+ */
+
+Event *ResourceIMAP::event( const QString &uid )
+{
+  kdDebug(5800) << "ResourceIMAP::event(): " << uid << endl;
+  Event *e;
+  for (e = mEventList.first();e;e = mEventList.next())
+    if (e->uid() == uid) return e;
+  // not found
+  return 0;
+}
+
+int ResourceIMAP::numEvents(const QDate &qd)
+{
+  Event *anEvent;
+  int count = 0;
+  int extraDays, i;
+
+  // first get the simple case from the dictionary.
+  for (anEvent=mEventList.first();anEvent;anEvent=mEventList.next()) {
+    if (qd == anEvent->dtStart().date()) count++;
+  }
+  // next, check for repeating events.  Even those that span multiple days...
+  for (anEvent = mRecursList.first(); anEvent; anEvent = mRecursList.next()) {
+    if (anEvent->isMultiDay()) {
+      extraDays = anEvent->dtStart().date().daysTo(anEvent->dtEnd().date());
+      //kdDebug(5800) << "multi day event w/" << extraDays << " days" << endl;
+      for (i = 0; i <= extraDays; i++) {
+        if (anEvent->recursOn(qd.addDays(i))) {
+          ++count;
+          break;
+        }
+      }
+    } else {
+      if (anEvent->recursOn(qd))
+        ++count;
+    }
+  }
+
+  return count;
+}
+
+// taking a QDate, this function will look for an eventlist in the dict
+// with that date attached -
+QPtrList<Event> ResourceIMAP::rawEventsForDate(const QDate &qd, bool sorted)
+{
+  // Search non-recurring events
+  QPtrList<Event> eventList;
+  // Search recurring events
+  QPtrList<Event> eventListSorted;
+  return eventListSorted;
+}
+
+
+QPtrList<Event> ResourceIMAP::rawEvents( const QDate &start, const QDate &end,
+                                          bool inclusive )
+{
+   QPtrList<Event> matchList, *tmpList, tmpList2;
+  return matchList;
+}
+
+QPtrList<Event> ResourceIMAP::rawEventsForDate(const QDateTime &qdt)
+{
+  return rawEventsForDate( qdt.date() );
+}
+
+QPtrList<Event> ResourceIMAP::rawEvents()
+{
+  QPtrList<Event> eventList;
+  return eventList;
+}
+
+/***********************************************
+ * Adding and removing Todos
+ */
+
+void ResourceIMAP::addTodo(Todo *todo)
+{
+  mTodoList.append(todo);
+  todo->registerObserver( this );
+
+  // call kmail ..
+  
+//  setModified( true );
+}
+
+void ResourceIMAP::deleteTodo(Todo *todo)
+{
+  mTodoList.findRef(todo);
+
+  // call kmail ...
+  
+  mTodoList.remove();
+
+//  setModified( true );
+}
+
+/***********************************************
+ * Getting Todos
+ */
+
+QPtrList<Todo> ResourceIMAP::rawTodos() const
+{
+  return mTodoList;
+}
+
+Todo *ResourceIMAP::todo( const QString &uid )
+{
+  Todo *aTodo;
+  for (aTodo = mTodoList.first(); aTodo;
+       aTodo = mTodoList.next())
+    if (aTodo->uid() == uid)
+      return aTodo;
+  // not found
+  return 0;
+}
+
+QPtrList<Todo> ResourceIMAP::todos( const QDate &date )
+{
+  QPtrList<Todo> todos;
+
+  Todo *aTodo;
+  for (aTodo = mTodoList.first();aTodo;aTodo = mTodoList.next()) {
+    if (aTodo->hasDueDate() && aTodo->dtDue().date() == date) {
+      todos.append(aTodo);
+    }
+  }
+
+  return todos;
+}
+
+/***********************************************
+ * Journal handling
+ */
+
+void ResourceIMAP::addJournal(Journal *journal)
+{
+  kdDebug(5800) << "Adding Journal on " << journal->dtStart().toString() << endl;
+
+  mJournalMap.insert(journal->dtStart().date(),journal);
+  journal->registerObserver( this );
+
+  // call kmail ...
+  
+//  setModified( true );
+}
+
+Journal *ResourceIMAP::journal(const QDate &date)
+{
+  kdDebug(5800) << "ResourceIMAP::journal() " << date.toString() << endl;
+
+  QMap<QDate,Journal *>::ConstIterator it = mJournalMap.find(date);
+  if (it == mJournalMap.end()) return 0;
+  else {
+//    kdDebug(5800) << "  Found" << endl;
+    return *it;
+  }
+}
+
+Journal *ResourceIMAP::journal(const QString &uid)
+{
+  QMap<QDate,Journal *>::ConstIterator it = mJournalMap.begin();
+  QMap<QDate,Journal *>::ConstIterator end = mJournalMap.end();
+  for(;it != end; ++it) {
+    if ((*it)->uid() == uid) return *it;
+  }
+  return 0;
+}
+
+QPtrList<Journal> ResourceIMAP::journals()
+{
+  QPtrList<Journal> list;
+
+  QMap<QDate,Journal *>::Iterator it;
+  for( it = mJournalMap.begin(); it != mJournalMap.end(); ++it ) {
+    list.append(*it);
+  }
+
+  return list;
+}
+
+/***********************************************
+ * Alarm handling
+ */
+
+Alarm::List ResourceIMAP::alarmsTo( const QDateTime &to )
+{
+  if( mOldestDate )
+    return alarms( *mOldestDate, to );
+  else
+    return alarms( QDateTime( QDate( 1900, 1, 1 ) ), to );
+}
+
+Alarm::List ResourceIMAP::alarms( const QDateTime &from, const QDateTime &to )
+{
+  kdDebug(5800) << "ResourceIMAP::alarms(" << from.toString() << " - " << to.toString() << ")\n";
+  Alarm::List alarms;
+
+//   // Check all non-recurring events.
+
+//   // Check all recurring events.
+
+//   // Check all todos.
+
+  return alarms;
+}
+
+void ResourceIMAP::appendAlarms( Alarm::List &alarms, Incidence *incidence,
+                                  const QDateTime &from, const QDateTime &to )
+{
+  QPtrList<Alarm> alarmList = incidence->alarms();
+  Alarm *alarm;
+  for( alarm = alarmList.first(); alarm; alarm = alarmList.next() ) {
+//    kdDebug(5800) << "ResourceIMAP::appendAlarms() '" << incidence->summary()
+//                  << "': " << alarm->time().toString() << " - " << alarm->enabled() << endl;
+    if ( alarm->enabled() ) {
+      if ( alarm->time() >= from && alarm->time() <= to ) {
+        kdDebug(5800) << "ResourceIMAP::appendAlarms() '" << incidence->summary()
+                      << "': " << alarm->time().toString() << endl;
+        alarms.append( alarm );
+      }
+    }
+  }
+}
+
+void ResourceIMAP::appendRecurringAlarms( Alarm::List &alarms, Incidence *incidence,
+                                  const QDateTime &from, const QDateTime &to )
+{
+  QPtrList<Alarm> alarmList = incidence->alarms();
+  Alarm *alarm;
+  QDateTime qdt;
+  for( alarm = alarmList.first(); alarm; alarm = alarmList.next() ) {
+    if (incidence->recursOn(from.date())) {
+      qdt.setTime(alarm->time().time());
+      qdt.setDate(from.date());
+    }
+    else qdt = alarm->time();
+    kdDebug(5800) << "ResourceIMAP::appendAlarms() '" << incidence->summary()
+                  << "': " << qdt.toString() << " - " << alarm->enabled() << endl;
+    if ( alarm->enabled() ) {
+//      kdDebug(5800) << "ResourceIMAP::appendAlarms() '" << incidence->summary()
+//                    << "': " << alarm->time().toString() << endl;
+      if ( qdt >= from && qdt <= to ) {
+        alarms.append( alarm );
+      }
+    }
+  }
+}
+
+/***********************************************
+ * update() (kind of slot)
+ */
+
+// after changes are made to an event, this should be called.
+void ResourceIMAP::update(IncidenceBase *incidence)
+{
+  incidence->setSyncStatus(Event::SYNCMOD);
+  incidence->setLastModified(QDateTime::currentDateTime());
+  // we should probably update the revision number here,
+  // or internally in the Event itself when certain things change.
+  // need to verify with ical documentation.
+
+  if ( incidence->type() == "Event" ) {
+    Event *anEvent = static_cast<Event *>(incidence);
+
+   // the first thing we do is REMOVE all occurances of the event from
+   // both the dictionary and the recurrence list.  Then we reinsert it.
+   // We don't bother about optimizations right now.
+
+   // take any instances of it out of the recurrence list
+
+    // ok the event is now GONE.  we want to re-insert it.
+    //insertEvent(anEvent);
+  }
+
+}
+
+
---
+++ libkcal/calendarimap/resourceimap.h
@@ -0,0 +1,170 @@
+ /*
+    This file is part of libkcal.
+    Copyright (c) 1998 Preston Brown
+    Copyright (c) 2001 Cornelius Schumacher <schumacher@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+#ifndef KCAL_RESOURCEIMAP_H
+#define KCAL_RESOURCEIMAP_H
+
+#include <qptrlist.h>
+#include <qstring.h>
+#include <qdatetime.h>
+
+#include <kconfig.h>
+
+#include <libkcal/incidence.h>
+
+#include "resourcecalendar.h"
+
+namespace KCal {
+
+/**
+  This class provides a calendar stored on an IMAP-server via kmail
+*/
+class ResourceIMAP : public ResourceCalendar, public IncidenceBase::Observer
+{
+  public:
+    ResourceIMAP( const KConfig * );
+    virtual ~ResourceIMAP();
+
+    virtual void writeConfig( KConfig* config );
+
+    bool sync();
+
+    /** Add Event to calendar. */
+    void addEvent(Event *anEvent);
+    /** deletes an event from this calendar. */
+    void deleteEvent(Event *);
+
+    /**
+      Retrieves an event on the basis of the unique string ID.
+    */
+    Event *event(const QString &UniqueStr);
+    /**
+      Return filtered list of all events in calendar.
+    */
+//    QPtrList<Event> events();
+    /**
+      Return unfiltered list of all events in calendar.
+    */
+    QPtrList<Event> rawEvents();
+    /**
+      Builds and then returns a list of all events that match for the
+      date specified. useful for dayView, etc. etc.
+    */
+    QPtrList<Event> rawEventsForDate( const QDate &date, bool sorted = false );
+    /**
+      Get unfiltered events for date \a qdt.
+    */
+    QPtrList<Event> rawEventsForDate( const QDateTime &qdt );
+    /**
+      Get unfiltered events in a range of dates. If inclusive is set to true,
+      only events are returned, which are completely included in the range.
+    */
+    QPtrList<Event> rawEvents( const QDate &start, const QDate &end,
+                               bool inclusive = false );
+
+    /** returns the number of events that are present on the specified date. */
+    int numEvents(const QDate &qd);
+  
+
+    /*
+      Returns a QString with the text of the holiday (if any) that falls
+      on the specified date.
+    */
+    // QString getHolidayForDate(const QDate &qd);
+    
+    /**
+      Add a todo to the todolist.
+    */
+    void addTodo( Todo *todo );
+    /**
+      Remove a todo from the todolist.
+    */
+    void deleteTodo( Todo * );
+    /**
+      Searches todolist for an event with this unique string identifier,
+      returns a pointer or null.
+    */
+    Todo *todo( const QString &uid );
+    /**
+      Return list of all todos.
+    */
+    QPtrList<Todo> rawTodos() const;
+    /**
+      Returns list of todos due on the specified date.
+    */
+    QPtrList<Todo> todos( const QDate &date );
+    /** Add a Journal entry to calendar */
+    virtual void addJournal(Journal *);
+    /** Return Journal for given date */
+    virtual Journal *journal(const QDate &);
+    /** Return Journal with given UID */
+    virtual Journal *journal(const QString &UID);
+    /** Return list of all Journals stored in calendar */
+    QPtrList<Journal> journals();
+
+    /** Return all alarms, which ocur in the given time interval. */
+    Alarm::List alarms( const QDateTime &from, const QDateTime &to );
+
+    /** Return all alarms, which ocur before given date. */
+    Alarm::List alarmsTo( const QDateTime &to );
+
+    
+    /** this method should be called whenever a Event is modified directly
+     * via it's pointer.  It makes sure that the calendar is internally
+     * consistent. */
+    void update(IncidenceBase *incidence);
+ 
+    friend class ResourceIMAPConfig;
+
+    // Public because needed in MultiCalendar::load()
+    bool doOpen();
+
+  protected:
+
+    /** Notification function of IncidenceBase::Observer. */
+    void incidenceUpdated( IncidenceBase *i ) { update( i ); }
+    /** Append alarms of incidence in interval to list of alarms. */
+    void appendAlarms( Alarm::List &alarms, Incidence *incidence,
+                       const QDateTime &from, const QDateTime &to );
+
+    /** Append alarms of recurring events in interval to list of alarms. */
+    void appendRecurringAlarms( Alarm::List &alarms, Incidence *incidence,
+                       const QDateTime &from, const QDateTime &to );
+   
+  private:
+    void init();
+
+    QPtrList<Event> mEventList;              // lists of events.
+    QPtrList<Event> mRecursList;             // list of repeating events.
+
+    QPtrList<Todo> mTodoList;               // list of todo items.
+
+    QMap<QDate,Journal *> mJournalMap;
+  
+    QDate *mOldestDate;
+    QDate *mNewestDate;
+
+    QString mServer;
+    
+};  
+
+}
+
+#endif
