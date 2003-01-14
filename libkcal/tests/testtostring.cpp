a00077e11e2e4a373447fbf140473a455c7e099f
Guenter Schwann <ggens@schwann.at>
2003-01-14 18:18:31 +0000

    test for individual incidence<->ical-text
    svn path=/trunk/kdepim/; revision=199905

---
+++ libkcal/tests/testtostring.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <kdebug.h>
+
+#include "event.h"
+#include "icalformat.h"
+
+using namespace KCal;
+
+int main(int argc,char **argv)
+{
+
+ // std::cout << "Hello World!" << std::endl;
+  Event *ev = new Event;
+  ev->setSummary("Griazi");
+  ICalFormat iformat;
+  QString icalstr = iformat.toICalString(ev);
+  kdDebug() << icalstr << endl;
+  Event *ev2 = iformat.fromString(icalstr);
+  kdDebug() << "Event reread!" << endl ;
+  
+  if (ev2)
+    kdDebug() << iformat.toICalString(ev2) << endl;
+  else
+    kdDebug() << "Could not read incidence" << endl;
+}
