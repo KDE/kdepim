3003e9464260c556b9fb7e343a40fedefafb747f
Tobias Koenig <tokoe@kde.org>
2003-01-18 01:13:42 +0000

    fixed kabc2mutt to work with the current version of libkabc/kresources
    svn path=/trunk/kdepim/; revision=200692

--- kabc/kabc2mutt/main.cpp
+++ kabc/kabc2mutt/main.cpp
@@ -1,7 +1,7 @@
 /*
     KAbc2Mutt
 
-	Copyright (c) 2002 Tobias Koenig <tokoe@kde.org>
+    Copyright (c) 2003 Tobias Koenig <tokoe@kde.org>
 
     This program is free software; you can redistribute it and/or
     modify it under the terms of version 2 of the GNU General Public
@@ -29,60 +29,43 @@
 
 #include <stdio.h>
 
-static KCmdLineOptions options[] = 
+int main( int argc, char **argv )
 {
-    { "read", I18N_NOOP( "Print a mutt alias configuration to stdout" ), 0 },
-    { "write", I18N_NOOP( "Add an email address to kabc" ), 0 },
-    { 0, 0, 0 }
-};
-
-int main(int argc, char **argv)
-{
-    KInstance instance( "kabc2mutt" );
-
     KCmdLineArgs::init( argc, argv, "kabc2mutt",
-	    i18n("kabc - mutt converter"), "0.1" );
-    KCmdLineArgs::addCmdLineOptions( options );
-    KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+                        i18n( "kabc - mutt converter" ), "0.1" );
+
+    KApplication app( false, false );
 
     KABC::AddressBook *ab = KABC::StdAddressBook::self();
 
-    if ( args->isSet( "read" ) ) {
-        // print all addressees
-	KABC::AddressBook::Iterator it;
-        for (it = ab->begin(); it != ab->end(); ++it) {
-	    if ( (*it).preferredEmail().isEmpty() )
-		continue;
+    // print all addressees
+    KABC::AddressBook::Iterator it;
+    for ( it = ab->begin(); it != ab->end(); ++it ) {
+      if ( (*it).preferredEmail().isEmpty() )
+        continue;
 
-	    QString key = (*it).givenName().left(3) + (*it).familyName().left(3);
-	
-	    printf("alias %s\t%s %s <%s>\n", key.local8Bit().data(),
-		    (*it).givenName().local8Bit().data(),
-		    (*it).familyName().local8Bit().data(),
-		    (*it).preferredEmail().local8Bit().data());
-	}
+      QString key = (*it).givenName().left( 3 ) + (*it).familyName().left( 3 );
 
-	// print all ditribution lists
-	KABC::DistributionListManager manager( ab );
-	manager.load();
-
-	QStringList dists = manager.listNames();
-	for (QStringList::Iterator it = dists.begin(); it != dists.end(); ++it) {
-	    KABC::DistributionList *list = manager.list( (*it) );
-	    if ( list ) {
-		QStringList emails = list->emails();
-		printf("alias %s\t %s\n",
-			(*it).replace(QRegExp(" "), "_").local8Bit().data(),
-			emails.join(",").local8Bit().data());
-	    }
-	}
-    } else if ( args->isSet( "write" ) ) {
-	/**
-	 * atm mutt doesn't support writing back email addresses to
-	 * a programm, so patching mutt would be the only way for
-	 * adding addresses from it.
-	 */
+      printf( "alias %s\t%s %s <%s>\n", key.local8Bit().data(),
+              (*it).givenName().local8Bit().data(),
+              (*it).familyName().local8Bit().data(),
+              (*it).preferredEmail().local8Bit().data() );
     }
 
-    return 0;
+    // print all ditribution lists
+    KABC::DistributionListManager manager( ab );
+    manager.load();
+
+    QStringList dists = manager.listNames();
+    for ( QStringList::Iterator it = dists.begin(); it != dists.end(); ++it ) {
+      KABC::DistributionList *list = manager.list( *it );
+      if ( list ) {
+        QStringList emails = list->emails();
+        printf( "alias %s\t %s\n",
+                (*it).replace( QRegExp( " " ), "_" ).local8Bit().data(),
+                emails.join( "," ).local8Bit().data() );
+    }
+  }
+
+  return 0;
 }
