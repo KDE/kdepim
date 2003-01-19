d5a51df41161de3526c315b63a241b61de12d427
Cornelius Schumacher <schumacher@kde.org>
2003-01-19 01:04:51 +0000

    Added resource storing its data one incidence a file in a local directory.
    svn path=/trunk/kdepim/; revision=200927

---
+++ libkcal/resourcelocaldir.cpp
@@ -0,0 +1,320 @@
+/*
+    This file is part of libkcal.
+
+    Copyright (c) 2003 Cornelius Schumacher <schumacher@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include <typeinfo>
+#include <stdlib.h>
+
+#include <qdatetime.h>
+#include <qstring.h>
+#include <qptrlist.h>
+
+#include <kdebug.h>
+#include <kurl.h>
+
+#include "vcaldrag.h"
+#include "vcalformat.h"
+#include "icalformat.h"
+#include "exceptions.h"
+#include "incidence.h"
+#include "event.h"
+#include "todo.h"
+#include "journal.h"
+#include "filestorage.h"
+
+#include <kresources/resourceconfigwidget.h>
+
+#include "resourcelocaldirconfig.h"
+
+#include "resourcelocaldir.h"
+
+using namespace KCal;
+
+extern "C"
+{
+  KRES::ResourceConfigWidget *config_widget( QWidget *parent ) {
+    return new ResourceLocalDirConfig( parent, "Configure Dir-Based Calendar" );
+  }
+
+  KRES::Resource *resource( const KConfig *config ) {
+    return new ResourceLocalDir( config );
+  }
+}
+
+ResourceLocalDir::ResourceLocalDir( const KConfig* config )
+  : ResourceCalendar( config )
+{
+  if ( config ) {
+    readConfig( config );
+  }
+
+  init();
+}
+
+ResourceLocalDir::ResourceLocalDir( const QString& dirName ) 
+  : ResourceCalendar( 0 )
+{
+  mURL = KURL( dirName );
+
+  init();
+}
+
+
+void ResourceLocalDir::readConfig( const KConfig *config )
+{
+  QString url = config->readEntry( "CalendarURL" );
+  mURL = KURL( url );
+}
+
+void ResourceLocalDir::writeConfig( KConfig *config )
+{
+  kdDebug() << "ResourceLocalDir::writeConfig()" << endl;
+
+  ResourceCalendar::writeConfig( config );
+
+  config->writeEntry( "CalendarURL", mURL.prettyURL() );
+}
+
+void ResourceLocalDir::init()
+{
+  setType( "dir" );
+
+  mOpen = false;
+
+  connect( &mDirWatch, SIGNAL( dirty( const QString & ) ),
+           SLOT( reload( const QString & ) ) );
+  connect( &mDirWatch, SIGNAL( created( const QString & ) ),
+           SLOT( reload( const QString & ) ) );
+  connect( &mDirWatch, SIGNAL( deleted( const QString & ) ),
+           SLOT( reload( const QString & ) ) );
+
+  mDirWatch.addDir( mURL.path(), true );
+  mDirWatch.startScan();
+}
+
+
+ResourceLocalDir::~ResourceLocalDir()
+{
+  close();
+}
+
+bool ResourceLocalDir::doOpen()
+{
+  kdDebug(5800) << "Opening resource " << resourceName() << " with URL " << mURL.prettyURL() << endl;
+
+  mOpen = true;
+
+  return true;
+}
+
+bool ResourceLocalDir::load()
+{
+  kdDebug() << "ResourceLocalDir::load()" << endl;
+
+  if ( !mOpen ) return true;
+
+  mCalendar.close();
+
+  QString dirName = mURL.path();
+
+  kdDebug() << "ResourceLocalDir::load(): '" << dirName << "'" << endl;
+
+  QDir dir( dirName );
+    
+  QStringList entries = dir.entryList( QDir::Files | QDir::Readable );
+  
+  QStringList::ConstIterator it;
+  for( it = entries.begin(); it != entries.end(); ++it ) {
+    QString fileName = dirName + "/" + *it;
+    kdDebug() << " read '" << fileName << "'" << endl;
+    CalendarLocal cal;
+    cal.load( fileName );
+    QPtrList<Incidence> incidences = cal.rawIncidences();
+    Incidence *i = incidences.first();
+    if ( i ) mCalendar.addIncidence( i->clone() );
+  }
+
+  return true;
+}
+
+bool ResourceLocalDir::sync()
+{
+  kdDebug() << "ResourceLocalDir::sync()" << endl;
+
+  if ( !mOpen ) return true;
+
+  QPtrList<Incidence> incidences = mCalendar.rawIncidences();
+
+  Incidence *i;
+  for( i = incidences.first(); i; i = incidences.next() ) {
+    QString fileName = mURL.path() + "/" + i->uid();
+    kdDebug() << "writing '" << fileName << "'" << endl;
+
+    CalendarLocal cal;
+    cal.addIncidence( i->clone() );
+    cal.save( fileName );
+  }
+
+  return true;
+}
+
+void ResourceLocalDir::reload( const QString &file )
+{
+  kdDebug() << "ResourceLocalDir::reload()" << endl;
+
+  if ( !mOpen ) return;
+
+  kdDebug() << "  File: '" << file << "'" << endl;
+
+  mCalendar.close();
+  load();
+
+  emit resourceChanged( this );
+}
+
+void ResourceLocalDir::doClose()
+{
+  if ( !mOpen ) return;
+
+  mCalendar.close();
+  mOpen = false;
+}
+
+
+void ResourceLocalDir::addEvent(Event *event)
+{
+  mCalendar.addEvent( event );
+}
+
+void ResourceLocalDir::deleteEvent(Event *event)
+{
+  kdDebug(5800) << "ResourceLocalDir::deleteEvent" << endl;
+
+  mCalendar.deleteEvent( event );
+}
+
+
+Event *ResourceLocalDir::event( const QString &uid )
+{
+  return mCalendar.event( uid );
+}
+
+int ResourceLocalDir::numEvents(const QDate &qd)
+{
+  return mCalendar.numEvents( qd );
+}
+
+QPtrList<Event> ResourceLocalDir::rawEventsForDate(const QDate &qd, bool sorted)
+{
+  return mCalendar.rawEventsForDate( qd, sorted );
+}
+
+
+QPtrList<Event> ResourceLocalDir::rawEvents( const QDate &start, const QDate &end,
+                                          bool inclusive )
+{
+  return mCalendar.rawEvents( start, end, inclusive );
+}
+
+QPtrList<Event> ResourceLocalDir::rawEventsForDate(const QDateTime &qdt)
+{
+  return mCalendar.rawEventsForDate( qdt.date() );
+}
+
+QPtrList<Event> ResourceLocalDir::rawEvents()
+{
+  return mCalendar.rawEvents();
+}
+
+void ResourceLocalDir::addTodo(Todo *todo)
+{
+  mCalendar.addTodo( todo );
+}
+
+void ResourceLocalDir::deleteTodo(Todo *todo)
+{
+  mCalendar.deleteTodo( todo );
+}
+
+
+QPtrList<Todo> ResourceLocalDir::rawTodos() const
+{
+  return mCalendar.rawTodos();
+}
+
+Todo *ResourceLocalDir::todo( const QString &uid )
+{
+  return mCalendar.todo( uid );
+}
+
+QPtrList<Todo> ResourceLocalDir::todos( const QDate &date )
+{
+  return mCalendar.todos( date );
+}
+
+
+void ResourceLocalDir::addJournal(Journal *journal)
+{
+  kdDebug(5800) << "Adding Journal on " << journal->dtStart().toString() << endl;
+
+  mCalendar.addJournal( journal );
+}
+
+Journal *ResourceLocalDir::journal(const QDate &date)
+{
+//  kdDebug(5800) << "ResourceLocalDir::journal() " << date.toString() << endl;
+
+  return mCalendar.journal( date );
+}
+
+Journal *ResourceLocalDir::journal(const QString &uid)
+{
+  return mCalendar.journal( uid );
+}
+
+QPtrList<Journal> ResourceLocalDir::journals()
+{
+  return mCalendar.journals();
+}
+
+
+Alarm::List ResourceLocalDir::alarmsTo( const QDateTime &to )
+{
+  return mCalendar.alarmsTo( to );
+}
+
+Alarm::List ResourceLocalDir::alarms( const QDateTime &from, const QDateTime &to )
+{
+//  kdDebug(5800) << "ResourceLocalDir::alarms(" << from.toString() << " - " << to.toString() << ")\n";
+
+  return mCalendar.alarms( from, to );
+}
+
+void ResourceLocalDir::update(IncidenceBase *)
+{
+}
+
+void ResourceLocalDir::dump() const
+{
+  ResourceCalendar::dump();
+  kdDebug(5800) << "  Url: " << mURL.url() << endl;
+}
+
+#include "resourcelocaldir.moc"
---
+++ libkcal/resourcelocaldir.h
@@ -0,0 +1,172 @@
+ /*
+    This file is part of libkcal.
+
+    Copyright (c) 2003 Cornelius Schumacher <schumacher@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+#ifndef KCAL_ResourceLocalDirDIR_H
+#define KCAL_ResourceLocalDirDIR_H
+
+#include <qptrlist.h>
+#include <qstring.h>
+#include <qdatetime.h>
+
+#include <kurl.h>
+#include <kconfig.h>
+#include <kdirwatch.h>
+
+#include "incidence.h"
+#include "calendarlocal.h"
+
+#include "resourcecalendar.h"
+
+namespace KCal {
+
+/**
+  This class provides a calendar stored as a file per incidence in a directory.
+*/
+class ResourceLocalDir : public ResourceCalendar
+{
+    Q_OBJECT
+
+    friend class ResourceLocalDirConfig;
+
+  public:
+    ResourceLocalDir( const KConfig * );
+    ResourceLocalDir( const QString& fileName );
+    virtual ~ResourceLocalDir();
+
+    void readConfig( const KConfig *config );
+    void writeConfig( KConfig* config );
+
+    bool load();
+
+    bool sync();
+
+    /** Add Event to calendar. */
+    void addEvent(Event *anEvent);
+    /** deletes an event from this calendar. */
+    void deleteEvent(Event *);
+
+    /**
+      Retrieves an event on the basis of the unique string ID.
+    */
+    Event *event(const QString &UniqueStr);
+    /**
+      Return filtered list of all events in calendar.
+    */
+//    QPtrList<Event> events();
+    /**
+      Return unfiltered list of all events in calendar.
+    */
+    QPtrList<Event> rawEvents();
+    /**
+      Builds and then returns a list of all events that match for the
+      date specified. useful for dayView, etc. etc.
+    */
+    QPtrList<Event> rawEventsForDate( const QDate &date, bool sorted = false );
+    /**
+      Get unfiltered events for date \a qdt.
+    */
+    QPtrList<Event> rawEventsForDate( const QDateTime &qdt );
+    /**
+      Get unfiltered events in a range of dates. If inclusive is set to true,
+      only events are returned, which are completely included in the range.
+    */
+    QPtrList<Event> rawEvents( const QDate &start, const QDate &end,
+                               bool inclusive = false );
+
+    /** returns the number of events that are present on the specified date. */
+    int numEvents(const QDate &qd);
+  
+
+    /*
+      Returns a QString with the text of the holiday (if any) that falls
+      on the specified date.
+    */
+    // QString getHolidayForDate(const QDate &qd);
+    
+    /**
+      Add a todo to the todolist.
+    */
+    void addTodo( Todo *todo );
+    /**
+      Remove a todo from the todolist.
+    */
+    void deleteTodo( Todo * );
+    /**
+      Searches todolist for an event with this unique string identifier,
+      returns a pointer or null.
+    */
+    Todo *todo( const QString &uid );
+    /**
+      Return list of all todos.
+    */
+    QPtrList<Todo> rawTodos() const;
+    /**
+      Returns list of todos due on the specified date.
+    */
+    QPtrList<Todo> todos( const QDate &date );
+    /** Add a Journal entry to calendar */
+    virtual void addJournal(Journal *);
+    /** Return Journal for given date */
+    virtual Journal *journal(const QDate &);
+    /** Return Journal with given UID */
+    virtual Journal *journal(const QString &UID);
+    /** Return list of all Journals stored in calendar */
+    QPtrList<Journal> journals();
+
+    /** Return all alarms, which ocur in the given time interval. */
+    Alarm::List alarms( const QDateTime &from, const QDateTime &to );
+
+    /** Return all alarms, which ocur before given date. */
+    Alarm::List alarmsTo( const QDateTime &to );
+
+    // Public because needed in MultiCalendar::load()
+    bool doOpen();
+
+    void dump() const;
+
+  protected slots:
+    void reload( const QString & );
+
+  protected:
+
+    /** clears out the current calendar, freeing all used memory etc. etc. */
+    void doClose();
+
+    /** this method should be called whenever a Event is modified directly
+     * via it's pointer.  It makes sure that the calendar is internally
+     * consistent. */
+    virtual void update(IncidenceBase *incidence);
+ 
+  private:
+    void init();
+
+    CalendarLocal mCalendar;
+
+    KURL mURL;
+    ICalFormat mFormat;
+
+    bool mOpen;
+
+    KDirWatch mDirWatch;
+};
+
+}
+
+#endif
