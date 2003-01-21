d9b3a6edaccd041c40fe4cedb8c5bbb6a062dc09
Zack Rusin <zack@kde.org>
2003-01-21 18:35:08 +0000

    CVS_SILENT not sure what that word means but Email sounds better when referring to email.
    svn path=/trunk/kdepim/; revision=201769

--- kaddressbook/details/look_html.cpp
+++ kaddressbook/details/look_html.cpp
@@ -1,23 +1,23 @@
-/*                                                                      
+/*
     This file is part of KAddressBook.
     Copyright (c) 2003 Tobias Koenig <tokoe@kde.org>
-                                                                        
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or   
-    (at your option) any later version.                                 
-                                                                        
-    This program is distributed in the hope that it will be useful,     
-    but WITHOUT ANY WARRANTY; without even the implied warranty of      
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        
-    GNU General Public License for more details.                        
-                                                                        
-    You should have received a copy of the GNU General Public License   
-    along with this program; if not, write to the Free Software         
-    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.           
-                                                                        
-    As a special exception, permission is given to link this program    
-    with any edition of Qt, and distribute the resulting executable,    
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+    As a special exception, permission is given to link this program
+    with any edition of Qt, and distribute the resulting executable,
     without including the source code for Qt in the source distribution.
 */
 
@@ -65,7 +65,7 @@ void KABHtmlView::setAddressee( const KABC::Addressee &addr )
   KABC::PhoneNumber::List phones = addr.phoneNumbers();
   KABC::PhoneNumber::List::ConstIterator phoneIt;
   for ( phoneIt = phones.begin(); phoneIt != phones.end(); ++phoneIt ) {
-    dynamicPart += QString( 
+    dynamicPart += QString(
       "<tr><td align=\"right\"><b>%1</b></td>"
       "<td align=\"left\">%2</td></tr>" )
       .arg( KABC::PhoneNumber::typeLabel( (*phoneIt).type() ) )
@@ -74,9 +74,9 @@ void KABHtmlView::setAddressee( const KABC::Addressee &addr )
 
   QStringList emails = addr.emails();
   QStringList::ConstIterator emailIt;
-  QString type = i18n( "Privat" );
+  QString type = i18n( "Email" );
   for ( emailIt = emails.begin(); emailIt != emails.end(); ++emailIt ) {
-    dynamicPart += QString( 
+    dynamicPart += QString(
       "<tr><td align=\"right\"><b>%1</b></td>"
       "<td align=\"left\"><a href=\"mailto:%2\">%3</a></td></tr>" )
       .arg( type )
@@ -99,7 +99,7 @@ void KABHtmlView::setAddressee( const KABC::Addressee &addr )
     QString formattedAddress = (*addrIt).formattedAddress().stripWhiteSpace();
     formattedAddress = formattedAddress.replace( '\n', "<br>" );
 
-    dynamicPart += QString( 
+    dynamicPart += QString(
       "<tr><td align=\"right\"><b>%1</b></td>"
       "<td align=\"left\">%2</td></tr>" )
       .arg( KABC::Address::typeLabel( (*addrIt).type() ) )
