6ca202d933d2d1fbf1c314c119c2aceb357e1972
David Jarvie <djarvie@kde.org>
2003-02-05 00:27:03 +0000

    Fix event DTEND being written as DATE only dependent only on DTSTART float status
    svn path=/trunk/kdepim/; revision=204944

--- libkcal/icalformatimpl.cpp
+++ libkcal/icalformatimpl.cpp
@@ -157,7 +157,7 @@ icalcomponent *ICalFormatImpl::writeEvent(Event *event)
 
   // end time
   icaltimetype end;
-  if (event->doesFloat()) {
+  if (event->doesEndFloat()) {
 //    kdDebug(5800) << "§§ Event " << event->summary() << " floats." << endl;
     // +1 day because end date is non-inclusive.
     end = writeICalDate( event->dtEnd().date().addDays( 1 ) );
