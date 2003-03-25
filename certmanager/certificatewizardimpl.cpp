78d4585f5fc36bbf02dd38d98bf82eb59e7c9015
Ralf Nolden <nolden@kde.org>
2003-03-25 07:21:05 +0000

    Add support for broken CAs on certificate requests here too
    svn path=/trunk/kdepim/; revision=216106

--- certmanager/certificatewizardimpl.cpp
+++ certmanager/certificatewizardimpl.cpp
@@ -12,6 +12,9 @@
 #include <qlineedit.h>
 #include <qtextedit.h>
 #include <qpushbutton.h>
+#include <qcheckbox.h>
+
+#include <iostream.h>
 
 extern CryptPlugWrapper* pWrapper;
 
@@ -103,10 +106,56 @@ void CertificateWizardImpl::slotGenerateCertificate()
         certParms += countryED->text();
         bFirst = false;
     }
+    if ( brokenCA->isChecked() ){
+	// a broken CA is described as a CA that requires an Email tag
+	// for X509v1 certificate requests. As the cryptplug stuff
+	// implements X509v3, we need to pass the number encoded already
+	// and also convert the QLineEdit 16 bit output to 8 bit first. 
+	// Then convert it to hex and put it tinto the certParms string:
+	QString emailstr=QString(emailED->text().ascii());
+	const unsigned short *chars = emailstr.ucs2();
+	QString hexString;
+	for(unsigned int i = 0; i < emailstr.length(); i++){
+		hexString.append("\\");
+		hexString.append(QString::number(chars[i], 16));
+	}
+	if( !bFirst )
+		certParms += ',';
+
+        /* Actually this encoding is not correct because the gpgsm API
+         says that an RFC-2253 encoding is to be used.  Due to wrong
+         encoding created by libksba the actual encoding is not RFC
+         conform but a hack which assumes that the argument is a kind
+         of string and libksba decides for itself what kind of ASN.1
+         type to use (printable-string or utf8-string).  Fortunately
+         we can easily discriminate between that bad encoding and
+         regular encoding based on the fact that the regular encoding
+         does not start with a byte that can represent a printable
+         character - well, at least for email addresses this is true.
+
+         The other compatible and regular choice we have, is to encode
+         it in a regular way; i.e as defined by rfc-2253 with regular
+         quoting (note, the SHOULD in the RFC is only relevant for the
+         DER -> RFC-2253 encoding direction).  I'd vote for that
+         because UCS-2 converting to UTF-8 is pretty simple and the
+         quoting rules can basically be ignored and simple
+         backslashquoting used for all characters.  An Email address of
+         <foo@bar> would then be encoded as:
+
+           1.2.840.113549.1.9.1=\66\6F\6F\40\62\61\72
+
+         I am not firm with Qt classes, so I won't do that but use the
+         irregular way. -- wk@gnupg.org */
+
+        certParms += QString("1.2.840.113549.1.9.1=%1").arg(hexString);
+        bFirst = false;
+    }
     certParms += '\n';
     certParms += QString( "name-email: %1\n" ).arg( emailED->text() );
     certParms += "</GnupgKeyParms>\n";
-
+    
+	cout << certParms.utf8() << endl;
+    
     QApplication::setOverrideCursor( Qt::waitCursor );
     if( !pWrapper->requestDecentralCertificate( certParms.utf8(),
                                                   &generatedKey,
