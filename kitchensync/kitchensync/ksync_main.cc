e6e42176834be0932e5856ccc285f4a538b52235
Daniel Molkentin <molkentin@kde.org>
2003-03-31 12:06:42 +0000

    sorry, no (semi) political statements in about boxes
    svn path=/trunk/kdepim/; revision=217301

--- kitchensync/kitchensync/ksync_main.cc
+++ kitchensync/kitchensync/ksync_main.cc
@@ -56,7 +56,7 @@ int main(int argc,  char* argv[] )
 		       "0.0 pre Alpha",
 		       I18N_NOOP("Synchronize Data with KDE"),
 		       KAboutData::License_GPL,
-		       "(c) 2001-2002 Holger Freyther\n(c) 2002 Maximilian Reiss\nThis software was created in old europe.",
+		       "(c) 2001-2002 Holger Freyther\n(c) 2002 Maximilian Reiss",
 		       0,
 		       "http://opie.handhelds.org" );
   aboutData.addAuthor("Maximilian Reiss",I18N_NOOP("Current Maintainer"),
