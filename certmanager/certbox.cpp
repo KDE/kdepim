fbab48816b096e29847451c1572d7af81c55a468
Ralf Nolden <nolden@kde.org>
2003-05-07 16:52:22 +0000

    fix issues here, too
    svn path=/trunk/kdepim/; revision=224483

--- certmanager/certbox.cpp
+++ certmanager/certbox.cpp
@@ -4,14 +4,19 @@
 
 CertBox::CertBox( QWidget* parent, const char* name ) :QListView( parent, name )
 {
-  QFontMetrics fm = fontMetrics();
-  addColumn( i18n("Subject"), fm.width( i18n("Subject") ) * 6  );
-  addColumn( i18n("Issuer"), fm.width( i18n("Issuer") ) * 4 );
-  addColumn( i18n("Serial")/*, fm.width( i18n("Serial") ) * 3*/ );
-  setColumnWidthMode( 0, QListView::Manual );
-  setColumnWidthMode( 1, QListView::Manual );
+//  QFontMetrics fm = fontMetrics();
+//  addColumn( i18n("Subject"), fm.width( i18n("Subject") ) * 6  );
+//  addColumn( i18n("Issuer"), fm.width( i18n("Issuer") ) * 4 );
+//  addColumn( i18n("Serial")/*, fm.width( i18n("Serial") ) * 3*/ );
+	
+  addColumn( i18n("Subject"));
+  addColumn( i18n("Issuer"));
+  addColumn( i18n("Serial"));
+  //setColumnWidthMode( 0, QListView::Manual );
+  //setColumnWidthMode( 1, QListView::Manual );
   setAllColumnsShowFocus( true );
-
+  setResizeMode( QListView::AllColumns );
+  
   connect( this, SIGNAL( doubleClicked (QListViewItem*) ), this, SLOT( handleDoubleClick( QListViewItem*) ) );
   connect( this, SIGNAL( returnPressed (QListViewItem*) ), this, SLOT( handleDoubleClick( QListViewItem*) ) );
 }
--- certmanager/certmanager.cpp
+++ certmanager/certmanager.cpp
@@ -210,6 +210,8 @@ CertManager::CertManager( bool remote, const QString& query,
   _leAction->setText(query);
   if ( !_remote || !query.isEmpty() )
     loadCertificates();
+
+  setMinimumWidth(_toolbar->sizeHint().width());
 }
 
 bool CertManager::checkExec( const QStringList& args )
