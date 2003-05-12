1c21404afad6e1b9caa9d0eefb2474b17d47f95a
Helge Deller <deller@kde.org>
2003-05-12 21:23:08 +0000

    initial version of a vCard and LDIF thumbnail creator for Konqui.
    svn path=/trunk/kdepim/; revision=225487

---
+++ kaddressbook/thumbnailcreator/ldifvcardcreator.cpp
@@ -0,0 +1,219 @@
+/*  This file is part of the KDE libraries
+    Copyright (C) 2003 Helge Deller <deller@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+/*
+ *  - ldifvcardthumbnail -
+ *
+ *  This kioslave generates tumbnails for vCard and LDIF files. 
+ *  The thumbnails are used e.g. by Konqueror or in the file selection
+ *  dialog.
+ *
+ *  $Id$
+ */
+
+#include <qfile.h>
+#include <qpixmap.h>
+#include <qimage.h>
+#include <qtextstream.h>
+#include <qdatetime.h>
+
+#include <kstandarddirs.h>
+#include <kpixmapsplitter.h>
+#include <kabc/vcardconverter.h>
+#include <kabc/ldifconverter.h>
+
+#include "ldifvcardcreator.h"
+
+extern "C"
+{
+    ThumbCreator *new_creator()
+    {
+        return new VCard_LDIFCreator;
+    }
+};
+
+VCard_LDIFCreator::VCard_LDIFCreator()
+    : m_splitter(0)
+{
+}
+
+VCard_LDIFCreator::~VCard_LDIFCreator()
+{
+    delete m_splitter;
+}
+
+bool VCard_LDIFCreator::create(const QString &path, int width, int height, QImage &img)
+{
+    if ( !m_splitter )
+    {
+        m_splitter = new KPixmapSplitter;
+        QString pixmap = locate( "data", "konqueror/pics/thumbnailfont_7x4.png" );
+        if ( !pixmap.isEmpty() )
+        {
+            // FIXME: make font/glyphsize configurable...
+            m_splitter->setPixmap( QPixmap( pixmap ));
+            m_splitter->setItemSize( QSize( 4, 7 ));
+        }
+    }
+
+    // determine some sizes...
+    // example: width: 60, height: 64
+    QSize pixmapSize( width, height );
+    if (height * 3 > width * 4)
+        pixmapSize.setHeight( width * 4 / 3 );
+    else
+        pixmapSize.setWidth( height * 3 / 4 );
+
+    if ( pixmapSize != m_pixmap.size() )
+        m_pixmap.resize( pixmapSize );
+    
+    // one pixel for the rectangle, the rest. whitespace
+    int xborder = 1 + pixmapSize.width()/16;  // minimum x-border
+    int yborder = 1 + pixmapSize.height()/16; // minimum y-border
+
+    QSize chSize = m_splitter->itemSize(); // the size of one char
+    int xOffset = chSize.width();
+    int yOffset = chSize.height();
+
+    // calculate a better border so that the text is centered
+    int canvasWidth  = pixmapSize.width()  - 2*xborder;
+    int canvasHeight = pixmapSize.height() -  2*yborder;
+    int numCharsPerLine = (int) (canvasWidth / chSize.width());
+    int numLines = (int) (canvasHeight / chSize.height());
+
+    // create text-preview
+    QFile file( path );
+    if ( !file.open( IO_ReadOnly ))
+	return false;
+
+    // read the file
+    QTextStream t( &file );
+    t.setEncoding( QTextStream::Latin1 );
+    QString contents = t.read();
+    file.close();
+
+    // convert the file contents to a KABC::Addressee address
+    bool ok;
+    KABC::Addressee addr;
+    KABC::VCardConverter converter;
+    ok = converter.vCardToAddressee( contents, addr, KABC::VCardConverter::v3_0 );
+    if (!ok) {
+      KABC::AddresseeList addrList;
+      ok = KABC::LDIFConverter::LDIFToAddressee( contents, addrList );
+      if (ok) addr = addrList[0];
+    }
+
+    if (!ok)
+	return false;
+
+    // prepare the text to show
+    QString text, info;
+
+    info = addr.formattedName().simplifyWhiteSpace();
+    if (!info.isEmpty())
+	text += info + "\n";
+    else
+	text += QString(addr.givenName() + " " + addr.familyName()).stripWhiteSpace() + "\n";
+
+    info = addr.organization().simplifyWhiteSpace();
+    if (!info.isEmpty())
+	text += info + "\n";
+
+    QString pn = addr.phoneNumber(KABC::PhoneNumber::Pref).number();
+    if (!pn.isEmpty())
+	text += pn + "\n";
+    pn = addr.phoneNumber(KABC::PhoneNumber::Work).number();
+    if (!pn.isEmpty())
+	text += pn + "\n";
+    pn = addr.phoneNumber(KABC::PhoneNumber::Home).number();
+    if (!pn.isEmpty())
+	text += pn + "\n";
+
+    // render the information
+    m_pixmap.fill( QColor( 245, 245, 245 ) ); // light-grey background
+    QRect rect;
+    int rest = m_pixmap.width() - (numCharsPerLine * chSize.width());
+    xborder = QMAX( xborder, rest/2); // center horizontally
+    rest = m_pixmap.height() - (numLines * chSize.height());
+    yborder = QMAX( yborder, rest/2); // center vertically
+    // end centering
+
+    int x = xborder, y = yborder; // where to paint the characters
+    int posNewLine  = m_pixmap.width() - (chSize.width() + xborder);
+    int posLastLine = m_pixmap.height() - (chSize.height() + yborder);
+    bool newLine = false;
+    Q_ASSERT( posNewLine > 0 );
+    const QPixmap *fontPixmap = &(m_splitter->pixmap());
+
+    for ( uint i = 0; i < text.length(); i++ )
+    {
+	if ( x > posNewLine || newLine ) // start a new line?
+	{
+	    x = xborder;
+	    y += yOffset;
+
+	    if ( y > posLastLine ) // more text than space
+		break;
+
+	    // after starting a new line, we also jump to the next
+	    // physical newline in the file if we don't come from one
+	    if ( !newLine )
+	    {
+		int pos = text.find( '\n', i );
+		if ( pos > (int) i )
+		i = pos +1;
+	    }
+
+	    newLine = false;
+	}
+
+	// check for newlines in the text (unix,dos)
+	QChar ch = text.at( i );
+	if ( ch == '\n' )
+	{
+	    newLine = true;
+	    continue;
+	}
+	else if ( ch == '\r' && text.at(i+1) == '\n' )
+	{
+	    newLine = true;
+	    i++; // skip the next character (\n) as well
+	    continue;
+	}
+
+	rect = m_splitter->coordinates( ch );
+	if ( !rect.isEmpty() )
+	{
+	    bitBlt( &m_pixmap, QPoint(x,y), fontPixmap, rect, Qt::CopyROP );
+	}
+
+	x += xOffset; // next character
+    }
+
+    if (ok)
+	img = m_pixmap.convertToImage();
+
+    return ok;
+}
+
+ThumbCreator::Flags VCard_LDIFCreator::flags() const
+{
+    return (Flags)(DrawFrame | BlendIcon);
+}
+
---
+++ kaddressbook/thumbnailcreator/ldifvcardcreator.h
@@ -0,0 +1,41 @@
+/*  This file is part of the KDE libraries
+    Copyright (C) 2003 Helge Deller <deller@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#ifndef VCARD_LDIFCREATOR_H
+#define VCARD_LDIFCREATOR_H "$Id$"
+
+#include <qpixmap.h>
+#include <kio/thumbcreator.h>
+
+class KPixmapSplitter;
+
+class VCard_LDIFCreator : public ThumbCreator
+{
+public:
+    VCard_LDIFCreator();
+    virtual ~VCard_LDIFCreator();
+    virtual bool create(const QString &path, int width, int height, QImage &img);
+    virtual Flags flags() const;
+
+private:
+    KPixmapSplitter *m_splitter;
+    QPixmap m_pixmap;
+};
+
+#endif
