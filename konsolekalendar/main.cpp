ec99ba8ae4e3f718a50282efa38de06dde9826be
Tuukka Pasanen <illuusio@nic.fi>
2003-06-08 18:49:04 +0000

    Started to use QDateTime instead of QDate. This is need in Adding and changing (Deleting also). added start-time and end-time (changed enddate and startdate to end-date and start-date)
    svn path=/trunk/kdepim/; revision=230438

--- konsolekalendar/main.cpp
+++ konsolekalendar/main.cpp
@@ -39,16 +39,14 @@ static KCmdLineOptions options[] =
 {
   { "help", I18N_NOOP("Prints this help"), 0 },
   { "verbose", I18N_NOOP("Output helpful (?) debug info"), 0 },
-  { "file <calendarfile>", I18N_NOOP("Specify which calendar you want to use."), 0 },
+  { "file <calendar file>", I18N_NOOP("Specify which calendar you want to use."), 0 },
   { "next", I18N_NOOP("Next activity in calendar"), 0 },
-  { "date <date>", I18N_NOOP("Show day info"), 0 },
-  { "startdate <startdate>", I18N_NOOP("From this day"), 0 },
-  { "enddate <enddate>", I18N_NOOP("To this day"), 0 },
+  { "date <date>", I18N_NOOP("Show selected day's calendar"), 0 },
+  { "start-date <start-date>", I18N_NOOP("Start from this day"), 0 },
+  { "start-time <start-time>", I18N_NOOP("Start from this time [mm:hh]"), 0 },
+  { "end-date <end-date>", I18N_NOOP("End to this day"), 0 },
+  { "end-time <end-time>", I18N_NOOP("End to this time [mm:hh]"), 0 },
   { "all", I18N_NOOP("Show all entries"), 0 },
-  { "date", I18N_NOOP("Date for which the calendar is shown."), 0},
-  { "startdate", I18N_NOOP("Starting date."), 0},
-  { "enddate", I18N_NOOP("Ending date."), 0},
-  { "file", I18N_NOOP("Location of your calendar file."), 0},
   KCmdLineLastOption
 };
 
@@ -56,7 +54,7 @@ int main(int argc, char *argv[])
 {
   KAboutData aboutData( "konsolekalendar", I18N_NOOP( "KonsoleKalendar" ),
                         "0.5", description, KAboutData::License_GPL,
-                        "(c) 2002, Tuukka Pasanen", 0, 0,
+                        "(c) 2002-2003, Tuukka Pasanen", 0, 0,
                         "illuusio@mailcity.com");
   aboutData.addAuthor("Tuukka Pasanen",0, "illuusio@mailcity.com");
 
@@ -67,6 +65,7 @@ int main(int argc, char *argv[])
 
   QString KalendarFile;
   QDate date;
+  QTime time;
   QString option;
 
   KApplication app( false, false );
@@ -109,10 +108,10 @@ int main(int argc, char *argv[])
    *  Set starting date for calendar
    *
    */
-  if ( args->isSet("startdate") ) {
-    option = args->getOption("startdate");
+  if ( args->isSet("start-date") ) {
+    option = args->getOption("start-date");
     if(variables.isVerbose()){
-      kdDebug() << "main.cpp::int main(int argc, char *argv[]) | Shows all entries from this date 30 days or to enddate: (" << option << ")" << endl;              
+      kdDebug() << "main.cpp::int main(int argc, char *argv[]) | Start date: (" << option << ")" << endl;              
     }
 
     date = variables.parseDate(option);
@@ -120,20 +119,58 @@ int main(int argc, char *argv[])
     variables.setStartDate(date);
   }
 
+
+  /*
+   *  Set starting date for calendar
+   *
+   */
+  if ( args->isSet("start-time") ) {
+    option = args->getOption("start-time");
+    if(variables.isVerbose()){
+      kdDebug() << "main.cpp::int main(int argc, char *argv[]) | Start time: (" << option << ")" << endl;              
+    }
+
+    time = variables.parseTime(option);
+	
+    //variables.setStartDate(date);
+  }
+
+	
+	
+	
+	
   /*
    *  Set starting end date for calendar
    *
    */
-  if ( args->isSet("enddate") ) {
-    QString option = args->getOption("enddate");
+  if ( args->isSet("end-date") ) {
+    QString option = args->getOption("end-date");
     if(variables.isVerbose()) {
-      kdDebug() << "main.cpp::int main(int argc, char *argv[]) | Shows all entries to this date: (" << option << ")" << endl;              
+      kdDebug() << "main.cpp::int main(int argc, char *argv[]) | End date: (" << option << ")" << endl;              
     }
 
     date = variables.parseDate(option);
 
     variables.setEndDate(date);
   }
+
+	  /*
+   *  Set starting date for calendar
+   *
+   */
+  if ( args->isSet("end-time") ) {
+    option = args->getOption("start-time");
+    if(variables.isVerbose()){
+      kdDebug() << "main.cpp::int main(int argc, char *argv[]) | End time: (" << option << ")" << endl;              
+    }
+
+    time = variables.parseTime(option);
+	
+    // variables.setStartDate(date);
+  }
+
+	
+	
 
   if( args->isSet("all") ) {
     variables.setAll( true );
