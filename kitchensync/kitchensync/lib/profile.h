4bfb24c67f5eee47102577e79a1a5033c0fa67ef
Cornelius Schumacher <schumacher@kde.org>
2003-06-17 16:47:05 +0000

    Add proper licence headers.
    svn path=/trunk/kdepim/; revision=231984

--- kitchensync/kitchensync/lib/profile.h
+++ kitchensync/kitchensync/lib/profile.h
@@ -1,5 +1,25 @@
-#ifndef KitchenSync_Profile_H
-#define KitchenSync_Profile_H
+/*
+    This file is part of KitchenSync.
+
+    Copyright (c) 2002 Holger Freyther <zecke@handhelds.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+#ifndef KITCHENSYNC_PROFILE_H
+#define KITCHENSYNC_PROFILE_H
 
 #include <qmap.h>
 #include <qstring.h>
@@ -146,7 +166,6 @@ namespace KSync {
         bool m_confirmDelete : 1;
     };
 
-};
-
+}
 
 #endif
