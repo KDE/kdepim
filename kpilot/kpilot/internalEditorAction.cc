eaa647116eab3db4ce56e1e476367417b54f5b61
Reinhold Kainhofer <reinhold@kainhofer.com>
2003-07-11 19:25:09 +0000

    Finished the sync action that copies the changes done in the internal editors of KPilot to the handheld. So, finally, the internal viewers/editors of KPilot are working again, unlike the last few years, where they were in a terribly broken state.
    CCMAIL: 54765-done@bugs.kde.org
    
    svn path=/trunk/kdepim/; revision=236579

--- kpilot/kpilot/internalEditorAction.cc
+++ kpilot/kpilot/internalEditorAction.cc
@@ -31,6 +31,10 @@
 #include <qtimer.h>
 #include <kmessagebox.h>
 
+#include <pilotRecord.h>
+#include <pilotLocalDatabase.h>
+#include <pilotDatabase.h>
+#include <pilotSerialDatabase.h>
 #include "kpilotConfig.h"
 #include "internalEditorAction.h"
 
@@ -60,8 +64,14 @@ void InternalEditorAction::syncDirtyDB()
 		emit logMessage(i18n("Databases with changed records: %1").arg(dirtyDBs.join(", ")));
 		dbIter=dirtyDBs.begin();
 	}
+	else
+	{
+		dbIter++;
+	}
 	if (dbIter==dirtyDBs.end())
 	{
+		KPilotConfig::getConfig().setDirtyDatabases(QStringList());
+		KPilotConfig::getConfig().sync();
 		QTimer::singleShot(0, this, SLOT(syncFlagsChangedDB()));
 		return;
 	}
@@ -71,10 +81,71 @@ void InternalEditorAction::syncDirtyDB()
 	// open the local and the serial database and copy every
 	// changed record from the PC to the handheld
 
-	// TODO:
+	PilotRecord*rec=0L;
+	PilotLocalDatabase*localDB=new PilotLocalDatabase(*dbIter);
+	PilotSerialDatabase*serialDB=new PilotSerialDatabase(pilotSocket(), *dbIter);
+	if (!localDB->isDBOpen() || !serialDB->isDBOpen())
+	{
+		emit logError(i18n("Unable to open the serial or local database for %1. "
+			"Skipping it.").arg(*dbIter));
+		goto nextDB;
+	}
+	while ( (rec=localDB->readNextModifiedRec()) )
+	{
+		int id=rec->getID();
+#ifdef DEBUG
+		DEBUGKPILOT<<"ID of modified record is "<<id<<endl;
+		DEBUGKPILOT<<endl<<endl;
+#endif
+		if (id>0)
+		{
+			PilotRecord*serrec=serialDB->readRecordById(id);
+			if (serrec && (serrec->getAttrib() & dlpRecAttrDirty))
+			{
+				// TODO: Treat the special databases different (i.e. for
+				// Addresses, Todos, Events and notes display the contents
+				// of the record.
+				int res=KMessageBox::questionYesNo(0L, i18n("The record with ID %1 of the database \"%2\" was changed on the handheld and in the internal editor. Which version is the current one?").arg(id).arg(*dbIter), i18n("Conflict in %1").arg(*dbIter), i18n("&Internal Editor"), i18n("&Handheld"));
+				if (res==KMessageBox::Ok)
+				{
+					serialDB->writeRecord(rec);
+				}
+				else
+				{
+					localDB->writeRecord(serrec);
+				}
+			}
+			else
+			{
+				serialDB->writeRecord(rec);
+			}
+		}
+		else
+		{
+#ifdef DEBUG
+			DEBUGKPILOT<<"Generating ID for Record "<<rec->getID()<<" with data "<<endl;
+			DEBUGKPILOT<<rec->getData()<<endl;
+			DEBUGKPILOT<<"-----------------------------------------"<<endl;
+#endif
+			int id=serialDB->writeRecord(rec);
+			rec->setID(id);
+#ifdef DEBUG
+			DEBUGKPILOT<<"New ID is "<<id<<endl;
+			DEBUGKPILOT<<endl<<endl<<endl;
+#endif
+			//localDB->writeRecord(rec);
+			localDB->writeID(rec);
+		}
+		KPILOT_DELETE(rec);
+	}
 
+nextDB:
+	localDB->resetSyncFlags();
+	KPILOT_DELETE(localDB);
+	KPILOT_DELETE(serialDB);
 	QTimer::singleShot(0, this, SLOT(syncDirtyDB()));
 }
+
 void InternalEditorAction::syncFlagsChangedDB()
 {
 	FUNCTIONSETUP;
@@ -85,18 +156,34 @@ void InternalEditorAction::syncFlagsChangedDB()
 		emit logMessage(i18n("Databases with changed flags: %1").arg(dirtyDBs.join(", ")));
 		dbIter=dirtyDBs.begin();
 	}
+	else
+	{
+		dbIter++;
+	}
 	if (dbIter==dirtyDBs.end())
 	{
+		KPilotConfig::getConfig().setFlagsChangedDatabases(QStringList());
+		KPilotConfig::getConfig().sync();
 		QTimer::singleShot(0, this, SLOT(syncAppBlockChangedDB()));
 		return;
 	}
+
 #ifdef DEBUG
 	DEBUGKPILOT<<"syncFlagsChangedDB for DB "<<(*dbIter)<<endl;
 #endif
+emit logError(i18n("Setting the database flags on the handheld is not yet supported."));
+QTimer::singleShot(0, this, SLOT(syncAppBlockChangedDB()));
+return;
+
+	PilotLocalDatabase*localDB=new PilotLocalDatabase(*dbIter);
+	PilotSerialDatabase*serialDB=new PilotSerialDatabase(pilotSocket(), *dbIter);
+
 	// open the local and the serial database and copy the flags over
-	// TODO
+	// TODO: Implement the copying
 	// TODO: Is there a way to detect if the flags were changed on the handheld?
 
+	KPILOT_DELETE(localDB);
+	KPILOT_DELETE(serialDB);
 	QTimer::singleShot(0, this, SLOT(syncAppBlockChangedDB()));
 }
 
@@ -110,18 +197,31 @@ void InternalEditorAction::syncAppBlockChangedDB()
 		emit logMessage(i18n("Databases with changed AppBlock: %1").arg(dirtyDBs.join(", ")));
 		dbIter=dirtyDBs.begin();
 	}
+	else
+	{
+		dbIter++;
+	}
 	if (dbIter==dirtyDBs.end())
 	{
+		KPilotConfig::getConfig().setAppBlockChangedDatabases(QStringList());
+		KPilotConfig::getConfig().sync();
 		QTimer::singleShot(0, this, SLOT(cleanup()));
 		return;
 	}
 #ifdef DEBUG
 	DEBUGKPILOT<<"syncAppBlockChangedDB for DB "<<(*dbIter)<<endl;
 #endif
-	// open the local and the serial database and copy the app block over
-	// TODO
-	// TODO: Is there a way to detect if the app block was changed on the handheld?
 
+	PilotLocalDatabase*localDB=new PilotLocalDatabase(*dbIter);
+	PilotSerialDatabase*serialDB=new PilotSerialDatabase(pilotSocket(), *dbIter);
+
+	unsigned char*appBlock=new unsigned char[0xFFFF];
+	int len=localDB->readAppBlock(appBlock, 0xFFFF);
+	// TODO: Check if the app block was changed on the handheld, and if so, do conflict resolution
+	serialDB->writeAppBlock(appBlock, len);
+
+	KPILOT_DELETE(localDB);
+	KPILOT_DELETE(serialDB);
 	QTimer::singleShot(0, this, SLOT(syncAppBlockChangedDB()));
 }
 
