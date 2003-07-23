efd5297e7992243e7d4ab920f8dd29bfff0465d3
Cornelius Schumacher <schumacher@kde.org>
2003-07-23 07:17:10 +0000

    Add missing file.
    svn path=/trunk/kdepim/; revision=238719

---
+++ libkcal/listbase.h
@@ -0,0 +1,64 @@
+/*
+    This file is part of libkcal.
+
+    Copyright (c) 2003 Cornelius Schumacher <schumacher@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+#ifndef KCAL_LISTBASE_H
+#define KCAL_LISTBASE_H
+
+#include <qvaluelist.h>
+
+namespace KCal {
+
+template<class T> 
+class ListBase : public QValueList<T *>
+{
+  public:
+    ListBase() : QValueList<T *>(), mAutoDelete( false ) {}
+    ~ListBase()
+    {
+      if ( mAutoDelete ) {
+        QValueListIterator<T *> it;
+        for( it = begin(); it != end(); ++it ) delete *it;
+      }
+    }
+
+    void setAutoDelete( bool autoDelete )
+    {
+      mAutoDelete = autoDelete;
+    }
+
+    bool removeRef( T *t )
+    {
+      QValueListIterator<T *> it = find( t );
+      if ( it == end() ) {
+        return false;
+      } else {
+        delete t;
+        remove( it );
+        return true;
+      }
+    }
+
+  private:
+    bool mAutoDelete;
+};
+
+}
+
+#endif
