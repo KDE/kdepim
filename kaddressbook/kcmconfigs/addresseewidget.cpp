6f0eee7cb1114874c86aa987c9843af2bf3be407
Carsten Pfeiffer <pfeiffer@kde.org>
2003-07-27 19:49:22 +0000

    notify KABC about config changes
    svn path=/trunk/kdepim/; revision=239520

--- kaddressbook/kcmconfigs/addresseewidget.cpp
+++ kaddressbook/kcmconfigs/addresseewidget.cpp
@@ -1,31 +1,34 @@
 /*
-    This file is part of KAddressBook.                                  
+    This file is part of KAddressBook.
     Copyright (c) 2003 Tobias Koenig <tokoe@kde.org>
-                                                                        
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or   
-    (at your option) any later version.                                 
-                                                                        
-    This program is distributed in the hope that it will be useful,     
-    but WITHOUT ANY WARRANTY; without even the implied warranty of      
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        
-    GNU General Public License for more details.                        
-                                                                        
-    You should have received a copy of the GNU General Public License   
-    along with this program; if not, write to the Free Software         
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           
-                                                                        
-    As a special exception, permission is given to link this program    
-    with any edition of Qt, and distribute the resulting executable,    
-    without including the source code for Qt in the source distribution.
-*/                                                                      
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+    As a special exception, permission is given to link this program
+    with any edition of Qt, and distribute the resulting executable,
+    without including the source code for Qt in the source distribution.
+*/
+
+#include <qcstring.h>
 #include <qgroupbox.h>
 #include <qlabel.h>
 #include <qlayout.h>
 #include <qlistbox.h>
 #include <qpushbutton.h>
+
+#include <dcopclient.h>
 
 #include <kbuttonbox.h>
 #include <kcombobox.h>
@@ -124,7 +127,7 @@ AddresseeWidget::AddresseeWidget( QWidget *parent, const char *name )
 
   mPrefix = new NamePartWidget( i18n( "Prefixes" ), this );
   layout->addWidget( mPrefix, 0, 0 );
-  
+
   mInclusion = new NamePartWidget( i18n( "Inclusions" ), this );
   layout->addWidget( mInclusion, 0, 1 );
 
@@ -177,6 +180,10 @@ void AddresseeWidget::saveSettings()
   KConfig cfg( "kaddressbookrc" );
   cfg.setGroup( "General" );
   cfg.writeEntry( "FormattedNameType", mFormattedNameCombo->currentItem() );
+  
+  DCOPClient *client = DCOPClient::mainClient();
+  if ( client )
+      client->emitDCOPSignal( "KABC::AddressBookConfig", "changed()", QByteArray() );
 }
 
 #include "addresseewidget.moc"
