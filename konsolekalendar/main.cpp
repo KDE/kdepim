036ad934c10b70e78051becdbf8404852d7c7bcd
Tuukka Pasanen <illuusio@nic.fi>
2003-09-26 08:26:42 +0000

    New version
    svn path=/trunk/kdepim/; revision=254065

--- konsolekalendar/main.cpp
+++ konsolekalendar/main.cpp
@@ -86,7 +86,7 @@ int main(int argc, char *argv[])
   KAboutData aboutData(
       "konsolekalendar",               // internal program name
       I18N_NOOP( "KonsoleKalendar" ),  // displayable program name.
-      "0.9.6",                           // version string
+      "0.9.7",                           // version string
       description,                     // short porgram description
       KAboutData::License_GPL,         // license type
       "(c) 2002-2003, Tuukka Pasanen and Allen Winter", // copyright statement
