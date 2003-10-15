569c9228c77091ee0e8d4b938325cf38031b1840
Tuukka Pasanen <illuusio@nic.fi>
2003-10-15 12:37:45 +0000

    add copyright
    svn path=/trunk/kdepim/; revision=259139

--- konsolekalendar/testkalendar.sh
+++ konsolekalendar/testkalendar.sh
@@ -1,3 +1,22 @@
+##########################################################################################
+#
+#  ***************************************************************************
+#  *                                                                         *
+#  *   This program is free software; you can redistribute it and/or modify  *
+#  *   it under the terms of the GNU General Public License as published by  *
+#  *   the Free Software Foundation; either version 2 of the License, or     *
+#  *   (at your option) any later version.                                   *
+#  *                                                                         *
+#  ***************************************************************************
+#
+##########################################################################################
+#
+# Purpose of this program is to test konsolekalendar in future this will be more smarter
+#
+# testkalendar.sh 							0.1
+#
+##########################################################################################
+
 #!/bin/sh
 
 function do_test(){
