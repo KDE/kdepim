890fe26016083e11e3dd1a57d1a0af2027028ebf
Daniel Molkentin <molkentin@kde.org>
2003-10-17 10:15:54 +0000

    deprecated--
    svn path=/trunk/kdepim/; revision=259577

--- libkpimexchange/resources/calendar/resourceexchange.cpp
+++ libkpimexchange/resources/calendar/resourceexchange.cpp
@@ -49,16 +49,6 @@ using namespace KPIM;
 
 extern "C"
 {
-/*  
-  KRES::ConfigWidget *config_widget( QWidget *parent ) {
-    return new KCal::ResourceExchangeConfig( parent, "Configure Exchange Calendar" );
-  }
-
-  KRES::Resource *resource( const KConfig *config ) {
-    kdDebug() << "In researchExchange static resource() function" << endl;
-    return new KCal::ResourceExchange( config );
-  }
-*/
   void* init_resourcecalendarexchange()
   {
     return new KRES::PluginFactory<ResourceExchange,ResourceExchangeConfig>();
