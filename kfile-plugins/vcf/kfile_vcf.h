adf7571b0e9ffa93224b05a73ab6ae2ed6857eb9
Pascal Ltourneau <pascal.letourneau@gmail.com>
2003-10-22 19:57:41 +0000

    Search & Replace gone mad?? s/softbmpre/software/ etc... in license
    svn path=/trunk/kdepim/; revision=261129

--- kfile-plugins/vcf/kfile_vcf.h
+++ kfile-plugins/vcf/kfile_vcf.h
@@ -1,18 +1,18 @@
 /* This file is part of the KDE project
  * Copyright (C) 2002 Shane Wright <me@shanewright.co.uk>
  *
- * This program is free softvcfre; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public
- * License as published by the Free Softvcfre Foundation version 2.
+ * License as published by the Free Software Foundation version 2.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied vcfrranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; see the file COPYING.  If not, write to
- * the Free Softvcfre Foundation, Inc., 59 Temple Place - Suite 330,
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
  */
