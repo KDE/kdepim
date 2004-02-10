ead0ae0d9d7d97a66f9dbb31a3b2408d8a069040
Dirk Mueller <mueller@kde.org>
2004-02-10 10:58:39 +0000

    CVS_SILENT explicit KURL/QRegExp
    svn path=/trunk/kdepim/; revision=286735

--- knode/knarticlewidget.cpp
+++ knode/knarticlewidget.cpp
@@ -722,7 +722,7 @@ QString KNArticleWidget::toHtmlString(const QString &line, bool parseURLs, bool 
       case '/' :
       case '*' :
         if(beautification) {
-          regExp=QString("\\%1[^\\s%2]+\\%3").arg(text[idx]).arg(text[idx]).arg(text[idx]);
+          regExp=QRegExp( QString("\\%1[^\\s%2]+\\%3").arg(text[idx]).arg(text[idx]).arg(text[idx]) );
           if (regExp.search(text,idx)==(int)idx) {
             matchLen = regExp.matchedLength();
             if (( matchLen > 3 ) &&
@@ -783,7 +783,7 @@ void KNArticleWidget::openURL(const QString &url)
     if(url.isEmpty()) return;
 
     if (knGlobals.cfgManager->readNewsViewer()->browser()==KNConfig::ReadNewsViewer::BTdefault)
-        (void) new KRun(url);
+        (void) new KRun(KURL( url ));
     if (knGlobals.cfgManager->readNewsViewer()->browser()==KNConfig::ReadNewsViewer::BTkonq)
         kapp->invokeBrowser(url);
     else if (knGlobals.cfgManager->readNewsViewer()->browser()==KNConfig::ReadNewsViewer::BTnetscape){
@@ -1569,7 +1569,7 @@ void KNArticleWidget::anchorClicked(const QString &a, ButtonState button, const 
         if (target.endsWith("/"))
           target.truncate(target.length()-1);
         kdDebug(5003) << "KNArticleWidget::anchorClicked() : news-url " << target << endl;
-        knGlobals.top->openURL(target);
+        knGlobals.top->openURL(KURL( target ));
       break;
       case ATmailto:
         kdDebug(5003) << "KNArticleWidget::anchorClicked() : mailto author" << endl;
