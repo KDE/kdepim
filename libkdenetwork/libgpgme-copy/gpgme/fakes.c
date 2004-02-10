817ea06815545c4655d59b6a8bc9d87b8a47b359
David Faure <faure@kde.org>
2004-02-10 23:59:55 +0000

    Grabbed all the tests from gpgme, they are needed.
    It also turns out that automake has a strange useless feature called
    AC_REPLACE_FUNCS(foo) that compiles foo.c if foo isn't available. Easily
    reimplemented with a fakes.c though.
    
    svn path=/trunk/kdepim/; revision=286979

---
+++ libkdenetwork/libgpgme-copy/gpgme/fakes.c
@@ -0,0 +1,21 @@
+#include <config.h>
+
+#ifndef HAVE_STPCPY
+#include "stpcpy.c"
+#endif
+
+#ifndef HAVE_FUNOPEN
+#include "funopen.c"
+#endif
+
+#ifndef HAVE_ISASCII
+#include "isascii.c"
+#endif
+
+#ifndef HAVE_PUTC_UNLOCKED
+#include "putc_unlocked.c"
+#endif
+
+#ifndef HAVE_MEMRCHR
+#include "memrchr.c"
+#endif
