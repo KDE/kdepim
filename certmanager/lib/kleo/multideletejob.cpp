30e4c46bc4c945de08033a8cc7251365cec83c8d
Marc Mutz <mutz@kde.org>
2004-02-19 11:33:45 +0000

    MultiDeleteJob, for - well - deleting a list of keys.
    svn path=/trunk/kdepim/; revision=289349

---
+++ certmanager/lib/kleo/multideletejob.cpp
@@ -0,0 +1,110 @@
+/*  -*- mode: C++; c-file-style: "gnu" -*-
+    multideletejob.cpp
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    Copyright (c) 2004 Klarälvdalens Datakonsult AB
+
+    Libkleopatra is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License,
+    version 2, as published by the Free Software Foundation.
+
+    Libkleopatra is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    In addition, as a special exception, the copyright holders give
+    permission to link the code of this program with any edition of
+    the Qt library by Trolltech AS, Norway (or with modified versions
+    of Qt that use the same license as Qt), and distribute linked
+    combinations including the two.  You must obey the GNU General
+    Public License in all respects for all of the code used other than
+    Qt.  If you modify this file, you may extend this exception to
+    your version of the file, but you are not obligated to do so.  If
+    you do not wish to do so, delete this exception statement from
+    your version.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "multideletejob.h"
+#include "cryptobackend.h"
+#include "deletejob.h"
+
+#include <klocale.h>
+
+#include <gpgmepp/key.h>
+#include <gpgmepp/context.h>
+#include <gpgmepp/data.h>
+
+#include <iterator>
+
+#include <assert.h>
+
+Kleo::MultiDeleteJob::MultiDeleteJob( const CryptoBackend * backend )
+  : Job( 0, "Kleo::MultiDeleteJob" ),
+    mBackend( backend ),
+    mJob( 0 )
+{
+  assert( backend );
+}
+
+Kleo::MultiDeleteJob::~MultiDeleteJob() {
+
+}
+
+GpgME::Error Kleo::MultiDeleteJob::start( const std::vector<GpgME::Key> & keys, bool allowSecretKeyDeletion ) {
+  mKeys = keys;
+  mAllowSecretKeyDeletion = allowSecretKeyDeletion;
+  mIt = mKeys.begin();
+
+  const GpgME::Error err = startAJob();
+
+  if ( err )
+    deleteLater();
+  return err;
+}
+
+void Kleo::MultiDeleteJob::slotCancel() {
+  if ( mJob ) mJob->slotCancel();
+  mIt = mKeys.end();
+}
+
+void Kleo::MultiDeleteJob::slotResult( const GpgME::Error & err ) {
+  if ( err || mIt == mKeys.end() || ++mIt == mKeys.end() ) {
+    emit done();
+    emit result( err, err && mIt != mKeys.end() ? *mIt : GpgME::Key::null );
+    deleteLater();
+  } else {
+    const GpgME::Error error = startAJob();
+    if ( error ) {
+      emit done();
+      emit result( error, mIt != mKeys.end() ? *mIt : GpgME::Key::null );
+      deleteLater();
+      return;
+    }
+    const int current = mIt - mKeys.begin();
+    const int total = mKeys.size();
+    emit progress( i18n("%1/%2").arg( current ).arg( total ), 0, current, total );
+  }
+}
+
+GpgME::Error Kleo::MultiDeleteJob::startAJob() {
+  if ( mIt == mKeys.end() )
+    return 0;
+  DeleteJob * job = mBackend->deleteJob();
+  assert( job ); // FIXME: we need a way to generate errors ourselves,
+		 // but I don't like the dependency on gpg-error :/
+
+  connect( job, SIGNAL(result(const GpgME::Error&)), SLOT(slotResult(const GpgME::Error&)) );
+
+  return job->start( *mIt, mAllowSecretKeyDeletion );
+}
+
+#include "multideletejob.moc"
---
+++ certmanager/lib/kleo/multideletejob.h
@@ -0,0 +1,97 @@
+/*  -*- mode: C++; c-file-style: "gnu" -*-
+    multideletejob.h
+
+    This file is part of libkleopatra, the KDE keymanagement library
+    Copyright (c) 2004 Klarälvdalens Datakonsult AB
+
+    Libkleopatra is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License,
+    version 2, as published by the Free Software Foundation.
+
+    Libkleopatra is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    In addition, as a special exception, the copyright holders give
+    permission to link the code of this program with any edition of
+    the Qt library by Trolltech AS, Norway (or with modified versions
+    of Qt that use the same license as Qt), and distribute linked
+    combinations including the two.  You must obey the GNU General
+    Public License in all respects for all of the code used other than
+    Qt.  If you modify this file, you may extend this exception to
+    your version of the file, but you are not obligated to do so.  If
+    you do not wish to do so, delete this exception statement from
+    your version.
+*/
+
+#ifndef __KLEO_MULTIDELETEJOB_H__
+#define __KLEO_MULTIDELETEJOB_H__
+
+#include "job.h"
+
+#include <vector>
+
+namespace GpgME {
+  class Error;
+  class Key;
+}
+
+namespace Kleo {
+  class CryptoBackend;
+  class DeleteJob;
+}
+
+namespace Kleo {
+
+  /**
+     @short A convenience class bundling together multiple DeleteJobs.
+
+     To use a MultiDeleteJob, pass it a CryptoBackend implementation,
+     connect the progress() and result() signals to suitable slots and
+     then start the delete with a call to start(). This call might
+     fail, in which case the MultiDeleteJob instance will have scheduled
+     it's own destruction with a call to QObject::deleteLater().
+
+     After result() is emitted, the MultiDeleteJob will schedule it's own
+     destruction by calling QObject::deleteLater().
+  */
+  class MultiDeleteJob : public Job {
+    Q_OBJECT
+  public:
+    MultiDeleteJob( const CryptoBackend * backend );
+    ~MultiDeleteJob();
+
+    /**
+       Starts the delete operation. \a keys is the list of keys to
+       delete, \a allowSecretKeyDeletion specifies if a key may also
+       be deleted if the secret key part is available, too.
+    */
+    GpgME::Error start( const std::vector<GpgME::Key> & keys, bool allowSecretKeyDeletion=false );
+
+  signals:
+    void result( const GpgME::Error & result, const GpgME::Key & errorKey );
+
+  private slots:
+    void slotResult( const GpgME::Error & );
+    /*! \reimp from Job */
+    void slotCancel();
+
+  private:
+    GpgME::Error startAJob();
+
+  private:
+    const CryptoBackend * mBackend;
+    DeleteJob * mJob;
+    std::vector<GpgME::Key> mKeys;
+    std::vector<GpgME::Key>::const_iterator mIt;
+    bool mAllowSecretKeyDeletion;
+  };
+
+}
+
+#endif // __KLEO_MULTIDELETEJOB_H__
