71bb019686075ae3dc27a06b28781f048814d9c0
Marc Mutz <mutz@kde.org>
2004-02-19 22:08:14 +0000

    Make all license headers the same: GPL(v2+) + Qt Exception
    svn path=/trunk/kdepim/; revision=289520

--- certmanager/lib/ui/progressdialog.h
+++ certmanager/lib/ui/progressdialog.h
@@ -5,8 +5,9 @@
     Copyright (c) 2004 Klarälvdalens Datakonsult AB
 
     Libkleopatra is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License,
-    version 2, as published by the Free Software Foundation.
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of the
+    License, or (at your option) any later version.
 
     Libkleopatra is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
