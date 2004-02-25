098f4ae2bb9df965bd81d58524bd768461afe795
Marc Mutz <mutz@kde.org>
2004-02-25 01:39:44 +0000

    Kleopatra 0.20
    svn path=/trunk/kdepim/; revision=291180

--- certmanager/aboutdata.cpp
+++ certmanager/aboutdata.cpp
@@ -34,7 +34,7 @@
 
 #include <klocale.h>
 
-static const char kleopatra_version[] = "0.11";
+static const char kleopatra_version[] = "0.20";
 static const char description[] = I18N_NOOP("KDE Key Manager");
 
 struct about_data {
