85bf770e6bf4f2e249e3e1048cad424fc9b1c8d3
Reinhold Kainhofer <reinhold@kainhofer.com>
2004-03-08 09:57:05 +0000

    Also connect the fDelay spin box to the modified slot so the apply button is activated when you change the value.
    svn path=/trunk/kdepim/; revision=294359

--- kpilot/kpilot/kpilotConfigDialog.cc
+++ kpilot/kpilot/kpilotConfigDialog.cc
@@ -42,6 +42,7 @@ static const char *kpilotconfigdialog_id =
 #include <qbuttongroup.h>
 #include <qlineedit.h>
 #include <qtabwidget.h>
+#include <qspinbox.h>
 
 #include <kmessagebox.h>
 #include <kcharsets.h>
@@ -85,6 +86,7 @@ DeviceConfigPage::DeviceConfigPage(QWidget * w, const char *n ) : ConduitConfigB
 	CM(fPilotSpeed, SIGNAL(activated(int)));
 	CM(fPilotEncoding, SIGNAL(textChanged(const QString &)));
 	CM(fUserName, SIGNAL(textChanged(const QString &)));
+	CM(fDelay, SIGNAL(valueChanged(int)));
 #undef CM
 
 	(void) kpilotconfigdialog_id;
