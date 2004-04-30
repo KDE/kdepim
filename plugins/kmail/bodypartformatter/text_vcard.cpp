1e3daff321a379ebfcbd3f93315daa2f5add9e50
Till Adam <adam@kde.org>
2004-04-30 13:53:56 +0000

    Now that AddresseeView::vCardAsHtml() is available, use it and remove temporarily duplicated code.
    Thanks, Tobias. :)
    
    svn path=/trunk/kdepim/; revision=307547

--- plugins/kmail/bodypartformatter/text_vcard.cpp
+++ plugins/kmail/bodypartformatter/text_vcard.cpp
@@ -47,6 +47,9 @@
 using KABC::VCardConverter;
 using KABC::Addressee;
 
+#include "addresseeview.h"
+using KPIM::AddresseeView;
+
 namespace {
 
   class Formatter : public KMail::Interface::BodyPartFormatter {
@@ -60,185 +63,14 @@ namespace {
        if ( al.empty() ) return AsIcon;
        QValueListIterator<KABC::Addressee> it = al.begin();
 
-
-       QString header = QString::fromLatin1(
-             "<div align=\"center\">"
-             "<table width=\"80%\" style=\"border: #000000 thin dashed 1px;\"  >"
-             );
-
-       writer->queue( header );
        for ( ; it != al.end(); ++it ) {
           KABC::Addressee a = (*it);
           if ( a.isEmpty() ) return AsIcon;
 
-          QString dynamicPart;
-          QString name = ( a.formattedName().isEmpty() ?
-                a.assembledName() : a.formattedName() );
-
-          QString rowFmtStr = QString::fromLatin1(
-                "<tr><td align=\"right\" width=\"30%\"><b>%1</b></td>"
-                "<td align=\"left\" width=\"70%\">%2</td></tr>\n"
-                );
-
-          QDate date = a.birthday().date();
-
-          if ( date.isValid() )
-             dynamicPart += rowFmtStr
-                .arg( KABC::Addressee::birthdayLabel() )
-                .arg( KGlobal::locale()->formatDate( date, true ) );
-
-          KABC::PhoneNumber::List phones = a.phoneNumbers();
-          KABC::PhoneNumber::List::ConstIterator phoneIt;
-          for ( phoneIt = phones.begin(); phoneIt != phones.end(); ++phoneIt ) {
-             QString number = (*phoneIt).number();
-
-             QString url;
-             if ( (*phoneIt).type() & KABC::PhoneNumber::Fax )
-                url = QString::fromLatin1("fax:") + number;
-             else
-                url = QString::fromLatin1("phone:") + number;
-
-             dynamicPart += rowFmtStr
-                .arg( KABC::PhoneNumber::typeLabel( (*phoneIt).type() ).replace( " ", "&nbsp;" ) )
-                .arg( QString::fromLatin1("<a href=\"%1\">%2</a>").arg(url).arg(number) );
-          }
-
-          QStringList emails = a.emails();
-          QStringList::ConstIterator emailIt;
-          QString type = i18n( "Email" );
-          for ( emailIt = emails.begin(); emailIt != emails.end(); ++emailIt ) {
-             QString fullEmail = a.fullEmail( *emailIt );
-             QUrl::encode( fullEmail );
-             dynamicPart += rowFmtStr
-                .arg( type )
-                .arg( QString::fromLatin1( "<a href=\"mailto:%1\">%2</a>" )
-                      .arg( fullEmail, *emailIt ) );
-             type = i18n( "Other" );
-          }
-
-          if ( !a.url().url().isEmpty() ) {
-             dynamicPart += rowFmtStr
-                .arg( i18n( "Homepage" ) )
-                .arg( KStringHandler::tagURLs( a.url().url() ) );
-          }
-
-          KABC::Address::List addresses = a.addresses();
-          KABC::Address::List::ConstIterator addrIt;
-          for ( addrIt = addresses.begin(); addrIt != addresses.end(); ++addrIt ) {
-             if ( (*addrIt).label().isEmpty() ) {
-                QString formattedAddress;
-
-                formattedAddress = (*addrIt).formattedAddress().stripWhiteSpace();
-#if 0
-                if ( !(*addrIt).street().isEmpty() )
-                   formattedAddress += (*addrIt).street() + "\n";
-
-                if ( !(*addrIt).postOfficeBox().isEmpty() )
-                   formattedAddress += (*addrIt).postOfficeBox() + "\n";
-
-                formattedAddress += (*addrIt).locality() + QString::fromLatin1(" ") + (*addrIt).region();
-
-                if ( !(*addrIt).postalCode().isEmpty() )
-                   formattedAddress += QString::fromLatin1(", ") + (*addrIt).postalCode();
-
-                formattedAddress += "\n";
-
-                if ( !(*addrIt).country().isEmpty() )
-                   formattedAddress += (*addrIt).country() + "\n";
-
-                formattedAddress += (*addrIt).extended();
-#endif
-
-                formattedAddress = formattedAddress.replace( '\n', "<br>" );
-
-                QString link = "<a href=\"addr:" + (*addrIt).id() + "\">" +
-                   formattedAddress + "</a>";
-
-                dynamicPart += rowFmtStr
-                   .arg( KABC::Address::typeLabel( (*addrIt).type() ) )
-                   .arg( link );
-             } else {
-                QString link = "<a href=\"addr:" + (*addrIt).id() + "\">" +
-                   (*addrIt).label().replace( '\n', "<br>" ) + "</a>";
-
-                dynamicPart += rowFmtStr
-                   .arg( KABC::Address::typeLabel( (*addrIt).type() ) )
-                   .arg( link );
-             }
-          }
-
-          QString notes;
-          if ( !a.note().isEmpty() ) {
-             notes = QString::fromLatin1(
-                   "<tr>"
-                   "<td align=\"right\" valign=\"top\" width=\"30%\"><b>%1:</b></td>"  // note label
-                   "<td align=\"left\" valign=\"top\">%2</td>"  // note
-                   "</tr>" ).arg( i18n( "Notes" ) ).arg( a.note().replace( '\n', "<br>" ) );
-          }
-
-          QString role, organization;
-          role = a.role();
-          organization = a.organization();
-
-          // when only an organization is set we use it as name
-          if ( !a.organization().isEmpty() && name.isEmpty() ||
-                a.formattedName() == a.organization() ) {
-             name = a.organization();
-             organization = QString::null;
-          }
-
-          QString personStr = QString::fromLatin1(
-                "<tr>"
-                "<td align=\"right\" valign=\"top\" width=\"30%\" rowspan=\"3\">"
-                "<img src=\"myimage\" width=\"50\" height=\"70\">"
-                "</td>"
-                "<td align=\"left\" width=\"70%\"><font size=\"+2\"><b>%1</b></font></td>"  // name
-                "</tr>"
-                "<tr>"
-                "<td align=\"left\" width=\"70%\">%2</td>"  // role
-                "</tr>"
-                "<tr>"
-                "<td align=\"left\" width=\"70%\">%3</td>"  // organization
-                "</tr>"
-                "</tr>"
-                "<tr><td colspan=\"2\">&nbsp;</td></tr>"
-                "%4"  // dynamic part
-                "%5"  // notes
-                )
-             .arg( name )
-             .arg( role )
-             .arg( organization )
-             .arg( dynamicPart, notes );
-
-
-          writer->queue( personStr );
-          KABC::Picture picture = a.photo();
-          if ( picture.isIntern() && !picture.data().isNull() )
-             QMimeSourceFactory::defaultFactory()->setImage( "myimage", picture.data() );
-          else {
-                   /*
-             if ( !picture.url().isEmpty() ) {
-                if ( mImageData.count() > 0 )
-                   QMimeSourceFactory::defaultFactory()->setImage( "myimage", mImageData );
-                else {
-                      mImageJob = KIO::get( KURL( picture.url() ), false, false );
-                      connect( mImageJob, SIGNAL( data( KIO::Job*, const QByteArray& ) ),
-                      this, SLOT( data( KIO::Job*, const QByteArray& ) ) );
-                      connect( mImageJob, SIGNAL( result( KIO::Job* ) ),
-                      this, SLOT( result( KIO::Job* ) ) );
-                }
-             } else {
-                    */
-                QMimeSourceFactory::defaultFactory()->setPixmap( "myimage",
-                      KGlobal::iconLoader()->loadIcon( "identity", KIcon::Desktop, 128 ) );
-                /*
-             }
-             */
-
-          }
+          QString contact = AddresseeView::vCardAsHTML( a );
+          writer->queue( contact );
        }
 
-       writer->queue( "</div></table>" );
        return Ok;
     }
   };
