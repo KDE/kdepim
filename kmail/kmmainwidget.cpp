e6ad61f44f4e31157b8d086114b4d7842d0f0302
Ingo Klcker <kloecker@kde.org>
2004-05-01 14:34:16 +0000

    Revert because this breaks Ctrl+S for Save As...
    svn path=/branches/KDE_3_2_BRANCH/kdepim/; revision=307811

--- kmail/kmmainwidget.cpp
+++ kmail/kmmainwidget.cpp
@@ -2077,8 +2077,9 @@ void KMMainWidget::setupActions()
 		      this, SLOT(slotNewMailReader()),
 		      actionCollection(), "new_mail_client" );
 
-  mSaveAsAction = KStdAction::saveAs( this, SLOT(slotSaveMsg()),
-                      actionCollection(), "file_save_as" );
+  mSaveAsAction = new KAction( i18n("Save &As..."), "filesave",
+    KStdAccel::shortcut(KStdAccel::Save),
+    this, SLOT(slotSaveMsg()), actionCollection(), "file_save_as" );
 
   (void) new KAction( i18n("&Compact All Folders"), 0,
 		      this, SLOT(slotCompactAll()),
