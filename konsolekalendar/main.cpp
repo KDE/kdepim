9e48afc7ee85e3384aae4259be5c43c6b7ecb1cb
Tuukka Pasanen <illuusio@nic.fi>
2004-05-10 20:28:22 +0000

    Okay be all happy puppies. KonsoleKalendar not LGPL-license anymore. It's now under GPL+QT license. All emails about changing goes straight to /dev/null. Thank you.
    svn path=/trunk/kdepim/; revision=310157

--- konsolekalendar/main.cpp
+++ konsolekalendar/main.cpp
@@ -1,25 +1,29 @@
-/*******************************************************************************
- *   main.cpp                                                                  *
- *                                                                             *
- *   KonsoleKalendar is a command line interface to KDE calendars              *
- *   Copyright (C) 2002-2004  Tuukka Pasanen <illuusio@mailcity.com>           *
- *   Copyright (C) 2003-2004  Allen Winter <awinterz@users.sourceforge.net>    *
- *                                                                             *
- *   This library is free software; you can redistribute it and/or             *
- *   modify it under the terms of the GNU Lesser General Public                *
- *   License as published by the Free Software Foundation; either              *
- *   version 2.1 of the License, or (at your option) any later version.        *
- *                                                                             *
- *   This library is distributed in the hope that it will be useful,           *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of            *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
- *   Lesser General Public License for more details.                           *
- *                                                                             *
- *   You should have received a copy of the GNU Lesser General Public          *
- *   License along with this library; if not, write to the Free Software       *
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA *
- *                                                                             *
- ******************************************************************************/
+/***********************************************************************************
+ *   main.cpp                                                                      *
+ *                                                                                 *
+ *   KonsoleKalendar is a command line interface to KDE calendars                  *
+ *   Copyright (C) 2002-2004  Tuukka Pasanen <illuusio@mailcity.com>               *
+ *   Copyright (C) 2003-2004  Allen Winter <awinterz@users.sourceforge.net>        * 
+ *                                                                                 *
+ *   This program is free software; you can redistribute it and/or modify          *
+ *   it under the terms of the GNU General Public License as published by          * 
+ *   the Free Software Foundation; either version 2 of the License, or             *
+ *   (at your option) any later version.                                           *
+ *                                                                                 *
+ *   This program is distributed in the hope that it will be useful,               * 
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  *
+ *   GNU General Public License for more details.                                  *
+ *                                                                                 * 
+ *    You should have received a copy of the GNU General Public License            *
+ *   along with this program; if not, write to the Free Software                   *
+ *   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.   *
+ *                                                                                 *
+ *   As a special exception, permission is given to link this program              * 
+ *   with any edition of Qt, and distribute the resulting executable,              *
+ *   without including the source code for Qt in the source distribution.          *
+ *                                                                                 *
+ ***********************************************************************************/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -66,7 +70,7 @@ using namespace std;
 
 static const char progName[] = "konsolekalendar";
 static const char progDisplay[] = "KonsoleKalendar";
-static const char progVersion[] = "1.1.3";
+static const char progVersion[] = "1.1.4";
 static const char progDesc[] = "A command line interface to KDE calendars";
 static const char progURL[] = "pim.kde.org/components/konsolekalendar.php";
 
