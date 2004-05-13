7c72088f0f1ff7ca70fd10e1bc5f5af9dde9c03e
David Faure <faure@kde.org>
2004-05-13 10:28:52 +0000

    Luká? LukÃ¡ err I mean Luk=C3=A1=C5=A1 had the final word :)
    svn path=/trunk/kdepim/; revision=310990

--- libkdenetwork/ksubscription.cpp
+++ libkdenetwork/ksubscription.cpp
@@ -757,12 +757,11 @@ void KSubscription::slotFilterTextChanged( const QString & text )
 //------------------------------------------------------------------------------
 void KSubscription::slotUpdateStatusLabel()
 {
-  QString matchingStr = QString::number( activeItemCount() );
   QString text;
   if (mLoading)
-    text = i18n("Loading...(%1 matching)").arg(matchingStr);
+    text = i18n("Loading...(%1 matching)").arg(activeItemCount());
   else
-    text = i18n("%1: (%2 matching)").arg(account()->name(), matchingStr);
+    text = i18n("%1: (1 matching)", "%1: (%n matching)", activeItemCount()).arg(account()->name());
 
   leftLabel->setText(text);
 }
