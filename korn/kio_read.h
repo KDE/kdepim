0dc09e3f085087d98ca9db1768e58cb188a23970
Mart Kelder <mart@kelder31.nl>
2004-05-14 17:19:52 +0000

    Added copyright information
    svn path=/trunk/kdepim/; revision=311461

--- korn/kio_read.h
+++ korn/kio_read.h
@@ -1,6 +1,25 @@
 #ifndef KIO_READ_H
 #define KIO_READ_H
 
+/*
+ * Copyright (C) 2004, Mart Kelder (mart.kde@hccnet.nl)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 //This class should be used if someone wants to read the Full Message
 
 #include <qobject.h>
