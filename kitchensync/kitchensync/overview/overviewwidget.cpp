af7ad597334bfd79712462bb7708a6d229c7d0f4
Tobias Koenig <tokoe@kde.org>
2004-06-03 18:50:45 +0000

    Coding style cleanup and save/restore splitter size
    svn path=/trunk/kdepim/; revision=317438

--- kitchensync/kitchensync/overview/overviewwidget.cpp
+++ kitchensync/kitchensync/overview/overviewwidget.cpp
@@ -1,15 +1,15 @@
 #include <qdatetime.h>
-#include <qvbox.h>
-#include <qtextedit.h>
 #include <qsplitter.h>
+#include <qtextedit.h>
+#include <qvbox.h>
 
-#include <klocale.h>
+#include <kconfig.h>
 #include <kdialog.h>
-
-#include <error.h>
-#include <progress.h>
+#include <klocale.h>
 
 #include <actionpart.h>
+#include <error.h>
+#include <progress.h>
 
 #include "overviewwidget.h"
 
@@ -17,123 +17,133 @@ using namespace KSync;
 using namespace KSync::OverView;
 
 Widget::Widget( QWidget* parent, const char* name )
-    : QWidget( parent, name )
+  : QWidget( parent, name )
 {
-    m_lay = new QVBoxLayout(this);
-    m_lay->setMargin( KDialog::marginHint() );
-    QLabel* label = new QLabel(this);
-    label->setText( "<qt><h1>" + i18n("Overview") + "</h1></qt>" );
+  QVBoxLayout *layout = new QVBoxLayout( this );
+  layout->setMargin( KDialog::marginHint() );
 
-    QFrame* line = new QFrame( this );
-    line->setFrameStyle( QFrame::HLine | QFrame::Sunken );
+  QLabel* label = new QLabel( this );
+  label->setText( "<qt><h1>" + i18n( "Overview" ) + "</h1></qt>" );
 
-    QHBox* info = new QHBox(this);
-    info->setSpacing( 10 );
-    info->setMargin( 10 );
+  QFrame* line = new QFrame( this );
+  line->setFrameStyle( QFrame::HLine | QFrame::Sunken );
 
-    QVBox *info2 = new QVBox(info);
-    m_device = new QLabel(info2);
-    m_profile= new QLabel(info2);
-    info->setStretchFactor( info2, 5 );
+  QHBox* info = new QHBox( this );
+  info->setSpacing( 10 );
+  info->setMargin( 10 );
 
-    m_logo = new QLabel( info );
+  QVBox *info2 = new QVBox(info);
+  m_device = new QLabel( info2 );
+  m_profile= new QLabel( info2 );
+  info->setStretchFactor( info2, 5 );
 
-    m_split = new QSplitter(this);
-    m_edit = new QTextEdit( m_split );
-    m_edit->setReadOnly(true);
-    m_edit->setTextFormat(Qt::LogText);
-    m_ab = new QWidget( m_split );
+  m_logo = new QLabel( info );
 
-#if 1
-    QValueList<int> list;
-    list.append( 0 );
-    list.append( 200 );
-    m_split->setSizes( list );
-    m_split->setResizeMode( m_edit, QSplitter::KeepSize );
-#endif
+  m_split = new QSplitter( this );
+  m_edit = new QTextEdit( m_split );
+  m_edit->setReadOnly( true );
+  m_edit->setTextFormat( Qt::LogText );
+  m_ab = new QWidget( m_split );
 
-    m_layout = new QVBoxLayout( m_ab );
-    m_layout->insertStretch( -1, 5 );
-    m_layoutFillIndex = 0;
+  KConfig config( "kitchensyncrc" );
+  config.setGroup( "OverviewPart" );
 
-    m_lay->addWidget( label );
-    m_lay->addWidget( line );
-    m_lay->addWidget( info );
-    m_lay->addWidget( m_split, 100 );
+  QValueList<int> sizes = config.readIntListEntry( "SplitterSize" );
+  if ( sizes.isEmpty() ) {
+    sizes.append( width() / 2 );
+    sizes.append( width() / 2 );
+  }
 
-    m_messageList.setAutoDelete( true );
+  m_split->setSizes( sizes );
+
+  m_layout = new QVBoxLayout( m_ab );
+  m_layout->insertStretch( -1, 5 );
+  m_layoutFillIndex = 0;
+
+  layout->addWidget( label );
+  layout->addWidget( line );
+  layout->addWidget( info );
+  layout->addWidget( m_split, 100 );
+
+  m_messageList.setAutoDelete( true );
 }
 
 Widget::~Widget()
 {
+  KConfig config( "kitchensyncrc" );
+  config.setGroup( "OverviewPart" );
+
+  config.writeEntry( "SplitterSize", m_split->sizes() );
 }
 
-void Widget::setProfile( const Profile& prof)
+void Widget::setProfile( const Profile& prof )
 {
-    m_profile->setText("<qt><b>" + i18n(" Profile: ") + "</b>" + prof.name() + "</qt>");
-    cleanView();
+  m_profile->setText( "<qt><b>" + i18n( " Profile: " ) + "</b>" + prof.name() + "</qt>" );
+  cleanView();
 }
 
-void Widget::setProfile( const QString& name, const QPixmap& pix)
+void Widget::setProfile( const QString& name, const QPixmap& pix )
 {
-    m_device->setText("<qt><b>"+ i18n(" Device: ") + "</b>" + name + "</qt>");
-    m_logo->setPixmap( pix );
-    cleanView();
+  m_device->setText( "<qt><b>"+ i18n( " Device: " ) + "</b>" + name + "</qt>" );
+  m_logo->setPixmap( pix );
+  cleanView();
 }
 
-void Widget::addProgress( Konnector *, const Progress& prog)
+void Widget::addProgress( Konnector *, const Progress& prog )
 {
-    m_edit->append( "<b>"+QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
+  m_edit->append( "<b>" + QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
 }
 
-void Widget::addProgress( ActionPart *, const Progress& prog)
+void Widget::addProgress( ActionPart *, const Progress& prog )
 {
-    m_edit->append( "<b>" + QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
+  m_edit->append( "<b>" + QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
 }
 
 void Widget::syncProgress( ActionPart * part, int status, int )
 {
-    OverViewProgressEntry* it;
-    for ( it = m_messageList.first(); it; it = m_messageList.next() ) {
-        if ( QString::compare( it->name(), part->name() ) == 0 ) {
-            it->setProgress( status );
-            return;
-        }
+  OverViewProgressEntry* it;
+  for ( it = m_messageList.first(); it; it = m_messageList.next() ) {
+    if ( QString::compare( it->name(), part->name() ) == 0 ) {
+      it->setProgress( status );
+      return;
     }
+  }
 
-    OverViewProgressEntry* test = new OverViewProgressEntry( m_ab, "test" );
-    m_messageList.append( test );
+  OverViewProgressEntry* test = new OverViewProgressEntry( m_ab, "test" );
+  m_messageList.append( test );
 
-    if ( !part->title().isEmpty() )  {
-        test->setText( part->title() );
-    }
-    if ( part->pixmap() ) {
-        test->setPixmap( *(part->pixmap()) );
-    }
-    test->setProgress( status );
-    m_layout->insertWidget(  m_layoutFillIndex , test, 0, AlignTop );
-    m_layoutFillIndex++;
-    test->show();
+  if ( !part->title().isEmpty() )  {
+    test->setText( part->title() );
+  }
+
+  if ( part->pixmap() ) {
+    test->setPixmap( *(part->pixmap()) );
+  }
+
+  test->setProgress( status );
+  m_layout->insertWidget( m_layoutFillIndex , test, 0, AlignTop );
+  m_layoutFillIndex++;
+  test->show();
 }
 
 void Widget::addError( Konnector *, const Error& prog )
 {
-    m_edit->append( "<b>"+ QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
+  m_edit->append( "<b>"+ QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
 }
 
 void Widget::addError( ActionPart *, const Error& prog )
 {
-    m_edit->append( "<b>"+ QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
+  m_edit->append( "<b>"+ QDateTime::currentDateTime().toString() + "</b> " + prog.text() );
 }
 
 void Widget::startSync()
 {
-    m_edit->append("Starting to sync now");
+  m_edit->append( "Starting to sync now" );
 }
 
 void Widget::cleanView()
 {
-    m_messageList.clear();
+  m_messageList.clear();
 }
 
 #include "overviewwidget.moc"
--- kitchensync/kitchensync/overview/overviewwidget.h
+++ kitchensync/kitchensync/overview/overviewwidget.h
@@ -1,55 +1,60 @@
 #ifndef KSYNC_OVERVIEW_WIDGET_H
 #define KSYNC_OVERVIEW_WIDGET_H
 
-#include <qwidget.h>
-#include <qlayout.h>
 #include <qlabel.h>
+#include <qlayout.h>
 #include <qptrlist.h>
+#include <qwidget.h>
 
 #include "overviewprogressentry.h"
 
-class QTextEdit;
 class QSplitter;
+class QTextEdit;
+
 namespace KSync {
-    class KonnectorProfile;
-    class Profile;
-    class Error;
-    class Progress;
+
+class Error;
+class KonnectorProfile;
+class Profile;
+class Progress;
 
 namespace OverView {
 
-    /**
-     * This is the MainWidget of the OverView and the only interface to the part...
-     *
-     */
-    class Widget : public QWidget {
-        Q_OBJECT
-    public:
-        Widget( QWidget* parent, const char* name );
-        ~Widget();
+/**
+  This is the MainWidget of the OverView and the only interface to the part...
+ */
+class Widget : public QWidget
+{
+  Q_OBJECT
 
-        void setProfile( const Profile& );
-        void setProfile( const QString&,const QPixmap& pix );
-        void addProgress( Konnector *, const Progress& );
-        void addProgress( ActionPart*, const Progress& );
-        void addError( Konnector *, const Error& );
-        void addError( ActionPart*, const Error& );
-	void syncProgress( ActionPart*, int, int);
-        void startSync();
-	void cleanView();
-    private:
-	int m_layoutFillIndex;
-        QVBoxLayout* m_lay;
-        QLabel* m_device;
-        QLabel* m_profile;
-        QLabel* m_logo;
-	QVBoxLayout* m_layout;
-	QPtrList<OverViewProgressEntry> m_messageList;
-	QSplitter *m_split;
-	QWidget* m_ab;
-        QTextEdit* m_edit;
+  public:
+    Widget( QWidget* parent, const char* name );
+    ~Widget();
 
-    };
+    void setProfile( const Profile& );
+    void setProfile( const QString&,const QPixmap& pix );
+    void addProgress( Konnector *, const Progress& );
+    void addProgress( ActionPart*, const Progress& );
+    void addError( Konnector *, const Error& );
+    void addError( ActionPart*, const Error& );
+    void syncProgress( ActionPart*, int, int);
+    void startSync();
+    void cleanView();
+
+  private:
+    int m_layoutFillIndex;
+    QLabel* m_device;
+    QLabel* m_profile;
+    QLabel* m_logo;
+    QVBoxLayout* m_layout;
+    QPtrList<OverViewProgressEntry> m_messageList;
+    QSplitter *m_split;
+    QWidget* m_ab;
+    QTextEdit* m_edit;
+};
+
 }
+
 }
+
 #endif
