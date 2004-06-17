8f2690b88462b2074a50df5047c0805f77c0f565
David Faure <faure@kde.org>
2004-06-17 12:57:02 +0000

    Improve the messagebox shown for alerts from the server (e.g. over quota, or shutting down soon),  by adding "Message from <hostname>" to it. Simon grepped all of kdepim for "shutting down soon" :)
    Improve error handling in ::put(): when the server answers NO "Over Quota",
    then better ship that string over to the dialog in kmail.
    
    svn path=/trunk/kdepim/; revision=321408

--- kioslaves/imap4/imap4.cc
+++ kioslaves/imap4/imap4.cc
@@ -570,7 +570,7 @@ IMAP4Protocol::listDir (const KURL & _url)
     }
   }
   if ( !selectInfo.alert().isNull() ) {
-    warning( selectInfo.alert() );
+    warning( i18n( "Message from %1: %2" ).arg( myHost ).arg( selectInfo.alert() ) );
     selectInfo.setAlert( 0 );
   }
 
@@ -827,7 +827,9 @@ IMAP4Protocol::put (const KURL & _url, int, bool, bool)
     }
     else
     {
-      error (ERR_COULD_NOT_WRITE, myHost);
+      //error (ERR_COULD_NOT_WRITE, myHost);
+      // Better ship the error message, e.g. "Over Quota"
+      error (ERR_SLAVE_DEFINED, cmd->resultInfo());
     }
 
     completeQueue.removeRef (cmd);
