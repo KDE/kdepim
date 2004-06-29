148fe3698f8a7407acbb1cdbac65306d0bcbbd73
Jrn Ahrens <joern.ahrens@kdemail.net>
2004-06-29 18:25:21 +0000

    corrected the behavior of the configuration dialog, when the screensaver option is changed
    svn path=/trunk/kdepim/; revision=324684

--- kpilot/kpilot/kpilotConfigDialog.cc
+++ kpilot/kpilot/kpilotConfigDialog.cc
@@ -208,6 +208,7 @@ SyncConfigPage::SyncConfigPage(QWidget * w, const char *n ) : ConfigPage( w, n )
 #define CM(a,b) connect(fConfigWidget->a,b,this,SLOT(modified()));
 	CM(fSpecialSync, SIGNAL(activated(int)));
 	CM(fFullBackupCheck, SIGNAL(toggled(bool)));
+	CM(fScreenlockSecure, SIGNAL(toggled(bool)));
 	CM(fConflictResolution, SIGNAL(activated(int)));
 #undef CM
 
