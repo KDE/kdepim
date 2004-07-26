6bf0e49a9ac5e95455d7ba69c54fd4e9eb388ffb
Mark Bucciarelli <mark@hubcapconsulting.com>
2004-07-26 18:04:42 +0000

    Output of decimal numbers was not respecting user locale settings.
    CCMAIL: 85946-done@bugs.kde.org
    
    svn path=/trunk/kdepim/; revision=332994

--- karm/karmutility.cpp
+++ karm/karmutility.cpp
@@ -3,12 +3,17 @@
 
 #include <stdlib.h>
 
+#include <kglobal.h>
+#include <klocale.h>
 #include "karmutility.h"
 
 QString formatTime( long minutes, bool decimal )
 {
   QString time;
-  if ( decimal ) time.sprintf("%.2f", minutes / 60.0);
+  if ( decimal ) {
+    time.sprintf("%.2f", minutes / 60.0);
+    time.replace( '.', KGlobal::locale()->decimalSymbol() );
+  }
   else time.sprintf("%ld:%02ld", minutes / 60, labs(minutes % 60));
   return time;
 }
