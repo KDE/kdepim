26b98fceef3ffbe118b7ecc6e4ea2919ae4ccee9
Till Adam <adam@kde.org>
2004-08-13 21:28:07 +0000

    Add plugin which automatically renders attached patches with a bit of syntax highlighting so they are more pleasant to read. This has lots of room for improvement, I'm hoping to add some real functionality via links (apply this chunk, save this chunk etc.) to this with Otto at aKademy.
    svn path=/trunk/kdepim/; revision=337793

---
+++ plugins/kmail/bodypartformatter/text_xdiff.cpp
@@ -0,0 +1,146 @@
+/*
+    This file is part of kdepim.
+
+    Copyright (c) 2004 Till Adam <adam@kde.org>
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    In addition, as a special exception, the copyright holders give
+    permission to link the code of this program with any edition of
+    the Qt library by Trolltech AS, Norway (or with modified versions
+    of Qt that use the same license as Qt), and distribute linked
+    combinations including the two.  You must obey the GNU General
+    Public License in all respects for all of the code used other than
+    Qt.  If you modify this file, you may extend this exception to
+    your version of the file, but you are not obligated to do so.  If
+    you do not wish to do so, delete this exception statement from
+    your version.
+*/
+
+#include <interfaces/bodypartformatter.h>
+#include <interfaces/bodypart.h>
+#include <interfaces/bodyparturlhandler.h>
+#include <khtmlparthtmlwriter.h>
+
+#include <kmail/callback.h>
+#include <kmail/kmmessage.h>
+
+#include <kglobal.h>
+#include <klocale.h>
+#include <kstringhandler.h>
+#include <kglobalsettings.h>
+#include <kiconloader.h>
+#include <kdebug.h>
+#include <kmessagebox.h>
+#include <kstandarddirs.h>
+#include <kapplication.h>
+
+#include <qurl.h>
+#include <qfile.h>
+#include <qdir.h>
+
+namespace {
+  class Formatter : public KMail::Interface::BodyPartFormatter {
+  public:
+    Result format( KMail::Interface::BodyPart *bodyPart, KMail::HtmlWriter *writer ) const {
+
+      if ( !writer ) return Ok;
+
+      const QString diff = bodyPart->asText();
+      if ( diff.isEmpty() ) return AsIcon;
+
+
+
+      QString addedLineStyle = QString::fromLatin1(
+        "style=\""
+        "padding-left: 4px; "
+        "font-family: monospace; "
+        //"border-right: #000 dashed 1px; "
+        "color: green;\"");
+      QString fileAddStyle( "style=\"font-weight: bold; font-family: monospace; color: green; \"" );
+
+      QString removedLineStyle = QString::fromLatin1(
+        "style=\""
+        "padding-left: 4px; "
+        "font-family: monospace; "
+        "color: red;\"");
+      QString fileRemoveStyle( "style=\"font-weight: bold; font-family: monospace; color: red ;\"" );
+
+      QString tableStyle = QString::fromLatin1(
+        "style=\""
+        "border: dashed 1px; "
+        "margin: 0em;\"");
+
+      QString sepStyle( "style=\"color: black; font-weight: bold; font-family: monospace; \"" );
+      QString chunkStyle( "style=\"color: blue; font-family: monospace;  \"" );
+
+      QString regularStyle( "style=\"font-family: monospace;\"" );
+
+      QString html = "<br><div align=\"center\">";
+      html += "<table " + tableStyle +">";
+
+      QStringList lines = QStringList::split( '\n', diff, true );
+      for ( QStringList::Iterator it = lines.begin(); it != lines.end(); ++it ) {
+        QString line( *it );
+        QString style;
+        if ( line.length() > 0 ) {
+          if ( line.startsWith( "+++" ) ) {
+            style = fileAddStyle;
+          } else if ( line.startsWith( "---" ) ) {
+            style = fileRemoveStyle;
+          } else if ( line.startsWith( "+" ) || line.startsWith( ">" ) ) {
+            style = addedLineStyle;
+          } else if ( line.startsWith( "-" ) || line.startsWith( "<" ) ) {
+            style = removedLineStyle;
+          } else if ( line.startsWith( "==") ) {
+            style = sepStyle;
+          } else if ( line.startsWith( "@@" ) ) {
+            style = chunkStyle;
+          } else {
+            style = regularStyle;
+          }
+        }
+        html += "<tr><td " + style + ">" + line + "</td></tr>";
+      }
+
+      html += "</table></div>";
+      writer->queue( html );
+
+      return Ok;
+    }
+  };
+
+  class Plugin : public KMail::Interface::BodyPartFormatterPlugin {
+  public:
+    const KMail::Interface::BodyPartFormatter * bodyPartFormatter( int idx ) const {
+      return idx == 0 ? new Formatter() : 0 ;
+    }
+    const char * type( int idx ) const {
+      return idx == 0 ? "text" : 0 ;
+    }
+    const char * subtype( int idx ) const {
+      return idx == 0 ? "x-diff" : 0 ;
+    }
+
+    const KMail::Interface::BodyPartURLHandler * urlHandler( int ) const { return 0; }
+  };
+
+}
+
+extern "C"
+KMail::Interface::BodyPartFormatterPlugin *
+libkmail_bodypartformatter_text_xdiff_create_bodypart_formatter_plugin() {
+  return new Plugin();
+}
