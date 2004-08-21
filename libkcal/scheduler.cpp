254a99cb57e0de68ebfb0c8ff81dc0b9eda02d9d
Reinhold Kainhofer <reinhold@kainhofer.com>
2004-08-21 09:40:39 +0000

    Fix the scheduler code to be Incidence* instead of Event*/Todo* based. This also means that a lot of code is cleaned up and easier to understand.
    svn path=/trunk/kdepim/; revision=339477

--- libkcal/scheduler.cpp
+++ libkcal/scheduler.cpp
@@ -49,8 +49,7 @@ QString ScheduleMessage::statusName(ScheduleMessage::Status status)
 {
   switch (status) {
     case PublishUpdate:
-// TODO: Add string when i18n freeze is over.
-//      return i18n("Updated Publish");
+      return i18n("Updated Publish");
     case PublishNew:
       return i18n("Publish");
     case Obsolete:
@@ -120,8 +119,7 @@ bool Scheduler::acceptTransaction(IncidenceBase *incidence,Method method,Schedul
     case Counter:
       return acceptCounter(incidence, status);
     default:
-      deleteTransaction(incidence);
-      return false;
+      break;
   }
   deleteTransaction(incidence);
   return false;
@@ -180,84 +178,69 @@ bool Scheduler::deleteTransaction(IncidenceBase *)
   return true;
 }
 
-bool Scheduler::acceptPublish( IncidenceBase *incidence,
+bool Scheduler::acceptPublish( IncidenceBase *newIncBase,
                                ScheduleMessage::Status status, Method method )
 {
-  if( incidence->type() == "FreeBusy" ) {
-    return acceptFreeBusy( incidence, method );
+  if( newIncBase->type() == "FreeBusy" ) {
+    return acceptFreeBusy( newIncBase, method );
   }
+  
+  bool res = false;
   kdDebug(5800) << "Scheduler::acceptPublish, status="
             << ScheduleMessage::statusName( status ) << endl;
-  Incidence *inc = static_cast<Incidence *>( incidence );
-  Event *even = mCalendar->event( incidence->uid() );
+  Incidence *newInc = static_cast<Incidence *>( newIncBase );
+  Incidence *calInc = mCalendar->incidence( newIncBase->uid() );
   switch ( status ) {
     case ScheduleMessage::Unknown:
     case ScheduleMessage::PublishNew:
     case ScheduleMessage::PublishUpdate:
-      if ( even ) {
-      	if ( even->revision() <= inc->revision() ) {
-	  if ( even->revision() == inc->revision() &&
-	      even->lastModified() > inc->lastModified() ) {
-	    deleteTransaction( incidence );
-	    return false;
-	  }
-	  mCalendar->deleteEvent( even );
-	} else {
-	  deleteTransaction( incidence );
-	  return false;
-	}
+      res = true;
+      if ( calInc ) {
+        if ( (newInc->revision() > calInc->revision()) ||
+             (newInc->revision() == calInc->revision() &&
+               newInc->lastModified() > calInc->lastModified() ) ) {
+          mCalendar->deleteIncidence( calInc );
+        } else
+          res = false;
       }
-      mCalendar->addIncidence( inc );
-      deleteTransaction( incidence );
-      return true;
+      if ( res )
+        mCalendar->addIncidence( newInc );
+      break;
     case ScheduleMessage::Obsolete:
-      return true;
+      res = true;
+      break;
     default:
-      deleteTransaction( incidence );
-      return false;
+      break;
   }
+  deleteTransaction( newIncBase );
+  return res;
 }
 
-bool Scheduler::acceptRequest(IncidenceBase *incidence,ScheduleMessage::Status /* status */)
+bool Scheduler::acceptRequest(IncidenceBase *newIncBase, ScheduleMessage::Status /* status */)
 {
-  Incidence *inc = static_cast<Incidence *>(incidence);
-  if (inc->type()=="FreeBusy") {
+  if (newIncBase->type()=="FreeBusy") {
     // reply to this request is handled in korganizer's incomingdialog
     return true;
-  } else {
-    Event *even = mCalendar->event(incidence->uid());
-    if (even) {
-      if ( even->revision()<=inc->revision() ) {
-        if ( even->revision()==inc->revision() &&
-             even->lastModified()>inc->lastModified()) {
-          deleteTransaction(incidence);
-          return false;
-        }
-        mCalendar->deleteEvent(even);
-      } else {
-        deleteTransaction(incidence);
-        return false;
-      }
-    } else {
-      Todo *todo = mCalendar->todo(incidence->uid());
-      if (todo) {
-        if ( todo->revision()<=inc->revision() ) {
-          if ( todo->revision()==inc->revision() &&
-               todo->lastModified()>inc->lastModified()) {
-            deleteTransaction(incidence);
-            return false;
-          }
-          mCalendar->deleteTodo(todo);
-        } else {
-          deleteTransaction(incidence);
-          return false;
-        }
+  }
+  Incidence *newInc = dynamic_cast<Incidence *>( newIncBase );
+  if ( newInc ) {
+    bool res = true;
+    Incidence *exInc = mCalendar->incidence( newIncBase->uid() );
+    if ( exInc ) {
+      res = false;
+      if ( (newInc->revision() > exInc->revision()) ||
+           (newInc->revision() == exInc->revision() &&
+             newInc->lastModified()>exInc->lastModified()) ) {
+        mCalendar->deleteIncidence( exInc );
+        res = true;
       }
     }
+    if ( res )
+      mCalendar->addIncidence(newInc);
+    deleteTransaction( newIncBase );
+    return res;
   }
-  mCalendar->addIncidence(inc);
-  deleteTransaction(incidence);
-  return true;
+  return false;
 }
 
 bool Scheduler::acceptAdd(IncidenceBase *incidence,ScheduleMessage::Status /* status */)
@@ -269,16 +252,10 @@ bool Scheduler::acceptAdd(IncidenceBase *incidence,ScheduleMessage::Status /* st
 bool Scheduler::acceptCancel(IncidenceBase *incidence,ScheduleMessage::Status /* status */)
 {
   bool ret = false;
-  Event *even = mCalendar->event(incidence->uid());
-  if (even) {
-    mCalendar->deleteEvent(even);
+  Incidence *inc = mCalendar->incidence( incidence->uid() );
+  if ( inc ) {
+    mCalendar->deleteIncidence( inc );
     ret = true;
-  } else {
-    Todo *todo = mCalendar->todo(incidence->uid());
-    if (todo) {
-      mCalendar->deleteTodo(todo);
-      ret = true;
-    }
   }
   deleteTransaction(incidence);
   return ret;
@@ -321,7 +298,7 @@ bool Scheduler::acceptReply(IncidenceBase *incidence,ScheduleMessage::Status /* 
           //update attendee-info
           kdDebug(5800) << "Scheduler::acceptTransaction update attendee" << endl;
           attEv->setStatus(attIn->status());
-	  attEv->setRSVP(false);
+          attEv->setRSVP(false);
           ret = true;
         }
       }
