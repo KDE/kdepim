a12c26f5e57eaad9e672ed15723474d86f248f27
Adriaan de Groot <groot@kde.org>
2004-09-29 09:22:36 +0000

    Bump version _again_, so 3.3.1 BRANCH and HEAD are different
    svn path=/trunk/kdepim/; revision=350203

--- kpilot/lib/options.h
+++ kpilot/lib/options.h
@@ -127,7 +127,7 @@ inline std::ostream& operator <<(std::ostream &o, const QCString &s) { if (s.isE
 #define DEBUGDB         kdDebug(LIBPILOTDB_AREA)
 #endif
 
-#define KPILOT_VERSION	"4.4.5 (Augsburg)"
+#define KPILOT_VERSION	"4.4.6 (Kiel)"
 
 // * KPilot debugging code looks like:
 //
