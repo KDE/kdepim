f5c39f52d17d3c4e08ff9e51c4fa7875a7ef3cd4
David Faure <faure@kde.org>
2004-10-07 11:08:20 +0000

    Backport parsing fix from HEAD (1.6->1.8). However we don't have libemailfunctions in the branch, so for now I'm just duplicating KPIM::getNameAndMail's code. Okayed by Reinhold.
    svn path=/branches/KDE_3_3_BRANCH/kdepim/; revision=352624

--- libkcal/person.cpp
+++ libkcal/person.cpp
@@ -21,26 +21,181 @@
 #include <kdebug.h>
 #include <klocale.h>
 
+
 #include "person.h"
 
 using namespace KCal;
 
+// Copy of KPIM::getNameAndMail from libkdepim/email.cpp, which we can't link to.
+// HEAD has a better solution, with a shared lib.
+static bool getNameAndMail(const QString& aStr, QString& name, QString& mail)
+{
+  name = QString::null;
+  mail = QString::null;
+
+  const int len=aStr.length();
+  const char cQuotes = '"';
+
+  bool bInComment, bInQuotesOutsideOfEmail;
+  int i=0, iAd=0, iMailStart=0, iMailEnd=0;
+  QChar c;
+
+  // Find the '@' of the email address
+  // skipping all '@' inside "(...)" comments:
+  bInComment = false;
+  while( i < len ){
+    c = aStr[i];
+    if( !bInComment ){
+      if( '(' == c ){
+        bInComment = true;
+      }else{
+        if( '@' == c ){
+          iAd = i;
+          break; // found it
+        }
+      }
+    }else{
+      if( ')' == c ){
+        bInComment = false;
+      }
+    }
+    ++i;
+  }
+
+  if( !iAd ){
+    // We suppose the user is typing the string manually and just
+    // has not finished typing the mail address part.
+    // So we take everything that's left of the '<' as name and the rest as mail
+    for( i = 0; len > i; ++i ) {
+      c = aStr[i];
+      if( '<' != c )
+        name.append( c );
+      else
+        break;
+    }
+    mail = aStr.mid( i+1 );
+
+  }else{
+
+    // Loop backwards until we find the start of the string
+    // or a ',' that is outside of a comment
+    //          and outside of quoted text before the leading '<'.
+    bInComment = false;
+    bInQuotesOutsideOfEmail = false;
+    for( i = iAd-1; 0 <= i; --i ) {
+      c = aStr[i];
+      if( bInComment ){
+        if( '(' == c ){
+          if( !name.isEmpty() )
+            name.prepend( ' ' );
+          bInComment = false;
+        }else{
+          name.prepend( c ); // all comment stuff is part of the name
+        }
+      }else if( bInQuotesOutsideOfEmail ){
+        if( cQuotes == c )
+          bInQuotesOutsideOfEmail = false;
+        name.prepend( c );
+      }else{
+        // found the start of this addressee ?
+        if( ',' == c )
+          break;
+        // stuff is before the leading '<' ?
+        if( iMailStart ){
+          if( cQuotes == c )
+            bInQuotesOutsideOfEmail = true; // end of quoted text found
+          name.prepend( c );
+        }else{
+          switch( c ){
+            case '<':
+              iMailStart = i;
+              break;
+            case ')':
+              if( !name.isEmpty() )
+                name.prepend( ' ' );
+              bInComment = true;
+              break;
+            default:
+              if( ' ' != c )
+                mail.prepend( c );
+          }
+        }
+      }
+    }
+
+    name = name.simplifyWhiteSpace();
+    mail = mail.simplifyWhiteSpace();
+
+    if( mail.isEmpty() )
+      return false;
+
+    mail.append('@');
+
+    // Loop forward until we find the end of the string
+    // or a ',' that is outside of a comment
+    //          and outside of quoted text behind the trailing '>'.
+    bInComment = false;
+    bInQuotesOutsideOfEmail = false;
+    for( i = iAd+1; len > i; ++i ) {
+      c = aStr[i];
+      if( bInComment ){
+        if( ')' == c ){
+          if( !name.isEmpty() )
+            name.append( ' ' );
+          bInComment = false;
+        }else{
+          name.append( c ); // all comment stuff is part of the name
+        }
+      }else if( bInQuotesOutsideOfEmail ){
+        if( cQuotes == c )
+          bInQuotesOutsideOfEmail = false;
+        name.append( c );
+      }else{
+        // found the end of this addressee ?
+        if( ',' == c )
+          break;
+        // stuff is behind the trailing '>' ?
+        if( iMailEnd ){
+          if( cQuotes == c )
+            bInQuotesOutsideOfEmail = true; // start of quoted text found
+          name.append( c );
+        }else{
+          switch( c ){
+            case '>':
+              iMailEnd = i;
+              break;
+            case '(':
+              if( !name.isEmpty() )
+                name.append( ' ' );
+              bInComment = true;
+              break;
+            default:
+              if( ' ' != c )
+                mail.append( c );
+          }
+        }
+      }
+    }
+  }
+
+  name = name.simplifyWhiteSpace();
+  mail = mail.simplifyWhiteSpace();
+
+  return ! (name.isEmpty() || mail.isEmpty());
+}
+
 Person::Person( const QString &fullName )
 {
-  // @TODO: This is completely messed. A space at the end messes up everyting! Fix this properly!
-  int emailPos = fullName.find( '<' );
-  if ( emailPos < 0 ) {
-    setEmail(fullName);
-  } else {
-    setEmail(fullName.mid( emailPos + 1, fullName.length() - 1 ));
-    setName(fullName.left( emailPos - 2 ));
-  }
+  QString name, email;
+  getNameAndMail( fullName, name, email );
+  setName( name );
+  setEmail( email );
 }
 
 Person::Person( const QString &name, const QString &email )
 {
-  setName(name);
-  setEmail(email);
+  setName( name );
+  setEmail( email );
 }
 
 
