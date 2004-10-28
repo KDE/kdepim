d123206f3ea30c14f19c99d3c8d3d10a7fcaffe3
David Faure <faure@kde.org>
2004-10-28 20:07:44 +0000

    Cancel LDAP search when cancelling the completion popup
    svn path=/branches/proko2/kdepim/; revision=358606

--- libkdepim/addresseelineedit.h
+++ libkdepim/addresseelineedit.h
@@ -99,6 +99,7 @@ class AddresseeLineEdit : public ClickLineEdit, public DCOPObject
     void slotStartLDAPLookup();
     void slotLDAPSearchData( const KPIM::LdapResultList& );
     void slotEditCompletionOrder();
+    void slotUserCancelled( const QString& );
 
   private:
     void init();
