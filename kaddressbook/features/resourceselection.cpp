cae1d15997d4c32797cc15244599243aadb91ad7
David Faure <faure@kde.org>
2004-10-29 23:20:09 +0000

    Implemented support for showing subresources, and activating/deactivating them. https://intevation.de/roundup/kolab/issue402
    svn path=/branches/proko2/kdepim/; revision=358883

--- kaddressbook/features/resourceselection.cpp
+++ kaddressbook/features/resourceselection.cpp
@@ -1,25 +1,25 @@
-/*                                                                      
-    This file is part of KAddressBook.                                  
-    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>                   
-                                                                        
+/*
+    This file is part of KAddressBook.
+    Copyright (c) 2004 Tobias Koenig <tokoe@kde.org>
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or   
-    (at your option) any later version.                                 
-                                                                        
-    This program is distributed in the hope that it will be useful,     
-    but WITHOUT ANY WARRANTY; without even the implied warranty of      
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the        
-    GNU General Public License for more details.                        
-                                                                        
-    You should have received a copy of the GNU General Public License   
-    along with this program; if not, write to the Free Software         
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           
-                                                                        
-    As a special exception, permission is given to link this program    
-    with any edition of Qt, and distribute the resulting executable,    
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+    As a special exception, permission is given to link this program
+    with any edition of Qt, and distribute the resulting executable,
     without including the source code for Qt in the source distribution.
-*/                                                                      
+*/
 
 #include <qlayout.h>
 #include <qpushbutton.h>
@@ -35,6 +35,7 @@
 #include "core.h"
 
 #include "resourceselection.h"
+#include <libkdepim/resourceabc.h>
 
 class AddressBookWrapper : public KABC::AddressBook
 {
@@ -51,33 +52,85 @@ class ResourceItem : public QCheckListItem
 {
   public:
     ResourceItem( KListView *parent, KABC::Resource *resource )
-      : QCheckListItem( parent, "", CheckBox ),
-        mResource( resource ), mChecked( false )
+      : QCheckListItem( parent, resource->resourceName(), CheckBox ),
+        mResource( resource ), mChecked( false ),
+        mIsSubresource( false ), mSubItemsCreated( false ),
+        mResourceIdentifier()
     {
       setOn( resource->isActive() );
-      mChecked = resource->isActive();
+      mChecked = isOn();
     }
 
-    virtual QString text( int column ) const
+    ResourceItem( KPIM::ResourceABC *resourceABC, ResourceItem* parent,
+                  const QString& resourceIdent )
+      : QCheckListItem( parent, resourceABC->subresourceLabel( resourceIdent ), CheckBox ),
+        mResource( resourceABC ), mChecked( false ),
+        mIsSubresource( true ), mSubItemsCreated( false ),
+        mResourceIdentifier( resourceIdent )
     {
-      if ( column == 0 )
-        return mResource->resourceName();
-      else
-        return QString::null;
+      KPIM::ResourceABC* res = dynamic_cast<KPIM::ResourceABC *>( mResource );
+      setOn( res->subresourceActive( mResourceIdentifier ) );
+      mChecked = isOn();
     }
 
-    void setChecked( bool state )
-    {
-      mChecked = state;
-    }
+    void createSubresourceItems();
 
+    void setChecked( bool state ) { mChecked = state; }
     bool checked() const { return mChecked; }
     KABC::Resource *resource() const { return mResource; }
+    QString resourceIdentifier() const { return mResourceIdentifier; }
+    bool isSubResource() const { return mIsSubresource; }
+
+    virtual void stateChange( bool active );
 
   private:
-    KABC::Resource *mResource;
+    KABC::Resource * const mResource;
     bool mChecked;
+    const bool mIsSubresource;
+    bool mSubItemsCreated;
+    const QString mResourceIdentifier;
 };
+
+// Comes from korganizer/resourceview.cpp
+void ResourceItem::createSubresourceItems()
+{
+  KPIM::ResourceABC* res = dynamic_cast<KPIM::ResourceABC *>( mResource );
+  QStringList subresources;
+  if ( res )
+    subresources = res->subresources();
+  if ( !subresources.isEmpty() ) {
+    setOpen( true );
+    setExpandable( true );
+    // This resource has subresources
+    QStringList::ConstIterator it;
+    for ( it = subresources.begin(); it != subresources.end(); ++it ) {
+      (void)new ResourceItem( res, this, *it );
+    }
+  }
+  mSubItemsCreated = true;
+}
+
+// TODO: connect this to some signalResourceModified
+// void ResourceItem::setGuiState()
+// {
+//   if ( mIsSubresource )
+//     setOn( mResource->subresourceActive( mResourceIdentifier ) );
+//   else
+//     setOn( mResource->isActive() );
+// }
+
+void ResourceItem::stateChange( bool active )
+{
+  //kdDebug(5720) << k_funcinfo << this << " " << text( 0 ) << " active=" << active << endl;
+  if ( active && !mIsSubresource ) {
+    if ( !mSubItemsCreated )
+      createSubresourceItems();
+  }
+
+  setOpen( active && childCount() > 0 );
+}
+
+////
 
 ResourceSelection::ResourceSelection( KAB::Core *core, QWidget *parent, const char *name )
   : KAB::ExtensionWidget( core, parent, name ), mManager( 0 )
@@ -91,7 +144,7 @@ ResourceSelection::ResourceSelection( KAB::Core *core, QWidget *parent, const ch
   connect( mEditButton, SIGNAL( clicked() ), SLOT( edit() ) );
   connect( mRemoveButton, SIGNAL( clicked() ), SLOT( remove() ) );
 
-  connect( mView, SIGNAL( clicked( QListViewItem* ) ),
+  connect( mListView, SIGNAL( clicked( QListViewItem* ) ),
            SLOT( currentChanged( QListViewItem* ) ) );
 
   QTimer::singleShot( 0, this, SLOT( updateView() ) );
@@ -189,35 +242,42 @@ void ResourceSelection::remove()
 
 void ResourceSelection::currentChanged( QListViewItem *item )
 {
-  bool state = (item != 0);
-
+  ResourceItem *resItem = static_cast<ResourceItem*>( item );
+  bool state = (resItem && !resItem->isSubResource() );
   mEditButton->setEnabled( state );
   mRemoveButton->setEnabled( state );
 
-  ResourceItem *resItem = static_cast<ResourceItem*>( item );
   if ( !resItem )
     return;
 
+  //kdDebug(5720) << k_funcinfo << resItem << " " << resItem->text( 0 ) << endl;
   KABC::Resource *resource = resItem->resource();
 
   if ( resItem->checked() != resItem->isOn() ) {
     resItem->setChecked( resItem->isOn() );
-    resource->setActive( resItem->isOn() );
+    kdDebug() << "checked=" << resItem->checked() << " isSubResource=" << resItem->isSubResource() << endl;
+    if ( resItem->isSubResource() ) {
+      KPIM::ResourceABC *res = dynamic_cast<KPIM::ResourceABC *>( resource );
+      res->setSubresourceActive( resItem->resourceIdentifier(), resItem->isOn() );
+      mManager->change( resource );
+    } else {
+      resource->setActive( resItem->isOn() );
+      mManager->change( resource );
 
-    mManager->change( resource );
+      if ( resItem->checked() ) {
+        if ( !resource->addressBook() )
+          resource->setAddressBook( core()->addressBook() );
 
-    if ( resItem->checked() ) {
-      if ( !resource->addressBook() )
-        resource->setAddressBook( core()->addressBook() );
+        if ( !resource->isOpen() )
+          resource->open();
 
-      if ( !resource->isOpen() )
-        resource->open();
-
-      resource->asyncLoad();
+        resource->asyncLoad();
+      }
     }
 
     mLastResource = resource->identifier();
-    updateView();
+    core()->addressBook()->emitAddressBookChanged();
+    //updateView();
   }
 }
 
@@ -226,18 +286,39 @@ void ResourceSelection::updateView()
   if ( !mManager )
     return;
 
-  mView->clear();
+  mListView->clear();
+  disconnect( this, SLOT( slotSubresourceAdded( KPIM::ResourceABC *, const QString &,
+                                                const QString & ) ) );
+  disconnect( this, SLOT( slotSubresourceRemoved( KPIM::ResourceABC *, const QString &,
+                                                  const QString & ) ) );
+  //disconnect( this, SLOT( closeResource( KPIM::ResourceABC * ) ) );
 
   KRES::Manager<KABC::Resource>::Iterator it;
-  for ( it = mManager->begin(); it != mManager->end(); ++it )
-    new ResourceItem( mView, *it );
+  for ( it = mManager->begin(); it != mManager->end(); ++it ) {
 
-  QListViewItemIterator itemIt( mView );
+    new ResourceItem( mListView, *it );
+    KPIM::ResourceABC* resource = dynamic_cast<KPIM::ResourceABC *>( *it );
+    if ( resource ) {
+      connect( resource, SIGNAL( signalSubresourceAdded( KPIM::ResourceABC *,
+                                                         const QString &, const QString & ) ),
+               SLOT( slotSubresourceAdded( KPIM::ResourceABC *,
+                                           const QString &, const QString & ) ) );
+
+      connect( resource, SIGNAL( signalSubresourceRemoved( KPIM::ResourceABC *,
+                                                           const QString &, const QString & ) ),
+               SLOT( slotSubresourceRemoved( KPIM::ResourceABC *,
+                                             const QString &, const QString & ) ) );
+      //connect( resource, SIGNAL( resourceSaved( KPIM::ResourceABC * ) ),
+      //         SLOT( closeResource( KPIM::ResourceABC * ) ) );
+    }
+  }
+
+  QListViewItemIterator itemIt( mListView );
   while ( itemIt.current() ) {
     ResourceItem *item = static_cast<ResourceItem*>( itemIt.current() );
     if ( item->resource()->identifier() == mLastResource ) {
-      mView->setSelected( item, true );
-      mView->ensureItemVisible( item );
+      mListView->setSelected( item, true );
+      mListView->ensureItemVisible( item );
       break;
     }
     ++itemIt;
@@ -246,9 +327,36 @@ void ResourceSelection::updateView()
   core()->addressBook()->emitAddressBookChanged();
 }
 
+
+// Add a new entry
+void ResourceSelection::slotSubresourceAdded( KPIM::ResourceABC *resource,
+                                              const QString& /*type*/,
+                                              const QString& subResource )
+{
+  kdDebug(5720) << k_funcinfo << resource->resourceName() << " " << subResource << endl;
+  QListViewItem *i = mListView->findItem( resource->resourceName(), 0 );
+  if ( !i )
+    // Not found
+    return;
+
+  ResourceItem *item = static_cast<ResourceItem *>( i );
+  (void)new ResourceItem( resource, item, subResource );
+}
+
+// Remove an entry
+void ResourceSelection::slotSubresourceRemoved( KPIM::ResourceABC* resource,
+                                                const QString& /*type*/,
+                                                const QString& subResource )
+{
+  kdDebug(5720) << k_funcinfo << resource->resourceName() << " " << subResource << endl;
+  // TODO
+  //delete findItemByIdentifier( resource );
+  //emitResourcesChanged();
+}
+
 ResourceItem* ResourceSelection::selectedItem() const
 {
-  return static_cast<ResourceItem*>( mView->selectedItem() );
+  return static_cast<ResourceItem*>( mListView->selectedItem() );
 }
 
 void ResourceSelection::initGUI()
@@ -256,10 +364,10 @@ void ResourceSelection::initGUI()
   QGridLayout *layout = new QGridLayout( this, 2, 3, KDialog::marginHint(),
                                          KDialog::spacingHint() );
 
-  mView = new KListView( this );
-  mView->addColumn( i18n( "Address Books" ) );
-  mView->setFullWidth( true );
-  layout->addMultiCellWidget( mView, 0, 0, 0, 2 );
+  mListView = new KListView( this );
+  mListView->addColumn( i18n( "Address Books" ) );
+  mListView->setFullWidth( true );
+  layout->addMultiCellWidget( mListView, 0, 0, 0, 2 );
 
   mAddButton = new QPushButton( i18n( "Add..." ), this );
   mEditButton = new QPushButton( i18n( "Edit..." ), this );
