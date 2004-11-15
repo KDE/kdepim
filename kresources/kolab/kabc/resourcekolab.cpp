285558d7601a49a8c6ff1f9ad3783c51638c8626
Till Adam <adam@kde.org>
2004-11-15 17:02:59 +0000

    Commenting out the processEvents as it leads to infinite loops if: - the startup part is the organizer part - it shows and event which needs to check if one of the attendee is me,   via thatIsMe
    Because that triggers addressbook loading, which loads the first (and only)
    chunk and calls process events which results in an immediate re-execution
    of the same code due to resourceChanged signals firing from the event
    reading from the resource and the nested calls create many dcop objects
    and produce what appears to be and endless loop, because none of the
    loadSubresource calls ever seems to get a chance to return.
    
    svn path=/branches/proko2/kdepim/; revision=363197

--- kresources/kolab/kabc/resourcekolab.cpp
+++ kresources/kolab/kabc/resourcekolab.cpp
@@ -201,7 +201,7 @@ bool KABC::ResourceKolab::loadSubResource( const QString& subResource )
     progress.setProgress( startIndex );
     // Note that this is a case where processEvents is OK - we have a modal dialog,
     // the only thing the user can do is cancel the dialog.
-    qApp->processEvents();
+    //qApp->processEvents();
     if ( progress.wasCanceled() )
       return false;
   }
