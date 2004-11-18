d2ee51ae1ee6a709b28c4da721eca42bc97e71d9
Jakob Schrter <cvsci@camaya.net>
2004-11-18 18:49:19 +0000

    clear searchline when changing group BUG: 93312
    svn path=/trunk/kdepim/; revision=364034

--- knode/knmainwidget.cpp
+++ knode/knmainwidget.cpp
@@ -167,7 +167,7 @@ KNMainWidget::KNMainWidget( KXMLGUIClient* client, bool detachable, QWidget* par
                                         "all messages are shown again." ) );
 
   QLabel *lbl = new QLabel(i18n("&Search:"), q_uicksearch, "kde toolbar widget");
-  QLineEdit *s_earchLineEdit = new KListViewSearchLine(q_uicksearch, h_drView, "KListViewSearchLine");
+  s_earchLineEdit = new KListViewSearchLine(q_uicksearch, h_drView, "KListViewSearchLine");
   q_uicksearch->setStretchableWidget(s_earchLineEdit);
   lbl->setBuddy(s_earchLineEdit);
   connect( resetQuickSearch, SIGNAL( activated() ), s_earchLineEdit, SLOT( clear() ));
@@ -1152,6 +1152,7 @@ void KNMainWidget::slotCollectionSelected(QListViewItem *i)
   KNGroup *selectedGroup=0;
   KNFolder *selectedFolder=0;
 
+  s_earchLineEdit->clear();
   h_drView->clear();
   slotArticleSelected(0);
 
