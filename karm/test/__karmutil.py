67fd73f8f035ac0e13a64971d9d54662df46a77f
Mark Bucciarelli <mark@hubcapconsulting.com>
2004-11-29 03:17:27 +0000

    Add KarmTestError we can distinguish and print full tracebacks for unexpected errors.  Delete exception trapping--let the test scripts do that.
    svn path=/trunk/kdepim/; revision=367066

--- karm/test/__karmutil.py
+++ karm/test/__karmutil.py
@@ -1,24 +1,20 @@
 import sys
 import os
 
+class KarmTestError( Exception ): pass
+
 def dcopid():
   '''Get dcop id of karm.  Fail if more than one instance running.'''
   id = stdin = stdout = None
-  try:
-    ( stdin, stdout ) = os.popen2( "dcop" )
+  ( stdin, stdout ) = os.popen2( "dcop" )
+  l = stdout.readline()
+  while l:
+    if l.startswith( "karm" ):
+      if not id: id = l
+      else: raise KarmTestError( "Only one instance of karm may be running." )
     l = stdout.readline()
-    while l:
-      if l.startswith( "karm" ):
-        if not id: id = l
-        else: raise "Only one instance of karm may be running."
-      l = stdout.readline()
-    if not id:
-      raise "No karm instance found.  Try running dcop at command-line to verify it works."
-  except:
-    if stdin: stdin.close()
-    if stdout: stdout.close()
-    print sys.exc_info()[0]
-    sys.exit(1)
+  if not id:
+    raise KarmTestError( "No karm instance found.  Try running dcop at command-line to verify it works." )
   stdin.close()
   stdout.close()
 
@@ -29,4 +25,4 @@ def test( goal, actual ):
   '''Raise exception if goal != actual.'''
   if goal != actual:
     path, scriptname = os.path.split( sys.argv[0] )
-    raise "%s: expected '%s', got '%s'" % ( scriptname, goal, actual )
+    raise KarmTestError( "%s: expected '%s', got '%s'" % ( scriptname, goal, actual ) )
