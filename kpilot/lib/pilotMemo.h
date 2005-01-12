6ea1fa6f0efb5fc8a0556d58048000070257bfd4
Adriaan de Groot <groot@kde.org>
2005-01-12 11:38:41 +0000

    Warnings--
    svn path=/trunk/kdepim/; revision=377728

--- kpilot/lib/pilotMemo.h
+++ kpilot/lib/pilotMemo.h
@@ -50,7 +50,7 @@ public:
 	QString getTitle(void) const ;
 	PilotRecord* pack() { return PilotAppCategory::pack(); } ;
 
-	static const unsigned int MAX_MEMO_LEN=8192;
+	static const int MAX_MEMO_LEN=8192;
 
 	/**
 	* Return a "short but sensible" title. getTitle() returns the
