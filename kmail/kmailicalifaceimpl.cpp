c016f1375e429726270750d94f9a23bb04d58c2a
Till Adam <adam@kde.org>
2005-07-25 16:00:34 +0000

    Backport of:
    Respect mResourceQuiet, otherwise the resorce produces spurious
    conflicts.
    
    svn path=/branches/KDE/3.4/kdepim/; revision=438634

--- kmail/kmailicalifaceimpl.cpp
+++ kmail/kmailicalifaceimpl.cpp
@@ -848,7 +848,7 @@ void KMailICalIfaceImpl::slotFolderRemoved( KMFolder* folder )
 void KMailICalIfaceImpl::slotIncidenceAdded( KMFolder* folder,
                                              Q_UINT32 sernum )
 {
-  if( !mUseResourceIMAP )
+  if( mResourceQuiet || !mUseResourceIMAP )
     return;
 
 //  kdDebug(5006) << "KMailICalIfaceImpl::slotIncidenceAdded" << endl;
