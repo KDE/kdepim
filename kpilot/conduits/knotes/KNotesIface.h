4e991276429200d867bfa5d1d8b2c3ee613ad3cd
Adriaan de Groot <groot@kde.org>
2005-09-15 08:40:42 +0000

    Meyertests: no __ in include guards
    svn path=/trunk/KDE/kdepim/; revision=460794

--- kpilot/conduits/knotes/KNotesIface.h
+++ kpilot/conduits/knotes/KNotesIface.h
@@ -19,8 +19,8 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 *******************************************************************/
 
-#ifndef __KNotesIface_h__
-#define __KNotesIface_h__
+#ifndef KNotesIface_h
+#define KNotesIface_h
 
 #include <qstring.h>
 #include <qmap.h>
