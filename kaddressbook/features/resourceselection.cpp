80e694bc79c539a248a44d3c0a80d8a51ed8a4dd
Till Adam <adam@kde.org>
2006-02-26 11:51:05 +0000

    Disconnect by sender, rather than by slot, since that actually works.
    svn path=/branches/KDE/3.5/kdepim/; revision=513748

--- kaddressbook/features/resourceselection.cpp
+++ kaddressbook/features/resourceselection.cpp
@@ -292,11 +292,6 @@ void ResourceSelection::updateView()
     return;
 
   mListView->clear();
-  disconnect( this, SLOT( slotSubresourceAdded( KPIM::ResourceABC *, const QString &,
-                                                const QString & ) ) );
-  disconnect( this, SLOT( slotSubresourceRemoved( KPIM::ResourceABC *, const QString &,
-                                                  const QString & ) ) );
-  //disconnect( this, SLOT( closeResource( KPIM::ResourceABC * ) ) );
 
   KRES::Manager<KABC::Resource>::Iterator it;
   for ( it = mManager->begin(); it != mManager->end(); ++it ) {
@@ -304,6 +299,7 @@ void ResourceSelection::updateView()
     new ResourceItem( mListView, *it );
     KPIM::ResourceABC* resource = dynamic_cast<KPIM::ResourceABC *>( *it );
     if ( resource ) {
+      disconnect( resource, 0, this, 0 );
       connect( resource, SIGNAL( signalSubresourceAdded( KPIM::ResourceABC *,
                                                          const QString &, const QString & ) ),
                SLOT( slotSubresourceAdded( KPIM::ResourceABC *,
