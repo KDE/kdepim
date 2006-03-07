90278ebee5dd278bc94b0be608ffe881283ec9d9
Laurent Montel <montel@kde.org>
2006-03-07 10:48:17 +0000

    Add option to link KDEPIM_NEW_DISTRLISTS
    svn path=/trunk/KDE/kdepim/; revision=516467

---
+++ cmake/modules/FindLIBKDEPIM.cmake
@@ -0,0 +1,6 @@
+# Add option for kttsd
+# Laurent Montel <montel@kde.org> 2006
+
+OPTION(KDE4_KDEPIM_NEW_DISTRLISTS   "Whether to use new distribution lists, to store them like normal contacts; useful for Kolab")
+
+
