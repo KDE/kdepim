e6e0e7dbefbddae2d1d5dead2ef6c9a61a2dc48c
Allen Winter <winter@kde.org>
2006-03-12 15:19:47 +0000

    backport 514938 by winterz:
    Fixes.
    
    svn path=/branches/KDE/3.5/kdepim/; revision=517914

--- kode/kxml_compiler/creator.cpp
+++ kode/kxml_compiler/creator.cpp
@@ -110,7 +110,8 @@ void Creator::createElementFunctions( KODE::Class &c, Element *e )
 
   QString name = lowerFirst( e->name );
 
-  if ( e->pattern.oneOrMore || e->pattern.zeroOrMore ) {
+  if ( e->pattern.oneOrMore || e->pattern.zeroOrMore ||
+       e->pattern.choice || e->pattern.optional ) {
     registerListTypedef( type );
 
     c.addHeaderInclude( "qvaluelist.h" );
@@ -754,7 +755,7 @@ void Creator::createFileParserCustom( Element *element )
   stateCode += "if ( c == '>' ) {";
   stateCode += "  state = WHITESPACE;";
   stateCode += "} else if ( foundText" + className + "() ) {";
-  stateCode += "  features = parseElement" + className + "();";
+  stateCode += "  " + element->name + " = parseElement" + className + "();";
   stateCode += "  state = WHITESPACE;";
   stateCode += "}";
 
