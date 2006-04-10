69acc513cfb80ae0569e3be61ef5056e53292b68
Allen Winter <winter@kde.org>
2006-04-10 00:13:40 +0000

    Cleaned-up a bit what Till posted to kde-buildsystem. This works for me and Till, so it must be correct ;>
    svn path=/trunk/KDE/kdepim/; revision=528066

--- libkcal/ical_file.cmake
+++ libkcal/ical_file.cmake
@@ -1,51 +1,50 @@
+# ORDERING OF HEADERS IS SIGNIFICANT. Don't change this ordering. It
+# is required to make the combined header ical.h properly
 set(COMBINEDHEADERSICAL
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalversion.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icaltime.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalduration.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalperiod.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalenums.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icaltypes.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalrecur.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalattach.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalderivedvalue.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalderivedparameter.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalvalue.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalparameter.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalderivedproperty.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalproperty.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/pvl.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalarray.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalcomponent.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icaltimezone.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalparser.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalmemory.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalerror.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalrestriction.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/sspm.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icalmime.h
-   ${CMAKE_SOURCE_DIR}/libical/src/libical/icallangbind.h
-
+   ${TOP}/libkcal/libical/src/libical/icalversion.h
+   ${TOP}/libkcal/libical/src/libical/icaltime.h
+   ${TOP}/libkcal/libical/src/libical/icalduration.h
+   ${TOP}/libkcal/libical/src/libical/icalperiod.h
+   ${TOP}/libkcal/libical/src/libical/icalenums.h
+   ${TOP}/libkcal/libical/src/libical/icaltypes.h
+   ${TOP}/libkcal/libical/src/libical/icalrecur.h
+   ${TOP}/libkcal/libical/src/libical/icalattach.h
+   ${TOP}/libkcal/libical/src/libical/icalderivedvalue.h
+   ${TOP}/libkcal/libical/src/libical/icalderivedparameter.h
+   ${TOP}/libkcal/libical/src/libical/icalvalue.h
+   ${TOP}/libkcal/libical/src/libical/icalparameter.h
+   ${TOP}/libkcal/libical/src/libical/icalderivedproperty.h
+   ${TOP}/libkcal/libical/src/libical/icalproperty.h
+   ${TOP}/libkcal/libical/src/libical/pvl.h
+   ${TOP}/libkcal/libical/src/libical/icalarray.h
+   ${TOP}/libkcal/libical/src/libical/icalcomponent.h
+   ${TOP}/libkcal/libical/src/libical/icaltimezone.h
+   ${TOP}/libkcal/libical/src/libical/icalparser.h
+   ${TOP}/libkcal/libical/src/libical/icalmemory.h
+   ${TOP}/libkcal/libical/src/libical/icalerror.h
+   ${TOP}/libkcal/libical/src/libical/icalrestriction.h
+   ${TOP}/libkcal/libical/src/libical/sspm.h
+   ${TOP}/libkcal/libical/src/libical/icalmime.h
+   ${TOP}/libkcal/libical/src/libical/icallangbind.h
 )
 
-FILE(WRITE ${KDE_FILE_H_FILE} "#ifdef __cplusplus\n")
+FILE(WRITE  ${KDE_FILE_H_FILE} "#ifdef __cplusplus\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "extern \"C\" {\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "#endif\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "/*\n")
 FILE(APPEND ${KDE_FILE_H_FILE} " $Id$\n")
-
 FILE(APPEND ${KDE_FILE_H_FILE} "*/\n")
 
 foreach (_current_FILE ${COMBINEDHEADERSICAL})
-    MESSAGE( ${CMAKE_SOURCE_DIR})
-	FILE(READ ${_current_FILE} _contents)
-    STRING(REGEX REPLACE "#include *\"ical.*\\.h\"" "" _contents "${_contents}" )
-    STRING(REGEX REPLACE "#include *\"config.*\\.h\"" "" _contents "${_contents}" )
-    STRING(REGEX REPLACE "#include *\"pvl\\.h\"" "" _contents "${_contents}" )
-    #STRING(REGEX REPLACE " *\$$[Id|Locker]: .+\$$" "" _contents "${_contents}" )
-	#STRING(REGEX REPLACE " *\$Id *: .+\$$" ""_contents "${_contents}" )
-	#STRING(REGEX REPLACE " *\$Locker *: .+\$$" ""_contents "${_contents}" )
-    FILE(APPEND ${KDE_FILE_H_FILE} "${_contents}")
-endforeach (_current_FILE ${KDE_FILE_HEADER})
+   FILE(APPEND ${KDE_FILE_H_FILE} "${_current_FILE}")
+   FILE(APPEND ${KDE_FILE_H_FILE} "\n")
+   FILE(READ ${_current_FILE} _contents)
+   STRING(REGEX REPLACE "#include *\"ical.*\\.h\"" "" _contents "${_contents}")
+   STRING(REGEX REPLACE "#include *\"config.*\\.h\"" "" _contents "${_contents}")
+   STRING(REGEX REPLACE "#include *\"pvl\\.h\"" "" _contents "${_contents}" )
+   #STRING(REGEX REPLACE " *\$$[Id|Locker]: .+\$$" "" _contents "${_contents}")
+   FILE(APPEND ${KDE_FILE_H_FILE} "${_contents}")
+endforeach (_current_FILE)
 
 FILE(APPEND ${KDE_FILE_H_FILE} "\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "#ifdef __cplusplus\n")
