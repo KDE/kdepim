410b990dff30efd11249f7e242340a3d5476225e
David Faure <faure@kde.org>
2006-04-12 11:39:37 +0000

    Now it works - including reusing the cache after gpgme is found once
    svn path=/trunk/KDE/kdepim/; revision=529005

--- cmake/modules/FindGpgme.cmake
+++ cmake/modules/FindGpgme.cmake
@@ -5,32 +5,38 @@
 #  GPGME_INCLUDE_DIR - the gpgme include directory
 #  GPGME_LIBRARIES - The libraries needed to use gpgme
 
-FIND_PROGRAM(GPGMECONFIG_EXECUTABLE NAMES gpgme-config)
+# if not already in cache
+IF (NOT GPGME_LIBRARIES OR NOT GPGME_INCLUDE_DIR)
+  set(GPGME_FOUND FALSE)
 
-#reset vars
-set(GPGME_LIBRARIES)
-set(GPGME_INCLUDE_DIR)
+  FIND_PROGRAM(GPGMECONFIG_EXECUTABLE NAMES gpgme-config)
 
-# if gpgme-config has been found
-IF (GPGMECONFIG_EXECUTABLE)
+  # if gpgme-config has been found
+  IF (GPGMECONFIG_EXECUTABLE)
+    EXEC_PROGRAM(${GPGMECONFIG_EXECUTABLE} ARGS --libs RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPGME_LIBRARIES)
 
-  EXEC_PROGRAM(${GPGMECONFIG_EXECUTABLE} ARGS --libs RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPGME_LIBRARIES)
+    EXEC_PROGRAM(${GPGMECONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPGME_CFLAGS)
+    IF (GPGME_CFLAGS)
+      # problem: here I get two identical -I directives, and having two paths in INCLUDE_DIR breaks
+      # so for now I just keep the first -Ifoo, but this might need to be fixed at some point...
+      STRING(REGEX REPLACE "-I([^ ]*).*" "\\1" GPGME_INCLUDE_DIR "${GPGME_CFLAGS}")
+    ENDIF (GPGME_CFLAGS)
 
-  EXEC_PROGRAM(${GPGMECONFIG_EXECUTABLE} ARGS --cflags RETURN_VALUE _return_VALUE OUTPUT_VARIABLE GPGME_INCLUDE_DIR)
+    # ensure that they are cached
+    set(GPGME_INCLUDE_DIR ${GPGME_INCLUDE_DIR} CACHE INTERNAL "The gpgme include path")
+    set(GPGME_LIBRARIES ${GPGME_LIBRARIES} CACHE INTERNAL "The gpgme libraries")
+
+  ENDIF (GPGMECONFIG_EXECUTABLE)
 
   IF (GPGME_LIBRARIES AND GPGME_INCLUDE_DIR)
-    SET(GPGME_FOUND TRUE)
-    STRING(REGEX REPLACE "-I(.+)" "\\1" GPGME_INCLUDE_DIR "${GPGME_INCLUDE_DIR}")
-    message(STATUS "Found gpgme: ${GPGME_LIBRARIES}")
+      set(GPGME_FOUND TRUE)
+      #message(STATUS "Found gpgme: includes: ${GPGME_INCLUDE_DIR} libs: ${GPGME_LIBRARIES}")
+      message(STATUS "Found gpgme using ${GPGMECONFIG_EXECUTABLE}.")
   ENDIF (GPGME_LIBRARIES AND GPGME_INCLUDE_DIR)
 
   IF (NOT GPGME_FOUND)
-    message(FATAL_ERROR "You are missing gpgme 0.4.5 or higher.
-  Download gpgme >= 0.4.5 from ftp://ftp.gnupg.org/gcrypt/alpha/gpgme
-  or use the --with-gpgme-prefix=/path/where/gpgme/is/installed option.")
-
+      message(FATAL_ERROR "You are missing gpgme 0.4.5 or higher.
+    Download gpgme >= 0.4.5 from ftp://ftp.gnupg.org/gcrypt/alpha/gpgme
+    or use the --with-gpgme-prefix=/path/where/gpgme/is/installed option.")
   ENDIF (NOT GPGME_FOUND)
-
-  MARK_AS_ADVANCED(GPGME_INCLUDE_DIR GPGME_LIBRARIES)
-
-ENDIF (GPGMECONFIG_EXECUTABLE)
+ENDIF (NOT GPGME_LIBRARIES OR NOT GPGME_INCLUDE_DIR)
