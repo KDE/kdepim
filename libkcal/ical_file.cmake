92455fada7a9d4657c2205b83378a09cb7b87aa1
Volker Krause <vkrause@kde.org>
2006-04-13 16:34:50 +0000

    Don't create files in the source dir.
    svn path=/trunk/KDE/kdepim/; revision=529515

--- libkcal/ical_file.cmake
+++ libkcal/ical_file.cmake
@@ -1,31 +1,31 @@
 # ORDERING OF HEADERS IS SIGNIFICANT. Don't change this ordering.
 # It is required to make the combined header ical.h properly.
 set(COMBINEDHEADERSICAL
-   ${TOP}/libkcal/libical/src/libical/icalversion.h
-   ${TOP}/libkcal/libical/src/libical/icaltime.h
-   ${TOP}/libkcal/libical/src/libical/icalduration.h
-   ${TOP}/libkcal/libical/src/libical/icalperiod.h
-   ${TOP}/libkcal/libical/src/libical/icalenums.h
-   ${TOP}/libkcal/libical/src/libical/icaltypes.h
-   ${TOP}/libkcal/libical/src/libical/icalrecur.h
-   ${TOP}/libkcal/libical/src/libical/icalattach.h
-   ${TOP}/libkcal/libical/src/libical/icalderivedvalue.h
-   ${TOP}/libkcal/libical/src/libical/icalderivedparameter.h
-   ${TOP}/libkcal/libical/src/libical/icalvalue.h
-   ${TOP}/libkcal/libical/src/libical/icalparameter.h
-   ${TOP}/libkcal/libical/src/libical/icalderivedproperty.h
-   ${TOP}/libkcal/libical/src/libical/icalproperty.h
-   ${TOP}/libkcal/libical/src/libical/pvl.h
-   ${TOP}/libkcal/libical/src/libical/icalarray.h
-   ${TOP}/libkcal/libical/src/libical/icalcomponent.h
-   ${TOP}/libkcal/libical/src/libical/icaltimezone.h
-   ${TOP}/libkcal/libical/src/libical/icalparser.h
-   ${TOP}/libkcal/libical/src/libical/icalmemory.h
-   ${TOP}/libkcal/libical/src/libical/icalerror.h
-   ${TOP}/libkcal/libical/src/libical/icalrestriction.h
-   ${TOP}/libkcal/libical/src/libical/sspm.h
-   ${TOP}/libkcal/libical/src/libical/icalmime.h
-   ${TOP}/libkcal/libical/src/libical/icallangbind.h
+   ${TOPS}/libkcal/libical/src/libical/icalversion.h
+   ${TOPS}/libkcal/libical/src/libical/icaltime.h
+   ${TOPS}/libkcal/libical/src/libical/icalduration.h
+   ${TOPS}/libkcal/libical/src/libical/icalperiod.h
+   ${TOPS}/libkcal/libical/src/libical/icalenums.h
+   ${TOPS}/libkcal/libical/src/libical/icaltypes.h
+   ${TOPS}/libkcal/libical/src/libical/icalrecur.h
+   ${TOPS}/libkcal/libical/src/libical/icalattach.h
+   ${TOPB}/libkcal/libical/src/libical/icalderivedvalue.h
+   ${TOPB}/libkcal/libical/src/libical/icalderivedparameter.h
+   ${TOPS}/libkcal/libical/src/libical/icalvalue.h
+   ${TOPS}/libkcal/libical/src/libical/icalparameter.h
+   ${TOPB}/libkcal/libical/src/libical/icalderivedproperty.h
+   ${TOPS}/libkcal/libical/src/libical/icalproperty.h
+   ${TOPS}/libkcal/libical/src/libical/pvl.h
+   ${TOPS}/libkcal/libical/src/libical/icalarray.h
+   ${TOPS}/libkcal/libical/src/libical/icalcomponent.h
+   ${TOPS}/libkcal/libical/src/libical/icaltimezone.h
+   ${TOPS}/libkcal/libical/src/libical/icalparser.h
+   ${TOPS}/libkcal/libical/src/libical/icalmemory.h
+   ${TOPS}/libkcal/libical/src/libical/icalerror.h
+   ${TOPS}/libkcal/libical/src/libical/icalrestriction.h
+   ${TOPS}/libkcal/libical/src/libical/sspm.h
+   ${TOPS}/libkcal/libical/src/libical/icalmime.h
+   ${TOPS}/libkcal/libical/src/libical/icallangbind.h
 )
 
 FILE(WRITE  ${KDE_FILE_H_FILE} "#ifdef __cplusplus\n")
