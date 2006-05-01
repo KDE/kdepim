6d3b3a21fe5216566ecc316ed7e16b5dbaff768c
Allen Winter <winter@kde.org>
2006-05-01 17:21:53 +0000

    forward port SVN commit 536225 by tilladam:
    Remove a call to processEvents, since now that sizeHint() is actually called
    (since r505888 made it const) it becomes aparent that that causes recursive
    size adjustments which QCanvas does not take kindly to. Fixes reports of
    slowness when creating new events in the calendar view.
    
    
    svn path=/trunk/KDE/kdepim/; revision=536243

--- kdgantt/KDGanttView.cpp
+++ kdgantt/KDGanttView.cpp
@@ -638,7 +638,12 @@ QSize KDGanttView::sizeHint() const
   bool block = myTimeTable->blockUpdating();
   myTimeTable->setBlockUpdating( false );
   myTimeTable->updateMyContent();
-  qApp->processEvents();
+  /* The below causes recursive calls to various size updating methods, which
+   * cause QCanvas to hide and show items like mad, which is very slow. If
+   * there is a legitimate gui updating issue here somewhere, it will need
+   * to be solved differently.
+   */
+  //qApp->processEvents();
   int hintHeight = myTimeHeader->height();
   int legendHeight = 0;
   if ( showLegendButton() )
