3e5d3e8adc95eb798d9ffd8d3105076f69cc0a88
Dirk Mueller <mueller@kde.org>
2006-05-03 17:55:00 +0000

    fix sequence error that caused the imap slave to endlessly eat memory when a mail with a > 71 char content disposition header was sent to a user. CCMAIL: security@kde.org
    svn path=/branches/KDE/3.5/kdepim/; revision=537001

--- kioslaves/imap4/imapparser.cc
+++ kioslaves/imap4/imapparser.cc
@@ -949,7 +949,9 @@ QAsciiDict < QString > imapParser::parseParameters (parseString & inWords)
 
     while (!inWords.isEmpty () && inWords[0] != ')')
     {
-      retVal.insert (parseLiteralC(inWords), new QString(parseLiteralC(inWords)));
+      QCString l1 = parseLiteralC(inWords);
+      QCString l2 = parseLiteralC(inWords);
+      retVal.insert (l1, new QString(l2));
     }
 
     if (inWords[0] != ')')
