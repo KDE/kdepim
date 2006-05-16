1a6e23d0264dcb887129dc370eb077dfff4bc7be
Ismail Onur Filiz <onurf@su.sabanciuniv.edu>
2006-05-16 06:04:00 +0000

    Backport the following coverity fixes:
    541203: fix crash (CID 2111)
    539126: looks like a clear bug, but my guess KDE never calls that function (CID 1539)
    538899: catch invalid data (CID 1371)
    538897: avoid crash (CID 1373)
    538545: it can return 0
    538539: don't crash (CID 1340)
    
    
    svn path=/branches/KDE/3.5/kdepim/; revision=541381

--- libkcal/libical/src/libical/sspm.c
+++ libkcal/libical/src/libical/sspm.c
@@ -215,8 +215,10 @@ char* sspm_value(char* line)
     char *c,*s, *p;
 
     /* Find the first colon and the next semicolon */
-
+    value[0] = 0;
     c = strchr(line,':');
+    if (!c)
+        return value;
     s = strchr(c,';');
 
     /* Skip the colon */
