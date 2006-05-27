c6ce54cde5ffdb3ffcda593f5c8200b6fc4dd2c4
Thorsten Roeder <thorsten.roeder@weihenstephan.org>
2006-05-27 13:16:10 +0000

    add check for boost libs
    svn path=/trunk/KDE/kdepim/; revision=545406

---
+++ cmake/modules/FindBoost.cmake
@@ -0,0 +1,36 @@
+# - Try to find the boost directory library
+# Once done this will define
+#
+#  BOOST_FOUND - system has BOOST
+#  BOOST_INCLUDE_DIR - the BOOST include directory
+#  BOOST_LIBRARIES - The libraries needed to use BOOST
+
+if (BOOST_INCLUDE_DIR)
+  # Already in cache, be silent
+  set(BOOST_FIND_QUIETLY TRUE)
+endif (BOOST_INCLUDE_DIR)
+
+FIND_PATH(BOOST_INCLUDE_DIR boost/format.hpp
+   /usr/include
+   /usr/local/include
+)
+
+FIND_LIBRARY(BOOST_LIBRARIES NAMES boost_program_options boost_iostreams
+   PATHS
+   /usr/lib
+   /usr/local/lib
+)
+
+
+if (BOOST_INCLUDE_DIR AND BOOST_LIBRARIES)
+   set(BOOST_FOUND TRUE)
+endif (BOOST_INCLUDE_DIR AND BOOST_LIBRARIES)
+
+
+if (BOOST_FOUND)
+   if (NOT BOOST_FIND_QUIETLY)
+      message(STATUS "Found boost: ${BOOST_LIBRARIES}")
+   endif (NOT BOOST_FIND_QUIETLY)
+endif (BOOST_FOUND)
+
+MARK_AS_ADVANCED(BOOST_INCLUDE_DIR BOOST_LIBRARIES)
