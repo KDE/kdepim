3ede9ad4893545b08e8e2ac1c6fea150d20c1ff8
Aron Bostrm <c02ab@efd.lth.se>
2006-06-24 21:38:14 +0000

    SoC It works! From this point, the road ahead seem to be straight (but long). It compiles, it produces the wanted result, and it's ****-ugly.
    svn path=/trunk/KDE/kdepim/; revision=554705

--- views/conversationview/conversationdelegate.cpp
+++ views/conversationview/conversationdelegate.cpp
@@ -34,20 +34,20 @@ ConversationDelegate::ConversationDelegate(QObject *parent) : QAbstractItemDeleg
 void ConversationDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
    painter->setRenderHint(QPainter::Antialiasing);
-   painter->setPen(Qt::NoPen);
+//   painter->setPen(Qt::NoPen);
 
    if (option.state & QStyle::State_Selected)
        painter->setBrush(option.palette.highlight());
    else
-       painter->setBrush(QBrush(Qt::white));
+       painter->setBrush(Qt::white);
    painter->drawRect(option.rect);
 
    if (option.state & QStyle::State_Selected)
        painter->setBrush(option.palette.highlightedText());
    else
-       painter->setBrush(QBrush(Qt::black));
-   painter->drawText(0, 0, "Authors");   
-   painter->drawText(100, 20, "Subject");   
+       painter->setBrush(Qt::black);
+   painter->drawText(5, index.row()*20+18, "Authors");   
+   painter->drawText(85, index.row()*20+18, "Subject");   
 
 }
 
