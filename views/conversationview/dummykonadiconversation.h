f57b9481ee75a200be84f649d859655d99e34d63
Aron Bostrm <c02ab@efd.lth.se>
2006-06-24 16:42:56 +0000

    SoC Added licence headers
    svn path=/trunk/KDE/kdepim/; revision=554646

--- views/conversationview/dummykonadiconversation.h
+++ views/conversationview/dummykonadiconversation.h
@@ -1,7 +1,19 @@
 /*
- * dummykonadiconversation.h
- * DummyKonadiConversation -- a dummy mockup of what a conversation should look like in my adapter for aKonadi
- * Oh, and GPL, LGPL, BSD, QPL or whatever OSI approved license you prefer
+ * copyright (c) Aron Boström <Aron.Bostrom at gmail.com>, 2006 
+ *
+ * this library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #ifndef DUMMYKONADICONVERSATION_H
