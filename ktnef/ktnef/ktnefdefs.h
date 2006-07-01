ebb5360ed7c61e7c503f79eee911d34af04fc01b
Allen Winter <winter@kde.org>
2006-07-01 16:03:01 +0000

    Change license from GPL to LGPL. Permission granted by the copyright holder Michael Goffioul <kdeprint AT swing DOT be> as shown by the following email correspondence:
    On Friday 30 June 2006 01:17, Allen Winter wrote:
    Michael, As you may be aware, we are attempting to move all the libraries we
    can from out of the kdepim module and into a new kdepimlibs module. I am
    currently attempting to move libkcal into kdepimlibs.However, because libkcal
    depends on your ktnef code, I will need to move ktnef into kdepimlibs as well.
    Here's the problem: all the code in kdepimlibs must be licensed as LGPL, BSD,
    or X11.  And ktnef is licensed as GPL.The question: would you be willing to
    re-license ktnef from GPL to LGPL?This would need to be done in SVN kdepim
    trunk only. Then I would do the moveinto kdepimlibs. Please let me know what
    you think about this as soon as you get a chance."
    
    On Friday 30 June 2006 05:20, Michael Goffioul wrote:
    Fine for me. Please go ahead.
    
    Bye.
    Michael.
    CCMAIL: kdeprint@swing.be
    
    svn path=/trunk/KDE/kdepim/; revision=556835
    (This rewritten git commit is part of the ktnef restore from old history)

--- ktnef/ktnef/ktnefdefs.h
+++ ktnef/ktnef/ktnefdefs.h
@@ -1,3 +1,26 @@
+/*
+    ktnefdefs.h
+
+    Copyright (C) 2002 Michael Goffioul <kdeprint@swing.be>
+
+    This file is part of KTNEF, the KDE TNEF support library/program.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+ */
+
 #ifndef KTNEFDEFS_H
 #define KTNEFDEFS_H
 
@@ -128,7 +151,7 @@
 #define MAPI_TAG_CONTACT_OTHERADDRESS              "0x801C"    // OtherAddress
 #define MAPI_TAG_CONTACT_SELECTEDADDRESS           "0x8022"    // SelectedMailingAddress:
                                                                // 0 = None
-                                                               // 1 = Home                                                    
+                                                               // 1 = Home
                                                                // 2 = Business
                                                                // 3 = Other
 
