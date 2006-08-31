9e3f3d981a2fdeb80be4068f3d17800d9d77e682
Laurent Montel <montel@kde.org>
2006-08-31 13:54:49 +0000

    Compile kaddressbook_gnokii
    svn path=/trunk/KDE/kdepim/; revision=579227

---
+++ cmake/modules/FindGNOKII.cmake
@@ -0,0 +1,36 @@
+# - Try to find the gnokii directory library
+# Once done this will define
+#
+#  GNOKII_FOUND - system has GNOKII
+#  GNOKII_INCLUDE_DIR - the GNOKII include directory
+#  GNOKII_LIBRARIES - The libraries needed to use GNOKII
+
+if (GNOKII_INCLUDE_DIR)
+  # Already in cache, be silent
+  set(GNOKII_FIND_QUIETLY TRUE)
+endif (GNOKII_INCLUDE_DIR)
+
+FIND_PATH(GNOKII_INCLUDE_DIR gnokii.h
+   /usr/include
+   /usr/local/include
+)
+
+FIND_LIBRARY(GNOKII_LIBRARIES NAMES gnokii
+   PATHS
+   /usr/lib
+   /usr/local/lib
+)
+
+
+if (GNOKII_INCLUDE_DIR AND GNOKII_LIBRARIES)
+   set(GNOKII_FOUND TRUE)
+endif (GNOKII_INCLUDE_DIR AND GNOKII_LIBRARIES)
+
+
+if (GNOKII_FOUND)
+   if (NOT GNOKII_FIND_QUIETLY)
+      message(STATUS "Found gnokii: ${GNOKII_LIBRARIES}")
+   endif (NOT GNOKII_FIND_QUIETLY)
+endif (GNOKII_FOUND)
+
+MARK_AS_ADVANCED(GNOKII_INCLUDE_DIR GNOKII_LIBRARIES)
