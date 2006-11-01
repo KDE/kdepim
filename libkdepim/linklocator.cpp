f9ca28e97bfc6b4509afba54e71dfe7ba82c6165
Allen Winter <winter@kde.org>
2006-11-01 15:32:45 +0000

    merge SVN commit 600917 by marten:
    Correctly detect a URL that ends with '-' or '_'
    
    CCBUGS: 81281
    
    
    svn path=/branches/KDE/3.5/kdepim/; revision=600981

--- libkdepim/linklocator.cpp
+++ libkdepim/linklocator.cpp
@@ -96,8 +96,8 @@ QString LinkLocator::getUrl()
     {
       ++mPos;
     }
-    /* some URLs really end with:  # / &     */
-    const QString allowedSpecialChars = QString("#/&");
+    /* some URLs really end with:  # / & - _    */
+    const QString allowedSpecialChars = QString("#/&-_");
     while(mPos > start && mText[mPos-1].isPunct() &&
 		    allowedSpecialChars.find(mText[mPos-1]) == -1 )
     {
