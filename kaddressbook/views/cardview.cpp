0c3a49c33f5023bc47c08bbb1b40b3551020afe7
Allen Winter <winter@kde.org>
2007-02-06 00:11:03 +0000

    forward port SVN commit 630087 by tokoe:
    Don't deselect all previous selected items on a RMB click, otherwise it's
    impossible to send all selected contacts via mail or delete them at once.
    
    svn path=/trunk/KDE/kdepim/; revision=630686

--- kaddressbook/views/cardview.cpp
+++ kaddressbook/views/cardview.cpp
@@ -1035,12 +1035,6 @@ void CardView::contentsMousePressEvent( QMouseEvent *e )
 
   // The RMB click
   if ( e->button() & Qt::RightButton ) {
-    // clear previous selection
-    bool blocked = signalsBlocked();
-    blockSignals( true );
-    selectAll( false );
-    blockSignals( blocked );
-
     // select current item
     item->setSelected( true );
 
