c44803352031e5590bac52f961ce40a424b22eca
Pradeepto Bhattacharya <pradeepto@kde.org>
2007-05-24 06:50:53 +0000

    Merge ... SVN commit 642196 by mueller:
    warnings--
    
    SVN commit 642195 by mueller:
    
    minor
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=667853

--- libkdepim/addresseelineedit.cpp
+++ libkdepim/addresseelineedit.cpp
@@ -130,7 +130,7 @@ void AddresseeLineEdit::init()
 
   if ( m_useCompletion ) {
     if ( !s_LDAPTimer ) {
-      ldapTimerDeleter.setObject( s_LDAPTimer, new QTimer );
+      ldapTimerDeleter.setObject( s_LDAPTimer, new QTimer( 0, "ldapTimerDeleter" ) );
       ldapSearchDeleter.setObject( s_LDAPSearch, new KPIM::LdapSearch );
       ldapTextDeleter.setObject( s_LDAPText, new QString );
 
