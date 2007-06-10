a242c0b52e3dc3e85c48bcb72b20d2f87d607e09
Jason vanRijn Kasper <vr@movingparts.net>
2007-06-10 05:57:59 +0000

    bumping version for next release, should there be one
    svn path=/branches/KDE/3.5/kdepim/; revision=673392

--- kpilot/lib/options.h
+++ kpilot/lib/options.h
@@ -140,7 +140,7 @@ inline std::ostream& operator <<(std::ostream &o, const KPilotDepthCount &d)
 #define FUNCTIONSETUPL(a) const int fname = a; Q_UNUSED(fname);
 #endif
 
-#define KPILOT_VERSION	"4.9.1 (rabid dolphin)"
+#define KPILOT_VERSION	"4.9.2-pre1 (moribund alleycat)"
 
 
 // Function to expand newlines in rich text to <br>\n
