faab5e2303546a44f9d087d2edaf657738104101
Marco Gulino <marco.gulino@gmail.com>
2007-06-20 12:33:28 +0000

    Fixing messages generation
    svn path=/trunk/KDE/kdepim/; revision=678014

--- kmobiletools/kmobiletools/Messages.sh
+++ kmobiletools/kmobiletools/Messages.sh
@@ -1,11 +1,13 @@
 #$PREPARETIPS > tips.cpp
 
 ABOUT_RCS="`find about -name \"*.ui\" -o -name \"*.rc\" -o -name \"*.kcfg\"`"
-LIBKMOBILETOOLS_RCS="`find engines\libkmobiletools -name \"*.ui\" -o -name \"*.rc\" -o -name \"*.kcfg\"`"
+LIBKMOBILETOOLS_RCS="`find libkmobiletools -name \"*.ui\" -o -name \"*.rc\" -o -name \"*.kcfg\"`"
+LIBKMOBILETOOLSENGINEUI_RCS="`find engines/libkmobiletoolsengineui -name \"*.ui\" -o -name \"*.rc\" -o -name \"*.kcfg\"`"
 MAINPART_RCS="`find mainpart -name \"*.ui\" -o -name \"*.rc\" -o -name \"*.kcfg\"`"
 
 ABOUT_SRCS="`find about -name \"*.cpp\" -o -name \"*.h\"`"
-LIBKMOBILETOOLS_SRCS="`find engines\libkmobiletools -name \"*.cpp\" -o -name \"*.h\"`"
+LIBKMOBILETOOLS_SRCS="`find libkmobiletools -name \"*.cpp\" -o -name \"*.h\"`"
+LIBKMOBILETOOLSENGINEUI_SRCS="`find engines/libkmobiletoolsengineui -name \"*.cpp\" -o -name \"*.h\"`"
 MAINPART_SRCS="`find mainpart -name \"*.cpp\" -o -name \"*.h\"`"
 
 KMOBILETOOLS_RCS="$ABOUT_RCS $LIBKMOBILETOOLS_RCS $LIBKMOBILETOOLSENGINEUI_RCS $MAINPART_RCS"
