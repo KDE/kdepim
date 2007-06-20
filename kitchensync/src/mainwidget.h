5b27d302c2d8a9f3bc94a1b2f4ef913bf5d2273f
Allen Winter <winter@kde.org>
2007-06-20 15:36:48 +0000

    KDE_EXPORT -> KITCHENSYNC_EXPORT
    svn path=/trunk/KDE/kdepim/; revision=678058

--- kitchensync/src/mainwidget.h
+++ kitchensync/src/mainwidget.h
@@ -1,28 +1,27 @@
 /*
-    This file is part of KitchenSync.
+  This file is part of KitchenSync.
 
-    Copyright (c) 2005 Tobias Koenig <tokoe@kde.org>
+  Copyright (c) 2005 Tobias Koenig <tokoe@kde.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 #ifndef MAINWIDGET_H
 #define MAINWIDGET_H
 
+#include "kitchensync_export.h"
 #include <libqopensync/engine.h>
-#include <kdemacros.h>
-
 #include <QtGui/QWidget>
 
 class KAboutData;
@@ -34,9 +33,9 @@ class SyncProcess;
 namespace QSync {
 }
 
-class KDE_EXPORT MainWidget : public QWidget
+class KITCHENSYNC_EXPORT MainWidget : public QWidget
 {
-    Q_OBJECT
+  Q_OBJECT
   public:
     explicit MainWidget( KXMLGUIClient *guiClient, QWidget *widget = 0 );
     ~MainWidget();
