2c63b91581c0aeaf0fd88c0bc6c7cdff71713fd4
Allen Winter <winter@kde.org>
2007-06-25 14:43:11 +0000

    Relicense from GPL to LGPL. Permission granted by Marc Mutz and KDAB.
    CCMAIL: mutz@kde.org
    
    svn path=/trunk/KDE/kdepim/; revision=680150

--- qgpgme/eventloopinteractor.cpp
+++ qgpgme/eventloopinteractor.cpp
@@ -2,20 +2,21 @@
    Copyright (C) 2003 Klarälvdalens Datakonsult AB
 
    This file is part of QGPGME.
- 
+
    QGPGME is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
- 
+
    QGPGME is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with QGPGME; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  */
+   You should have received a copy of the GNU Library General Public License
+   along with QGPGME; see the file COPYING.LIB.  If not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA. */
 
 // -*- c++ -*-
 
@@ -63,7 +64,7 @@ QGpgME::EventLoopInteractor * QGpgME::EventLoopInteractor::instance() {
 }
 
 void * QGpgME::EventLoopInteractor::registerWatcher( int fd, Direction dir, bool & ok ) {
-  QSocketNotifier * sn = new QSocketNotifier( fd, 
+  QSocketNotifier * sn = new QSocketNotifier( fd,
       dir == Read ? QSocketNotifier::Read : QSocketNotifier::Write );
   if ( dir == Read )
     connect( sn, SIGNAL(activated(int)), SLOT(slotReadActivity(int)) );
--- qgpgme/eventloopinteractor.h
+++ qgpgme/eventloopinteractor.h
@@ -2,20 +2,21 @@
    Copyright (C) 2003 Klarälvdalens Datakonsult AB
 
    This file is part of QGPGME.
- 
+
    QGPGME is free software; you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
- 
+
    QGPGME is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Library General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with QGPGME; if not, write to the Free Software Foundation,
-   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.  */
+   You should have received a copy of the GNU Library General Public License
+   along with QGPGME; see the file COPYING.LIB.  If not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA. */
 
 // -*- c++ -*-
 #ifndef __QGPGME_EVENTLOOPINTERACTOR_H__
@@ -32,7 +33,7 @@ namespace GpgME {
   class TrustItem;
   class Key;
 } // namespace GpgME
-  
+
 namespace QGpgME {
 
   class QGPGME_EXPORT EventLoopInteractor : public QObject, public GpgME::EventLoopInteractor {
@@ -54,7 +55,7 @@ namespace QGpgME {
   protected Q_SLOTS:
     void slotWriteActivity( int socket );
     void slotReadActivity( int socket );
-    
+
   protected:
     //
     // IO Notification Interface
