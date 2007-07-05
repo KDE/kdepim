0bb5580e9ccb2177efa097e3bf5c05fb670e3a0b
smail Dnmez <ismail@kde.org>
2007-07-05 23:44:27 +0000

    errors from the operating system should be filtered through QString::fromLocal8Bit()
    svn path=/branches/KDE/3.5/kdepim/; revision=684042

--- certmanager/lib/backends/qgpgme/qgpgmecryptoconfig.cpp
+++ certmanager/lib/backends/qgpgme/qgpgmecryptoconfig.cpp
@@ -306,7 +306,7 @@ void QGpgMECryptoConfigComponent::sync( bool runtime )
   }
   else if( rc != 0 ) // Happens due to bugs in gpgconf (e.g. issues 104/115)
   {
-    QString wmsg = i18n( "Error from gpgconf while saving configuration: %1" ).arg( strerror( rc ) );
+    QString wmsg = i18n( "Error from gpgconf while saving configuration: %1" ).arg( QString::fromLocal8Bit( strerror( rc ) ) );
     kdWarning(5150) << k_funcinfo << ":" << strerror( rc ) << endl;
     KMessageBox::error(0, wmsg);
   }
