db78131c99483ca8c4c1f5e380ba4573ba8404f0
Jason vanRijn Kasper <vr@movingparts.net>
2007-07-30 00:03:49 +0000

    - fixing unsigned int to int comparison warnings
    svn path=/trunk/KDE/kdepim/; revision=694079

--- kpilot/lib/pilotLocalDatabase.cc
+++ kpilot/lib/pilotLocalDatabase.cc
@@ -62,7 +62,7 @@ public:
 
 	void deleteRecords()
 	{
-		for (unsigned int i=0; i<size(); i++)
+		for (int i=0; i<size(); i++)
 		{
 			delete at(i);
 		}
@@ -76,7 +76,7 @@ public:
 		pending = -1;
 	}
 
-	unsigned int current;
+	int current;
 	int pending;
 } ;
 
@@ -300,7 +300,7 @@ PilotRecord *PilotLocalDatabase::readRecordById(recordid_t id)
 
 	d->pending = -1;
 
-	for (unsigned int i = 0; i < d->size(); i++)
+	for (int i = 0; i < d->size(); i++)
 	{
 		if ((*d)[i]->id() == id)
 		{
@@ -472,7 +472,7 @@ recordid_t PilotLocalDatabase::writeRecord(PilotRecord * newRecord)
 	// First check to see if we have this record:
   	if (newRecord->id() != 0)
   	{
- 		for (unsigned int i = 0; i < d->size(); i++)
+ 		for (int i = 0; i < d->size(); i++)
  			if ((*d)[i]->id() == newRecord->id())
   			{
  				delete (*d)[i];
@@ -534,7 +534,7 @@ int PilotLocalDatabase::resetSyncFlags()
 		return -1;
 	}
 	d->pending = -1;
-	for (unsigned int i = 0; i < d->size(); i++)
+	for (int i = 0; i < d->size(); i++)
 	{
 		(*d)[i]->setModified( false );
 	}
@@ -673,7 +673,7 @@ void PilotLocalDatabase::closeDatabase()
 	dbFile = pi_file_create(QFile::encodeName(newName),&fDBInfo);
 	pi_file_set_app_info(dbFile, fAppInfo, fAppLen);
 
-	for (unsigned int i = 0; i < d->size(); i++)
+	for (int i = 0; i < d->size(); i++)
 	{
 		// How did a NULL pointer sneak in here?
 		if (!(*d)[i])
