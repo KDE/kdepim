303f6c3f6b4d805ba22e80c19fc06093da8e2173
Marc Mutz <mutz@kde.org>
2007-08-03 13:50:14 +0000

    UTF-8 encode
    svn path=/trunk/KDE/kdepim/kleopatra/; revision=695997

--- kleopatra/utils/pimpl_ptr.h
+++ kleopatra/utils/pimpl_ptr.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-** Copyright (C) 2001-2007 Klar‰lvdalens Datakonsult AB.  All rights reserved.
+** Copyright (C) 2001-2007 Klar√§lvdalens Datakonsult AB.  All rights reserved.
 **
 ** This file is part of the KD Tools library.
 **
