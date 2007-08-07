2c157a65f8cf20e42cd5b7de3818a570ec9282d0
Pino Toscano <pino@kde.org>
2007-08-07 11:36:03 +0000

    there are no .h files in the top-level
    svn path=/trunk/KDE/kdepim/; revision=697294

--- libkleo/Messages.sh
+++ libkleo/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC */*.ui >> rc.cpp || exit 11
-$XGETTEXT *.cpp *.h kleo/*.cpp kleo/*.h ui/*.cpp ui/*.h \
+$XGETTEXT *.cpp kleo/*.cpp kleo/*.h ui/*.cpp ui/*.h \
 	backends/*/*.cpp backends/*/*.h -o $podir/libkleopatra.pot
