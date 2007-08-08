953787992faef33c8c8207ae5fbc5a8ca8214d04
Pradeepto Bhattacharya <pradeepto@kde.org>
2007-08-08 12:26:02 +0000

    Version bump for fixed release - 2.1.9-2-rc1
    svn path=/branches/kdepim/proko2/kdepim/; revision=697696

--- kontact/src/main.cpp
+++ kontact/src/main.cpp
@@ -44,7 +44,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.0 (proko2 branch after 2.1.9)";
+static const char version[] = "1.0 (proko2 branch after  2.1.9-2-rc1)";
 
 class KontactApp : public KUniqueApplication {
   public:
