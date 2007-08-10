56f5bb538c8c5bf45bfccfe9537df9e7ab1ac34a
Jason vanRijn Kasper <vr@movingparts.net>
2007-08-10 22:32:39 +0000

    ick. const fixes for 0.12.2 again for winterz...
    svn path=/trunk/KDE/kdepim/; revision=698757

--- kpilot/conduits/keyringconduit/pi-keyring.cc
+++ kpilot/conduits/keyringconduit/pi-keyring.cc
@@ -2,14 +2,24 @@
 
 #include <QtDebug>
 
-int unpack_KeyringAppInfo( KeyringAppInfo_t *ai
-	, unsigned char *record, size_t len )
+#if PILOT_LINK_IS(0,12,2)
+int unpack_KeyringAppInfo( KeyringAppInfo_t *ai, 
+	const unsigned char *record, size_t len )
+#else
+int unpack_KeyringAppInfo( KeyringAppInfo_t *ai, 
+	unsigned char *record, size_t len )
+#endif
 {
 	return unpack_CategoryAppInfo( &ai->category, record, len + 4 );
 }
 
-int pack_KeyringAppInfo( KeyringAppInfo_t *ai, unsigned char *record
-	, size_t len )
+#if PILOT_LINK_IS(0,12,2)
+int pack_KeyringAppInfo( const KeyringAppInfo_t *ai, 
+	unsigned char *record, size_t len )
+#else
+int pack_KeyringAppInfo( KeyringAppInfo_t *ai, 
+	unsigned char *record, size_t len )
+#endif
 {
 	int i = pack_CategoryAppInfo( &ai->category, record, len );
 	qDebug() << "Length:" << i << len;
