45d8cd53c36101073bea675a5c0044770d0985c8
Marc Mutz <mutz@kde.org>
2007-08-17 17:13:42 +0000

    And a signature/user-id model, untested.
    svn path=/trunk/KDE/kdepim/kleopatra/; revision=701217

--- kleopatra/utils/formatting.cpp
+++ kleopatra/utils/formatting.cpp
@@ -1,3 +1,35 @@
+/* -*- mode: c++; c-basic-offset:4 -*-
+    utils/formatting.cpp
+
+    This file is part of Kleopatra, the KDE keymanager
+    Copyright (c) 2007 Klarälvdalens Datakonsult AB
+
+    Kleopatra is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kleopatra is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    In addition, as a special exception, the copyright holders give
+    permission to link the code of this program with any edition of
+    the Qt library by Trolltech AS, Norway (or with modified versions
+    of Qt that use the same license as Qt), and distribute linked
+    combinations including the two.  You must obey the GNU General
+    Public License in all respects for all of the code used other than
+    Qt.  If you modify this file, you may extend this exception to
+    your version of the file, but you are not obligated to do so.  If
+    you do not wish to do so, delete this exception statement from
+    your version.
+*/
+
 #include "formatting.h"
 
 #include <kleo/dn.h>
@@ -55,6 +87,10 @@ QString Formatting::prettyName( const UserID & uid ) {
     return prettyName( uid.parent().protocol(), uid.id(), uid.name(), uid.comment() );
 }
 
+QString Formatting::prettyName( const UserID::Signature & sig ) {
+    return prettyName( OpenPGP, sig.signerUserID(), sig.signerName(), sig.signerComment() );
+}
+
 //
 // EMail
 //
@@ -69,13 +105,21 @@ QString Formatting::prettyEMail( const Key & key ) {
 }
 
 QString Formatting::prettyEMail( const UserID & uid ) {
-    const QString email = QString::fromUtf8( uid.email() ).trimmed();
+    return prettyEMail( uid.email(), uid.id() );
+}
+
+QString Formatting::prettyEMail( const UserID::Signature & sig ) {
+    return prettyEMail( sig.signerEmail(), sig.signerUserID() );
+}
+
+QString Formatting::prettyEMail( const char * email_, const char * id ) {
+    const QString email = QString::fromUtf8( email_ ).trimmed();
     if ( !email.isEmpty() )
 	if ( email.startsWith( '<' ) && email.endsWith( '>' ) )
 	    return email.mid( 1, email.length() - 2 );
 	else
 	    return email;
-    return DN( uid.id() )["EMAIL"].trimmed();
+    return DN( id )["EMAIL"].trimmed();
 }
 
 //
@@ -179,39 +223,68 @@ namespace {
 	dt.setTime_t( t );
 	return dt.date();
     }
+
+    template <typename T>
+    QString expiration_date_string( const T & tee ) {
+	return tee.neverExpires() ? QString() : time_t2date( tee.expirationTime() ).toString() ;
+    }
+    template <typename T>
+    QDate creation_date( const T & tee ) {
+	return time_t2date( tee.creationTime() );
+    }
+    template <typename T>
+    QDate expiration_date( const T & tee ) {
+	return time_t2date( tee.expirationTime() );
+    }
 }
 
 QString Formatting::expirationDateString( const Key & key ) {
-    return expirationDateString( key.subkey( 0 ) );
+    return expiration_date_string( key.subkey( 0 ) );
 }
 
 QString Formatting::expirationDateString( const Subkey & subkey ) {
-    return subkey.neverExpires() ? QString() : expirationDate( subkey ).toString() ;
+    return expiration_date_string( subkey );
+}
+
+QString Formatting::expirationDateString( const UserID::Signature & sig ) {
+    return expiration_date_string( sig );
 }
 
 QDate Formatting::expirationDate( const Key & key ) {
-    return expirationDate( key.subkey( 0 ) );
+    return expiration_date( key.subkey( 0 ) );
 }
 
 QDate Formatting::expirationDate( const Subkey & subkey ) {
-    return time_t2date( subkey.expirationTime() );
+    return expiration_date( subkey );
+}
+
+QDate Formatting::expirationDate( const UserID::Signature & sig ) {
+    return expiration_date( sig );
 }
 
 
 QString Formatting::creationDateString( const Key & key ) {
-    return expirationDateString( key.subkey( 0 ) );
+    return creation_date( key.subkey( 0 ) ).toString();
 }
 
 QString Formatting::creationDateString( const Subkey & subkey ) {
-    return creationDate( subkey ).toString();
+    return creation_date( subkey ).toString();
+}
+
+QString Formatting::creationDateString( const UserID::Signature & sig ) {
+    return creation_date( sig ).toString();
 }
 
 QDate Formatting::creationDate( const Key & key ) {
-    return creationDate( key.subkey( 0 ) );
+    return creation_date( key.subkey( 0 ) );
 }
 
 QDate Formatting::creationDate( const Subkey & subkey ) {
-    return time_t2date( subkey.creationTime() );
+    return creation_date( subkey );
+}
+
+QDate Formatting::creationDate( const UserID::Signature & sig ) {
+    return creation_date( sig );
 }
 
 //
@@ -242,3 +315,37 @@ QString Formatting::validityShort( const Subkey & subkey ) {
     return hack::tr("good");
 }
 
+QString Formatting::validityShort( const UserID & uid ) {
+    if ( uid.isRevoked() )
+	return hack::tr("revoked");
+    if ( uid.isInvalid() )
+	return hack::tr("invalid");
+    switch ( uid.validity() ) {
+    case UserID::Unknown:   return hack::tr("unknown");
+    case UserID::Undefined: return hack::tr("undefined");
+    case UserID::Never:     return hack::tr("untrusted");
+    case UserID::Marginal:  return hack::tr("marginal");
+    case UserID::Full:      return hack::tr("full");
+    case UserID::Ultimate:  return hack::tr("ultimate");
+    }
+    return QString();
+}
+
+QString Formatting::validityShort( const UserID::Signature & sig ) {
+    switch ( sig.status() ) {
+    case UserID::Signature::NoError:
+	if ( !sig.isInvalid() )
+	    if ( sig.certClass() > 0 )
+		return hack::tr("class %d").arg( sig.certClass() );
+	    else
+		return hack::tr("good");
+	// fall through:
+    case UserID::Signature::GeneralError:
+	return hack::tr("invalid");
+    case UserID::Signature::SigExpired:   return hack::tr("expired");
+    case UserID::Signature::KeyExpired:   return hack::tr("key expired");
+    case UserID::Signature::BadSignature: return hack::tr("bad");
+    case UserID::Signature::NoPublicKey:  return QString();
+    }
+    return QString();
+}
--- kleopatra/utils/formatting.h
+++ kleopatra/utils/formatting.h
@@ -1,11 +1,39 @@
+/* -*- mode: c++; c-basic-offset:4 -*-
+    utils/formatting.h
+
+    This file is part of Kleopatra, the KDE keymanager
+    Copyright (c) 2007 Klarälvdalens Datakonsult AB
+
+    Kleopatra is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kleopatra is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    In addition, as a special exception, the copyright holders give
+    permission to link the code of this program with any edition of
+    the Qt library by Trolltech AS, Norway (or with modified versions
+    of Qt that use the same license as Qt), and distribute linked
+    combinations including the two.  You must obey the GNU General
+    Public License in all respects for all of the code used other than
+    Qt.  If you modify this file, you may extend this exception to
+    your version of the file, but you are not obligated to do so.  If
+    you do not wish to do so, delete this exception statement from
+    your version.
+*/
+
 #ifndef __KLEOPATRA_UTILS_FORMATTING_H__
 #define __KLEOPATRA_UTILS_FORMATTING_H__
 
-namespace GpgME {
-    class Key;
-    class UserID;
-    class Subkey;
-}
+#include <gpgme++/key.h>
 
 class QString;
 class QDate;
@@ -16,10 +44,13 @@ namespace Formatting {
     QString prettyName( int proto, const char * id, const char * name, const char * comment );
     QString prettyName( const GpgME::Key & key );
     QString prettyName( const GpgME::UserID & uid );
+    QString prettyName( const GpgME::UserID::Signature & sig );
 
 
+    QString prettyEMail( const char * email, const char * id );
     QString prettyEMail( const GpgME::Key & key );
     QString prettyEMail( const GpgME::UserID & uid );
+    QString prettyEMail( const GpgME::UserID::Signature & sig );
 
 
     QString toolTip( const GpgME::Key & key );
@@ -27,13 +58,17 @@ namespace Formatting {
 
     QString expirationDateString( const GpgME::Key & key );
     QString expirationDateString( const GpgME::Subkey & subkey );
+    QString expirationDateString( const GpgME::UserID::Signature & sig );
     QDate expirationDate( const GpgME::Key & key );
     QDate expirationDate( const GpgME::Subkey & subkey );
+    QDate expirationDate( const GpgME::UserID::Signature & sig );
 
     QString creationDateString( const GpgME::Key & key );
     QString creationDateString( const GpgME::Subkey & subkey );
+    QString creationDateString( const GpgME::UserID::Signature & sig );
     QDate creationDate( const GpgME::Key & key );
     QDate creationDate( const GpgME::Subkey & subkey );
+    QDate creationDate( const GpgME::UserID::Signature & sig );
 
 
     QString type( const GpgME::Key & key );
@@ -41,6 +76,9 @@ namespace Formatting {
 
 
     QString validityShort( const GpgME::Subkey & subkey );
+    QString validityShort( const GpgME::UserID & uid );
+    QString validityShort( const GpgME::UserID::Signature & sig );
+
 }
 }
 
