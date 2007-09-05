f83cde6941a085a54d8a4582b3c72b15ffa6beff
Marc Mutz <mutz@kde.org>
2007-09-05 12:09:46 +0000

    And 400 more lines just for assuan...
    svn path=/branches/work/kdab-post-4.0/kdepim/; revision=708688

--- kleopatra/ConfigureChecks.cmake
+++ kleopatra/ConfigureChecks.cmake
@@ -1,14 +1,32 @@
 # assuan configure checks
 include(CheckFunctionExists)
 
-set(CMAKE_REQUIRED_INCLUDES ${GPGME_INCLUDES}) ### ASSUAN_INCLUDES
-set(CMAKE_REQUIRED_LIBRARIES ${GPGME_LIBRARIES} -lassuan -lgpg-error) ### ASSUAN_LIBRARIES
+find_package(Assuan)
 
-# check whether assuan and gpgme may be linked to simultaneously
-check_function_exists( "assuan_get_pointer" ASSUAN_AND_GPGME_IN_SAME_PROCESS )
+set( USABLE_ASSUAN_FOUND false )
 
-if ( ASSUAN_AND_GPGME_IN_SAME_PROCESS )
+if ( ASSUAN_FOUND )
 
+  set( CMAKE_REQUIRED_INCLUDES ${ASSUAN_INCLUDES} )
+
+  if ( WIN32 AND ASSUAN_VANILLA_FOUND )
+    set( CMAKE_REQUIRED_LIBRARIES ${ASSUAN_VANILLA_LIBRARIES} )
+    set( USABLE_ASSUAN_FOUND true )
+  elseif( NOT WIN32 AND ASSUAN_PTHREAD_FOUND )
+    set( CMAKE_REQUIRED_LIBRARIES ${ASSUAN_PTHREAD_LIBRARIES} )
+    set( USABLE_ASSUAN_FOUND true )
+  endif( WIN32 AND ASSUAN_VANILLA_FOUND )
+
+endif( ASSUAN_FOUND )
+
+if ( USABLE_ASSUAN_FOUND )
+
+  # check whether assuan and gpgme may be linked to simultaneously
+  check_function_exists( "assuan_get_pointer" USABLE_ASSUAN_FOUND )
+
+endif( USABLE_ASSUAN_FOUND )
+
+if ( USABLE_ASSUAN_FOUND )
   # check if assuan has assuan_fd_t
   check_cxx_source_compiles("
         #include <assuan.h>
@@ -19,8 +37,16 @@ if ( ASSUAN_AND_GPGME_IN_SAME_PROCESS )
         "
     HAVE_ASSUAN_FD_T )
 
+  if ( WIN32 AND NOT HAVE_ASSUAN_FD_T )
+    set( USABLE_ASSUAN_FOUND false )
+  endif ( WIN32 AND NOT HAVE_ASSUAN_FD_T )
+
   # check if assuan has assuan_inquire_ext, old style
   check_function_exists( "assuan_inquire_ext" HAVE_ASSUAN_INQUIRE_EXT )
+
+  if ( NOT HAVE_ASSUAN_INQUIRE_EXT )
+    set( USABLE_ASSUAN_FOUND false )
+  endif( NOT HAVE_ASSUAN_INQUIRE_EXT )
 
   # check if assuan has new-style assuan_inquire_ext:
   check_cxx_source_compiles("
@@ -35,15 +61,7 @@ if ( ASSUAN_AND_GPGME_IN_SAME_PROCESS )
         "
     HAVE_NEW_STYLE_ASSUAN_INQUIRE_EXT )
 
-endif ( ASSUAN_AND_GPGME_IN_SAME_PROCESS )
-
-set( USABLE_ASSUAN_FOUND ${ASSUAN_AND_GPGME_IN_SAME_PROCESS} )
-if ( USABLE_ASSUAN_FOUND AND HAVE_ASSUAN_INQUIRE_EXT )
-   set( USABLE_ASSUAN_FOUND true )
-endif( USABLE_ASSUAN_FOUND AND HAVE_ASSUAN_INQUIRE_EXT )
-if ( WIN32 AND NOT HAVE_ASSUAN_FD_T )
-   set( USABLE_ASSUAN_FOUND false )
-endif ( WIN32 AND NOT HAVE_ASSUAN_FD_T )
+endif ( USABLE_ASSUAN_FOUND )
 
 macro_bool_to_01( USABLE_ASSUAN_FOUND  HAVE_USABLE_ASSUAN )
 
