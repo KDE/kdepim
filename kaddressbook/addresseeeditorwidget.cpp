8bc35258ea2059c87f49bbd63bcd6c77d32482a3
Andreas Hartmetz <ahartmetz@gmail.com>
2007-09-24 14:41:29 +0000

    forward port of enterprise revision 715564: KAddressBook: Detect early enough if the user can actually edit a contact and show the readonly variant of the GUI if appropriate.
    svn path=/trunk/KDE/kdepim/; revision=716333

--- kaddressbook/addresseeeditorwidget.cpp
+++ kaddressbook/addresseeeditorwidget.cpp
@@ -91,11 +91,22 @@ AddresseeEditorWidget::~AddresseeEditorWidget()
 void AddresseeEditorWidget::setAddressee( const KABC::Addressee &addr )
 {
   if ( mAddressee.uid() == addr.uid() )
-	  return;
-
+    return;
   mAddressee = addr;
 
-  bool readOnly = ( !addr.resource() ? false : addr.resource()->readOnly() );
+  bool readOnly = false;
+  if ( KABC::Resource *res = addr.resource() ) {
+    if ( res->readOnly() ) {
+      readOnly = true;
+
+    //HACK: some resources have finer access control than "generic" resources
+    } else if ( res->inherits( "KPIM::ResourceABC" ) ) {
+      KPIM::ResourceABC *resAbc = static_cast<KPIM::ResourceABC *>( res );
+
+      QString subresource = resAbc->uidToResourceMap()[ addr.uid() ];
+      readOnly = !resAbc->subresourceWritable( subresource );
+    }
+  }
   setReadOnly( readOnly );
 
   load();
