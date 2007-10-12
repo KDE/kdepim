b5aca484c3c5121cc1623a2df31fa8c051b9a552
Marc Mutz <mutz@kde.org>
2007-10-12 10:53:35 +0000

    Added assuan_assert, an assertion that throws an assuan_exception instead of aborting the program
    svn path=/branches/work/kdab-post-4.0/kdepim/; revision=724456

--- kleopatra/uiserver/kleo-assuan.h
+++ kleopatra/uiserver/kleo-assuan.h
@@ -36,7 +36,10 @@
 #include <gpg-error.h>
 #include <assuan.h>
 
+#include <boost/preprocessor/stringize.hpp>
+
 #include <stdexcept>
+#include <assert.h>
 
 class QString;
 
@@ -61,5 +64,29 @@ namespace Kleo {
     };
 
 }
+
+#define assuan_assert_impl( cond, file, line )                          \
+    if ( cond ) {}                                                      \
+    else throw Kleo::assuan_exception( gpg_error( GPG_ERR_INTERNAL ),   \
+                                       "assertion \"" #cond "\" failed at " file ":" BOOST_PP_STRINGIZE( line ) )
+#define assuan_assert_impl_func( cond, file, line, func )               \
+    if ( cond ) {}                                                      \
+    else throw Kleo::assuan_exception( gpg_error( GPG_ERR_INTERNAL ),   \
+                                       std::string( "assertion \"" #cond "\" failed in " ) + func + " (" file ":" BOOST_PP_STRINGIZE( line ) ")" )
+// from glibc's assert.h:
+/* Version 2.4 and later of GCC define a magical variable `__PRETTY_FUNCTION__'
+   which contains the name of the function currently being defined.
+   This is broken in G++ before version 2.6.
+   C9x has a similar variable called __func__, but prefer the GCC one since
+   it demangles C++ function names.  */
+#if defined __GNUC_PREREQ && __GNUC_PREREQ (2, 6)
+# define assuan_assert( cond ) assuan_assert_impl_func( cond, __FILE__, __LINE__, __PRETTY_FUNCTION__ )
+#elif defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L
+# define assuan_assert( cond ) assuan_assert_impl_func( cond, __FILE__, __LINE__, __func__ )
+#endif
+
+#ifndef assuan_assert
+# define assuan_assert( cond ) assuan_assert_impl( cond, __FILE__, __LINE__ )
+#endif
 
 #endif /* __KLEOPATRA_UISERVER_KLEO_ASSUAN_H__ */
