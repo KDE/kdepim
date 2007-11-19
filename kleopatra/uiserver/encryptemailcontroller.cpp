9c6912756fb7db83e3a0238eb5b6da06b32810d4
Marc Mutz <mutz@kde.org>
2007-11-19 14:26:52 +0000

    protocol() is now defined in the baseclass
    svn path=/branches/work/kdab-post-4.0/kdepim/; revision=738714

--- kleopatra/uiserver/encryptemailcontroller.cpp
+++ kleopatra/uiserver/encryptemailcontroller.cpp
@@ -220,7 +220,7 @@ void EncryptEMailController::Private::schedule() {
 shared_ptr<EncryptEMailTask> EncryptEMailController::Private::takeRunnable( GpgME::Protocol proto ) {
     const std::vector< shared_ptr<EncryptEMailTask> >::iterator it
         = std::find_if( runnable.begin(), runnable.end(),
-                        bind( &EncryptEMailTask::protocol, _1 ) == proto );
+                        bind( &Task::protocol, _1 ) == proto );
     if ( it == runnable.end() )
         return shared_ptr<EncryptEMailTask>();
 
