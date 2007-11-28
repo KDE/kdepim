d58b4bfb3a6b8282682419939c7384e151b6b746
Marc Mutz <mutz@kde.org>
2007-11-28 10:20:41 +0000

    Compile
    svn path=/branches/kdepim/enterprise/kdepim/; revision=742603

--- libkdenetwork/gpgmepp/context.cpp
+++ libkdenetwork/gpgmepp/context.cpp
@@ -39,6 +39,7 @@
 #include "util.h"
 
 #include <gpgme.h>
+#include <gpg-error.h>
 
 //#include <string>
 //using std::string;
@@ -450,7 +451,7 @@ namespace GpgME {
     return Error( d->lasterr = gpgme_op_getauditlog_start( d->ctx, odp ? odp->data : 0, to_auditlog_flags( flags ) ) );
 #else
     (void)output; (void)flags;
-    return Error( d->lasterr = makeError( GPG_ERR_NOT_IMPLEMENTED ) );
+    return Error( d->lasterr = gpg_error( GPG_ERR_NOT_IMPLEMENTED ) );
 #endif
   }
 
@@ -461,7 +462,7 @@ namespace GpgME {
     return Error( d->lasterr = gpgme_op_getauditlog( d->ctx, odp ? odp->data : 0, to_auditlog_flags( flags ) ) );
 #else
     (void)output; (void)flags;
-    return Error( d->lasterr = makeError( GPG_ERR_NOT_IMPLEMENTED ) );
+    return Error( d->lasterr = gpg_error( GPG_ERR_NOT_IMPLEMENTED ) );
 #endif
   }
 
