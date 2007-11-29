8f51053d38d2962e0141189c997c3a4416881e92
Albert Astals Cid <tsdgeos@terra.es>
2007-11-29 19:37:48 +0000

    Use >> instead of > to get translators placeholders
    svn path=/trunk/KDE/kdepim/; revision=743060

--- kmobiletools/kmobiletools/Messages.sh
+++ kmobiletools/kmobiletools/Messages.sh
@@ -13,6 +13,6 @@ MAINPART_SRCS="`find mainpart -name \"*.cpp\" -o -name \"*.h\"`"
 KMOBILETOOLS_RCS="$ABOUT_RCS $LIBKMOBILETOOLS_RCS $LIBKMOBILETOOLSENGINEUI_RCS $MAINPART_RCS"
 KMOBILETOOLS_SRCS="$ABOUT_SRCS $LIBKMOBILETOOLS_SRCS $LIBKMOBILETOOLSENGINEUI_SRCS $MAINPART_SRCS"
 
-$EXTRACTRC *.rc *.ui *.kcfg $KMOBILETOOLS_RCS > rc.cpp
+$EXTRACTRC *.rc *.ui *.kcfg $KMOBILETOOLS_RCS >> rc.cpp
 $XGETTEXT -ktranslate:1,1t -ktranslate:1c,2,2t *.cpp *.h $KMOBILETOOLS_SRCS -o $podir/kmobiletools.pot
 rm -f tips.cpp rc.cpp
