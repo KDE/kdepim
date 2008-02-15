6054f7db9f615576e98deb188f2fe707f54c1b9a
Pino Toscano <pino@kde.org>
2008-02-15 14:30:24 +0000

    extra ';'
    svn path=/trunk/KDE/kdepim/; revision=775335

--- kdgantt/kdganttglobal.h
+++ kdgantt/kdganttglobal.h
@@ -265,7 +265,7 @@ namespace KDGantt {
     inline bool operator!=( const DateTimeSpan& s1, const DateTimeSpan& s2) { return !s1.equals( s2 ); }
 }
 
-Q_DECLARE_METATYPE(KDGantt::ItemType);
+Q_DECLARE_METATYPE(KDGantt::ItemType)
 Q_DECLARE_TYPEINFO(KDGantt::Span, Q_MOVABLE_TYPE);
 Q_DECLARE_TYPEINFO(KDGantt::DateTimeSpan, Q_MOVABLE_TYPE);
 
