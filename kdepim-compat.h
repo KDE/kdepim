e51c909c10bddc813c73465038aab354dedf9954
Allen Winter <winter@kde.org>
2008-03-14 16:12:21 +0000

    add KDE_signal definition
    svn path=/trunk/KDE/kdepim/; revision=785662

--- kdepim-compat.h
+++ kdepim-compat.h
@@ -11,3 +11,11 @@
 #define KPATH_SEPARATOR ':'
 #endif
 #endif
+
+#if !defined(KDE_signal)
+#if defined _WIN32 || defined _WIN64
+#define KDE_signal      kdewin32_signal
+#else
+#define KDE_signal      ::signal
+#endif
+#endif
