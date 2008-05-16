7ef4a471ccdb9382430af3693a8c176221a4025e
Will Stephenson <wstephenson@kde.org>
2008-05-16 15:16:46 +0000

    Extend the filters used when importing certificates to the full range of file extensions in the mime database of the mimetypes kleopatra_import.desktop registers for.
    svn path=/trunk/KDE/kdepim/kleopatra/; revision=808425

--- kleopatra/commands/importcertificatefromfilecommand.cpp
+++ kleopatra/commands/importcertificatefromfilecommand.cpp
@@ -158,7 +158,7 @@ void ImportCertificateFromFileCommand::doStart()
 }
 
 static QStringList get_file_name( QWidget * parent ) {
-    const QString certificateFilter = i18n("Certificates (*.asc *.pem *.der *.p7c *.p12)");
+    const QString certificateFilter = i18n("Certificates (*.asc *.cer *.cert *.crt *.der *.pem *.der *.p7c *.p12 *.pfx)");
     const QString anyFilesFilter = i18n("Any files (*)" );
     QString previousDir;
     if ( const KSharedConfig::Ptr config = KGlobal::config() ) {
