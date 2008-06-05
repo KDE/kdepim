c24cca56f8585cdac551b4502767f53ff8cc86c7
Marc Mutz <mutz@kde.org>
2008-06-05 05:47:04 +0000

    Merged revisions 816688-817026 via svnmerge from svn+ssh://svn.kde.org/home/kde/trunk/KDE/kdepim
    ........
      r816688 | thrainer | 2008-06-04 17:38:42 +0200 (Wed, 04 Jun 2008) | 4 lines
      
      Style.
      
      SVN_SILENT:
    ........
      r816720 | winterz | 2008-06-04 18:51:41 +0200 (Wed, 04 Jun 2008) | 6 lines
      
      init() before the ActionManager is initialized.
      must do this because the componentData needs to be set beforehand
      so we don't get warning messages like:
      "KActionCollection::setComponentData: this does not work on a KActionCollection
       containing actions!"
    ........
      r816746 | bbroeksema | 2008-06-04 20:24:01 +0200 (Wed, 04 Jun 2008) | 2 lines
      
      HHCategory is actually saved to the mapping file now. Added test for it.
    ........
      r816750 | tmcguire | 2008-06-04 20:35:05 +0200 (Wed, 04 Jun 2008) | 3 lines
      
      KTextEdit -> KRichTextWidget
      C++ needs a 'super' keyword like in Java...
    ........
      r816762 | bbroeksema | 2008-06-04 21:10:22 +0200 (Wed, 04 Jun 2008) | 3 lines
      
      PC categories are now also saved in the mapping files.
      Added some tests for it.
    ........
      r816803 | tstaerk | 2008-06-04 22:05:18 +0200 (Wed, 04 Jun 2008) | 1 line
      
      Show the user what unit the duration is in.
    ........
      r816878 | winterz | 2008-06-04 23:21:41 +0200 (Wed, 04 Jun 2008) | 6 lines
      
      Raphael's new sidebar port to Qt4 itemviews.
      This does include a couple of very short, new strings.
      But I received no objections from the translators.
      
    ........
      r816888 | ereslibre | 2008-06-04 23:44:17 +0200 (Wed, 04 Jun 2008) | 2 lines
      
      Fix canDecodeMimeData on plugins. This way for example, we can't drop an URL dragged from Konqui to Contacts plugin.
    ........
      r816891 | rjarosz | 2008-06-04 23:49:29 +0200 (Wed, 04 Jun 2008) | 3 lines
      
      Dispatcher::registerComponent can't take SLOT(slotName) as slotName argument.
    ........
      r816901 | tmcguire | 2008-06-05 00:13:08 +0200 (Thu, 05 Jun 2008) | 11 lines
      
      - Return the correct return code when not all messages could be moved in KMMoveCommand
      - In the action scheduler, don't delete the original mail if the filtered copy couldn't
        be moved back
      
      This is a hotfix for IMAP, which will eat messages if one of the filters has online IMAP
      target folder (in which case the action scheduler will be used).
      
      The real problem is of course that the move command fails in the frist place, will have to
      figure that out later.
    ........
      r817026 | scripty | 2008-06-05 04:27:16 +0200 (Thu, 05 Jun 2008) | 1 line
      
      SVN_SILENT made messages (.desktop file)
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=817055

--- kpilot/conduits/base/idmappingxmlsource.cc
+++ kpilot/conduits/base/idmappingxmlsource.cc
@@ -185,6 +185,22 @@ bool IDMappingXmlSource::saveMapping()
 		mappingElement.setAttribute( CSL1("hh"), it.key() );
 		mappingElement.setAttribute( CSL1("pc"), it.value() );
 		
+		if( !fHHCategory.value( it.key() ).isEmpty() )
+		{
+			QDomElement hhCatElement = doc.createElement( CSL1("hhcategory") );
+			hhCatElement.setAttribute( CSL1("value"), fHHCategory.value( it.key() ) );
+			
+			mappingElement.appendChild( hhCatElement );
+		}
+		
+		foreach( const QString& pcCat, fPCCategories.value( it.value() ) )
+		{
+			QDomElement pcCatElement = doc.createElement( CSL1("pccategory") );
+			pcCatElement.setAttribute( CSL1("value"), pcCat );
+			
+			mappingElement.appendChild( pcCatElement );
+		}
+		
 		if( fArchivedRecords.contains( it.value() ) )
 		{
 			mappingElement.setAttribute( CSL1( "archived" ), CSL1( "yes" ) );
@@ -256,6 +272,9 @@ bool IDMappingXmlSource::startElement( const QString &namespaceURI
 	
 	if( qName == CSL1( "mapping" ) )
 	{
+		fCurrentHHId = attribs.value( CSL1( "hh" ) );
+		fCurrentPCId = attribs.value( CSL1( "pc" ) );
+		
 		QString hh( attribs.value( CSL1( "hh" ) ) );
 		QString pc( attribs.value( CSL1( "pc" ) ) );
 		
@@ -265,6 +284,20 @@ bool IDMappingXmlSource::startElement( const QString &namespaceURI
 		}
 		
 		fMappings.insert( hh, pc );
+	}
+	else if( qName == CSL1( "hhcategory" ) )
+	{
+		QString category = attribs.value( CSL1("value") );
+		fHHCategory.insert( fCurrentHHId, category );
+	}
+	else if( qName == CSL1( "pccategory" ) )
+	{
+		QString category = attribs.value( CSL1("value") );
+		
+		QStringList cats = fPCCategories.value( fCurrentPCId );
+		cats << category;
+		
+		fPCCategories.insert( fCurrentPCId, cats );
 	}
 	else if( qName == CSL1( "lastsync" ) )
 	{
--- kpilot/conduits/base/tests/idmappingxmlsourcetest.cc
+++ kpilot/conduits/base/tests/idmappingxmlsourcetest.cc
@@ -48,6 +48,10 @@ private slots:
 	void testLastSyncedPC();
 	void testMappings();
 	void testSaveLoad();
+	void testHHCategory();
+	void testOnePCCategory();
+	void testOneHHAndOnePCCategory();
+	void testOneHHAndMultiplePCCategories();
 	void cleanupTestCase();
 
 private:
@@ -153,6 +157,121 @@ void TestIDMappingXmlSource::testSaveLoad()
 		
 	QVERIFY( source2.archivedRecords()->size() == 1 );
 	QVERIFY( source2.archivedRecords()->contains( CSL1( "test-pc2" ) ) );
+}
+
+void TestIDMappingXmlSource::testHHCategory()
+{
+	
+	QDateTime dt = QDateTime::currentDateTime();
+	// Correct msecs, these wont be saved and the test 
+	// dt == source2.lastSyncedDate() will fail.
+	dt = dt.addMSecs( -dt.time().msec() );
+	QString pc( CSL1( "test-pc" ) );
+
+	IDMappingXmlSource source( fUser, fConduit );
+	source.setLastSyncedDate( dt );
+	source.setLastSyncedPC( pc );
+	source.mappings()->insert( CSL1( "test-hh" ), CSL1( "test-pc" ) );
+	source.setHHCategory( CSL1( "test-hh" ), CSL1( "TestCategory" ) );
+	source.saveMapping();
+	
+	IDMappingXmlSource source2( fUser, fConduit );
+	source2.loadMapping();
+	
+	QVERIFY( source2.hhCategory( "test-hh" ) == "TestCategory" );
+}
+
+void TestIDMappingXmlSource::testOnePCCategory()
+{
+	
+	QDateTime dt = QDateTime::currentDateTime();
+	// Correct msecs, these wont be saved and the test 
+	// dt == source2.lastSyncedDate() will fail.
+	dt = dt.addMSecs( -dt.time().msec() );
+	QString pc( CSL1( "test-pc" ) );
+
+	IDMappingXmlSource source( fUser, fConduit );
+	source.setLastSyncedDate( dt );
+	source.setLastSyncedPC( pc );
+	source.mappings()->insert( CSL1( "test-hh" ), CSL1( "test-pc" ) );
+	
+	QStringList categories;
+	categories << CSL1( "TestCategory" );
+	
+	source.setPCCategories( CSL1( "test-pc" ), categories );
+	source.saveMapping();
+	
+	IDMappingXmlSource source2( fUser, fConduit );
+	source2.loadMapping();
+	
+	QVERIFY( source2.pcCategories( "test-pc" ).size() == 1 );
+	QVERIFY( source2.pcCategories( "test-pc" ).contains( CSL1( "TestCategory" ) ) );
+}
+
+void TestIDMappingXmlSource::testOneHHAndOnePCCategory()
+{
+	QDateTime dt = QDateTime::currentDateTime();
+	// Correct msecs, these wont be saved and the test 
+	// dt == source2.lastSyncedDate() will fail.
+	dt = dt.addMSecs( -dt.time().msec() );
+	QString pc( CSL1( "test-pc" ) );
+
+	IDMappingXmlSource source( fUser, fConduit );
+	source.setLastSyncedDate( dt );
+	source.setLastSyncedPC( pc );
+	source.mappings()->insert( CSL1( "test-hh" ), CSL1( "test-pc" ) );
+	source.setHHCategory( CSL1( "test-hh" ), CSL1( "TestCategory" ) );
+	
+	QStringList categories;
+	categories << CSL1( "TestPCCategory" );
+	
+	source.setPCCategories( CSL1( "test-pc" ), categories );
+	source.saveMapping();
+	
+	IDMappingXmlSource source2( fUser, fConduit );
+	source2.loadMapping();
+	
+	QVERIFY( source2.hhCategory( "test-hh" ) == "TestCategory" );
+	
+	QStringList cats = source2.pcCategories( "test-pc" );
+	
+	QVERIFY( cats.size() == 1 );
+	QVERIFY( cats.contains( CSL1( "TestPCCategory" ) ) );
+}
+
+void TestIDMappingXmlSource::testOneHHAndMultiplePCCategories()
+{
+	QDateTime dt = QDateTime::currentDateTime();
+	// Correct msecs, these wont be saved and the test 
+	// dt == source2.lastSyncedDate() will fail.
+	dt = dt.addMSecs( -dt.time().msec() );
+	QString pc( CSL1( "test-pc" ) );
+
+	IDMappingXmlSource source( fUser, fConduit );
+	source.setLastSyncedDate( dt );
+	source.setLastSyncedPC( pc );
+	source.mappings()->insert( CSL1( "test-hh" ), CSL1( "test-pc" ) );
+	source.setHHCategory( CSL1( "test-hh" ), CSL1( "TestCategory" ) );
+	
+	QStringList categories;
+	categories << CSL1( "TestPCCategory1" );
+	categories << CSL1( "TestPCCategory2" );
+	categories << CSL1( "TestPCCategory3" );
+	
+	source.setPCCategories( CSL1( "test-pc" ), categories );
+	source.saveMapping();
+	
+	IDMappingXmlSource source2( fUser, fConduit );
+	source2.loadMapping();
+	
+	QVERIFY( source2.hhCategory( "test-hh" ) == "TestCategory" );
+	
+	QStringList cats = source2.pcCategories( "test-pc" );
+	
+	QVERIFY( cats.size() == 3 );
+	QVERIFY( cats.contains( CSL1( "TestPCCategory1" ) ) );
+	QVERIFY( cats.contains( CSL1( "TestPCCategory2" ) ) );
+	QVERIFY( cats.contains( CSL1( "TestPCCategory3" ) ) );
 }
 
 void TestIDMappingXmlSource::cleanDir()
