b64f6bbbf165556e536e600c4fee9675b9abdf89
Thomas McGuire <mcguire@kde.org>
2008-07-03 15:29:56 +0000

    Get rid of kleopatra and libkleo, they'll be copied over from the e4 branch in my next commit, to preserve history. Splitting up the already commited kleo merge did not work, there were all kind of problems and conflicts.
    svn path=/trunk/KDE/kdepim/kleopatra/; revision=827660

--- kleopatra/Messages.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kleopatra.pot
