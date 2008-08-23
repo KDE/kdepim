053f62cc60a140f52aa4d3166ded26d4de0c08da
Volker Krause <vkrause@kde.org>
2008-08-23 13:58:56 +0000

    Only show the incidence difference if we are comparing with an older version.
    Kolab issue 2680
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=851291

--- libkcal/incidenceformatter.cpp
+++ libkcal/incidenceformatter.cpp
@@ -1112,6 +1112,9 @@ class IncidenceFormatter::IncidenceCompareVisitor :
     IncidenceCompareVisitor() : mExistingIncidence(0) {}
     bool act( IncidenceBase *incidence, Incidence* existingIncidence )
     {
+      Incidence *inc = dynamic_cast<Incidence*>( incidence );
+      if ( inc && inc->revision() <= existingIncidence->revision() )
+        return false;
       mExistingIncidence = existingIncidence;
       return incidence->accept( *this );
     }
