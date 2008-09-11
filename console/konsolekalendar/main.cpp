00a10b01a16bf8fc42a3a9c872d76e9fddbc71c6
Allen Winter <winter@kde.org>
2008-09-11 23:46:35 +0000

    backport SVN commit 860086 by winterz:
    work around what krake and I think is a Qt bug when converting hh:mm to a QTime.
    we do a silly check to see if the user supplied seconds for the start/end times,
    and if not, we append ":00" for them.
    
    i.e. "10:00" becomes "10:00:00" so QTime::fromString( Qt::ISODate) doesn't fail.
    
    svn path=/branches/KDE/4.1/kdepim/; revision=860088

--- console/konsolekalendar/main.cpp
+++ console/konsolekalendar/main.cpp
@@ -1,29 +1,29 @@
-/*******************************************************************************
- * main.cpp                                                                    *
- *                                                                             *
- * KonsoleKalendar is a command line interface to KDE calendars                *
- * Copyright (C) 2002-2004  Tuukka Pasanen <illuusio@mailcity.com>             *
- * Copyright (C) 2003-2005,2008  Allen Winter <winter@kde.org>                 *
- *                                                                             *
- * This program is free software; you can redistribute it and/or modify        *
- * it under the terms of the GNU General Public License as published by        *
- * the Free Software Foundation; either version 2 of the License, or           *
- * (at your option) any later version.                                         *
- *                                                                             *
- * This program is distributed in the hope that it will be useful,             *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                *
- * GNU General Public License for more details.                                *
- *                                                                             *
- * You should have received a copy of the GNU General Public License along     *
- * with this program; if not, write to the Free Software Foundation, Inc.,     *
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.                *
- *                                                                             *
- * As a special exception, permission is given to link this program            *
- * with any edition of Qt, and distribute the resulting executable,            *
- * without including the source code for Qt in the source distribution.        *
- *                                                                             *
- ******************************************************************************/
+/******************************************************************************
+ * main.cpp                                                                   *
+ *                                                                            *
+ * KonsoleKalendar is a command line interface to KDE calendars               *
+ * Copyright (C) 2002-2004  Tuukka Pasanen <illuusio@mailcity.com>            *
+ * Copyright (C) 2003-2005,2008  Allen Winter <winter@kde.org>                *
+ *                                                                            *
+ * This program is free software; you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the               *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.               *
+ *                                                                            *
+ * As a special exception, permission is given to link this program           *
+ * with any edition of Qt, and distribute the resulting executable,           *
+ * without including the source code for Qt in the source distribution.       *
+ *                                                                            *
+ *****************************************************************************/
 /**
  * @file main.cpp
  * KonsoleKalendar main program.
@@ -460,6 +460,10 @@ int main( int argc, char *argv[] )
              << "(" << option << ")";
 
     if ( option.toUpper() != "FLOAT" ) {
+      if ( option.count( ':' ) < 2 ) {
+        // need to append seconds
+        option.append( ":00" );
+      }
       starttime = QTime::fromString( option, Qt::ISODate );
       if ( !starttime.isValid() ) {
         cout << i18n( "Invalid Start Time Specified: %1", option ).toLocal8Bit().data()
@@ -533,6 +537,10 @@ int main( int argc, char *argv[] )
              << "(" << option << ")";
 
     if ( option.toUpper() != "FLOAT" ) {
+      if ( option.count( ':' ) < 2 ) {
+        // need to append seconds
+        option.append( ":00" );
+      }
       endtime = QTime::fromString( option, Qt::ISODate );
       if ( !endtime.isValid() ) {
         cout << i18n( "Invalid End Time Specified: %1", option ).toLocal8Bit().data()
