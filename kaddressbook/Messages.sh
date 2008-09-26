1206cc8fd7d12ea42650056880664e5d322f705f
Albert Astals Cid <tsdgeos@terra.es>
2008-09-26 17:31:40 +0000

    Fix problem with forcing context in a wrong way
    svn path=/trunk/KDE/kdepim/; revision=865100

--- kaddressbook/Messages.sh
+++ kaddressbook/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-$EXTRACTRC --context="Instant messaging" editors/im*.ui >> rc.cpp || exit 11
+$EXTRACTRC editors/im*.ui >> rc.cpp || exit 11
 $EXTRACTRC printing/*.ui >> rc.cpp || exit 11
 $EXTRACTRC */*.kcfg >> rc.cpp || exit 11
 $XGETTEXT *.h *.cpp common/*.cpp editors/*.cpp features/*.cpp printing/*.cpp views/*.cpp xxport/*.cpp interfaces/*.h -o $podir/kaddressbook.pot
