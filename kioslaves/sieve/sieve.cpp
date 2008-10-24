834be434484d95ac4c4640b45df43e45b2845ec6
Pradeepto Bhattacharya <pradeepto@kde.org>
2008-10-24 04:15:10 +0000

    Merged revisions 875280 via svnmerge from svn+ssh://pradeepto@svn.kde.org/home/kde/branches/KDE/3.5/kdepim
    ........
      r875280 | kloecker | 2008-10-24 03:17:43 +0530 (Fri, 24 Oct 2008) | 3 lines
      
      Merging revision 875279 from trunk:
      Fix recognition of Cyrus < 2.3.11. Patch by Martin Fahrendorf.
    ........
    
    svn path=/branches/kdepim/enterprise/kdepim/; revision=875331

--- kioslaves/sieve/sieve.cpp
+++ kioslaves/sieve/sieve.cpp
@@ -1290,7 +1290,7 @@ bool kio_sieveProtocol::requestCapabilitiesAfterStartTLS() const
     const int minor = regExp.cap( 2 ).toInt();
     const int patch = regExp.cap( 3 ).toInt();
     const QString vendor = regExp.cap( 4 );
-    if ( major < 2 || (major == 2 && (minor < 3 || (major == 3 && patch < 11))) || (vendor == "-kolab-nocaps") ) {
+    if ( major < 2 || (major == 2 && (minor < 3 || (minor == 3 && patch < 11))) || (vendor == "-kolab-nocaps") ) {
       ksDebug() << k_funcinfo << "Enabling compat mode for Cyrus < 2.3.11 or Cyrus marked as \"kolab-nocaps\"" << endl;
       return true;
     }
