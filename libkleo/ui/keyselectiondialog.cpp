fd33e3a3d0252b8232c9b95b60cc62ce822ebacc
Volker Krause <vkrause@kde.org>
2008-11-22 14:10:27 +0000

    Merged revisions 865018 via svnmerge from https://vkrause@svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r865018 | vkrause | 2008-09-26 14:10:53 +0200 (Fri, 26 Sep 2008) | 4 lines
      
      Be a bit more helpful when there are no matching keys found.
      
      Kolab issue 2875
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=887624

--- libkleo/ui/keyselectiondialog.cpp
+++ libkleo/ui/keyselectiondialog.cpp
@@ -60,6 +60,7 @@
 #include <kconfiggroup.h>
 #include <kmenu.h>
 #include <klineedit.h>
+
 // Qt
 #include <QCheckBox>
 #include <QToolButton>
@@ -356,6 +357,7 @@ void Kleo::KeySelectionDialog::init( bool rememberChoice, bool extendedSelection
   if ( !text.isEmpty() ) {
     QLabel* textLabel = new QLabel( text, page );
     textLabel->setWordWrap( true );
+    connect( textLabel, SIGNAL(linkActivated(QString)), SLOT(slotStartCertificateManager()) );
     mTopLayout->addWidget( textLabel );
   }
 
