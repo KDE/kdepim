f4d2ca532e6e243f74f7e025be12d63e7969b3c8
Tobias Koenig <tokoe@kde.org>
2009-01-26 19:45:34 +0000

    Check for all fields of a contact in the filter model
    svn path=/trunk/KDE/kdepim/kcontactmanager/; revision=917065

--- kaddressbook/contactfiltermodel.cpp
+++ kaddressbook/contactfiltermodel.cpp
@@ -22,6 +22,8 @@
 #include <akonadi/itemmodel.h>
 #include <kabc/addressee.h>
 
+static bool contactMatchesFilter( const KABC::Addressee &contact, const QString &filterString );
+
 ContactFilterModel::ContactFilterModel( QObject *parent )
   : QSortFilterProxyModel( parent )
 {
@@ -46,10 +48,102 @@ bool ContactFilterModel::filterAcceptsRow( int row, const QModelIndex &parent ) 
 
   if ( item.mimeType() == QLatin1String( "text/directory" ) ) {
     const KABC::Addressee contact = item.payload<KABC::Addressee>();
-    return contact.assembledName().contains( mFilter );
+    return contactMatchesFilter( contact, mFilter );
   }
 
   return true;
+}
+
+static bool addressMatchesFilter( const KABC::Address &address, const QString &filterString )
+{
+  const QString lowerFilterString( filterString.toLower() );
+
+  if ( address.street().toLower().contains( lowerFilterString ) )
+    return true;
+
+  if ( address.locality().toLower().contains( lowerFilterString ) )
+    return true;
+
+  if ( address.region().toLower().contains( lowerFilterString ) )
+    return true;
+
+  if ( address.postalCode().toLower().contains( lowerFilterString ) )
+    return true;
+
+  if ( address.country().toLower().contains( lowerFilterString ) )
+    return true;
+
+  if ( address.label().toLower().contains( lowerFilterString ) )
+    return true;
+
+  if ( address.postOfficeBox().toLower().contains( lowerFilterString ) )
+    return true;
+
+  return false;
+}
+
+static bool contactMatchesFilter( const KABC::Addressee &contact, const QString &filterString )
+{
+  const QString lowerFilterString( filterString.toLower() );
+
+  if ( contact.assembledName().toLower().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.formattedName().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.nickName().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.birthday().toString().toLower().contains( lowerFilterString ) )
+    return true;
+
+  const KABC::Address::List addresses = contact.addresses();
+  for ( int i = 0; i < addresses.count(); ++i ) {
+    if ( addressMatchesFilter( addresses.at( i ), filterString ) )
+      return true;
+  }
+
+  const KABC::PhoneNumber::List phoneNumbers = contact.phoneNumbers();
+  for ( int i = 0; i < phoneNumbers.count(); ++i ) {
+    if ( phoneNumbers.at( i ).number().toLower().contains( lowerFilterString ) )
+      return true;
+  }
+
+  const QStringList emails = contact.emails();
+  for ( int i = 0; i < emails.count(); ++i ) {
+    if ( emails.at( i ).toLower().contains( lowerFilterString ) )
+      return true;
+  }
+
+  if ( contact.mailer().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.title().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.role().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.organization().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.department().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.note().contains( lowerFilterString ) )
+    return true;
+
+  if ( contact.url().url().contains( lowerFilterString ) )
+    return true;
+
+  const QStringList customs = contact.customs();
+  for ( int i = 0; i < customs.count(); ++i ) {
+    if ( customs.at( i ).toLower().contains( lowerFilterString ) )
+      return true;
+  }
+
+  return false;
 }
 
 #include "contactfiltermodel.moc"
