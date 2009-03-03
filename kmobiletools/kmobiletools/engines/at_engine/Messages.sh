57aba626cdf226fa12e7c4ff9700b36fdfbf4474
Allen Winter <winter@kde.org>
2009-03-03 19:31:04 +0000

    move kmobiletools from kdepim to playground/pim
    svn path=/trunk/KDE/kdepim/; revision=934784

--- kmobiletools/kmobiletools/engines/at_engine/Messages.sh
+++
@@ -1,8 +0,0 @@
-RESOURCE_FILES="`find -name \"*.ui\" -o -name \"*.rc\" -o -name \"*.kcfg\"`"
-
-$EXTRACTRC $RESOURCE_FILES >> rc.cpp
-
-SOURCE_FILES="`find -name \"*.cpp\" -o -name \"*.h\"`"
-
-$XGETTEXT -ktranslate:1,1t -ktranslate:1c,2,2t $SOURCE_FILES -o $podir/kmobiletools_at_engine.pot
-rm -f tips.cpp rc.cpp
