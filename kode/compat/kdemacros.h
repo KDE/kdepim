7bd97b60b7fa187fdbb3ac9b03a8ca780d0efb48
Allen Winter <winter@kde.org>
2009-03-08 14:07:31 +0000

    moved into playground/pim
    svn path=/trunk/KDE/kdepim/; revision=936810

--- kode/compat/kdemacros.h
+++
@@ -1,1 +0,0 @@
-#define KDE_EXPORT
--- kode/docs/create_html.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-
-`erb index.rhtml >index.html`
-`erb schemaparser.rhtml >schemaparser.html`
-`erb xmlcompiler.rhtml >xmlcompiler.html`
-`erb kxforms.rhtml >kxforms.html`
-`erb webservices.rhtml >webservices.html`
