15e20ed6e0ce847df78068a6d4e3f27f8d695e64
Allen Winter <winter@kde.org>
2009-04-03 14:07:19 +0000

    Merged revisions 948580 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r948580 | mlaurent | 2009-04-03 05:00:51 -0400 (Fri, 03 Apr 2009) | 3 lines
      
      Return somethink
      fix minor mem leak
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=948687

--- wizards/scalixmain.cpp
+++ wizards/scalixmain.cpp
@@ -46,8 +46,8 @@ int main(int argc,char **argv)
 
   bool verbose = false;
   if ( args->isSet( "verbose" ) ) verbose = true;
-
+  args->clear();
   ScalixWizard wizard;
 
-  wizard.exec();
+  return wizard.exec();
 }
