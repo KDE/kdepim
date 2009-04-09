0394ba0351e89ed8165bf4f1ccf6a95cbc10e36c
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 21:46:40 +0000

    Updated Messages.sh files to pick up strings from .rc files.
    svn path=/trunk/KDE/kdepim/; revision=951700

--- kaddressbook/Messages.sh
+++ kaddressbook/Messages.sh
@@ -1,2 +1,6 @@
 #! /bin/sh
-$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/kcontactmanager.pot
+$EXTRACTRC editors/im*.ui >> rc.cpp || exit 11
+$EXTRACTRC printing/*.ui >> rc.cpp || exit 11
+$EXTRACTRC */*.kcfg >> rc.cpp || exit 11
+$EXTRACTRC */*.rc >> rc.cpp || exit 11
+$XGETTEXT *.h *.cpp common/*.cpp editors/*.cpp features/*.cpp printing/*.cpp views/*.cpp xxport/*.cpp interfaces/*.h -o $podir/kaddressbook.pot
