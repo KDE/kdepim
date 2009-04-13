9dcec450e46a8a027b8469b5bb2f9b29a836171e
Laurent Montel <montel@kde.org>
2009-04-13 11:54:17 +0000

    fix it
    svn path=/trunk/KDE/kdepim/kmailcvt/; revision=953095

--- kmailcvt/Messages.sh
+++ kmailcvt/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
+LIST=`find . -name \*.cpp -o -name \*.cxx`
 if test -n "$LIST"; then
 $XGETTEXT $LIST -o $podir/kmailcvt.pot
 fi
