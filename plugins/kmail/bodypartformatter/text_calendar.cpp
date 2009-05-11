de7fc63cc48272751e99e92c1facb2855c4f6fd0
Allen Winter <winter@kde.org>
2009-05-11 18:33:39 +0000

    Merged revisions 966689 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r966689 | winterz | 2009-05-11 14:26:04 -0400 (Mon, 11 May 2009) | 10 lines
      
      Invitation decline comments are now sent as DESCRIPTION property only
      when the user has selected the "exchange compatible naming" groupware option.
      Otherwise, we adhere to standards and use the COMMENT property.
      
      This should fix kolab/issue3424 more to my satisfaction.
      Also makes r965355 acceptable and that should also be merged forward,
      but before this revision.
      
      MERGE: e4,trunk,4.2
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=966693

--- plugins/kmail/bodypartformatter/text_calendar.cpp
+++ plugins/kmail/bodypartformatter/text_calendar.cpp
@@ -378,8 +378,13 @@ class UrlHandler : public KMail::Interface::BodyPartURLHandler
             i18n("Comment:"), QString(), &ok );
         if ( !ok )
           return true;
-        if ( !comment.isEmpty() )
-          incidence->setDescription( comment );
+        if ( !comment.isEmpty() ) {
+          if ( callback.exchangeCompatibleInvitations() ) {
+            incidence->setDescription( comment );
+          } else {
+            incidence->addComment( comment );
+          }
+        }
       }
 
       // First, save it for KOrganizer to handle
@@ -520,8 +525,13 @@ class UrlHandler : public KMail::Interface::BodyPartURLHandler
             i18n("Comment:"), QString(), &ok );
         if ( !ok )
           return true;
-        if ( !comment.isEmpty() )
-          incidence->setDescription( comment );
+        if ( !comment.isEmpty() ) {
+          if ( callback.exchangeCompatibleInvitations() ) {
+            incidence->setDescription( comment );
+          } else {
+            incidence->addComment( comment );
+          }
+        }
       }
       return mail( incidence, callback, "declinecounter", KCal::iTIPDeclineCounter,
                    callback.sender(), DeclineCounter );
