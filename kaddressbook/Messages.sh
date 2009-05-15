f2aafbcd4625187860d905fe179a812e6b5b44b5
Laurent Montel <montel@kde.org>
2009-05-15 12:26:28 +0000

    Fix extract messages
    svn path=/trunk/KDE/kdepim/kcontactmanager/; revision=968234

--- kaddressbook/Messages.sh
+++ kaddressbook/Messages.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-$EXTRACTRC editors/im*.ui >> rc.cpp || exit 11
-$EXTRACTRC printing/*.ui >> rc.cpp || exit 11
-$EXTRACTRC */*.kcfg >> rc.cpp || exit 11
-$EXTRACTRC */*.rc >> rc.cpp || exit 11
-$XGETTEXT *.h *.cpp common/*.cpp editors/*.cpp features/*.cpp printing/*.cpp views/*.cpp xxport/*.cpp interfaces/*.h -o $podir/kaddressbook.pot
+$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
+$XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/kcontactmanager.pot
+rm -f rc.cpp
