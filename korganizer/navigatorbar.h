d68c6a9dcd866751edfb42c3df3dd043fd3c376e
Allen Winter <winter@kde.org>
2009-05-24 22:02:48 +0000

    backport SVN commit 972383 by winterz:
    replace our ugly custom widget ActiveLabel with pretty QToolButton for Month and Year button+menus
    
    svn path=/branches/KDE/4.2/kdepim/; revision=972432

--- korganizer/navigatorbar.h
+++ korganizer/navigatorbar.h
@@ -26,25 +26,10 @@
 
 #include <kcal/incidencebase.h>
 
-#include <QLabel>
+#include <QWidget>
 #include <QMouseEvent>
 
 class QToolButton;
-
-class ActiveLabel : public QLabel
-{
-  Q_OBJECT
-  public:
-    explicit ActiveLabel( QWidget *parent );
-
-  signals:
-    void clicked();
-
-  protected:
-    void enterEvent( QEvent * );
-    void leaveEvent( QEvent * );
-    void mouseReleaseEvent ( QMouseEvent * );
-};
 
 class NavigatorBar: public QWidget
 {
@@ -76,14 +61,12 @@ class NavigatorBar: public QWidget
     void selectYear();
 
   private:
-    bool mHasMinWidth;
-
     QDate mDate;
 
     QToolButton *mPrevYear;
     QToolButton *mPrevMonth;
-    ActiveLabel *mMonth;
-    ActiveLabel *mYear;
+    QToolButton *mMonth;
+    QToolButton *mYear;
     QToolButton *mNextMonth;
     QToolButton *mNextYear;
 };
