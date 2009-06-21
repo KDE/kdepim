114b0c0a1973e4f155c7a70783bf6a92c75304e7
Allen Winter <winter@kde.org>
2009-06-21 15:04:57 +0000

    I had forgotten to add my copyright.
    svn path=/trunk/KDE/kdepim/; revision=984855

--- libkdepim/ktimezonecombobox.cpp
+++ libkdepim/ktimezonecombobox.cpp
@@ -1,5 +1,6 @@
 /*
   Copyright (C) 2007 Bruno Virlet <bruno.virlet@gmail.com>
+  Copyright 2008 Allen Winter <winter@kde.org>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
