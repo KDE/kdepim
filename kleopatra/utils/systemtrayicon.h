812d5d52941d2cd1af56d96afdaa69db0ec8fce6
Marc Mutz <mutz@kde.org>
2009-07-08 16:40:52 +0000

    Kleo::SystemTrayIcon: spinoff from ::SystemTrayIcon that contains generic functionality (attention handling: blinking icon). Not added to build yet, b/c of automoc dumbness when two source files in different directories are named the same.
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=993410

---
+++ kleopatra/utils/systemtrayicon.h
@@ -0,0 +1,84 @@
+/* -*- mode: c++; c-basic-offset:4 -*-
+    systemtrayicon.h
+
+    This file is part of Kleopatra, the KDE keymanager
+    Copyright (c) 2007,2009 Klarälvdalens Datakonsult AB
+
+    Kleopatra is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    Kleopatra is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    In addition, as a special exception, the copyright holders give
+    permission to link the code of this program with any edition of
+    the Qt library by Trolltech AS, Norway (or with modified versions
+    of Qt that use the same license as Qt), and distribute linked
+    combinations including the two.  You must obey the GNU General
+    Public License in all respects for all of the code used other than
+    Qt.  If you modify this file, you may extend this exception to
+    your version of the file, but you are not obligated to do so.  If
+    you do not wish to do so, delete this exception statement from
+    your version.
+*/
+
+#ifndef __KLEOPATRA_UTILS_SYSTEMTRAYICON_H__
+#define __KLEOPATRA_UTILS_SYSTEMTRAYICON_H__
+
+#include <QSystemTrayIcon>
+
+#include <utils/pimpl_ptr.h>
+
+namespace Kleo {
+
+class SystemTrayIcon : public QSystemTrayIcon {
+    Q_OBJECT
+public:
+    explicit SystemTrayIcon( QObject * parent=0 );
+    explicit SystemTrayIcon( const QIcon & icon, QObject * parent=0 );
+    ~SystemTrayIcon();
+
+    void setMainWindow( QWidget * w );
+    QWidget * mainWindow() const;
+
+    void setAttentionWindow( QWidget * w );
+    QWidget * attentionWindow() const;
+
+    QIcon attentionIcon() const;
+    QIcon normalIcon() const;
+    bool attentionWanted() const;
+
+public Q_SLOTS:
+    void setAttentionIcon( const QIcon & icon );
+    void setNormalIcon( const QIcon & icon );
+    void setAttentionWanted( bool );
+
+protected Q_SLOTS:
+    virtual void slotEnableDisableActions() = 0;
+
+private:
+    virtual void doMainWindowClosed( QWidget * );
+    virtual void doAttentionWindowClosed( QWidget * );
+    virtual void doActivated() = 0;
+
+private:
+    /* reimp */ bool eventFilter( QObject *, QEvent * );
+
+private:
+    class Private;
+    kdtools::pimpl_ptr<Private> d;
+    Q_PRIVATE_SLOT( d, void slotAttentionAnimationTimerTimout() )
+    Q_PRIVATE_SLOT( d, void slotActivated( QSystemTrayIcon::ActivationReason ) )
+};
+
+} // namespace Kleo
+
+#endif /* __KLEOPATRA_UTILS_SYSTEMTRAYICON_H__ */
