7d7513fbac74588ceb249cfc3f883bd1f0198018
Andras Mantia <amantia@kde.org>
2009-07-10 13:58:45 +0000

    Use isPlainText() as we have it.
    svn path=/branches/work/akonadi-ports/kdepim/; revision=994279

--- libmailreader/partnodebodypart.cpp
+++ libmailreader/partnodebodypart.cpp
@@ -58,7 +58,7 @@ QString KMail::PartNodeBodyPart::makeLink( const QString & path ) const {
 }
 
 QString KMail::PartNodeBodyPart::asText() const {
-  if ( mContent->contentType()->mimeType() != "text/plain" )
+  if ( !mContent->contentType()->isPlainText() )
     return QString();
   return mCodec->toUnicode( mContent->decodedContent() );
 }
