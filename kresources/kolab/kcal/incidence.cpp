d5d8e306d4d9de4e3ca55828db874a2052622e11
Thomas McGuire <mcguire@kde.org>
2009-07-22 09:41:35 +0000

    SVN_MERGE Merged revisions 1000512 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepim
    ................
      r1000512 | winterz | 2009-07-21 15:39:07 +0200 (Tue, 21 Jul 2009) | 13 lines
      
      Merged revisions 1000510 via svnmerge from 
      https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
      
      ........
        r1000510 | winterz | 2009-07-21 09:30:54 -0400 (Tue, 21 Jul 2009) | 6 lines
        
        add status "needs-action" to a blacklist of unknown statuses, thereby reverting
        my former idea that we should all it.
        
        kolab/issue3766
        MERGE: e4,trunk,4.3
      ........
    ................
    
    svn path=/trunk/KDE/kdepim/; revision=1000898

--- kresources/kolab/kcal/incidence.cpp
+++ kresources/kolab/kcal/incidence.cpp
@@ -593,17 +593,16 @@ static KCal::Attendee::PartStat attendeeStringToStatus( const QString& s )
 {
   if ( s == "none" )
     return KCal::Attendee::NeedsAction;
-  if ( s == "needs-action" ) {
-    // kolab 2.2.0 sets needs-action (invalid according the standard),
-    // but let's be nice and support it anyway
-    return KCal::Attendee::NeedsAction;
-  }
   if ( s == "tentative" )
     return KCal::Attendee::Tentative;
   if ( s == "declined" )
     return KCal::Attendee::Declined;
   if ( s == "delegated" )
     return KCal::Attendee::Delegated;
+  if ( s == "needs-action" ) {
+    // kolab 2.2.0 sets needs-action (invalid according the standard),
+    return KCal::Attendee::None;
+  }
 
   // Default:
   return KCal::Attendee::Accepted;
@@ -626,6 +625,8 @@ static QString attendeeStatusToString( KCal::Attendee::PartStat status )
   case KCal::Attendee::InProcess:
     // These don't have any meaning in the Kolab format, so just use:
     return "accepted";
+  case KCal::Attendee::None:
+    return "unknown";
   }
 
   // Default for the case that there are more added later:
