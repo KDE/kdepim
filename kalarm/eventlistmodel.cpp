6ce6c9127899c0ce0393438a8834e1c09317a681
David Jarvie <djarvie@kde.org>
2009-07-24 20:33:56 +0000

    Fix error warning colour
    svn path=/branches/KDE/4.3/kdepim/; revision=1002047

--- kalarm/eventlistmodel.cpp
+++ kalarm/eventlistmodel.cpp
@@ -203,10 +203,15 @@ QVariant EventListModel::data(const QModelIndex& index, int role) const
 					||  event->action() == KAEvent::FILE
 					||  (event->action() == KAEvent::COMMAND && event->commandDisplay()))
 						return event->bgColour();
+					if (event->action() == KAEvent::COMMAND
+					&&  event->commandError() != KAEvent::CMD_NO_ERROR)
+						return Qt::red;
 					break;
 				case Qt::ForegroundRole:
 					if (event->commandError() != KAEvent::CMD_NO_ERROR)
 					{
+						if (event->action() == KAEvent::COMMAND && !event->commandDisplay())
+							return Qt::white;
 						QColor colour = Qt::red;
 						int r, g, b;
 						event->bgColour().getRgb(&r, &g, &b);
