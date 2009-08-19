3a2a0ffae8d10ed78c779878c52ae6989713fe63
David Jarvie <djarvie@kde.org>
2009-08-19 21:15:59 +0000

    Bug 203957: fix alarms being marked invalid when restoring from previous session.
    svn path=/branches/KDE/4.3/kdepim/; revision=1013437

--- kalarm/alarmevent.cpp
+++ kalarm/alarmevent.cpp
@@ -2589,36 +2589,6 @@ bool KAEvent::setDisplaying(const KAEvent& event, KAAlarm::Type alarmType, const
 }
 
 /******************************************************************************
-* Return the original alarm which the displaying alarm refers to.
-*/
-KAAlarm KAEvent::convertDisplayingAlarm() const
-{
-	KAAlarm al;
-	if (mDisplaying)
-	{
-		al = alarm(KAAlarm::DISPLAYING_ALARM);
-		if (mDisplayingFlags & REPEAT_AT_LOGIN)
-		{
-			al.mRepeatAtLogin = true;
-			al.mType = KAAlarm::AT_LOGIN__ALARM;
-		}
-		else if (mDisplayingFlags & DEFERRAL)
-		{
-			al.mDeferred = true;
-			al.mType = (mDisplayingFlags == (REMINDER | DATE_DEFERRAL)) ? KAAlarm::DEFERRED_REMINDER_DATE__ALARM
-			         : (mDisplayingFlags == (REMINDER | TIME_DEFERRAL)) ? KAAlarm::DEFERRED_REMINDER_TIME__ALARM
-			         : (mDisplayingFlags == DATE_DEFERRAL) ? KAAlarm::DEFERRED_DATE__ALARM
-			         : KAAlarm::DEFERRED_TIME__ALARM;
-		}
-		else if (mDisplayingFlags & REMINDER)
-			al.mType = KAAlarm::REMINDER__ALARM;
-		else
-			al.mType = KAAlarm::MAIN__ALARM;
-	}
-	return al;
-}
-
-/******************************************************************************
 * Reinstate the original event from the 'displaying' event.
 */
 void KAEvent::reinstateFromDisplaying(const Event* kcalEvent, QString& resourceID, bool& showEdit, bool& showDefer)
@@ -2635,6 +2605,35 @@ void KAEvent::reinstateFromDisplaying(const Event* kcalEvent, QString& resourceI
 		--mAlarmCount;
 		mUpdated = true;
 	}
+}
+
+/******************************************************************************
+* Return the original alarm which the displaying alarm refers to.
+* Note that the caller is responsible for ensuring that the event was a
+* displaying event, since this is normally called after
+* reinstateFromDisplaying(), which clears mDisplaying.
+*/
+KAAlarm KAEvent::convertDisplayingAlarm() const
+{
+	KAAlarm al = alarm(KAAlarm::DISPLAYING_ALARM);
+	if (mDisplayingFlags & REPEAT_AT_LOGIN)
+	{
+		al.mRepeatAtLogin = true;
+		al.mType = KAAlarm::AT_LOGIN__ALARM;
+	}
+	else if (mDisplayingFlags & DEFERRAL)
+	{
+		al.mDeferred = true;
+		al.mType = (mDisplayingFlags == (REMINDER | DATE_DEFERRAL)) ? KAAlarm::DEFERRED_REMINDER_DATE__ALARM
+			 : (mDisplayingFlags == (REMINDER | TIME_DEFERRAL)) ? KAAlarm::DEFERRED_REMINDER_TIME__ALARM
+			 : (mDisplayingFlags == DATE_DEFERRAL) ? KAAlarm::DEFERRED_DATE__ALARM
+			 : KAAlarm::DEFERRED_TIME__ALARM;
+	}
+	else if (mDisplayingFlags & REMINDER)
+		al.mType = KAAlarm::REMINDER__ALARM;
+	else
+		al.mType = KAAlarm::MAIN__ALARM;
+	return al;
 }
 
 /******************************************************************************
