ed325473ec7be47cf69f73dc44f7f5647d9fa09e
Martin Koller <kollix@aon.at>
2009-09-06 17:20:48 +0000

    backport of r1020581 for bug 143237
    Check for an attachment not only in the first BodyPart but in the Message itself if it has no first BodyPart.
    
    
    svn path=/branches/KDE/4.3/kdepim/; revision=1020587

--- kmail/kmmessage.cpp
+++ kmail/kmmessage.cpp
@@ -3237,18 +3237,17 @@ void KMMessage::updateBodyPart(const QString partSpecifier, const QByteArray & d
 }
 
 //-----------------------------------------------------------------------------
-void KMMessage::updateAttachmentState( DwBodyPart *part )
+void KMMessage::updateAttachmentState( DwBodyPart *partGiven )
 {
+  DwEntity *part = partGiven;
+  DwBodyPart *firstPart = partGiven;
+
   if ( !part ) {
-    part = getFirstDwBodyPart();
+    part = firstPart = getFirstDwBodyPart();
   }
 
   if ( !part ) {
-    // kDebug() <<"updateAttachmentState - no part!";
-    if ( mStatus.hasAttachment() ) {
-      toggleStatus( MessageStatus::statusHasAttachment() );
-    }
-    return;
+    part = mMsg;  // no part, use message itself
   }
 
   bool filenameEmpty = true;
@@ -3294,8 +3293,8 @@ void KMMessage::updateAttachmentState( DwBodyPart *part )
   }
 
   // next part
-  if ( part->Next() ) {
-    updateAttachmentState( part->Next() );
+  if ( firstPart && firstPart->Next() ) {
+    updateAttachmentState( firstPart->Next() );
   } else if ( attachmentState() == KMMsgAttachmentUnknown &&
               mStatus.hasAttachment() ) {
     toggleStatus( MessageStatus::statusHasAttachment() );
