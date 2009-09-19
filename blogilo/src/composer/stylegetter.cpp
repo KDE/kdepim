1992bd9bac4ff64857a632b422935537e1e8420d
Mehrdad Momeny <mehrdad.momeny@gmail.com>
2009-09-19 10:39:48 +0000

    License header files updated.
    svn path=/trunk/playground/pim/blogilo/; revision=1025643

--- blogilo/src/composer/stylegetter.cpp
+++ blogilo/src/composer/stylegetter.cpp
@@ -1,27 +1,32 @@
-/***************************************************************************
- *   Copyright (C) 2008-2009 Mehrdad Momeny <mehrdad.momeny@gmail.com>     *
- *   Copyright (C) 2008-2009 Golnaz Nilieh <g382nilieh@gmail.com>          *
- *                                                                         *
- *   This file is part of the Bilbo Blogger.                               *
- *   It is a modified version of "weblogstylegetter.cpp" from              *
- *   KBlogger project. it has been modified for use in Bilbo Blogger, at   *
- *   February 2009.                                                        *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+/*
+    This file is part of Blogilo, A KDE Blogging Client
+
+    It is a modified version of "weblogstylegetter.h" from
+    KBlogger project. it has been modified for use in Blogilo, at
+    February 2009.
+
+    Copyright (C) 2007-2008-2008 by Christian Weilbach <christian_weilbach@web.de>
+    Copyright (C) 2007-2008 Antonio Aloisio <gnuton@gnuton.org>
+    Copyright (C) 2008-2009 Mehrdad Momeny <mehrdad.momeny@gmail.com>
+    Copyright (C) 2008-2009 Golnaz Nilieh <g382nilieh@gmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2 of
+    the License or (at your option) version 3 or any later version
+    accepted by the membership of KDE e.V. (or its successor approved
+    by the membership of KDE e.V.), which shall act as a proxy
+    defined in Section 14 of version 3 of the license.
+
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see http://www.gnu.org/licenses/
+*/
 
 #include "stylegetter.h"
 
