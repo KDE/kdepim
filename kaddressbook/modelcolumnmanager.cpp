7a504beef5db7acc431e3e0fd8f4ea24d185aa04
Tobias Koenig <tokoe@kde.org>
2009-09-28 15:33:40 +0000

    Resize the header after columns has changed
    BUG: 208729
    
    svn path=/trunk/KDE/kdepim/kaddressbook/; revision=1028943

--- kaddressbook/modelcolumnmanager.cpp
+++ kaddressbook/modelcolumnmanager.cpp
@@ -26,7 +26,9 @@
 #include <klocale.h>
 
 #include <QtCore/QEvent>
+#include <QtCore/QTimer>
 #include <QtGui/QContextMenuEvent>
+#include <QtGui/QHeaderView>
 #include <QtGui/QMenu>
 #include <QtGui/QWidget>
 
@@ -143,6 +145,7 @@ bool ModelColumnManager::eventFilter( QObject *watched, QEvent* event )
           columns << Akonadi::ContactsTreeModel::Note;
 
         mModel->setColumns( columns );
+        QTimer::singleShot( 0, this, SLOT( adaptHeaderView() ) );
       }
 
       return true;
@@ -151,4 +154,11 @@ bool ModelColumnManager::eventFilter( QObject *watched, QEvent* event )
   }
 
   return false;
+}
+
+void ModelColumnManager::adaptHeaderView()
+{
+  QHeaderView *view = qobject_cast<QHeaderView*>( mWidget );
+  if ( view )
+    view->resizeSections( QHeaderView::Stretch );
 }
