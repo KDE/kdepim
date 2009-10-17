a21416c4be81e95d943ad5b26fb52296c536e3fe
Frank Osterfeld <frank.osterfeld@kdemail.net>
2009-10-17 11:12:20 +0000

    add simple example app for feeditemmodel
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1036523

---
+++ krss/examples/feeditemmodel_example/main.cpp
@@ -0,0 +1,44 @@
+#include "krss/feeditemmodel.h"
+
+#include <Akonadi/ChangeRecorder>
+#include <Akonadi/EntityDisplayAttribute>
+#include <Akonadi/ItemFetchScope>
+#include <Akonadi/Session>
+
+#include <KAboutData>
+#include <KApplication>
+#include <KCmdLineArgs>
+
+#include <QApplication>
+#include <QByteArray>
+#include <QTreeView>
+
+using namespace Akonadi;
+using namespace KRss;
+
+int main( int argc, char** argv ) {
+    const QByteArray ba = "example_feeditemmodel";
+    const KLocalizedString name = ki18n( "KRss feed list model example example" );
+    KAboutData aboutData( ba, ba, name, ba, name );
+    KCmdLineArgs::init( argc, argv, &aboutData );
+    KApplication app;
+    ItemFetchScope scope;
+    scope.fetchFullPayload( true );
+    scope.fetchAttribute<EntityDisplayAttribute>();
+
+    Session session( QByteArray( "FeedReaderApplication-" ) + QByteArray::number( qrand() ) );
+
+    ChangeRecorder recorder;
+    recorder.fetchCollection( true );
+    recorder.setItemFetchScope( scope );
+    recorder.setCollectionMonitored( Collection::root() );
+    recorder.setMimeTypeMonitored( QLatin1String( "application/rss+xml" ) );
+
+    FeedItemModel model( &session, &recorder );
+    QTreeView view;
+    view.setModel( &model );
+    view.show();
+
+    return app.exec();
+}
+
