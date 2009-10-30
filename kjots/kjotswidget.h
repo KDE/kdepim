3c014185aa81449b2c3ecf74429073e57d632175
Stephen Kelly <steveire@gmail.com>
2009-10-30 13:42:00 +0000

    Make sure the copyright notices are copied over too.
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1042682

--- kjots/kjotswidget.h
+++ kjots/kjotswidget.h
@@ -1,7 +1,11 @@
 /*
     This file is part of KJots.
 
-    Copyright (c) 2008-2009 Stephen Kelly <steveire@gmail.com>
+    Copyright (C) 1997 Christoph Neerfeld <Christoph.Neerfeld@home.ivm.de>
+    Copyright (C) 2002, 2003 Aaron J. Seigo <aseigo@kde.org>
+    Copyright (C) 2003 Stanislav Kljuhhin <crz@hot.ee>
+    Copyright (C) 2005-2006 Jaison Lee <lee.jaison@gmail.com>
+    Copyright (C) 2007-2009 Stephen Kelly <steveire@gmail.com>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
