13bed943b576437ab32fdd8c2f6a70d5def93087
Allen Winter <winter@kde.org>
2009-11-13 02:30:04 +0000

    Merged revisions 1048213 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r1048213 | tmcguire | 2009-11-12 17:20:37 -0500 (Thu, 12 Nov 2009) | 2 lines
      
      Set MBox as the first entry, it is by far the most used.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1048295

--- kmailcvt/kselfilterpage.cpp
+++ kmailcvt/kselfilterpage.cpp
@@ -49,6 +49,7 @@ KSelFilterPage::KSelFilterPage(QWidget *parent ) : KSelFilterPageDlg(parent) {
         // For now, we have to live without the warm and fuzzy feeling a refactoring might give. 
         // Patches appreciated. (danimo)
 
+        addFilter(new FilterMBox);
         addFilter(new FilterEvolution);
         addFilter(new FilterEvolution_v2);
         addFilter(new FilterKMail_maildir);
@@ -60,7 +61,6 @@ KSelFilterPage::KSelFilterPage(QWidget *parent ) : KSelFilterPageDlg(parent) {
         addFilter(new FilterOE);
 //        addFilter(new FilterOutlook);
         addFilter(new FilterPMail);
-        addFilter(new FilterMBox);
         addFilter(new FilterLNotes);
         addFilter(new FilterPlain);
 }
