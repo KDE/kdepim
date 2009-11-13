42bda7e46cdc11638c2fb0469fac9ee98cc10161
Thomas McGuire <mcguire@kde.org>
2009-11-13 13:20:35 +0000

    Fix dbus reply signature
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1048511

--- kmailcvt/filters.cxx
+++ kmailcvt/filters.cxx
@@ -213,7 +213,7 @@ void Filter::showKMailImportArchiveDialog( FilterInfo* info )
 
 
   org::kde::kmail::kmail kmail("org.kde.kmail", "/KMail", QDBusConnection::sessionBus());
-  QDBusReply<int> reply = kmail.showImportArchiveDialog();
+  QDBusReply<void> reply = kmail.showImportArchiveDialog();
 
   if ( !reply.isValid() )
   {
