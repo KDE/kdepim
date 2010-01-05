4fc54af1ef382e44cb56bb582068652b8036ccd4
Torgny Nyblom <kde@nyblom.org>
2010-01-05 16:21:03 +0000

    Remove unneeded code
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1070426

--- messageviewer/objecttreeparser.cpp
+++ messageviewer/objecttreeparser.cpp
@@ -350,10 +350,6 @@ void ObjectTreeParser::defaultHandling( KMime::Content * node, ProcessResult & r
     QString fileName = mNodeHelper->writeNodeToTempFile( node );
     QString href = "file://" + fileName;
     QByteArray cid = node->contentID()->identifier();
-    if ( cid.startsWith('<') )
-      cid = cid.mid(1);
-    if ( cid.endsWith('>') )
-      cid = cid.left( cid.length() - 1 );
     htmlWriter()->embedPart( cid, href );
     return;
   }
