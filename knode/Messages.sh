c39391466dbb39e22a3c9eef95cc3646d3c47098
Olivier Trichet <nive@nivalis.org>
2010-01-09 13:36:52 +0000

    Correct the extraction of i18n message.
    svn path=/trunk/KDE/kdepim/knode/; revision=1072147

--- knode/Messages.sh
+++ knode/Messages.sh
@@ -1,2 +1,10 @@
-#! /bin/sh
-$XGETTEXT *.cpp *.h -o $podir/knode.pot
+#!/bin/sh
+
+$EXTRACTRC \
+    $( find .                                                            \
+            -name '*.rc' -a ! -name 'headers.rc' -a ! -name 'filters.rc' \
+         -o -name '*.ui'                                                 \
+         -o -name '*.kcfg'                                               \
+    ) > rc.cpp || exit 1
+
+$XGETTEXT $(find . -name '*.cpp' -o -name '*.h') -o $podir/knode.pot
