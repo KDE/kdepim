475f14fc6c302d5efe75a9bb672be1a63842df12
Thomas McGuire <mcguire@kde.org>
2010-01-16 21:32:22 +0000

    Make sure HAVE_SYS_INOTIFY_H is actually properly defined
    svn path=/branches/work/akonadi-ports/kdepim/; revision=1075839

---
+++ messageviewer/config-messageviewer.h.cmake
@@ -0,0 +1,1 @@
+#cmakedefine HAVE_SYS_INOTIFY_H 1
--- messageviewer/editorwatcher.cpp
+++ messageviewer/editorwatcher.cpp
@@ -16,6 +16,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
+#include <config-messageviewer.h>
 #include "editorwatcher.h"
 #include "autoqpointer.h"
 
