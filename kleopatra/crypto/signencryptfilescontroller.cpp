07854ecdc39c1c2c72620ad4488b568e044a8498
Marc Mutz <mutz@kde.org>
2010-01-18 15:14:38 +0000

    SignEncryptFilesController: don't enable archiving by default
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=1076642

--- kleopatra/crypto/signencryptfilescontroller.cpp
+++ kleopatra/crypto/signencryptfilescontroller.cpp
@@ -250,7 +250,7 @@ void SignEncryptFilesController::Private::updateWizardMode() {
     default:
         assert( !"Shouldn't happen" );
     case ArchiveAllowed:
-        wizard->setCreateArchivePreset( true );
+        wizard->setCreateArchivePreset( false );
         wizard->setEncryptionUserMutable( true );
         break;
     };
