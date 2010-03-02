0d276ee811462ab1bae657fa28fb8f2840958b94
Thomas McGuire <mcguire@kde.org>
2010-03-02 19:04:04 +0000

    Allow async mode again, as we now require a gpgme version that doesn't crash.
    BUG: 208353
    
    svn path=/trunk/KDE/kdepim/messageviewer/; revision=1098056

--- messageviewer/viewer_p.cpp
+++ messageviewer/viewer_p.cpp
@@ -1166,8 +1166,7 @@ void ViewerPrivate::parseMsg()
   // show message content
   MailViewerSource otpSource( this );
   ObjectTreeParser otp( &otpSource, mNodeHelper );
-  otp.setAllowAsync( false ); // Async mode disabled for now, see bug 208353
-  //otp.setAllowAsync( true );
+  otp.setAllowAsync( true );
   otp.parseObjectTree( mMessageItem, mMessage.get() );
 
   bool emitReplaceMsgByUnencryptedVersion = false;
