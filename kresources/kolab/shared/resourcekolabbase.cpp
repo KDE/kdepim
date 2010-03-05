7b0b17f523d170bd124e842c21c027b9c4311143
Thomas McGuire <mcguire@kde.org>
2010-03-05 10:17:07 +0000

    With i18n(), you can not properly translate those strings either :)
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1099228

--- kresources/kolab/shared/resourcekolabbase.cpp
+++ kresources/kolab/shared/resourcekolabbase.cpp
@@ -232,24 +232,24 @@ QString ResourceKolabBase::findWritableResource( const ResourceType &type,
     QString errorText;
     switch( type ) {
       case Events:
-        errorText = "You have no writable event folders so saving will not be possible.\n"
-                    "Please create or activate at least one writable event folder and try again.";
+        errorText = i18n( "You have no writable event folders so saving will not be possible.\n"
+                          "Please create or activate at least one writable event folder and try again." );
         break;
       case Tasks:
-        errorText = "You have no writable task folders so saving will not be possible.\n"
-                    "Please create or activate at least one writable task folder and try again.";
+        errorText = i18n( "You have no writable task folders so saving will not be possible.\n"
+                          "Please create or activate at least one writable task folder and try again." );
         break;
       case Incidences:
-        errorText = "You have no writable calendar folder so saving will not be possible.\n"
-                    "Please create or activate at least one writable calendar folder and try again.";
+        errorText = i18n( "You have no writable calendar folder so saving will not be possible.\n"
+                          "Please create or activate at least one writable calendar folder and try again." );
         break;
       case Notes:
-        errorText = "You have no writable notes folders so saving will not be possible.\n"
-                    "Please create or activate at least one writable notes folder and try again.";
+        errorText = i18n( "You have no writable notes folders so saving will not be possible.\n"
+                          "Please create or activate at least one writable notes folder and try again." );
         break;
       case Contacts:
-        errorText = "You have no writable addressbook folder so saving will not be possible.\n"
-                    "Please create or activate at least one writable addressbook folder and try again.";
+        errorText = i18n( "You have no writable addressbook folder so saving will not be possible.\n"
+                          "Please create or activate at least one writable addressbook folder and try again." );
         break;
     }
 
