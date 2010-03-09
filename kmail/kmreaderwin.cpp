e09907e2d971d9c102765360d92464d123d1f992
Thomas McGuire <mcguire@kde.org>
2010-03-09 21:51:35 +0000

    Revert parts that shouldn't have been committed.
    svn path=/branches/KDE/4.4/kdepim/; revision=1101315

--- kmail/kmreaderwin.cpp
+++ kmail/kmreaderwin.cpp
@@ -1560,11 +1560,6 @@ void KMReaderWin::parseMsg(KMMessage* aMsg)
 
   assert(aMsg!=0);
 
-  if ( aMsg->isBeingParsed() ) {
-    kWarning() << "Recursive parsing detected, aborting!";
-    return;
-  }
-
   aMsg->setIsBeingParsed( true );
 
   if ( mRootNode && !mRootNode->processed() ) {
