05983e99748dc4c143f450d29ecd65039aeaac2e
Thomas McGuire <mcguire@kde.org>
2010-03-16 13:31:10 +0000

    SVN_MERGE: Merged revisions 1077801 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r1077801 | winterz | 2010-01-20 23:52:25 +0100 (Wed, 20 Jan 2010) | 4 lines
      
      handle addresses of the form Last,First <username@domain>
      part1 of the fix for kolab/issue3989
      MERGE: trunk,4.4
    ........
    
    svn path=/trunk/KDE/kdepim/plugins/messageviewer/; revision=1104008

--- plugins/messageviewer/bodypartformatter/attendeeselector.cpp
+++ plugins/messageviewer/bodypartformatter/attendeeselector.cpp
@@ -20,6 +20,7 @@
 #include "attendeeselector.h"
 
 #include <libkdepim/addresseelineedit.h>
+#include <KPIMUtils/Email>
 
 #include <klocale.h>
 #include <kpushbutton.h>
@@ -48,8 +49,15 @@ AttendeeSelector::AttendeeSelector(QWidget * parent)
 QStringList AttendeeSelector::attendees() const
 {
   QStringList rv;
-  for ( uint i = 0; i < ui.attendeeList->count(); ++i )
-    rv << ui.attendeeList->item( i )->text();
+  for ( uint i = 0; i < ui.attendeeList->count(); ++i ) {
+    const QString addr = ui.attendeeList->item( i )->text();
+
+    // Build a nice address for this attendee including the CN.
+    QString tname, temail;
+    KPIMUtils::extractEmailAddressAndName( addr, tname, temail );  // ignore return value
+                                                                   // which is always false
+    rv << temail;
+  }
   return rv;
 }
 
