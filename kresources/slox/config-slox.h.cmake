af1e38ff6d1e47b555199b1018126a640404765e
Tobias Koenig <tokoe@kde.org>
2010-03-18 16:45:16 +0000

    Remove the KResource based SLOX resource, we have the Akonadi based Open-Xchange resource in kdepim/runtime/resources now
    svn path=/trunk/KDE/kdepim/; revision=1104802

--- kresources/slox/config-slox.h.cmake
+++
@@ -1,6 +0,0 @@
-/* Define to 1 if you have the <sys/limits.h> header file. */
-#cmakedefine HAVE_SYS_LIMITS_H 1
-
-/* Define to 1 if you have the <values.h> header file. */
-#cmakedefine HAVE_VALUES_H 1
-
