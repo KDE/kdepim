e60574fdf3ecc41f9f6dd4025eb50c7777569981
Tobias Koenig <tokoe@kde.org>
2010-03-29 09:47:54 +0000

    add missing api docs
    svn path=/trunk/KDE/kdepim/; revision=1108568

--- libkdepim/kdateedit.h
+++ libkdepim/kdateedit.h
@@ -35,7 +35,8 @@ class QMouseEvent;
 namespace KPIM {
 
 /**
-  A date editing widget that consists of an editable combo box.
+  @short A date editing widget that consists of an editable combo box.
+
   The combo box contains the date in text form, and clicking the combo
   box arrow will display a 'popup' style date picker.
 
@@ -56,51 +57,65 @@ class KDEPIM_EXPORT KDateEdit : public QComboBox
   Q_OBJECT
 
   public:
+    /**
+     * Creates a new date edit.
+     *
+     * @param parent The parent widget.
+     */
     explicit KDateEdit( QWidget *parent = 0 );
+
+    /**
+     * Destroys the date edit.
+     */
     virtual ~KDateEdit();
 
     /**
-      @return The date entered. This date could be invalid,
-              you have to check validity yourself.
+     * Returns the date entered.
+     *
+     * @note This date could be invalid,
+     *       you have to check validity yourself.
      */
     QDate date() const;
 
     /**
-      Sets whether the widget is read-only for the user. If read-only, the
-      date pop-up is inactive, and the displayed date cannot be edited.
-
-      @param readOnly True to set the widget read-only, false to set it read-write.
+     * Sets whether the widget is read-only for the user. If read-only, the
+     * date pop-up is inactive, and the displayed date cannot be edited.
+     *
+     * @param readOnly True to set the widget read-only, false to set it read-write.
      */
     void setReadOnly( bool readOnly );
 
     /**
-      @return True if the widget is read-only, false if read-write.
+     * Returns whether the widget is read-only.
      */
     bool isReadOnly() const;
 
+    /**
+     * Shows the date picker popup menu.
+     */
     virtual void showPopup();
 
   Q_SIGNALS:
     /**
-      This signal is emitted whenever the user has entered a new date.
-      When the user changes the date by editing the line edit field,
-      the signal is not emitted until focus leaves the line edit field.
-      The passed date can be invalid.
+     * This signal is emitted whenever the user has entered a new date.
+     * When the user changes the date by editing the line edit field,
+     * the signal is not emitted until focus leaves the line edit field.
+     * The passed date can be invalid.
      */
     void dateEntered( const QDate &date );
 
     /**
-      This signal is emitted whenever the user modifies the date.
-      The passed date can be invalid.
+     * This signal is emitted whenever the user modifies the date.
+     * The passed date can be invalid.
      */
     void dateChanged( const QDate &date );
 
   public Q_SLOTS:
     /**
-      Sets the date.
-
-      @param date The new date to display. This date must be valid or
-                  it will not be set
+     * Sets the date.
+     *
+     * @param date The new date to display. This date must be valid or
+     *             it will not be set
      */
     void setDate( const QDate &date );
 
@@ -111,15 +126,15 @@ class KDEPIM_EXPORT KDateEdit : public QComboBox
     virtual void keyPressEvent( QKeyEvent* );
 
     /**
-      Sets the date, without altering the display.
-      This method is used internally to set the widget's date value.
-      As a virtual method, it allows derived classes to perform additional
-      validation on the date value before it is set. Derived classes should
-      return true if QDate::isValid(@p date) returns false.
-
-      @param date The new date to set.
-      @return True if the date was set, false if it was considered invalid and
-              remains unchanged.
+     * Sets the date, without altering the display.
+     * This method is used internally to set the widget's date value.
+     * As a virtual method, it allows derived classes to perform additional
+     * validation on the date value before it is set. Derived classes should
+     * return true if QDate::isValid(@p date) returns false.
+     *
+     * @param date The new date to set.
+     * @return True if the date was set, false if it was considered invalid and
+     *         remains unchanged.
      */
     virtual bool assignDate( const QDate &date );
 
@@ -130,7 +145,7 @@ class KDEPIM_EXPORT KDateEdit : public QComboBox
 
     Q_PRIVATE_SLOT( d, void lineEnterPressed() )
     Q_PRIVATE_SLOT( d, void slotTextChanged( const QString& ) )
-    Q_PRIVATE_SLOT( d, void dateSelected( const QDate ) )
+    Q_PRIVATE_SLOT( d, void dateSelected( const QDate& ) )
     //@endcond
 };
 
