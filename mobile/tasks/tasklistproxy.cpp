66b9e72a41026cd652e1a1d72e15da585700c08a
Bertjan Broeksema <broeksema@kde.org>
2010-04-20 12:15:53 +0000

    Implement the remainings of the TaskListProxy and fix license headers.
    svn path=/trunk/KDE/kdepim/; revision=1116866

--- mobile/tasks/tasklistproxy.cpp
+++ mobile/tasks/tasklistproxy.cpp
@@ -1,10 +1,57 @@
+/*
+    Copyright (c) 2010 Bertjan Broeksema <b.broeksema@home.nl>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
 #include "tasklistproxy.h"
+
+#include <kcal/incidence.h>
+
+using namespace Akonadi;
 
 TaskListProxy::TaskListProxy( QObject* parent ) : ListProxy( parent )
 { }
 
 QVariant TaskListProxy::data( const QModelIndex& index, int role ) const
 {
-  // TODO: Implement
-  return QVariant();
+  const Akonadi::Item item = QSortFilterProxyModel::data( index, Akonadi::EntityTreeModel::ItemRole ).value<Akonadi::Item>();
+
+  if ( item.isValid() && item.hasPayload<KCal::Incidence::Ptr>() ) {
+    const KCal::Incidence::Ptr incidence = item.payload<KCal::Incidence::Ptr>();
+    switch ( role ) {
+    case Summary:
+      return incidence->summary();
+    case Description:
+      return incidence->description();
+    }
+  }
+
+  return QSortFilterProxyModel::data(index, role);
 }
+
+void TaskListProxy::setSourceModel( QAbstractItemModel* sourceModel )
+{
+  ListProxy::setSourceModel(sourceModel);
+
+  QHash<int, QByteArray> names = roleNames();
+  names.insert( EntityTreeModel::ItemIdRole, "itemId" );
+  names.insert( Summary, "summary" );
+  names.insert( Description, "description" );
+  setRoleNames( names );
+
+  kDebug() << names << sourceModel->roleNames();
+}
+
--- mobile/tasks/tasklistproxy.h
+++ mobile/tasks/tasklistproxy.h
@@ -1,14 +1,42 @@
+/*
+    Copyright (c) 2010 Bertjan Broeksema <b.broeksema@home.nl>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
 #ifndef TASKLISTPROXY_H
 #define TASKLISTPROXY_H
 
 #include "listproxy.h"
 
+#include <akonadi/entitytreemodel.h>
+
 class TaskListProxy : public ListProxy
 {
 public:
-    explicit TaskListProxy( QObject* parent = 0 );
+  enum Role {
+    Summary = Akonadi::EntityTreeModel::UserRole,
+    Description
+  };
 
-    virtual QVariant data( const QModelIndex& index, int role = Qt::DisplayRole ) const;
+public:
+  explicit TaskListProxy( QObject* parent = 0 );
+
+  virtual QVariant data( const QModelIndex& index, int role = Qt::DisplayRole ) const;
+
+  virtual void setSourceModel( QAbstractItemModel* sourceModel );
 };
 
 #endif // TASKLISTPROXY_H
