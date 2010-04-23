f38bd883e906e8285a565ecde7ef4bd19127d295
Kevin Krammer <kevin.krammer@gmx.at>
2010-04-23 13:20:54 +0000

    Add "viewer" app for calendarviews to run them in a standalone environment. Currently hardcodes AgendaView creation.
    svn path=/trunk/KDE/kdepim/calendarviews/; revision=1117931

---
+++ calendarviews/viewerapp/main.cpp
@@ -0,0 +1,49 @@
+/*
+    Copyright (C) 2010 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.net
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include "mainwindow.h"
+
+#include <kapplication.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+#include <KLocale>
+
+static const char description[] = I18N_NOOP( "A test app for embedding calendarviews" );
+
+static const char version[] = "0.1";
+
+int main( int argc, char **argv )
+{
+    KAboutData about( "viewerapp", 0, ki18n( "ViewerApp" ), version, ki18n( description ),
+                     KAboutData::License_GPL,
+                     ki18n( "Copyright (C) 2010 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.net" ), KLocalizedString(), 0, "info@kdab.net");
+    about.addAuthor( ki18n( "Kevin Krammer" ), KLocalizedString(), "krake@kdab.com" );
+    KCmdLineArgs::init( argc, argv, &about );
+
+    //TODO add options to specify the view(s)
+
+    KApplication app;
+
+    MainWindow *widget = new MainWindow;
+
+    widget->show();
+
+    return app.exec();
+}
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
---
+++ calendarviews/viewerapp/mainwindow.cpp
@@ -0,0 +1,95 @@
+/*
+    Copyright (C) 2010 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.net
+    Author: Kevin Krammer, krake@kdab.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include "mainwindow.h"
+
+#include "agenda.h"
+#include "agendaview.h"
+
+#include <akonadi/kcal/calendar.h>
+#include <akonadi/kcal/calendarmodel.h>
+#include <akonadi/kcal/incidencemimetypevisitor.h>
+
+#include <akonadi/changerecorder.h>
+#include <akonadi/collection.h>
+#include <akonadi/control.h>
+#include <akonadi/entitydisplayattribute.h>
+#include <akonadi/entitymimetypefiltermodel.h>
+#include <akonadi/itemfetchscope.h>
+
+#include <KSystemTimeZones>
+
+using namespace Akonadi;
+
+MainWindow::MainWindow()
+  : QMainWindow(),
+    mChangeRecorder( 0 ),
+    mCalendar( 0 ),
+    mEventView( 0 )
+{
+  mUi.setupUi( this );
+
+  Akonadi::Control::widgetNeedsAkonadi( this );
+
+  QMetaObject::invokeMethod( this, "delayedInit", Qt::QueuedConnection );
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::delayedInit()
+{
+  mChangeRecorder = new ChangeRecorder( this );
+  mChangeRecorder->setCollectionMonitored( Collection::root(), true );
+
+  ItemFetchScope scope;
+  scope.fetchFullPayload( true );
+  scope.fetchAttribute<EntityDisplayAttribute>();
+
+  mChangeRecorder->fetchCollection( true );
+  mChangeRecorder->setItemFetchScope( scope );
+
+  mChangeRecorder->setMimeTypeMonitored( IncidenceMimeTypeVisitor::eventMimeType(), true );
+
+  CalendarModel* calendarModel = new CalendarModel( mChangeRecorder, this );
+
+  // no collections, just items
+  calendarModel->setCollectionFetchStrategy( EntityTreeModel::InvisibleFetch );
+
+  EntityMimeTypeFilterModel *filterModel = new EntityMimeTypeFilterModel( this );
+  filterModel->setHeaderGroup( EntityTreeModel::ItemListHeaders );
+  filterModel->setSourceModel( calendarModel );
+  filterModel->setSortRole( CalendarModel::SortRole );
+
+  mCalendar = new Akonadi::Calendar( calendarModel, filterModel, KSystemTimeZones::local() );
+
+  mEventView = new AgendaView( this );
+  mEventView->setCalendar( mCalendar );
+  mEventView->setDateRange( KDateTime::currentLocalDateTime().addDays( -1 ),
+                            KDateTime::currentLocalDateTime().addDays( 1 ) );
+  mEventView->updateConfig();
+
+  setCentralWidget( mEventView );
+  mEventView->show();
+}
+
+#include "mainwindow.moc"
+
+// kate: space-indent on; indent-width 2; replace-tabs on;
