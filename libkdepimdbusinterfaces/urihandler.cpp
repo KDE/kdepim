ec3db8d6ad382030b7ba99c4aa00670a719561ec
Laurent Montel <montel@kde.org>
2010-05-03 21:58:48 +0000

    Forward port: Fix bug #235353 In new kaddressbook we can't call "kaddressbook --editor-only --uid" We don't have this option now. I think it will a good idea to readd it for trunk. For the moment to avoid a messagebox I open just kaddressbook, not very good CCMAIL: tokoe@kde.org
    svn path=/trunk/KDE/kdepim/libkdepimdbusinterfaces/; revision=1122445

--- libkdepimdbusinterfaces/urihandler.cpp
+++ libkdepimdbusinterfaces/urihandler.cpp
@@ -76,8 +76,11 @@ bool UriHandler::process( const QString &uri )
       */
       QString iconPath =
         KIconLoader::global()->iconPath( "view-pim-contacts", KIconLoader::SizeSmall );
+#if 0      
       QString tmpStr = "kaddressbook --editor-only --uid ";
       tmpStr += KShell::quoteArg( uri.mid( ::qstrlen( "uid:" ) ) );
+#endif
+      QString tmpStr = "kaddressbook";      
       KRun::runCommand( tmpStr, "KAddressBook", iconPath, 0 );
       return true;
     }
