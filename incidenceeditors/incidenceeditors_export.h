a2f611e1b45cd80caf250e9a068bef2edc872e31
Laurent Montel <montel@kde.org>
2010-05-03 17:49:40 +0000

    Update version with script from kdesdk
    svn path=/trunk/KDE/kdepim/; revision=1122364

--- incidenceeditors/incidenceeditors_export.h
+++ incidenceeditors/incidenceeditors_export.h
@@ -25,12 +25,16 @@
 
 #ifndef INCIDENCEEDITORS_EXPORT
 # if defined(MAKE_INCIDENCEEDITORS_LIB)
-   /* We are building this library */
+   /* We are building this library */ 
 #  define INCIDENCEEDITORS_EXPORT KDE_EXPORT
 # else
-   /* We are using this library */
+   /* We are using this library */ 
 #  define INCIDENCEEDITORS_EXPORT KDE_IMPORT
 # endif
 #endif
+
+# ifndef INCIDENCEEDITORS_EXPORT_DEPRECATED
+#  define INCIDENCEEDITORS_EXPORT_DEPRECATED KDE_DEPRECATED INCIDENCEEDITORS_EXPORT
+# endif
 
 #endif
