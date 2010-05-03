7d9a83355f27693b81ed0afffaff7e60bb36acb0
Laurent Montel <montel@kde.org>
2010-05-03 17:51:28 +0000

    Update version with kdesdk script
    svn path=/trunk/KDE/kdepim/calendarviews/; revision=1122366

--- calendarviews/agenda/eventviews_export.h
+++ calendarviews/agenda/eventviews_export.h
@@ -25,12 +25,16 @@
 
 #ifndef EVENTVIEWS_EXPORT
 # if defined(MAKE_EVENTVIEWS_LIB)
-   /* We are building this library */
+   /* We are building this library */ 
 #  define EVENTVIEWS_EXPORT KDE_EXPORT
 # else
-   /* We are using this library */
+   /* We are using this library */ 
 #  define EVENTVIEWS_EXPORT KDE_IMPORT
 # endif
 #endif
+
+# ifndef EVENTVIEWS_EXPORT_DEPRECATED
+#  define EVENTVIEWS_EXPORT_DEPRECATED KDE_DEPRECATED EVENTVIEWS_EXPORT
+# endif
 
 #endif
