8d693f81ec044a694d6eff10df73dc49331feab1
Stephen Kelly <steveire@gmail.com>
2010-05-04 14:49:52 +0000

    Update this class to the kdelibs version using KModelIndexProxyMapper.
    svn path=/trunk/KDE/kdepim/akonadi/akonadi_next/; revision=1122718

--- akonadi_next/kproxyitemselectionmodel.cpp
+++ akonadi_next/kproxyitemselectionmodel.cpp
@@ -22,239 +22,77 @@
 
 #include "kproxyitemselectionmodel.h"
 
-#include "kdebug.h"
-
-using namespace Future;
+#include "kmodelindexproxymapper.h"
 
 namespace Future
 {
-
 class KProxyItemSelectionModelPrivate
 {
 public:
-  KProxyItemSelectionModelPrivate(KProxyItemSelectionModel *proxySelectionModel, QAbstractItemModel *model,
-                                          QItemSelectionModel *selectionModel)
-    : q_ptr( proxySelectionModel ), m_model(model), m_proxySelector(selectionModel), m_ignoreCurrentChanged(false)
-  {
-    createProxyChain();
-  }
+    KProxyItemSelectionModelPrivate(KProxyItemSelectionModel *proxySelectionModel, QAbstractItemModel *model,
+                                    QItemSelectionModel *selectionModel)
+      : q_ptr(proxySelectionModel),
+        m_model(model),
+        m_proxySelector(selectionModel),
+        m_ignoreCurrentChanged(false),
+        m_indexMapper(new Future::KModelIndexProxyMapper(model, selectionModel->model(), proxySelectionModel))
+    {
+    }
 
-  void createProxyChain();
+    Q_DECLARE_PUBLIC(KProxyItemSelectionModel)
+    KProxyItemSelectionModel * const q_ptr;
 
-  bool assertValid();
+    QList<const QAbstractProxyModel *> m_proxyChainUp;
+    QList<const QAbstractProxyModel *> m_proxyChainDown;
 
-  Q_DECLARE_PUBLIC(KProxyItemSelectionModel)
-  KProxyItemSelectionModel * const q_ptr;
-
-  QList<const QAbstractProxyModel *> m_proxyChainUp;
-  QList<const QAbstractProxyModel *> m_proxyChainDown;
-
-  QAbstractItemModel *m_model;
-  QItemSelectionModel *m_proxySelector;
-  bool m_ignoreCurrentChanged;
+    QAbstractItemModel *m_model;
+    QItemSelectionModel *m_proxySelector;
+    bool m_ignoreCurrentChanged;
+    Future::KModelIndexProxyMapper *m_indexMapper;
 };
 
 }
 
-/*
+using namespace Future;
 
-  The idea here is that <tt>this</tt> selection model and proxySelectionModel might be in different parts of the
-  proxy chain. We need to build up to two chains of proxy models to create mappings between them.
-
-  Example 1:
-
-     Root model
-          |
-        /    \
-    Proxy 1   Proxy 3
-       |       |
-    Proxy 2   Proxy 4
-
-  Need Proxy 1 and Proxy 2 in one chain, and Proxy 3 and 4 in the other.
-
-  Example 2:
-
-     Root model
-          |
-        Proxy 1
-          |
-        Proxy 2
-        /     \
-    Proxy 3   Proxy 6
-       |       |
-    Proxy 4   Proxy 7
-       |
-    Proxy 5
-
-  We first build the chain from 1 to 5, then start building the chain from 7 to 1. We stop when we find that proxy 2 is
-  already in the first chain.
-
-  Stephen Kelly, 30 March 2010.
-*/
-
-void KProxyItemSelectionModelPrivate::createProxyChain()
+KProxyItemSelectionModel::KProxyItemSelectionModel(QAbstractItemModel *model, QItemSelectionModel *proxySelector, QObject *parent)
+        : QItemSelectionModel(model, parent),
+        d_ptr(new KProxyItemSelectionModelPrivate(this, model, proxySelector))
 {
-  const QAbstractItemModel *selectionTargetModel = m_proxySelector->model();
-
-  if (m_model == selectionTargetModel)
-    return;
-
-  QList<const QAbstractProxyModel *> proxyChainDown;
-
-  const QAbstractProxyModel *selectionTargetProxyModel = qobject_cast<const QAbstractProxyModel*>( selectionTargetModel );
-
-  while( selectionTargetProxyModel )
-  {
-    proxyChainDown.prepend( selectionTargetProxyModel );
-
-    selectionTargetProxyModel = qobject_cast<const QAbstractProxyModel*>(selectionTargetProxyModel->sourceModel());
-
-    if ( selectionTargetProxyModel == m_model )
-    {
-      m_proxyChainDown = proxyChainDown;
-      return;
-    }
-  }
-
-  const QAbstractItemModel *sourceModel = m_model;
-  const QAbstractProxyModel *sourceProxyModel = qobject_cast<const QAbstractProxyModel*>( sourceModel );
-
-  while( sourceProxyModel )
-  {
-    m_proxyChainUp.append( sourceProxyModel );
-
-    sourceProxyModel = qobject_cast<const QAbstractProxyModel*>(sourceProxyModel->sourceModel());
-
-    int targetIndex = proxyChainDown.indexOf(sourceProxyModel );
-
-    if ( targetIndex != -1 )
-    {
-      m_proxyChainDown = proxyChainDown.mid(targetIndex, proxyChainDown.size());
-      return;
-    }
-  }
-  m_proxyChainDown = proxyChainDown;
-  Q_ASSERT(assertValid());
-}
-
-bool KProxyItemSelectionModelPrivate::assertValid()
-{
-  if ( m_proxyChainDown.isEmpty())
-  {
-    Q_ASSERT( !m_proxyChainUp.isEmpty() );
-    Q_ASSERT( m_proxyChainUp.last()->sourceModel() == m_proxySelector->model() );
-  }
-  else if ( m_proxyChainUp.isEmpty())
-  {
-    Q_ASSERT( !m_proxyChainDown.isEmpty() );
-    Q_ASSERT( m_proxyChainDown.first()->sourceModel() == m_model );
-  } else {
-    Q_ASSERT( m_proxyChainDown.first()->sourceModel() == m_proxyChainUp.last()->sourceModel() );
-  }
-  return true;
-}
-
-KProxyItemSelectionModel::KProxyItemSelectionModel( QAbstractItemModel *model, QItemSelectionModel *proxySelector, QObject *parent)
-  : QItemSelectionModel(model, parent),
-    d_ptr(new KProxyItemSelectionModelPrivate(this, model, proxySelector))
-{
-  connect(proxySelector, SIGNAL(selectionChanged(QItemSelection,QItemSelection)), SLOT(sourceSelectionChanged(QItemSelection,QItemSelection)));
-}
-
-QItemSelection KProxyItemSelectionModel::mapSelectionFromSource(const QModelIndex& sourceIndex) const
-{
-  return mapSelectionFromSource(QItemSelection(sourceIndex, sourceIndex));
-}
-
-QItemSelection KProxyItemSelectionModel::mapSelectionFromSource(const QItemSelection& sourceSelection) const
-{
-  Q_D(const KProxyItemSelectionModel);
-
-  QItemSelection seekSelection = sourceSelection;
-  QListIterator<const QAbstractProxyModel*> iUp(d->m_proxyChainUp);
-
-  while (iUp.hasNext())
-  {
-    const QAbstractProxyModel *proxy = iUp.next();
-    seekSelection = proxy->mapSelectionToSource(seekSelection);
-  }
-
-  QListIterator<const QAbstractProxyModel*> iDown(d->m_proxyChainDown);
-
-  while (iDown.hasNext())
-  {
-    const QAbstractProxyModel *proxy = iDown.next();
-    seekSelection = proxy->mapSelectionFromSource(seekSelection);
-  }
-
-  Q_ASSERT( ( !seekSelection.isEmpty() && seekSelection.first().model() == d->m_proxySelector->model() ) || true );
-  return seekSelection;
-}
-
-QItemSelection KProxyItemSelectionModel::mapSelectionToSource(const QModelIndex& sourceIndex) const
-{
-  return mapSelectionToSource(QItemSelection(sourceIndex, sourceIndex));
-}
-
-QItemSelection KProxyItemSelectionModel::mapSelectionToSource(const QItemSelection& proxySelection) const
-{
-  Q_D(const KProxyItemSelectionModel);
-
-  QItemSelection seekSelection = proxySelection;
-  QListIterator<const QAbstractProxyModel*> iDown(d->m_proxyChainDown);
-
-  iDown.toBack();
-  while (iDown.hasPrevious())
-  {
-    const QAbstractProxyModel *proxy = iDown.previous();
-    seekSelection = proxy->mapSelectionToSource(seekSelection);
-  }
-
-  QListIterator<const QAbstractProxyModel*> iUp(d->m_proxyChainUp);
-
-  iUp.toBack();
-  while (iUp.hasPrevious())
-  {
-    const QAbstractProxyModel *proxy = iUp.previous();
-    seekSelection = proxy->mapSelectionFromSource(seekSelection);
-  }
-
-  Q_ASSERT( ( !seekSelection.isEmpty() && seekSelection.first().model() == d->m_model ) || true );
-  return seekSelection;
+    connect(proxySelector, SIGNAL(selectionChanged(QItemSelection, QItemSelection)), SLOT(sourceSelectionChanged(QItemSelection, QItemSelection)));
 }
 
 void KProxyItemSelectionModel::select(const QModelIndex &index, QItemSelectionModel::SelectionFlags command)
 {
-  Q_D(KProxyItemSelectionModel);
-  // When an item is removed, the current index is set to the top index in the model.
-  // That causes a selectionChanged signal with a selection which we do not want.
-  if ( d->m_ignoreCurrentChanged )
-  {
-    return;
-  }
-  QItemSelectionModel::select(index, command);
-  if (index.isValid())
-    d->m_proxySelector->select(mapSelectionFromSource(index), command);
-  else
-  {
-    d->m_proxySelector->clearSelection();
-  }
+    Q_D(KProxyItemSelectionModel);
+    // When an item is removed, the current index is set to the top index in the model.
+    // That causes a selectionChanged signal with a selection which we do not want.
+    if (d->m_ignoreCurrentChanged) {
+        return;
+    }
+    QItemSelectionModel::select(index, command);
+    if (index.isValid())
+        d->m_proxySelector->select(d->m_indexMapper->mapSelectionLeftToRight(QItemSelection(index, index)), command);
+    else {
+        d->m_proxySelector->clearSelection();
+    }
 }
 
 void KProxyItemSelectionModel::select(const QItemSelection &selection, QItemSelectionModel::SelectionFlags command)
 {
-  Q_D(KProxyItemSelectionModel);
-  d->m_ignoreCurrentChanged = true;
-  QItemSelectionModel::select(selection, command);
-  d->m_proxySelector->select(mapSelectionFromSource(selection), command);
-  d->m_ignoreCurrentChanged = false;
+    Q_D(KProxyItemSelectionModel);
+    d->m_ignoreCurrentChanged = true;
+    QItemSelectionModel::select(selection, command);
+    d->m_proxySelector->select(d->m_indexMapper->mapSelectionLeftToRight(selection), command);
+    d->m_ignoreCurrentChanged = false;
 }
 
 void KProxyItemSelectionModel::sourceSelectionChanged(const QItemSelection& selected, const QItemSelection& deselected)
 {
-  QItemSelection mappedDeselection = mapSelectionToSource(deselected);
-  QItemSelection mappedSelection = mapSelectionToSource(selected);
+    Q_D(KProxyItemSelectionModel);
+    const QItemSelection mappedDeselection = d->m_indexMapper->mapSelectionRightToLeft(deselected);
+    const QItemSelection mappedSelection = d->m_indexMapper->mapSelectionRightToLeft(selected);
 
-  QItemSelectionModel::select(mappedDeselection, Deselect);
-  QItemSelectionModel::select(mappedSelection, Select);
+    QItemSelectionModel::select(mappedDeselection, Deselect);
+    QItemSelectionModel::select(mappedSelection, Select);
 }
--- akonadi_next/kproxyitemselectionmodel.h
+++ akonadi_next/kproxyitemselectionmodel.h
@@ -29,52 +29,92 @@
 
 namespace Future
 {
-
 class KProxyItemSelectionModelPrivate;
 
 /**
   @brief Makes it possible to share a selection in multiple views which do not have the same source model
 
-  Although multiple views can share the same QItemSelectionModel, the views then need to have the same source model.
+  Although <a href="http://doc.trolltech.com/4.6/model-view-view.html#sharing-selections-between-views">multiple views can share the same QItemSelectionModel</a>, the views then need to have the same source model.
 
   If there is a proxy model between the model and one of the views, or different proxy models in each, this class makes
   it possible to share the selection between the views.
 
+  @image html kproxyitemselectionmodel-simple.png "Sharing a QItemSelectionModel between views on the same model is trivial"
+  @image html kproxyitemselectionmodel-error.png "If a proxy model is used, it is no longer possible to share the QItemSelectionModel directly"
+  @image html kproxyitemselectionmodel-solution.png "A KProxyItemSelectionModel can be used to map the selection through the proxy model"
+
+  @code
+    QAbstractItemModel *model = getModel();
+
+    QSortFilterProxyModel *proxy = new QSortFilterProxyModel();
+    proxy->setSourceModel(model);
+
+    QTreeView *view1 = new QTreeView(splitter);
+    view1->setModel(model);
+
+    KProxyItemSelectionModel *view2SelectionModel = new KProxyItemSelectionModel( proxy, view1->selectionModel());
+
+    QTreeView *view2 = new QTreeView(splitter);
+    // Note that the QAbstractItemModel passed to KProxyItemSelectionModel must be the same as what is used in the view
+    view2->setModel(proxy);
+    view2->setSelectionModel( view2SelectionModel );
+  @endcode
+
+  @image html kproxyitemselectionmodel-complex.png "Arbitrarily complex proxy configurations on the same root model can be used"
+
+  @code
+    QAbstractItemModel *model = getModel();
+
+    QSortFilterProxyModel *proxy1 = new QSortFilterProxyModel();
+    proxy1->setSourceModel(model);
+    QSortFilterProxyModel *proxy2 = new QSortFilterProxyModel();
+    proxy2->setSourceModel(proxy1);
+    QSortFilterProxyModel *proxy3 = new QSortFilterProxyModel();
+    proxy3->setSourceModel(proxy2);
+
+    QTreeView *view1 = new QTreeView(splitter);
+    view1->setModel(proxy3);
+
+    QSortFilterProxyModel *proxy4 = new QSortFilterProxyModel();
+    proxy4->setSourceModel(model);
+    QSortFilterProxyModel *proxy5 = new QSortFilterProxyModel();
+    proxy5->setSourceModel(proxy4);
+
+    KProxyItemSelectionModel *view2SelectionModel = new KProxyItemSelectionModel( proxy5, view1->selectionModel());
+
+    QTreeView *view2 = new QTreeView(splitter);
+    // Note that the QAbstractItemModel passed to KProxyItemSelectionModel must be the same as what is used in the view
+    view2->setModel(proxy5);
+    view2->setSelectionModel( view2SelectionModel );
+  @endcode
+
+  See also <a href="http://websvn.kde.org/trunk/KDE/kdelibs/kdeui/tests/proxymodeltestapp/proxyitemselectionwidget.cpp?view=markup">kdelibs/kdeui/tests/proxymodeltestapp/proxyitemselectionwidget.cpp</a>.
+
   @since 4.5
 
 */
-class AKONADI_NEXT_EXPORT KProxyItemSelectionModel : public QItemSelectionModel
+class KDEUI_EXPORT KProxyItemSelectionModel : public QItemSelectionModel
 {
-  Q_OBJECT
+    Q_OBJECT
 public:
-  /**
-    Constructor.
-  */
-  KProxyItemSelectionModel( QAbstractItemModel *targetModel, QItemSelectionModel *proxySelector, QObject *parent = 0);
+    /**
+      Constructor.
+    */
+    KProxyItemSelectionModel(QAbstractItemModel *targetModel, QItemSelectionModel *proxySelector, QObject *parent = 0);
 
-  /* reimp */ void select(const QModelIndex &index, QItemSelectionModel::SelectionFlags command);
-  /* reimp */ void select(const QItemSelection &selection, QItemSelectionModel::SelectionFlags command);
-
-private:
-  /**
-    Maps the @p sourceIndex though the proxy models to a selection in the proxySelector
-  */
-  QItemSelection mapSelectionFromSource( const QModelIndex &sourceIndex ) const;
-  QItemSelection mapSelectionFromSource( const QItemSelection &sourceSelection ) const;
-  QItemSelection mapSelectionToSource( const QModelIndex &sourceIndex ) const;
-  QItemSelection mapSelectionToSource( const QItemSelection &sourceSelection ) const;
+    /* reimp */ void select(const QModelIndex &index, QItemSelectionModel::SelectionFlags command);
+    /* reimp */ void select(const QItemSelection &selection, QItemSelectionModel::SelectionFlags command);
 
 private slots:
-  void sourceSelectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
+    void sourceSelectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
 
 protected:
-  KProxyItemSelectionModelPrivate * const d_ptr;
+    KProxyItemSelectionModelPrivate * const d_ptr;
 
 private:
-  Q_DECLARE_PRIVATE(KProxyItemSelectionModel)
+    Q_DECLARE_PRIVATE(KProxyItemSelectionModel)
 };
 
 }
 
 #endif
-
