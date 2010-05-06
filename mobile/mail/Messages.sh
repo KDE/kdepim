7bd8d403d522d9acd16bca36543a6b9f95a4c8bc
Volker Krause <vkrause@kde.org>
2010-05-06 07:38:14 +0000

    Add message extraction file, the QML part is still disabled since it requires a patch for l10n-kde4/scripts which hasn't been committed yet.
    svn path=/trunk/KDE/kdepim/mobile/mail/; revision=1123561

---
+++ mobile/mail/Messages.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+$EXTRACTRC --ignore-no-input `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
+# TODO: enable once the QML extraction support has been committed to l10n-kde4/scripts
+#$EXTRACTQML `find . -name '*.qml'` >> rc.cpp || exit 11
+$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/kmail-mobile.pot
+rm -f rc.cpp
