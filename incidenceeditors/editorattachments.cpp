5c5c3c34fe3449a2c03fa7b0c5d8476f6d9acd7a
Allen Winter <winter@kde.org>
2010-05-10 23:01:04 +0000

    merge forward SVN commit 1120617 by winterz:
    add the [Delete] shortcut to the remove content menu option.
    kolab/issue4340
    
    svn path=/trunk/KDE/kdepim/; revision=1125197

--- incidenceeditors/editorattachments.cpp
+++ incidenceeditors/editorattachments.cpp
@@ -505,7 +505,7 @@ EditorAttachments::EditorAttachments( int spacing, QWidget *parent )
                                       "&Remove" ), this );
   connect( mDeleteAction, SIGNAL(triggered(bool)), this, SLOT(slotRemove()) );
   ac->addAction( "remove", mDeleteAction );
-  mDeleteAction->setShortcut(
+  mDeleteAction->setShortcut( Qt::Key_Delete );
   mPopupMenu->addAction( mDeleteAction );
   mPopupMenu->addSeparator();
 
