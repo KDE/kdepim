f7a09232b793793382c4b3acfbc2f7821654877d
Bertjan Broeksema <broeksema@kde.org>
2010-05-13 10:14:38 +0000

    Disable the first refactoring attempt.
    svn path=/trunk/KDE/kdepim/; revision=1126217

--- incidenceeditors/eventeditor.cpp
+++ incidenceeditors/eventeditor.cpp
@@ -145,7 +145,7 @@ void EventEditor::setupGeneral()
 
   QFrame *topFrame = new QFrame();
   mTabWidget->addTab( topFrame, i18nc( "@title:tab general event settings", "&General" ) );
-  mTabWidget->addTab( new EventGeneralEditor( this ), "&New General" );
+//   mTabWidget->addTab( new EventGeneralEditor( this ), "&New General" );
   topFrame->setWhatsThis( i18nc( "@info:whatsthis",
                                  "The General tab allows you to set the most "
                                  "common options for the event." ) );
--- incidenceeditors/todoeditor.cpp
+++ incidenceeditors/todoeditor.cpp
@@ -119,11 +119,11 @@ void TodoEditor::init()
 void TodoEditor::setupGeneral()
 {
   mGeneral = new EditorGeneralTodo( this );
-  mNewGeneral = new TodoGeneralEditor( this );
+//   mNewGeneral = new TodoGeneralEditor( this );
 
   QFrame *topFrame = new QFrame();
   mTabWidget->addTab( topFrame, i18nc( "@title:tab general to-do settings", "&General" ) );
-  mTabWidget->addTab( mNewGeneral, "&New General" );
+//   mTabWidget->addTab( mNewGeneral, "&New General" );
 
   QVBoxLayout *topLayout = new QVBoxLayout( topFrame );
 
@@ -293,7 +293,7 @@ bool TodoEditor::read( const Item &todoItem, const QDate &date, bool tmpl )
   }
 
   mGeneral->readTodo( todo.get(), date, tmpl );
-  mNewGeneral->load( todo, date, tmpl );
+//   mNewGeneral->load( todo, date, tmpl );
 #ifdef HAVE_QT3SUPPORT
   mDetails->readIncidence( todo.get() );
 #endif
