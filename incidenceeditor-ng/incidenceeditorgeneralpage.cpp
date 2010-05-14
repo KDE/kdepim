c197f7629b6802cbb6cc6876826e266e9b366779
Bertjan Broeksema <broeksema@kde.org>
2010-05-14 08:46:15 +0000

    Start a new general page based on IncidenceEditorsNG.
    svn path=/trunk/KDE/kdepim/incidenceeditors/incidenceeditor-ng/; revision=1126537

---
+++ incidenceeditor-ng/incidenceeditorgeneralpage.cpp
@@ -0,0 +1,46 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include "incidenceeditorgeneralpage.h"
+
+#include <QtGui/QGridLayout>
+
+#include "incidencegeneraleditor.h"
+
+using namespace IncidenceEditorsNG;
+
+IncidenceEditorGeneralPage::IncidenceEditorGeneralPage( QWidget *parent )
+  : CombinedIncidenceEditor( parent )
+{
+  QVBoxLayout *layout = new QVBoxLayout( this );
+
+  IncidenceGeneralEditor *ieGeneral = new IncidenceGeneralEditor( this );
+  layout->addWidget( ieGeneral );
+
+  QSpacerItem *verticalSpacer =
+    new QSpacerItem( 20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding );
+  layout->addItem( verticalSpacer );
+}
+
+void IncidenceEditorGeneralPage::load( KCal::Incidence::ConstPtr incidence )
+{ }
+
+void IncidenceEditorGeneralPage::save( KCal::Incidence::Ptr incidence )
+{ }
---
+++ incidenceeditor-ng/incidenceeditorgeneralpage.h
@@ -0,0 +1,40 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#ifndef INCIDENCEEDITORGENERALPAGE_H
+#define INCIDENCEEDITORGENERALPAGE_H
+
+#include "combinedincidenceeditor.h"
+
+namespace IncidenceEditorsNG {
+
+class IncidenceEditorGeneralPage : public CombinedIncidenceEditor
+{
+  Q_OBJECT
+  public:
+    IncidenceEditorGeneralPage( QWidget *parent = 0 );
+
+    virtual void load( KCal::Incidence::ConstPtr incidence );
+    virtual void save( KCal::Incidence::Ptr incidence );
+};
+
+} // IncidenceEditorsNG
+
+#endif // INCIDENCEEDITORGENERALPAGE_H
