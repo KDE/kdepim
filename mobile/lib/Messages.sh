68fcf6198ec1e8c848db63e835bf1b22c77fb55b
Volker Krause <vkrause@kde.org>
2010-05-17 08:53:08 +0000

    enable QML message extraction now that the necessary patches are in l10n-kde4/scripts
    svn path=/trunk/KDE/kdepim/; revision=1127647

--- mobile/lib/Messages.sh
+++ mobile/lib/Messages.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 $EXTRACTRC --ignore-no-input `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
-# TODO: enable once the QML extraction support has been committed to l10n-kde4/scripts
-#$EXTRACTQML `find . -name '*.qml'` >> rc.cpp || exit 11
+$EXTRACTQML `find . -name '*.qml'` >> rc.cpp || exit 11
 $XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/libkdepimmobileui.pot
 rm -f rc.cpp
