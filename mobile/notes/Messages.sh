c9996f532b47ec2646d0bae67311ab8aad256b20
Laurent Montel <montel@kde.org>
2010-05-18 11:37:26 +0000

    Fix catalog name
    svn path=/trunk/KDE/kdepim/; revision=1128128

--- mobile/notes/Messages.sh
+++ mobile/notes/Messages.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 $EXTRACTRC --ignore-no-input `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
 $EXTRACTQML `find . -name '*.qml'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/kjots-mobile.pot
+$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/notes-mobile.pot
 rm -f rc.cpp
