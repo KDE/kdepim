b09c7f7cc4f983b4cfce0b641403fa4c5fc2cbe5
Timothy Pearson <kb9vqf@pearsoncomputing.net>
2010-05-24 23:28:52 +0000

    More compilation fixes
    svn path=/branches/trinity/kdepim/; revision=1130285

--- kresources/caldav/reader.cpp
+++ kresources/caldav/reader.cpp
@@ -16,6 +16,7 @@
 
 #include "reader.h"
 #include <kdebug.h>
+#include <string>
 
 /*=========================================================================
 | NAMESPACE
--- kresources/caldav/writer.cpp
+++ kresources/caldav/writer.cpp
@@ -16,6 +16,7 @@
 
 #include "writer.h"
 #include <kdebug.h>
+#include <string>
 
 /*=========================================================================
 | DEFINES
