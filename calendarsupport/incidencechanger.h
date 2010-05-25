5a4c8c819096f3b9514a6539fd969c43346af1b0
Sergio Luis Martins <iamsergio@gmail.com>
2010-05-25 14:09:11 +0000

    Renamed canDelete() so it's more explicit what it does.
    svn path=/trunk/KDE/kdepim/akonadi/kcal/; revision=1130485

--- calendarsupport/incidencechanger.h
+++ calendarsupport/incidencechanger.h
@@ -119,7 +119,7 @@ class AKONADI_KCAL_NEXT_EXPORT IncidenceChanger : public QObject
      * This is more accurate than querying the ETM because when a delete
      * job ends the ETM still has the item for a short period of time.
      */
-    bool canDelete( Akonadi::Item::Id ) const;
+    bool wasntDeleted( Akonadi::Item::Id ) const;
 
   public Q_SLOTS:
     void cancelAttendees( const Akonadi::Item &incidence );
