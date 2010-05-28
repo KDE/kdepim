22ed26cb802e733dc1ff2ef04306b0b33a7cba83
Bjoern Ricks <bjoern.ricks@intevation.de>
2010-05-28 09:21:22 +0000

    use shell scripts to set LD_LIBRARY_PATH for qt 4.7 and run mobile applications on maemo
    svn path=/branches/work/komo/kdepim/; revision=1131483

---
+++ mobile/calendar/korganizer-mobile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+QT_DIR=/opt/qt4-maemo5
+
+echo $LD_LIBRARY_PATH | grep "$QT_DIR" > /dev/null 2>&1
+
+if [ "$?" -ne "0"  ]; then
+	export LD_LIBRARY_PATH=$QT_DIR:$LD_LIBRARY_PATH
+fi
+
+/usr/bin/korganizer-mobile
