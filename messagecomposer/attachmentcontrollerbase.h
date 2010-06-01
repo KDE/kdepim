4b13a660e5f7ffe46c6266e09b3fb8e49ff9249d
Volker Krause <vkrause@kde.org>
2010-06-01 07:40:56 +0000

    fix kmail-mobile build
    svn path=/trunk/KDE/kdepim/messagecomposer/; revision=1133092

--- messagecomposer/attachmentcontrollerbase.h
+++ messagecomposer/attachmentcontrollerbase.h
@@ -74,7 +74,7 @@ class MESSAGECOMPOSER_EXPORT AttachmentControllerBase : public QObject
     void addAttachment( const KUrl &url );
     void addAttachments( const KUrl::List &urls );
     void showAttachPublicKeyDialog();
-    virtual void attachMyPublicKey() = 0;
+    virtual void attachMyPublicKey() {};
 
   signals:
     void actionsCreated();
