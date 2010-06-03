4fd338ff3f7ef4142669d0ea41c09fcd506340fd
Bertjan Broeksema <broeksema@kde.org>
2010-06-03 08:38:06 +0000

    Extract item logic (e.g. creation, modification and moving) from EventEditorfor reuse in mobile
    svn path=/branches/work/komo/kdepim/; revision=1134035

---
+++ incidenceeditors/incidenceeditor-ng/itemeditor.cpp
@@ -0,0 +1,206 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#include "itemeditor.h"
+
+#include <QtCore/QPointer>
+#include <QtGui/QMessageBox>
+
+#include <KLocale>
+
+#include <Akonadi/Item>
+#include <Akonadi/ItemCreateJob>
+#include <Akonadi/ItemFetchJob>
+#include <Akonadi/ItemFetchScope>
+#include <Akonadi/ItemModifyJob>
+#include <Akonadi/ItemMoveJob>
+#include <Akonadi/Monitor>
+#include <Akonadi/Session>
+
+using namespace Akonadi;
+
+/// ItemEditorPrivate
+
+class ItemEditorPrivate
+{
+  ItemEditor *q;
+
+  public:
+    Item mItem;
+    Monitor *mItemMonitor;
+    ItemEditorUi *mItemUi;
+
+  public:
+    ItemEditorPrivate( ItemEditor *qq );
+    void itemFetchResult( KJob *job );
+    void itemMoveResult( KJob *job );
+    void setupMonitor();
+};
+
+ItemEditorPrivate::ItemEditorPrivate( ItemEditor *qq )
+  : q( qq ), mItemMonitor( 0 )
+{ }
+
+void ItemEditorPrivate::itemFetchResult( KJob *job )
+{
+  Q_ASSERT( job );
+
+  if ( job->error() ) {
+    mItemUi->reject( ItemEditorUi::ItemFetchFailed, job->errorString() );
+    return;
+  }
+
+  ItemFetchJob *fetchJob = qobject_cast<ItemFetchJob*>( job );
+  if ( fetchJob->items().isEmpty() ) {
+    mItemUi->reject( ItemEditorUi::ItemFetchFailed );
+    return;
+  }
+
+  Item item = fetchJob->items().first();
+  if ( mItemUi->hasSupportedPayload( item ) ) {
+    q->load( item );
+  } else {
+    mItemUi->reject( ItemEditorUi::ItemHasInvalidPayload );
+  }
+}
+
+void ItemEditorPrivate::itemMoveResult( KJob *job )
+{
+  Q_ASSERT( job );
+
+  if ( job->error() ) {
+    // TODO: What is reasonable behavior at this point?
+//     mItemUi->reject( ItemEditorUi::ItemFetchFailed, job->errorString() );
+    return;
+  }
+}
+
+void ItemEditorPrivate::setupMonitor()
+{
+  delete mItemMonitor;
+  mItemMonitor = new Akonadi::Monitor;
+  mItemMonitor->ignoreSession( Akonadi::Session::defaultSession() );
+  mItemMonitor->itemFetchScope().fetchFullPayload();
+  if ( mItem.isValid() )
+    mItemMonitor->setItemMonitored( mItem );
+
+  q->connect( mItemMonitor, SIGNAL( itemChanged( const Akonadi::Item&, const QSet<QByteArray>& ) ),
+              SLOT( itemChanged( const Akonadi::Item&, const QSet<QByteArray>& ) ) );
+}
+
+void ItemEditorPrivate::itemChanged( const Akonadi::Item &item,
+                                     const QSet<QByteArray> &partIdentifiers )
+{
+  if ( mItemUi->containsPayloadIdentifiers( partIdentifiers ) ) {
+    QPointer<QMessageBox> dlg = new QMessageBox( q ); //krazy:exclude=qclasses
+    dlg->setIcon( QMessageBox::Question );
+    dlg->setInformativeText( i18n( "The item has been changed by another application.\nWhat should be done?" ) );
+    dlg->addButton( i18n( "Take over changes" ), QMessageBox::AcceptRole );
+    dlg->addButton( i18n( "Ignore and Overwrite changes" ), QMessageBox::RejectRole );
+
+    if ( dlg->exec() == QMessageBox::AcceptRole ) {
+      Akonadi::ItemFetchJob *job = new Akonadi::ItemFetchJob( mItem );
+      job->fetchScope().fetchFullPayload();
+      job->fetchScope().setAncestorRetrieval( Akonadi::ItemFetchScope::Parent );
+
+      mItem = item;
+
+      q->load( mItem );
+    } else {
+      mItem.setRevision( item.revision() );
+      q->save();
+    }
+
+    delete dlg;
+  }
+
+  // Overwrite or not, we need to update the revision and the remote id to be able
+  // to store item later on.
+  mItem.setRevision( item.revision() );
+}
+
+/// ItemEditor
+
+ItemEditor::ItemEditor( ItemEditorUi *ui )
+  : d_ptr( new ItemEditorPrivate( this ) )
+{
+  Q_D( ItemEditor );
+  d->mItemUi = ui;
+}
+
+
+ItemEditor::~ItemEditor()
+{
+  delete d_ptr;
+}
+
+
+void ItemEditor::load( const Akonadi::Item &item )
+{
+  Q_ASSERT( item.isValid() );
+  Q_D( ItemEditor );
+
+  if ( item.hasPayload() ) {
+    d->mItem = item;
+    d->mItemUi->load( item );
+    d->setupMonitor();
+  } else {
+    ItemFetchJob *job = new ItemFetchJob( item, this );
+    job->fetchScope().fetchFullPayload();
+    job->fetchScope().setAncestorRetrieval( Akonadi::ItemFetchScope::Parent );
+
+    connect( job, SIGNAL(result(KJob*)), SLOT(itemFetchResult(KJob*)) );
+    return;
+  }
+}
+
+void ItemEditor::save()
+{
+  Q_D( ItemEditor );
+
+  d->mItem = d->mItemUi->save( d->mItem );
+
+  if ( d->mItem.isValid() ) { // A valid item needs to be modified.
+    Q_ASSERT( d->mItem.parentCollection().isValid() );
+
+    if ( d->mItem.parentCollection() == d->mItemUi->selectedCollection() ) {
+      ItemModifyJob *modifyJob = new ItemModifyJob( d->mItem );
+      connect( modifyJob, SIGNAL(result(KJob*)), SLOT(modifyFinished(KJob*)) );
+    } else {
+      Q_ASSERT( d->mItemUi->selectedCollection().isValid() );
+
+      if ( d->mItemUi->isDirty() )
+        Q_ASSERT_X( false, "ItemEditor::save()",
+                    "Moving of modified items not implemented yet" );
+        // 1) ItemModify( d->mItem );
+        // 2) ItemMove( d->mItem,d->mItemUi->selectedCollection() )
+      else {
+        ItemMoveJob *imjob = new ItemMoveJob( d->mItem, d->mItemUi->selectedCollection() );
+        connect( imjob, SIGNAL(result(KJob*)), SLOT(itemMoveResult(KJob*)) );
+      }
+    }
+  } else { // An invalid item needs to be created.
+    Q_ASSERT( d->mItemUi->selectedCollection().isValid() );
+
+    ItemCreateJob *createJob =
+      new ItemCreateJob( d->mItem, d->mItemUi->selectedCollection() );
+    connect( createJob, SIGNAL(result(KJob*)), SLOT(modifyFinished(KJob*)) );
+  }
+}
---
+++ incidenceeditors/incidenceeditor-ng/itemeditor.h
@@ -0,0 +1,133 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+#ifndef ITEMEDITOR_H
+#define ITEMEDITOR_H
+
+#include <QtCore/QObject>
+
+class ItemEditorPrivate;
+
+namespace Akonadi {
+
+class Collection;
+class Item;
+class ItemEditorUi;
+
+/**
+ * Helper class for creating dialogs that let the user create and edit Akonadi
+ * items with a specific payload (e.g. events, contacts, etc). This class supports
+ * editting of one item at a time and hanldes all Akonadi specific logic like
+ * Item creation, Item modifying and monitoring of changes to the item during
+ * editing.
+ */
+// template <typename PayloadT>
+class ItemEditor : public QObject
+{
+  Q_OBJECT
+  public:
+    /**
+     * Creates an ItemEditor for a new Item.
+     */
+    ItemEditor( ItemEditorUi *ui );
+
+    /**
+     * Destructs the ItemEditor. Unsaved changes will get lost at this point.
+     */
+    ~ItemEditor();
+
+    /**
+     * Loads the @param item into the editor. The item passed <em>must</em> be
+     * a valid item. When the payload is not set it will be fetched.
+     */
+    void load( const Akonadi::Item &item );
+
+    /**
+     * Saves the new or modified item. This mCollectionethod does nothing when isDirty
+     * returns false.
+     */
+    void save();
+
+  private:
+    ItemEditorPrivate *d_ptr;
+    Q_DECLARE_PRIVATE( ItemEditor );
+    Q_DISABLE_COPY( ItemEditor );
+};
+
+class ItemEditorUi
+{
+  public:
+    enum RejectReason {
+      ItemFetchFailed,      ///> Either the fetchjob failed or no items where returned
+      ItemHasInvalidPayload ///> The fetched item has an invalid payload
+    };
+
+    virtual ~ItemEditorUi() {}
+
+    /**
+     * Returns wether or not the identifier set contains payload identifiers that
+     * are displayed/editable in the Gui.
+     */
+    virtual bool containsPayloadIdentifiers( const QSet<QByteArray> &partIdentifiers ) const = 0;
+
+    /**
+     * Returns wether or not @param item has a payload type that is supported by
+     * the gui.
+     */
+    virtual bool hasSupportedPayload( const Akonadi::Item &item ) const = 0;
+
+    /**
+     * Returns wether or not the values in the ui differ from the original (i.e.
+     * either an empty or a loaded item). This method <em>only</em> involves
+     * payload fields. I.e. if only the collection in which the item should be
+     * stored has changed, this method should return false.
+     */
+    virtual bool isDirty() const = 0;
+
+    /**
+     * Fills the ui with the values of the payload of @param item. The item is
+     * guaranteed to have a payload.
+     */
+    virtual void load( const Akonadi::Item &item ) = 0;
+
+    /**
+     * Stores the values of the ui into the payload of @param item and returns the
+     * item with an updated payload. The returned item must have a valid mimetype
+     * too.
+     */
+    virtual Akonadi::Item save( const Akonadi::Item &item ) = 0;
+
+
+    /**
+     * Returns the currently sellected collection in which the item will be stored.
+     */
+    virtual Akonadi::Collection selectedCollection() const = 0;
+    
+    /**
+     * This function is called if for some reason the creation or editting of the
+     * item cannot be continued. The implementing class must abort editting at
+     * this point.
+     */
+    virtual void reject( RejectReason reason, const QString &errorMessage = QString() );
+};
+
+}
+
+#endif // ITEMEDITOR_H
