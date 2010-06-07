0d6162b2a07d55a597ee0085547f32a4b31bc004
Volker Krause <vkrause@kde.org>
2010-06-07 14:29:03 +0000

    Set the Maemo5 default palette when running on the desktop. Helps to detect color bugs way earlier.
    svn path=/branches/work/komo/kdepim/; revision=1135530

--- mobile/lib/kdeclarativeapplication.cpp
+++ mobile/lib/kdeclarativeapplication.cpp
@@ -29,6 +29,35 @@ KDeclarativeApplication::KDeclarativeApplication()
   QFont f = font();
   f.setPointSize( 16 );
   setFont( f );
+
+  QPalette p;
+  p.setColor( QPalette::Window,          QColor( 0,     0,   0 ) );
+  p.setColor( QPalette::WindowText,      QColor( 255, 255, 255 ) );
+  p.setColor( QPalette::Base,            QColor( 255, 255, 255 ) );
+  p.setColor( QPalette::AlternateBase,   QColor( 239, 239, 239 ) );
+  p.setColor( QPalette::Text,            QColor(   0,   0,   0 ) );
+  p.setColor( QPalette::Button,          QColor(   0,   0,   0 ) );
+  p.setColor( QPalette::ButtonText,      QColor( 255, 255, 255 ) );
+  p.setColor( QPalette::BrightText,      QColor( 255, 255, 255 ) );
+  p.setColor( QPalette::Light,           QColor(   0,   0,   0 ) );
+  p.setColor( QPalette::Midlight,        QColor( 203, 199, 196 ) );
+  p.setColor( QPalette::Dark,            QColor(   0,   0,   0 ) );
+  p.setColor( QPalette::Mid,             QColor( 184, 181, 178 ) );
+  p.setColor( QPalette::Shadow,          QColor(   0,   0,   0 ) );
+  p.setColor( QPalette::Highlight,       QColor(  55, 180, 252 ) );
+  p.setColor( QPalette::HighlightedText, QColor(   0,  16,  26 ) );
+  p.setColor( QPalette::Link,            QColor(   0,   0, 255 ) );
+  p.setColor( QPalette::LinkVisited,     QColor( 255,   0, 255 ) );
+
+  p.setColor( QPalette::Disabled, QPalette::WindowText,      QColor( 127, 127, 127 ) );
+  p.setColor( QPalette::Disabled, QPalette::Text,            QColor( 127, 127, 127 ) );
+  p.setColor( QPalette::Disabled, QPalette::ButtonText,      QColor( 127, 127, 127 ) );
+  p.setColor( QPalette::Disabled, QPalette::Highlight,       QColor( 252, 252, 252 ) );
+  p.setColor( QPalette::Disabled, QPalette::HighlightedText, QColor(  26,  26,  26 ) );
+
+  setPalette( p );
+
+  setStyle( "plastique" ); // to avoid oxygen artefacts
 #endif
 
   // start with the oxygen palette (which is not necessarily the default on all platforms)
