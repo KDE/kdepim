f792f53fb8f22782b63c097d0e21192daab965c7
Ryan James Rix <ry@n.rix.si>
2010-06-07 22:18:02 +0000

    - Added a default layout - Added a shell KCM
    svn path=/branches/work/soc-pim/kdepim/; revision=1135675

---
+++ kontact/plugins/summary/kcmkontactsummary.cpp
@@ -0,0 +1,52 @@
+/*
+ *   Copyright 2010 Ryan Rix <ry@n.rix.si>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "kcmkontactsummary.h"
+
+#include <KontactInterface/Plugin>
+
+#include <KAboutData>
+#include <KCModule>
+#include <KComponentData>
+#include <KDialog>
+#include <KIcon>
+#include <KLocale>
+#include <KPluginInfo>
+#include <KService>
+#include <KServiceTypeTrader>
+
+#include <QLabel>
+#include <QVBoxLayout>
+
+extern "C"
+{
+    KDE_EXPORT KCModule* create_kontactsummary( QWidget* parent, const char* )
+    {
+        KComponentData inst( "kcmkontactsummary" );
+        return new KCMKontactSummary( inst, parent );
+    }
+}
+
+KCMKontactSummary::KCMKontactSummary( KComponentData comp, QWidget* parent )
+    : KCModule(comp,parent)
+{
+    
+}
+
+#include "kcmkontactsummary.moc"
