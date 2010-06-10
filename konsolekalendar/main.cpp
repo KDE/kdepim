0c811129e780027fb35ef0578a19ee7a811e6282
Sergio Luis Martins <iamsergio@gmail.com>
2010-06-10 20:38:26 +0000

    compile with gcc 4.5
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1136816

--- konsolekalendar/main.cpp
+++ konsolekalendar/main.cpp
@@ -799,13 +799,13 @@ int main( int argc, char *argv[] )
   if ( !args->isSet( "time" )     && !args->isSet( "epoch-start" ) &&
        !args->isSet( "end-time" ) && !args->isSet( "epoch-end" ) ) {
     // set default start date/time
-    startdatetime = QDateTime::QDateTime( startdate, starttime );
+    startdatetime = QDateTime( startdate, starttime );
     kdDebug() << "main | datetimestamp | "
               << "setting startdatetime from "
               << "default startdate (today) and starttime"
               << endl;
     // set default end date/time
-    enddatetime = QDateTime::QDateTime( enddate, endtime );
+    enddatetime = QDateTime( enddate, endtime );
     kdDebug() << "main | datetimestamp | "
               << "setting enddatetime from "
               << "default enddate (today) and endtime"
@@ -814,13 +814,13 @@ int main( int argc, char *argv[] )
 
   // Set startdatetime, enddatetime if still necessary
   if ( startdatetime.isNull() ) {
-    startdatetime = QDateTime::QDateTime( startdate, starttime );
+    startdatetime = QDateTime( startdate, starttime );
     kdDebug() << "main | datetimestamp | "
               << "setting startdatetime from startdate and starttime"
               << endl;
   }
   if ( enddatetime.isNull() ) {
-    enddatetime = QDateTime::QDateTime( enddate, endtime );
+    enddatetime = QDateTime( enddate, endtime );
     kdDebug() << "main | datetimestamp | "
               << "setting enddatetime from enddate and endtime"
               << endl;
