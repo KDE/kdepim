c7fde18c7b3fd1abb6deaed42c8a8d0479d9ce0a
Ryan James Rix <ry@n.rix.si>
2010-06-11 06:53:05 +0000

    A buttload of changes. The applet is actually nice now :)
    svn path=/branches/work/soc-pim/kdepim/; revision=1136925
    (cherry picked from commit 83c00121886171e33e3a491a842f558418be0dec)

---
+++ kontact/plugins/summary/applets/special_dates/specialdatewidget.cpp
@@ -0,0 +1,110 @@
+/*
+ *   Copyright 2010 Ryan Rix <ry@n.rix.si>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "specialdatewidget.h"
+
+#include <Plasma/Svg>
+#include <Plasma/Theme>
+#include <Plasma/DataEngine>
+#include <Plasma/IconWidget>
+#include <Plasma/Label>
+
+#include <KIcon>
+#include <KUrl>
+
+#include <QGraphicsLinearLayout>
+#include <QDate>
+
+SpecialDateWidget::SpecialDateWidget(QGraphicsWidget* parent, QString text, QString iconName, KUrl uri, QDate date )
+    : Plasma::GroupBox(parent),
+      m_date(date),
+      m_icon(0),
+      m_layout(0),
+      m_uri(uri)
+{    
+    // create icon
+    KIcon ic = KIcon(iconName);
+    
+    setIcon(new Plasma::IconWidget( ic, "" ));
+    m_layout = new QGraphicsLinearLayout(Qt::Horizontal, this);
+    
+    m_layout->addItem( icon() );
+    
+    // create text    
+    Plasma::Label* label = new Plasma::Label();
+    label->setText(text);
+    m_layout->addItem( label );
+    
+    // create daysTo
+    QDate currentDate = QDate::currentDate();
+    
+    int daysTo = currentDate.daysTo(date);
+    
+    Plasma::Label* dayLabel = new Plasma::Label();
+    QString dateString = QString("<p style='%2'>%1</p>").arg(daysTo).arg( daysTo > 1 ? "" : "background-color:red;");
+    dayLabel->setScaledContents(true);
+    dayLabel->setText(dateString);
+    m_layout->addItem( dayLabel );
+    
+    // Cleanup the layout
+    m_layout->setAlignment(icon(),Qt::AlignLeft);
+    m_layout->setAlignment(label,Qt::AlignLeft);
+    m_layout->setAlignment(dayLabel,Qt::AlignLeft);
+    
+    connect(icon(), SIGNAL(clicked()), this, SLOT(click()));
+    
+}
+
+void SpecialDateWidget::click()
+{
+    kDebug() << "click :)";
+}
+
+Plasma::IconWidget* SpecialDateWidget::icon()
+{
+    return m_icon;
+}
+
+void SpecialDateWidget::setIcon(Plasma::IconWidget* icon)
+{
+    m_icon = icon;
+}
+
+QDate SpecialDateWidget::date()
+{
+    return m_date;
+}
+
+void SpecialDateWidget::setDate(QDate date)
+{
+    m_date = date;
+}
+
+KUrl SpecialDateWidget::uri()
+{
+    return m_uri;
+}
+
+void SpecialDateWidget::setUri(KUrl uri)
+{
+    m_uri = uri;
+}
+
+
+#include "specialdatewidget.moc"
