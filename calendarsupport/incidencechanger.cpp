437c4c8802d4800e7ef98831ab3be4f9fbafec56
Sergio Luis Martins <iamsergio@gmail.com>
2010-06-15 20:17:54 +0000

    include kcalprefs.h in incidencechanger.cpp explicitly and remove it from calendarAdaptor's header file.
    svn path=/trunk/KDE/kdepim/akonadi/kcal/; revision=1138356

--- calendarsupport/incidencechanger.cpp
+++ calendarsupport/incidencechanger.cpp
@@ -25,6 +25,7 @@
 #include "incidencechanger.h"
 #include "incidencechanger_p.h"
 #include "groupware.h"
+#include "kcalprefs.h"
 
 #include <akonadi/kcal/calendar.h>
 #include <akonadi/kcal/calendaradaptor.h>
