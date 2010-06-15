70145a9426d8e9d933c94bbc30d66d636f73489d
Sergio Luis Martins <iamsergio@gmail.com>
2010-06-15 12:15:54 +0000

    SVN_MERGE: Merged revisions 1137641 via svnmerge from svn+ssh://smartins@svn.kde.org/home/kde/trunk/KDE/kdepim
    ........
      r1137641 | smartins | 2010-06-13 20:18:36 +0000 (Sun, 13 Jun 2010) | 8 lines
      
      Fwd port commit 1137634 by smartins from branch 4.4 to trunk:
      
      Keep the real sub-category in the UserRole so we can use it for filtering.
      
      MERGE: none ( no sub-categories in enterprise )
      
      CCBUG: 171484
    ........
    
    svn path=/branches/work/komo/kdepim/; revision=1138224

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -7,7 +7,7 @@ set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 2000)
 
 # Warnings that will be ignored
 set(CTEST_CUSTOM_WARNING_EXCEPTION
-  ${CTEST_CUSTOM_WARNING_EXCEPTION}
+  
   "groupwise/soap"
   "mk4storage"
   "too big, try a different debug format"
@@ -15,7 +15,7 @@ set(CTEST_CUSTOM_WARNING_EXCEPTION
 
 # Errors that will be ignored
 set(CTEST_CUSTOM_ERROR_EXCEPTION
-  ${CTEST_CUSTOM_ERROR_EXCEPTION}
+  
   "ICECC"
   "Segmentation fault"
   )
