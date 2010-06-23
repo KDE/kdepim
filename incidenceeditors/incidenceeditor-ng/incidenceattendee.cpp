e3ba0afc1ad28f4cb69ac2d59c207e35854652c7
Casey Link <unnamedrambler@gmail.com>
2010-06-23 20:02:20 +0000

    add copyrights from the old editor which i've copied some code from.
    svn path=/branches/work/komo/kdepim/; revision=1141913

--- incidenceeditors/incidenceeditor-ng/incidenceattendee.cpp
+++ incidenceeditors/incidenceeditor-ng/incidenceattendee.cpp
@@ -2,6 +2,11 @@
     Copyright (C) 2010 Casey Link <unnamedrambler@gmail.com>
     Copyright (C) 2009-2010 Klaralvdalens Datakonsult AB, a KDAB Group company <info@kdab.net>
 
+    Based on old attendeeeditor.cpp:
+    Copyright (c) 2000,2001 Cornelius Schumacher <schumacher@kde.org>
+    Copyright (C) 2003-2004 Reinhold Kainhofer <reinhold@kainhofer.com>
+    Copyright (c) 2007 Volker Krause <vkrause@kde.org>
+
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
     the Free Software Foundation; either version 2 of the License, or (at your
