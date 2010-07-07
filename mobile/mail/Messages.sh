f8e8535be0cd7a019804a60504b36cd9b7d10bff
Volker Krause <vkrause@kde.org>
2010-07-07 14:40:16 +0000

    SVN_SILENT prepare for merge
    svn path=/trunk/KDE/kdepim/mobile/; revision=1147182

--- mobile/mail/Messages.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-$EXTRACTRC --ignore-no-input `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
-$EXTRACTQML `find . -name '*.qml'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/kmail-mobile.pot
-rm -f rc.cpp
