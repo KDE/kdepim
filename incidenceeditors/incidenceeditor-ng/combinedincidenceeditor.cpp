4e7fb755fdd677380c98c70f3c803828831f16aa
Bertjan Broeksema <broeksema@kde.org>
2010-07-09 09:27:14 +0000

    Store the loaded incidence so that type() also works on combined incidence editors
    svn path=/trunk/KDE/kdepim/; revision=1147844

--- incidenceeditors/incidenceeditor-ng/combinedincidenceeditor.cpp
+++ incidenceeditors/incidenceeditor-ng/combinedincidenceeditor.cpp
@@ -77,6 +77,7 @@ void CombinedIncidenceEditor::handleDirtyStatusChange( bool isDirty )
 
 void CombinedIncidenceEditor::load( KCal::Incidence::ConstPtr incidence )
 {
+  mLoadedIncidence = incidence;
   foreach ( IncidenceEditor *editor, mCombinedEditors  ) {
     // load() may fire dirtyStatusChanged(), reset mDirtyEditorCount to make sure
     // we don't end up with an invalid dirty count.
