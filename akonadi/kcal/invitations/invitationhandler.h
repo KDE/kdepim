352fe0a5e32dba07991bf5ff30f05497501fbcd4
Romain Pokrzywka <romain@kdab.net>
2010-07-20 09:36:22 +0000

    Calendar needs to be disambiguated, as it can be Akonadi::Calendar or KCal::Calendar
    svn path=/trunk/KDE/kdepim/; revision=1152073

--- akonadi/kcal/invitations/invitationhandler.h
+++ akonadi/kcal/invitations/invitationhandler.h
@@ -116,7 +116,7 @@ Q_SIGNALS:
     void editorRequested( const KCal::Incidence::Ptr &incidence );
 
 private:
-    class Private;
+    struct Private;
     Private * const d;
     Q_DISABLE_COPY( InvitationHandler )
 };
