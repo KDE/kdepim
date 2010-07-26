e5e93b0d4f4b2b9f44d23ea180a023361713ae4b
Andras Mantia <amantia@kde.org>
2010-07-26 12:19:36 +0000

    Add missing files.
    svn path=/trunk/KDE/kdepim/mobile/; revision=1154868

---
+++ mobile/calendar/calendarinterface.cpp
@@ -0,0 +1,113 @@
+/* -*- mode: C++; c-file-style: "gnu" -*-
+  Copyright (C) 2010 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.net
+  Copyright (c) 2010 Andras Mantia <andras@kdab.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+
+
+#include "calendarinterface.h"
+#include "mainview.h"
+
+#include <KDebug>
+#include <QGraphicsItem>
+
+void CalendarInterface::showDate(const QDate& date)
+{
+  emit showDateSignal(QVariant::fromValue<QDate>(date));
+}
+
+void CalendarInterface::goDate(const QDate& )
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::goDate(const QString& )
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openEventEditor(const QString& )
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openEventEditor(const QString& summary, const QString& description, const QStringList& attachments)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openEventEditor(const QString& summary, const QString& description, const QStringList& attachments, const QStringList& attendees)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openEventEditor(const QString& summary, const QString& description, const QString& uri, const QString& file, const QStringList& attendees, const QString& attachmentMimetype)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openJournalEditor(const QDate& date)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openJournalEditor(const QString& text, const QDate& date)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openJournalEditor(const QString& text)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openTodoEditor(const QString& )
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openTodoEditor(const QString& summary, const QString& description, const QStringList& attachments)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+
+void CalendarInterface::openTodoEditor(const QString& summary, const QString& description, const QStringList& attachments, const QStringList& attendees)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::openTodoEditor(const QString& summary, const QString& description, const QString& uri, const QString& file, const QStringList& attendees, const QString& attachmentMimetype, bool isTask)
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::showEventView()
+{
+  emit showEventViewSignal();
+}
+
+void CalendarInterface::showJournalView()
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
+void CalendarInterface::showTodoView()
+{
+  kWarning() << Q_FUNC_INFO << " is not yet implemented in korganzier-mobile";
+}
+
---
+++ mobile/calendar/calendarinterface.h
@@ -0,0 +1,81 @@
+/* -*- mode: C++; c-file-style: "gnu" -*-
+  Copyright (C) 2010 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.net
+  Copyright (c) 2010 Andras Mantia <andras@kdab.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+
+#ifndef CALENDARINTERFACE_H
+#define CALENDARINTERFACE_H
+#include <QObject>
+class QDate;
+
+/** Implement the www.kde.korganizer/Calendar interface for korganizer-mobile */
+class CalendarInterface : public QObject
+{
+  Q_OBJECT
+public:
+    CalendarInterface(QObject * parent = 0) :QObject(parent){};
+public slots:
+  void showDate(const QDate& date);
+    void openEventEditor( const QString &);
+    void openEventEditor( const QString &summary,
+                          const QString &description,
+                          const QStringList &attachments );
+    void openEventEditor( const QString &summary,
+                          const QString &description,
+                          const QStringList &attachments,
+                          const QStringList &attendees );
+    void openEventEditor( const QString &summary,
+                          const QString &description,
+                          const QString &uri,
+                          const QString &file,
+                          const QStringList &attendees,
+                          const QString &attachmentMimetype );
+
+    void openTodoEditor( const QString &);
+    void openTodoEditor( const QString &summary,
+                         const QString &description,
+                         const QStringList &attachments );
+    void openTodoEditor( const QString &summary,
+                         const QString &description,
+                         const QStringList &attachments,
+                         const QStringList &attendees );
+    void openTodoEditor( const QString &summary,
+                         const QString &description,
+                         const QString &uri,
+                         const QString &file,
+                         const QStringList &attendees,
+                         const QString &attachmentMimetype,
+                         bool isTask );
+
+    void openJournalEditor( const QDate &date );
+    void openJournalEditor( const QString &text, const QDate &date );
+    void openJournalEditor( const QString &text );
+
+    void showJournalView();
+    void showTodoView();
+    void showEventView();
+
+    void goDate( const QDate &);
+    void goDate( const QString &);
+
+signals:
+    void showDateSignal(const QVariant& date);
+    void showEventViewSignal();
+};
+
+#endif // CALENDARINTERFACE_H
