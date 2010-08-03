b5b181356ea5359a408f473be7fd0013d620e4ce
Thomas McGuire <mcguire@kde.org>
2010-08-03 11:42:44 +0000

    SVN_MERGE: Merged revisions 1158664 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/trunk/KDE/kdepim
    ........
      r1158664 | tmcguire | 2010-08-03 13:36:51 +0200 (Tue, 03 Aug 2010) | 7 lines
      
      Don't show the attachment description unless we don't have a name or a filename
      
      SVN_MERGE of r1158480
      
      in the mimepart tree, don't show the attachment description unless
      we don't have a name or a filename.  same algorithm we use in the header.
      kolab/issue3908
    ........
    
    svn path=/branches/KDE/4.5/kdepim/; revision=1158668

--- messageviewer/mimetreemodel.cpp
+++ messageviewer/mimetreemodel.cpp
@@ -19,6 +19,8 @@
 
 #include "mimetreemodel.h"
 
+#include "nodehelper.h"
+
 #include <kmime/kmime_content.h>
 #include <KMime/Message>
 
@@ -53,21 +55,13 @@ class MimeTreeModel::Private
     {
       if ( content->hasHeader( "Subject" ) )
         return content->getHeaderByType( "Subject" )->asUnicodeString();
+      const QString name = NodeHelper::fileName( content );
+      if ( !name.isEmpty() )
+        return name;
       if ( content->contentDescription( false ) ) {
         const QString desc = content->contentDescription()->asUnicodeString();
         if ( !desc.isEmpty() )
           return desc;
-      }
-      if ( content->contentType( false ) ) {
-        const QString name = content->contentType()->name();
-        if ( !name.isEmpty() )
-          return name;
-      }
-      if ( content->contentDisposition( false ) )
-      {
-        const QString name = content->contentDisposition()->filename();
-        if ( !name.isEmpty() )
-          return name;
       }
       return i18n( "body part" );
     }
