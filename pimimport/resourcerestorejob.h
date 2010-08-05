43045d99b51585fa9133339fdaea56a4585c1618
Maciek Zarzycki <zarzych@gmail.com>
2010-08-05 13:42:19 +0000

    pimimport: initial implementation of ResourceRestoreJob
    svn path=/branches/work/soc-pim/kdepim/; revision=1159486
    (cherry picked from commit 612eb50903bacc65715184070746309fedfbdabd)

---
+++ pimimport/resourcerestorejob.h
@@ -0,0 +1,27 @@
+#ifndef RESOURCERESTOREJOB_H
+#define RESOURCERESTOREJOB_H
+
+#include <kjob.h>
+#include <akonadi/agentinstance.h>
+#include <QtCore/QDir>
+
+class ResourceRestoreJob : public KJob
+{
+  Q_OBJECT
+
+public:
+  ResourceRestoreJob( const QDir &path, QObject *parent = 0);
+
+  virtual void start();
+
+private slots:
+  void createResult( KJob *job );
+  void restoreMainCollection();
+  void mainFetchResult( KJob *job );
+
+private:
+  Akonadi::AgentInstance m_instance;
+  QDir m_path;
+};
+
+#endif // RESOURCERESTOREJOB_H
