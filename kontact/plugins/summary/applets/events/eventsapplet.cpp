41ccb706c7ae061ed1e993d56e9325d0f9c2b8d0
Ryan James Rix <ry@n.rix.si>
2010-08-05 22:14:32 +0000

    Soo.... I rewrote the applet using the calendar dataengine and it works, I think. The Plasma::Frame's don't populate with data but everything else works afaict. It's rough but it's good ;)
    svn path=/branches/work/soc-pim/kdepim/; revision=1159661
    (cherry picked from commit 0c22b5f1098c5f23a983aca2b4924bd3d1025276)

--- kontact/plugins/summary/applets/events/eventsapplet.cpp
+++ kontact/plugins/summary/applets/events/eventsapplet.cpp
@@ -19,27 +19,71 @@
  */
 
 #include "eventsapplet.h"
+#include "eventwidget.h"
+
+#include <plasma/dataengine.h>
+#include <plasma/widgets/scrollwidget.h>
 
 #include <kconfig.h>
 #include <kconfiggroup.h>
 #include <kdebug.h>
 #include <kstandarddirs.h>
+#include <kdatetime.h>
 
 #include <akonadi/collection.h>
 #include <akonadi/collectionfetchjob.h>
 #include <akonadi/collectionfetchscope.h>
 
+#include <qtimer.h>
+#include <qgraphicslinearlayout.h>
+
 K_EXPORT_PLASMA_APPLET(events, EventsApplet)
 
 EventsApplet::EventsApplet( QObject* parent, QVariantList args )
+    : Plasma::Applet( parent, args ),
+      m_timer( 0 ),
+      m_scrollWidget( 0 ),
+      m_scroller( 0 ),
+      m_scrollerLayout( 0 )
 {
 }
 
 void EventsApplet::init()
 {
-    m_collectionJobsInProgress = 0;
-
+    ui();
     configChanged();
+    updateEvents();
+    updateUI();
+
+    m_timer = new QTimer(this);
+    m_timer->setSingleShot(true);
+    m_timer->setInterval(1000);
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(updateUI()));
+
+    setBusy( true );
+}
+
+void EventsApplet::ui()
+{
+    // Start by setting up the Applet with a layout
+    QGraphicsLinearLayout* lay = new QGraphicsLinearLayout(Qt::Vertical);
+    setLayout(lay);
+
+    // Create the scroll widget and set the desired size policy if different than the default
+    // this ScrollWidget will handle the scrollbars as well actual scrolling, including
+    // kinetic and gesture-based (e.g. flicking) scrolling for us!
+    m_scrollWidget = new Plasma::ScrollWidget(this);
+    m_scrollWidget->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+
+    // Create the QGraphicsWidget we will be scrolling across
+    m_scroller = new QGraphicsWidget(m_scroller);
+    m_scrollerLayout = new QGraphicsLinearLayout(Qt::Vertical, m_scroller);
+
+    // Put that widget inside the ScrollWidget
+    m_scrollWidget->setWidget(m_scroller);
+
+    // add the scrollwidget to the top level layout of the Applet
+    lay->addItem(m_scrollWidget);
 }
 
 void EventsApplet::configChanged()
@@ -49,65 +93,58 @@ void EventsApplet::configChanged()
     // This can be either "Todo", "Events" or "Agenda" (merged of both)
     m_incidenceType = cg.readEntry( "incidenceType", "Agenda" );
     m_numDays = cg.readEntry( "numDays", 7 );
-
-    m_collectionIds = cg.readEntry( "collections", QStringList() );
-
-    // Now find and load the collections we need
-    Akonadi::Collection::List collections;
-
-    if ( m_collectionIds.length() > 0 ) {
-        m_noCollections = false;
-    } else {
-        m_noCollections = true;
-        getCollectionFromKorg();
-    }
-
-    foreach( const QString Id, m_collectionIds ) {
-        Akonadi::Collection collection = Akonadi::Collection( Id.toInt() );
-        collections << collection;
-        kDebug() << Id << "created collection for Id";
-    }
-
-    Akonadi::CollectionFetchJob* job = new Akonadi::CollectionFetchJob( collections, this );
-    connect( job, SIGNAL( result(  KJob* ) ), this, SLOT(  collectionFetchResult(  KJob* ) ) );
-
-    setBusy( m_collectionJobsInProgress );
 }
 
-void EventsApplet::getCollectionFromKorg()
+void EventsApplet::updateEvents()
 {
-    QString korgrc = KStandardDirs::locate( "config", "korganizerrc" );
+    QString source = "events:%2:%3";
+    QDate currentDate = QDate::currentDate();
+    QDate endDate = currentDate.addDays( m_numDays );
 
-    if ( korgrc.length() > 0 ) {
-        KSharedConfigPtr config = KSharedConfig::openConfig( korgrc );
-        KConfigGroup cg = config->group( "GlobalCollectionSelection" );
+    source = source.arg( currentDate.toString( Qt::ISODate ) );
+    source = source.arg( currentDate.toString( Qt::ISODate ) );
 
-        QString collections = cg.readEntry( "Role_CheckState" );
+    kDebug() << "Connecting source:" << source;
 
+    dataEngine( "calendar" )->connectSource( source, this, 6000 );
+}
 
-        // TODO: Any better way to parse this??
-        QStringList splitCollections = collections.split( "," );
-        for ( int i = 0; i < splitCollections.length(); i += 2 ) {
-            // So splitCollections[i] will have the collection and splitCollections[i+1] will
-            // have its state. 0 is disabled, 2(?) is enabled. 1 causes the LHC to destroy the
-            // universe?
+void EventsApplet::updateUI()
+{
+    setBusy( false );
 
-            if ( splitCollections[ i+1 ] == "2" ) {
-                QString nonParsed = splitCollections[ i ];
-                QString collectionId = nonParsed.right( nonParsed.length()-1 );
-                m_collectionIds << collectionId;
-            }
+    // Clear the current layout...
+    for ( int i = 0; i < m_scrollerLayout->count(); i++ ) {
+        m_scrollerLayout->removeItem( m_scrollerLayout->itemAt( i ) );
+    }
+
+    // ... and repopulate it.
+    QMapIterator<QString,EventWidget*> it( m_incidences );
+    while ( it.hasNext() ) {
+        it.next();
+        m_scrollerLayout->addItem( it.value() );
+    }
+}
+
+void EventsApplet::dataUpdated( QString source, Plasma::DataEngine::Data data )
+{
+    if ( source.startsWith( "events" ) ) { // ++insurance
+        QHashIterator<QString,QVariant> it( data );
+        while ( it.hasNext() ) {
+            it.next();
+            QVariantHash data = it.value().toHash();
+            KDateTime sd = qVariantValue<KDateTime>( data[ "StartDate" ] );
+
+            m_incidences[ sd.toString() ] = new EventWidget( data );
+            kDebug() << sd;
         }
+
+        // Wait for more data before updating UI. I like doing it
+        // this way. QTimer::start() restarts the timer from the
+        // beginning every time it starts, which is fun :)
+        m_timer->start();
     }
 }
 
-void EventsApplet::collectionFetchResult( KJob* job )
-{
-    setBusy( m_collectionJobsInProgress );
+#include "eventsapplet.moc"
 
-    Akonadi::CollectionFetchJob* fetchJob = qobject_cast<Akonadi::CollectionFetchJob*>( job );
-
-    m_collections << fetchJob->collections();
-
-    kDebug() << m_collections;
-}
