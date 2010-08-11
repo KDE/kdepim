ae70dcded5a12fee53bab4be97d15f0a535944e5
Bertjan Broeksema <broeksema@kde.org>
2010-08-11 14:33:19 +0000

    When clicking apply, we disable the Ok button to avoid quick consecutive changes. We should however no forget to reenable the button when the save has finished or failed.
    svn path=/trunk/KDE/kdepim/incidenceeditors/incidenceeditor-ng/; revision=1162160

--- incidenceeditor-ng/eventortododialog.cpp
+++ incidenceeditor-ng/eventortododialog.cpp
@@ -348,6 +348,7 @@ void EventOrTodoDialogPrivate::handleItemSaveFail( Akonadi::EditorItemManager::S
     mItemManager->save();
   } else {
     updateButtonStatus( mEditor->isDirty() );
+    q->enableButtonOk( true );
     q->enableButtonCancel( true );
     q->enableButtonOk( true );
   }
@@ -370,6 +371,7 @@ void EventOrTodoDialogPrivate::handleItemSaveFinish( Akonadi::EditorItemManager:
 
     // Set the buttons to a reasonable state as well (ok and apply should be
     // disabled at this point).
+    q->enableButtonOk( true );
     q->enableButtonCancel( true );
     q->enableButtonOk( true );
     q->enableButtonApply( mEditor->isDirty() );
