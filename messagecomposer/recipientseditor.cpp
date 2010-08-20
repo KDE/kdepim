84a2f006aff67900f91643bfd52e05834243d489
Andras Mantia <amantia@kde.org>
2010-08-20 11:38:11 +0000

    Init your variables
    svn path=/trunk/KDE/kdepim/messagecomposer/; revision=1165899

--- messagecomposer/recipientseditor.cpp
+++ messagecomposer/recipientseditor.cpp
@@ -44,7 +44,7 @@
 using namespace MessageComposer;
 using namespace KPIM;
 
-RecipientsEditor::RecipientsEditor( QWidget* parent ): MultiplyingLineEditor( new RecipientLineFactory( 0 ), parent )
+RecipientsEditor::RecipientsEditor( QWidget* parent ): MultiplyingLineEditor( new RecipientLineFactory( 0 ), parent ), mRecentAddressConfig( 0 )
 {
   factory()->setParent( this ); // HACK: can't use 'this' above since it's not yet constructed at that point
   mSideWidget = new RecipientsEditorSideWidget( this, this );
