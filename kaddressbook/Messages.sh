aaa7b7e998346cc35aac1758f7d79e8de8ebea22
Albert Astals Cid <tsdgeos@terra.es>
2010-08-22 13:26:16 +0000

    fix Messages.sh after yesterday change in extract-messages.sh
    svn path=/trunk/KDE/kdepim/; revision=1166684

--- kaddressbook/Messages.sh
+++ kaddressbook/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC `find . -name \*.ui` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name \*.ui` `find . -name \*.rc` >> rc.cpp || exit 11
 $XGETTEXT `find -name \*.cpp -o -name \*.h` -o $podir/kaddressbook.pot
 rm -f rc.cpp
