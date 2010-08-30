6f111c31a1676ffb0a50b72ca16a51e58826706e
Stephen Kelly <steveire@gmail.com>
2010-08-30 15:17:32 +0000

    Add the FavoritesListModel with some controls for moving and removing items.
    svn path=/trunk/KDE/kdepim/mobile/; revision=1170025

---
+++ mobile/lib/favoriteslistmodel.cpp
@@ -0,0 +1,123 @@
+/*
+    Copyright (C) 2010 Klarälvdalens Datakonsult AB,
+        a KDAB Group company, info@kdab.net,
+        author Stephen Kelly <stephen@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "favoriteslistmodel.h"
+
+static const char * const sFavoritePrefix = "Favorite_";
+static const char * const sFavoriteOrder = "FavoriteOrder";
+static const int sFavoritePrefixLength = 9;
+
+
+FavoritesListModel::FavoritesListModel(KSharedConfigPtr config, QObject *parent)
+  : QStringListModel(parent), cfg(config)
+{
+  reparseConfiguration();
+}
+
+void FavoritesListModel::reparseConfiguration()
+{
+  QStringList list;
+  foreach ( const QString &group, cfg->groupList() )
+    if ( group.startsWith( sFavoritePrefix ) )
+      list.append( QString( group ).remove( 0, sFavoritePrefixLength ) );
+
+  KConfigGroup group = cfg->group("FavoriteGeneral");
+  QStringList favsList = group.readEntry("Order", QStringList());
+  QStringList::iterator it = favsList.begin();
+  while ( it != favsList.end())
+  {
+    if (!list.contains(*it))
+    {
+      it = favsList.erase(it);
+    } else {
+      ++it;
+    }
+  }
+  foreach (const QString &item, list)
+    if (!favsList.contains(item))
+      favsList.append(item);
+  setStringList(favsList);
+}
+
+void FavoritesListModel::saveConfig()
+{
+  QStringList favsList = stringList();
+  foreach ( const QString &group, cfg->groupList() ) {
+    if ( group.startsWith( sFavoritePrefix ) ) {
+      const QString name = QString( group ).remove( 0, sFavoritePrefixLength );
+      if (!favsList.contains(name)) {
+        cfg->deleteGroup(group);
+      }
+    }
+  }
+  QStringList::iterator it = favsList.begin();
+  while ( it != favsList.end())
+  {
+    if (!cfg->groupList().contains(sFavoritePrefix + *it))
+    {
+      it = favsList.erase(it);
+    } else {
+      ++it;
+    }
+  }
+
+  KConfigGroup group = cfg->group("FavoriteGeneral");
+  group.writeEntry("Order", favsList);
+  cfg->sync();
+}
+
+void FavoritesListModel::moveUp(int row)
+{
+  if (row <= 0)
+    return;
+  QStringList list = stringList();
+
+  if (row >= list.size())
+    return;
+
+  list.move(row, row - 1);
+
+  // resets the model.
+  setStringList(list);
+  saveConfig();
+}
+
+void FavoritesListModel::moveDown(int row)
+{
+  if (row < 0)
+    return;
+  QStringList list = stringList();
+
+  if (row >= list.size() - 1)
+    return;
+
+  list.move(row, row + 1);
+
+  // resets the model.
+  setStringList(list);
+  saveConfig();
+}
+
+void FavoritesListModel::removeItem(int row)
+{
+  QStringListModel::removeRow(row);
+  saveConfig();
+}
