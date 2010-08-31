20400fa9fcfb49d477e4ca9ca87b602c4499513c
Artur Duque de Souza <asouza@kde.org>
2010-08-31 21:45:23 +0000

    Add a helper to the calendar widget
    This is a C++ helper that will help the widget to do
    simple but important math with dates. This helper can
    be shared with different date widgets.
    
    svn path=/trunk/KDE/kdepim/mobile/; revision=1170436

---
+++ mobile/lib/calendar/calendarhelper.cpp
@@ -0,0 +1,139 @@
+/*
+    Copyright (C) 2010 Artur Duque de Souza <asouza@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "calendarhelper.h"
+
+#define MAX_DAYS_ON_WIDGET 35
+#define MAX_WEEKS_ON_WIDGET 5
+
+
+CalendarHelper::CalendarHelper( QObject *parent )
+    : QObject( parent )
+{
+    //setDate(QDateTime::currentDateTime());
+    setDate(QDateTime(QDate(2010, 8, 31)));
+}
+
+CalendarHelper::~CalendarHelper()
+{
+}
+
+QDateTime CalendarHelper::date() const
+{
+  return m_original;
+}
+
+void CalendarHelper::setDate( const QDateTime datetime )
+{
+  m_original = datetime;
+  m_day = datetime.date().day();
+  m_month = datetime.date().month();
+  m_year = datetime.date().year();
+  m_daysInMonth = m_original.date().daysInMonth();
+
+  // calculate the offsets for day and week
+  QDate firstDay = QDate(m_year, m_month, 1);
+  m_offset = firstDay.dayOfWeek();
+  m_weekOffset = firstDay.weekNumber();
+
+  emit dateChanged();
+}
+
+int CalendarHelper::day() const
+{
+  return m_day;
+}
+
+void CalendarHelper::setDay( const int day )
+{
+  if ( m_day == day )
+      return;
+
+  m_day = day;
+  emit dayChanged();
+}
+
+QString CalendarHelper::monthName() const
+{
+  return QDate::longMonthName( m_month );
+}
+
+int CalendarHelper::month() const
+{
+  return m_month;
+}
+
+void CalendarHelper::setMonth( const int month )
+{
+  if ( m_month == month )
+      return;
+
+  m_month = month;
+  m_daysInMonth = m_original.date().daysInMonth();
+  emit monthChanged();
+}
+
+int CalendarHelper::year() const
+{
+  return m_year;
+}
+
+void CalendarHelper::setYear( const int year )
+{
+  if ( m_year == year )
+      return;
+
+  m_year = year;
+  emit yearChanged();
+}
+
+QString CalendarHelper::dayForPosition( const int pos ) const
+{
+  int res = (pos - m_offset);
+
+  // out of range
+  if ( pos < 1 || pos > MAX_DAYS_ON_WIDGET || res < 1)
+    return QString();
+
+  // if the position is the firsts days (0 to m_offset)
+  if ( pos <= m_offset )
+    return QString::number(pos);
+
+
+  if ( res >= m_daysInMonth )
+    return QString();
+
+  return QString::number(res);
+}
+
+int CalendarHelper::weekForPosition( const int pos ) const
+{
+  // out of range
+  if ( pos < 1 || pos > MAX_WEEKS_ON_WIDGET )
+      return -1;
+
+  // if the position is the first week
+  if (pos == 1)
+      return m_weekOffset;
+
+  // for all other weeks do the math
+  return pos + m_weekOffset - 1;
+}
+
+#include "calendarhelper.moc"
---
+++ mobile/lib/calendar/calendarhelper.h
@@ -0,0 +1,73 @@
+/*
+    Copyright (C) 2010 Artur Duque de Souza <asouza@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef CALENDARHELPER_H
+#define CALENDARHELPER_H
+
+#include <QObject>
+#include <QDateTime>
+
+
+class CalendarHelper : public QObject
+{
+  Q_OBJECT;
+  Q_PROPERTY(int day READ day WRITE setDay NOTIFY dayChanged)
+  Q_PROPERTY(int month READ month WRITE setMonth NOTIFY monthChanged)
+  Q_PROPERTY(QString monthName READ monthName NOTIFY monthChanged)
+  Q_PROPERTY(int year READ year WRITE setYear NOTIFY yearChanged)
+  Q_PROPERTY(QDateTime date READ date WRITE setDate NOTIFY dateChanged)
+
+  public:
+    CalendarHelper( QObject *parent = 0 );
+    ~CalendarHelper();
+
+    QDateTime date() const;
+    void setDate( const QDateTime datetime );
+
+    int day() const;
+    void setDay( const int day );
+
+    int month() const;
+    QString monthName() const;
+    void setMonth( const int month );
+
+    int year() const;
+    void setYear( const int year );
+
+  public slots:
+    QString dayForPosition( const int pos ) const;
+    int weekForPosition( const int pos ) const;
+
+  signals:
+    void dateChanged();
+    void dayChanged();
+    void monthChanged();
+    void yearChanged();
+
+  private:
+    QDateTime m_original;
+    int m_day;
+    int m_month;
+    int m_year;
+    int m_offset;
+    int m_weekOffset;
+    int m_daysInMonth;
+};
+
+#endif
