5a78b303118f0b9a69dea38b4dcee8c83cc0e7ae
Andreas Holzammer <andy@kdab.com>
2010-09-10 15:39:57 +0000

    - make serializer plugins static
    svn path=/trunk/KDE/kdepim/mobile/; revision=1173852

--- mobile/mail/main.cpp
+++ mobile/mail/main.cpp
@@ -27,6 +27,12 @@
 
 #include "mainview.h"
 
+#ifdef MAIL_SERIALIZER_PLUGIN_STATIC
+#include <QtPlugin>
+
+Q_IMPORT_PLUGIN(akonadi_serializer_mail)
+#endif
+
 int main( int argc, char **argv )
 {
   kWarning() << "Starting main function" << QDateTime::currentDateTime();
