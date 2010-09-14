896cbf74fedaf4b022d4d543c2ae5c5788332015
Volker Krause <vkrause@kde.org>
2010-09-14 15:46:48 +0000

    Don't show this message box for now, it's too often triggered on the N900 without good reason (probably a too small timeout).
    CCBUG: 251123
    
    svn path=/trunk/KDE/kdepim/mobile/; revision=1175262

--- mobile/mail/mainview.cpp
+++ mobile/mail/mainview.cpp
@@ -546,9 +546,10 @@ void MainView::createDefaultCollectionDone( KJob *job)
   if ( job->error() ) {
     kDebug() << "Error creating default collection: " << job->errorText();
     //###: review error string
-    KMessageBox::sorry( this,
+    // diabled for now, triggers too often without good reason on the n900 (too short timeouts probably)
+/*    KMessageBox::sorry( this,
                         i18n("Error creating default collection."),
-                        i18n("Internal Error"));
+                        i18n("Internal Error"));*/
     return;
   }
 
