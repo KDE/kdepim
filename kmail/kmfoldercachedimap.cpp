60e1925efa269983afbfffd4c09ee27ff73766bc
Allen Winter <winter@kde.org>
2010-10-03 23:38:47 +0000

    merge SVN commit 1182241 by winterz: check for folder() non-null in the debug output
    merge SVN commit 1182243 by winterz:
    another check for folder() non-null in the debug output
    
    
    
    svn path=/tags/kdepim/enterprise35.0.20101001.1181590/kdepim/; revision=1182246

--- kmail/kmfoldercachedimap.cpp
+++ kmail/kmfoldercachedimap.cpp
@@ -357,7 +357,11 @@ void KMFolderCachedImap::writeConfig()
       }
       configGroup.writeEntry( "UIDSDeletedSinceLastSync", uidstrings );
       if ( GlobalSettings::self()->mailLossDebug() ) {
-        kdDebug( 5006 ) << "WRITING OUT UIDSDeletedSinceLastSync in: " << folder( )->prettyURL( ) << endl << uidstrings << endl;
+        if ( folder() ) {
+          kdDebug( 5006 ) << "WRITING OUT UIDSDeletedSinceLastSync in: " << folder()->prettyURL( ) << endl << uidstrings << endl;
+        } else {
+          kdDebug( 5006 ) << "CANNOT WRITE OUT UIDSDeletedSinceLastSync since folder() is NULL" << endl;
+        }
       }
   } else {
     configGroup.deleteEntry( "UIDSDeletedSinceLastSync" );
@@ -467,7 +471,11 @@ int KMFolderCachedImap::writeUidCache()
     return 0;
   }
   if ( GlobalSettings::self()->mailLossDebug() ) {
-    kdDebug(5006) << "Writing out UID cache lastuid: " << lastUid()  << " in: " << folder()->prettyURL() << endl;
+    if ( folder() ) {
+      kdDebug(5006) << "Writing out UID cache lastuid: " << lastUid()  << " in: " << folder()->prettyURL() << endl;
+    } else {
+      kdDebug(5006) << "CANNOT WRITE OUT UID cache lastuid since folder() is NULL" << endl;
+    }
   }
   QFile uidcache( uidCacheLocation() );
   if( uidcache.open( IO_WriteOnly ) ) {
