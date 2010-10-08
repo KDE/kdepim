e3b5a98c1c57d70c2d32d48d4e024f264ed8561f
Allen Winter <winter@kde.org>
2010-10-08 13:47:30 +0000

    SVN_SILENT Update version numbers for today's release.
    svn path=/branches/kdepim/enterprise/kdepim/; revision=1183838

--- kmail/kmversion.h
+++ kmail/kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20101001.1181590)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20101008.1183837)"
 
 #endif /*kmversion_h*/
--- kontact/src/main.cpp
+++ kontact/src/main.cpp
@@ -47,7 +47,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.2.9 (enterprise35 0.20101001.1181590)";
+static const char version[] = "1.2.9 (enterprise35 0.20101008.1183837)";
 
 class KontactApp : public KUniqueApplication {
   public:
