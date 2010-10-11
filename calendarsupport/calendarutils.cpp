b78916b24fcc227449768bda9cb66eece522cf87
Bertjan Broeksema <broeksema@kde.org>
2010-10-11 16:31:18 +0000

    Start extracting some code from korganizer/calenderview.* so that it can be reused in the mobile applications.
    svn path=/trunk/KDE/kdepim/calendarsupport/; revision=1184862

---
+++ calendarsupport/calendarutils.cpp
@@ -0,0 +1,117 @@
+/*
+  Copyright (C) 2010 Bertjan Broeksema <broeksema@kde.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
+*/
+
+// NOTE: The code of the following methods is taken from
+//       kdepim/korganizer/calendarview.cpp:
+//       - makeIndependent (was incidence_unsub)
+//       - makeChildrenIndependent
+
+#include "calendarutils.h"
+
+#include "calendar.h"
+#include "incidencechanger.h"
+#include "utils.h"
+
+#include <KCalCore/Incidence>
+
+using namespace Akonadi;
+using namespace CalendarSupport;
+using namespace KCalCore;
+
+/// CalendarUtilsPrivate
+
+namespace CalendarSupport {
+
+struct CalendarUtilsPrivate
+{
+  /// Methods
+  CalendarUtilsPrivate( Calendar *calendar, CalendarUtils *qq );
+
+  /// Members
+  Calendar         *mCalendar;
+  IncidenceChanger *mChanger;
+
+private:
+  CalendarUtils * const q_ptr;
+  Q_DECLARE_PUBLIC( CalendarUtils )
+};
+
+}
+
+CalendarUtilsPrivate::CalendarUtilsPrivate( Calendar *calendar, CalendarUtils *qq )
+  : mCalendar( calendar )
+  , mChanger( new IncidenceChanger( calendar, qq ) )
+  , q_ptr( qq )
+{
+  Q_ASSERT( mCalendar );
+}
+
+/// CalendarUtils
+
+CalendarUtils::CalendarUtils( Calendar *calendar, QObject *parent)
+  : QObject( parent )
+  , d_ptr( new CalendarUtilsPrivate( calendar, this ) )
+{
+  Q_ASSERT( calendar );
+}
+
+CalendarUtils::~CalendarUtils()
+{
+  delete d_ptr;
+}
+
+bool CalendarUtils::makeIndependent( const Akonadi::Item &item )
+{
+  Q_D( CalendarUtils );
+  const Incidence::Ptr inc = CalendarSupport::incidence( item );
+
+  if ( !inc || inc->relatedTo().isEmpty() ) {
+    return false;
+  }
+
+  Incidence::Ptr oldInc( inc->clone() );
+  inc->setRelatedTo( 0 );
+  // HACK: This is not a widget, so pass 0 for now
+  d->mChanger->changeIncidence( oldInc, item, CalendarSupport::IncidenceChanger::RELATION_MODIFIED, 0 );
+
+  return true;
+}
+
+bool CalendarUtils::makeChildrenIndependent( const Akonadi::Item &item )
+{
+  Q_D( CalendarUtils );
+  const Incidence::Ptr inc = CalendarSupport::incidence( item );
+
+  Akonadi::Item::List subIncs = d->mCalendar->findChildren( item );
+
+  if ( !inc || subIncs.isEmpty() ) {
+    return false;
+  }
+  //startMultiModify ( i18n( "Make sub-to-dos independent" ) );
+
+  foreach( const Akonadi::Item &item, subIncs ) {
+    makeIndependent( item );
+  }
+
+  //endMultiModify();
+  return true;
+}
---
+++ calendarsupport/calendarutils.h
@@ -0,0 +1,62 @@
+/*
+  Copyright (C) 2010 Bertjan Broeksema <broeksema@kde.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
+*/
+#ifndef CALENDARUTILS_H
+#define CALENDARUTILS_H
+
+#include <QtCore/QObject>
+
+namespace Akonadi {
+class Item;
+}
+
+namespace CalendarSupport {
+
+class Calendar;
+class CalendarUtilsPrivate;
+
+class CalendarUtils : public QObject
+{
+  Q_OBJECT
+public:
+  explicit CalendarUtils( Calendar *calendar, QObject *parent = 0 );
+  ~CalendarUtils();
+
+  /** Makes the incidence from @param item independent from its parent. Returns
+      true when the incidence was made independent, false otherwise.
+   */
+  bool makeIndependent( const Akonadi::Item &item );
+
+  /** Make all children of the incindence from @param item independent
+      Works with any incidence type, although currently we only pass to-dos.
+      Returns true when one or more incidence(s) where made independent, false
+      otherwise.
+   */
+  bool makeChildrenIndependent( const Akonadi::Item &item );
+
+private:
+  CalendarUtilsPrivate * const d_ptr;
+  Q_DECLARE_PRIVATE( CalendarUtils )
+};
+
+}
+
+#endif // CALENDARUTILS_H
