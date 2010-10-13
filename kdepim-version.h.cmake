f9a0d97097890816996e0b5bdcd9ef41f1c59d2b
Bjoern Ricks <bjoern.ricks@intevation.de>
2010-10-13 14:45:16 +0000

    move comment about kde version from header into cmake file
    svn path=/trunk/KDE/kdepim/; revision=1185508

--- kdepim-version.h.cmake
+++ kdepim-version.h.cmake
@@ -30,33 +30,6 @@
 #ifndef KDEPIM_VERSION_H
 #define KDEPIM_VERSION_H
 
-/*
-  Version scheme: "x.y.z build".
-
-  x is the version number.
-  y is the major release number.
-  z is the minor release number.
-
-  "x.y.z" follow the kdelibs version kdepim is released with.
-
-  If "z" is 0, it the version is "x.y"
-
-  "build" is empty for final versions. For development versions "build" is
-  something like "pre", "alpha1", "alpha2", "beta1", "beta2", "rc1", "rc2".
-
-  Examples in chronological order:
-
-    3.0
-    3.0.1
-    3.1 alpha1
-    3.1 beta1
-    3.1 beta2
-    3.1 rc1
-    3.1
-    3.1.1
-    3.2 pre
-    3.2 alpha1
-*/
 
 #define KDEPIM_VERSION "@KDEPIM_VERSION@"
 
