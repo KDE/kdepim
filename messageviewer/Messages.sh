5aa6a7ec4f69cb8227164822ed4b43ed0ae00ff8
Laurent Montel <montel@kde.org>
2010-10-14 08:39:49 +0000

    Extract all strings
    svn path=/trunk/KDE/kdepim/messageviewer/; revision=1185782

--- messageviewer/Messages.sh
+++ messageviewer/Messages.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-$EXTRACTRC *.ui *.kcfg >> rc.cpp
+$EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name '*.cpp'` -o $podir/libmessageviewer.pot
+rm -f rc.cpp
