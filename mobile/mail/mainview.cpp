6f011f32f6b2696acf99d949d286cc5c4d83a097
Stephen Kelly <steveire@gmail.com>
2010-10-21 13:08:26 +0000

    Enable threading in kmail-mobile.
    svn path=/trunk/KDE/kdepim/mobile/; revision=1188117

--- mobile/mail/mainview.cpp
+++ mobile/mail/mainview.cpp
@@ -138,7 +138,6 @@ MainView::~MainView()
 
 QAbstractItemModel* MainView::createItemModelContext(QDeclarativeContext* context, QAbstractItemModel* model)
 {
-  return KDeclarativeMainView::createItemModelContext(context, model);
 
   ThreadGrouperModel *grouper = new ThreadGrouperModel(this);
   grouper->setSourceModel(model);
