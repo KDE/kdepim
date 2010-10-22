8e1af1fd1535ae61f66a9c8c3b4929b34a02b428
Sergio Luis Martins <iamsergio@gmail.com>
2010-10-22 15:22:02 +0000

    Refactor and save 100 code lines. Should be easier to insert the preset that's in config.
    svn path=/trunk/KDE/kdepim/incidenceeditor-ng/; revision=1188532

--- incidenceeditor-ng/alarmpresets.cpp
+++ incidenceeditor-ng/alarmpresets.cpp
@@ -35,148 +35,54 @@ K_GLOBAL_STATIC( QList<KCalCore::Alarm::Ptr>, sBeforeEndPresets )
 
 void initPresets( AlarmPresets::When when )
 {
-  KCalCore::Alarm::Ptr alarm( new KCalCore::Alarm( 0 ) );
+  QList<int> hardcodedPresets;
+  hardcodedPresets << 5           // 5 minutes
+                   << 10
+                   << 15
+                   << 30
+                   << 45
+                   << 60          // 1 hour
+                   << 2 * 60      // 2 hours
+                   << 24 * 60     // 1 day
+                   << 2 * 24 * 60 // 2 days
+                   << 5 * 24 * 60;// 5 days
+
   switch ( when ) {
   case AlarmPresets::BeforeStart:
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -5 * 60 ); // 5 minutes before
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "5 minutes before start" ) );
-    sBeforeStartPresets->append( alarm );
 
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -10 * 60 ); // 10 minutes before
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "10 minutes before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -15 * 60 ); // 15 minutes before
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "15 minutes before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -30 * 60 ); // 30 minutes before
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "30 minutes before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -45 * 60 ); // 45 minutes before
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "45 minutes before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "1 hour before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -2 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "2 hours before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -24 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "1 day before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -2 * 24 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "2 days before start" ) );
-    sBeforeStartPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setStartOffset( -5 * 24 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "5 days before start" ) );
-    sBeforeStartPresets->append( alarm );
+    for ( int i = 0; i < hardcodedPresets.count(); ++i ) {
+      KCalCore::Alarm::Ptr alarm( new KCalCore::Alarm( 0 ) );
+      alarm->setType( KCalCore::Alarm::Display );
+      const int minutes = hardcodedPresets[i];
+      alarm->setStartOffset( -minutes * 60 );
+      alarm->setEnabled( true );
+      if ( minutes < 60 ) {
+        sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "%1 minutes before start", minutes ) );
+      } else if ( minutes <= 2*60 ) {
+        sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "%1 hours before start", minutes/60 ) );
+      } else {
+        sBeforeStartPresetNames->append( i18nc( "@item:inlistbox", "%1 days before start", minutes/(24*60) ) );
+      }
+      sBeforeStartPresets->append( alarm );
+    }
     break;
 
   case AlarmPresets::BeforeEnd:
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -5 * 60 ); // 5 minutes before
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "5 minutes before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -10 * 60 ); // 10 minutes before
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "10 minutes before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -15 * 60 ); // 15 minutes before
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "15 minutes before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -30 * 60 ); // 30 minutes before
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "30 minutes before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -45 * 60 ); // 45 minutes before
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "45 minutes before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "1 hour before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -2 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "2 hours before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -24 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "1 day before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -2 * 24 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "2 days before due" ) );
-    sBeforeEndPresets->append( alarm );
-
-    alarm = KCalCore::Alarm::Ptr( new KCalCore::Alarm( 0 ) );
-    alarm->setType( KCalCore::Alarm::Display );
-    alarm->setEndOffset( -5 * 24 * 60 * 60 );
-    alarm->setEnabled( true );
-    sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "5 days before due" ) );
-    sBeforeEndPresets->append( alarm );
+    for ( int i = 0; i < hardcodedPresets.count(); ++i ) {
+      KCalCore::Alarm::Ptr alarm( new KCalCore::Alarm( 0 ) );
+      alarm->setType( KCalCore::Alarm::Display );
+      const int minutes = hardcodedPresets[i];
+      alarm->setEndOffset( -minutes * 60 );
+      alarm->setEnabled( true );
+      if ( minutes < 60 ) {
+        sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "%1 minutes before due", minutes ) );
+      } else if ( minutes <= 2*60 ) {
+        sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "%1 hours before due", minutes/60 ) );
+      } else {
+        sBeforeEndPresetNames->append( i18nc( "@item:inlistbox", "%1 days before due", minutes/(24*60) ) );
+      }
+      sBeforeEndPresets->append( alarm );
+    }
     break;
   }
 }
