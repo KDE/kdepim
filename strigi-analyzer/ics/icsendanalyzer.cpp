3f86ee6faa4acdb997fe6e03fbdb76d2ff199c81
Tobias Koenig <tokoe@kde.org>
2010-10-25 09:20:23 +0000

    Coding style cleanup
    svn path=/trunk/KDE/kdepim/strigi-analyzer/; revision=1189562

--- strigi-analyzer/ics/icsendanalyzer.cpp
+++ strigi-analyzer/ics/icsendanalyzer.cpp
@@ -6,8 +6,7 @@
  * modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation version 2.
  *
- * This program is distributed in the hope that
-t it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
@@ -21,62 +20,63 @@ t it will be useful,
 
 #include "icsendanalyzer.h"
 
-#include <kcalcore/memorycalendar.h>
 #include <kcalcore/icalformat.h>
-#include <kcalcore/vcalformat.h>
+#include <kcalcore/memorycalendar.h>
 #include <kcalcore/todo.h>
+#include <kcalcore/vcalformat.h>
 
-#include <strigi/fieldtypes.h>
 #include <strigi/analysisresult.h>
+#include <strigi/fieldtypes.h>
 #include <strigi/streamendanalyzer.h>
-
-//#include <KDebug>
 
 using namespace KCalCore;
 
-IcsEndAnalyzer::IcsEndAnalyzer( const IcsEndAnalyzerFactory* f )
-  : m_factory( f )
+IcsEndAnalyzer::IcsEndAnalyzer( const IcsEndAnalyzerFactory *factory )
+  : m_factory( factory )
 {
+}
+
+const char* IcsEndAnalyzer::name() const
+{
+  return "IcsEndAnalyzer";
 }
 
 bool IcsEndAnalyzer::checkHeader( const char* header, qint32 headersize ) const
 {
   const char* magic = "BEGIN:VCALENDAR";
-  qint32 magicLength = strlen( magic );
+  const qint32 magicLength = strlen( magic );
 
   return headersize >= magicLength && !strncmp( magic, header, magicLength );
 }
 
 /*
-I chose to use libkcal instead of reading the calendar manually. It's easier to
-maintain this way.
-*/
-STRIGI_ENDANALYZER_RETVAL IcsEndAnalyzer::analyze( Strigi::AnalysisResult& idx, Strigi::InputStream* in )
+ * I chose to use libkcal instead of reading the calendar manually. It's easier to
+ * maintain this way.
+ */
+STRIGI_ENDANALYZER_RETVAL IcsEndAnalyzer::analyze( Strigi::AnalysisResult &index, Strigi::InputStream *stream )
 {
-  MemoryCalendar::Ptr cal( new MemoryCalendar( QString::fromLatin1( "UTC" ) ) );
+  MemoryCalendar::Ptr calendar( new MemoryCalendar( QString::fromLatin1( "UTC" ) ) );
 
-  const char* data;
-  //FIXME: large calendars will exhaust memory; incremental loading would be
-  // nice
-  qint32 nread = in->read( data, in->size(), in->size() );
-  if ( nread <= 0 ) {
-    //kDebug() <<"Reading data from input stream failed";
+  const char* data = 0;
+
+  //FIXME: large calendars will exhaust memory; incremental loading would be nice
+  const qint32 size = stream->read( data, stream->size(), stream->size() );
+  if ( size <= 0 ) {
     return Strigi::Error;
   }
 
   ICalFormat ical;
-  if ( !ical.fromRawString( cal, QByteArray::fromRawData( data, nread ) ) ) {
+  if ( !ical.fromRawString( calendar, QByteArray::fromRawData( data, size ) ) ) {
     VCalFormat vcal;
-    if ( !vcal.fromRawString( cal, data ) ) {
-      //kDebug() <<"Could not load calendar";
+    if ( !vcal.fromRawString( calendar, data ) ) {
       return Strigi::Error;
     }
   }
 
-  idx.addValue( m_factory->field( ProductId ), cal->productId().toUtf8().data() );
-  idx.addValue( m_factory->field( Events ), static_cast<quint32>( cal->events().count() ) );
-  idx.addValue( m_factory->field( Journals ), static_cast<quint32>( cal->journals().count() ) );
-  Todo::List todos = cal->todos();
+  index.addValue( m_factory->productIdField, calendar->productId().toUtf8().data() );
+  index.addValue( m_factory->eventsField, static_cast<quint32>( calendar->events().count() ) );
+  index.addValue( m_factory->journalsField, static_cast<quint32>( calendar->journals().count() ) );
+  Todo::List todos = calendar->todos();
 
   // count completed and overdue
   int completed = 0;
@@ -89,46 +89,47 @@ STRIGI_ENDANALYZER_RETVAL IcsEndAnalyzer::analyze( Strigi::AnalysisResult& idx, 
     }
   }
 
-  idx.addValue( m_factory->field( Todos ), (quint32)todos.count() );
-  idx.addValue( m_factory->field( TodosCompleted ), (quint32)completed );
-  idx.addValue( m_factory->field( TodosOverdue ), (quint32)overdue );
+  index.addValue( m_factory->todosField, static_cast<quint32>( todos.count() ) );
+  index.addValue( m_factory->todosCompletedField, static_cast<quint32>( completed ) );
+  index.addValue( m_factory->todosOverdueField, static_cast<quint32>( overdue ) );
 
-  cal->close();
+  calendar->close();
 
   return Strigi::Ok;
 }
 
-const Strigi::RegisteredField* IcsEndAnalyzerFactory::field( IcsEndAnalyzer::Field f ) const
+void IcsEndAnalyzerFactory::registerFields( Strigi::FieldRegister& reg )
 {
-  switch ( f ) {
-    case IcsEndAnalyzer::ProductId:
-      return productIdField;
-      break;
-    case IcsEndAnalyzer::Events:
-      return eventsField;
-      break;
-    case IcsEndAnalyzer::Journals:
-      return journalsField;
-      break;
-    case IcsEndAnalyzer::Todos:
-      return todosField;
-      break;
-    case IcsEndAnalyzer::TodosCompleted:
-      return todosCompletedField;
-      break;
-    case IcsEndAnalyzer::TodosOverdue:
-    default:
-      return todosOverdueField;
-      break;
-  }
+  productIdField = reg.registerField( "Product Id", Strigi::FieldRegister::stringType, 1, 0 );
+  eventsField = reg.registerField( "Events", Strigi::FieldRegister::integerType, 1, 0 );
+  journalsField = reg.registerField( "Journals", Strigi::FieldRegister::integerType, 1, 0 );
+  todosField = reg.registerField( "Todos", Strigi::FieldRegister::integerType, 1, 0 );
+  todosCompletedField = reg.registerField( "Todos Completed", Strigi::FieldRegister::integerType, 1, 0 );
+  todosOverdueField = reg.registerField( "Todos Overdue", Strigi::FieldRegister::integerType, 1, 0 );
 }
 
-void IcsEndAnalyzerFactory::registerFields( Strigi::FieldRegister& reg ) {
-  productIdField = reg.registerField("Product Id", Strigi::FieldRegister::stringType, 1, 0 );
-  eventsField = reg.registerField("Events", Strigi::FieldRegister::integerType, 1, 0 );
-  journalsField = reg.registerField("Journals", Strigi::FieldRegister::integerType, 1, 0 );
-  todosField = reg.registerField("Todos", Strigi::FieldRegister::integerType, 1, 0 );
-  todosCompletedField = reg.registerField("Todos Completed", Strigi::FieldRegister::integerType, 1, 0 );
-  todosOverdueField = reg.registerField("Todos Overdue", Strigi::FieldRegister::integerType, 1, 0 );
+const char* IcsEndAnalyzerFactory::name() const
+{
+  return "IcsEndAnalyzer";
 }
 
+Strigi::StreamEndAnalyzer* IcsEndAnalyzerFactory::newInstance() const
+{
+  return new IcsEndAnalyzer( this );
+}
+
+IcsFactoryFactory::IcsFactoryFactory()
+  : componentData( "IcsFactoryFactory" )
+{
+}
+
+std::list<Strigi::StreamEndAnalyzerFactory*> IcsFactoryFactory::streamEndAnalyzerFactories() const
+{
+  if ( !componentData.isValid() )
+    qFatal( "KComponentData is not valid." );
+
+  std::list<Strigi::StreamEndAnalyzerFactory*> factories;
+  factories.push_back( new IcsEndAnalyzerFactory );
+
+  return factories;
+}
--- strigi-analyzer/ics/icsendanalyzer.h
+++ strigi-analyzer/ics/icsendanalyzer.h
@@ -21,64 +21,58 @@
 #define ICSENDANALYZER_H
 
 #define STRIGI_IMPORT_API
+
+#include "config-strigi.h"
+#include "pimstrigi-analyzer_export.h"
+
+#include <kcomponentdata.h>
+
 #include <strigi/analyzerplugin.h>
 #include <strigi/streamendanalyzer.h>
-#include <kcomponentdata.h>
-#include "pimstrigi-analyzer_export.h"
-#include "config-strigi.h"
 
 class IcsEndAnalyzerFactory;
 
 class PIMSTRIGI_ANALYZER_EXPORT IcsEndAnalyzer : public Strigi::StreamEndAnalyzer
 {
-public:
-  IcsEndAnalyzer( const IcsEndAnalyzerFactory* f );
+  public:
+    IcsEndAnalyzer( const IcsEndAnalyzerFactory *factory );
 
-  enum Field { ProductId = 0, Events, Journals, Todos, TodosCompleted, TodosOverdue };
+    const char* name() const;
+    bool checkHeader( const char* header, qint32 headersize ) const;
+    STRIGI_ENDANALYZER_RETVAL analyze( Strigi::AnalysisResult &index, Strigi::InputStream *stream );
 
-  const char* name() const { return "IcsEndAnalyzer"; }
-  bool checkHeader( const char* header, qint32 headersize ) const;
-  STRIGI_ENDANALYZER_RETVAL analyze(  Strigi::AnalysisResult& idx, Strigi::InputStream* in );
-
-private:
-  const IcsEndAnalyzerFactory* m_factory;
+  private:
+    const IcsEndAnalyzerFactory* m_factory;
 };
 
 class PIMSTRIGI_ANALYZER_EXPORT IcsEndAnalyzerFactory : public Strigi::StreamEndAnalyzerFactory
 {
-friend class IcsEndAnalyzer;
-public:
-  const Strigi::RegisteredField* field( IcsEndAnalyzer::Field ) const;
+  friend class IcsEndAnalyzer;
 
-private:
-  const Strigi::RegisteredField* productIdField;
-  const Strigi::RegisteredField* eventsField;
-  const Strigi::RegisteredField* journalsField;
-  const Strigi::RegisteredField* todosField;
-  const Strigi::RegisteredField* todosCompletedField;
-  const Strigi::RegisteredField* todosOverdueField;
+  private:
+    const Strigi::RegisteredField* productIdField;
+    const Strigi::RegisteredField* eventsField;
+    const Strigi::RegisteredField* journalsField;
+    const Strigi::RegisteredField* todosField;
+    const Strigi::RegisteredField* todosCompletedField;
+    const Strigi::RegisteredField* todosOverdueField;
 
-  const char* name() const { return "IcsEndAnalyzer"; }
-  Strigi::StreamEndAnalyzer* newInstance() const { return new IcsEndAnalyzer( this ); }
-  void registerFields( Strigi::FieldRegister& );
+    const char* name() const;
+    Strigi::StreamEndAnalyzer* newInstance() const;
+    void registerFields( Strigi::FieldRegister& );
 };
 
 class PIMSTRIGI_ANALYZER_EXPORT IcsFactoryFactory : public Strigi::AnalyzerFactoryFactory
 {
-private:
-  KComponentData kcomponentdata;
-public:
-  IcsFactoryFactory() :kcomponentdata("IcsFactoryFactory") {}
-  std::list<Strigi::StreamEndAnalyzerFactory*> streamEndAnalyzerFactories() const {
-     if (!kcomponentdata.isValid()) {
-         qFatal("KComponentData is not valid.");
-     }
-     std::list<Strigi::StreamEndAnalyzerFactory*> af;
-     af.push_back( new IcsEndAnalyzerFactory );
-     return af;
-  }
+  public:
+    IcsFactoryFactory();
+
+    std::list<Strigi::StreamEndAnalyzerFactory*> streamEndAnalyzerFactories() const;
+
+  private:
+    KComponentData componentData;
 };
 
-STRIGI_ANALYZER_FACTORY(IcsFactoryFactory)
+STRIGI_ANALYZER_FACTORY( IcsFactoryFactory )
 
 #endif
