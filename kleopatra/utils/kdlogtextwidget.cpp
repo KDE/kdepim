862d6696fbf6f828580c97742adaec7120704787
Allen Winter <winter@kde.org>
2010-10-29 14:50:49 +0000

    SVN_MERGE: Merged revisions 1190976 via svnmerge from svn+ssh://winterz@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepim
    ........
      r1190976 | mutz | 2010-10-29 05:38:00 -0400 (Fri, 29 Oct 2010) | 1 line
      
      KDLogTextWidget: upgrade to version from KDTools v2.2.2, pt.6: relicense to LGPLv2+
    ........
    
    svn path=/trunk/KDE/kdepim/kleopatra/; revision=1191088

--- kleopatra/utils/kdlogtextwidget.cpp
+++ kleopatra/utils/kdlogtextwidget.cpp
@@ -3,14 +3,14 @@
 **
 ** This file is part of the KD Tools library.
 **
-** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software
-** Foundation and appearing in the file LICENSE.GPL included in the
-** packaging of this file.
-**
 ** Licensees holding valid commercial KD Tools licenses may use this file in
 ** accordance with the KD Tools Commercial License Agreement provided with
 ** the Software.
+**
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU Lesser General Public License version 2 and version 3 as published by the
+** Free Software Foundation and appearing in the file LICENSE.LGPL included.
 **
 ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
