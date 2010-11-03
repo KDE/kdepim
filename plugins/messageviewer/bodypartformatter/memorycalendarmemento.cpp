e99e35b725c3e58968367d11a19a327f53529ce4
Sergio Luis Martins <iamsergio@gmail.com>
2010-11-03 15:38:42 +0000

    Add a new BodyFormatterMemento called MemoryCalendarMemento.
    ( Mementos are used by the object tree parser to do async work like crypto or fetching a contact's photo ).
    
    MemoryCalendarMemento fetches incidences into a KCalCore::MemoryCalendar.
    
    Will be used by the text_calendar plugin.
    
    svn path=/trunk/KDE/kdepim/plugins/messageviewer/; revision=1192631

---
+++ plugins/messageviewer/bodypartformatter/memorycalendarmemento.cpp
@@ -0,0 +1,69 @@
+/* Copyright (C) 2010 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.net>
+   Author: Sérgio Martins <sergio.martins@kdab.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <config-messageviewer.h>
+
+#include "memorycalendarmemento.h"
+
+#include <calendarsupport/incidencesearchjob.h>
+#include <KSystemTimeZones>
+
+using namespace MessageViewer;
+
+MemoryCalendarMemento::MemoryCalendarMemento()
+  : QObject( 0 ), mFinished( false )
+{
+  CalendarSupport::IncidenceSearchJob *job = new CalendarSupport::IncidenceSearchJob();
+  connect( job, SIGNAL( result( KJob* ) ), this, SLOT( slotSearchJobFinished( KJob* ) ) );
+}
+
+void MemoryCalendarMemento::slotSearchJobFinished( KJob *job )
+{
+  mFinished = true;
+  CalendarSupport::IncidenceSearchJob *searchJob = static_cast<CalendarSupport::IncidenceSearchJob*>( job );
+  if ( searchJob->error() ) {
+    kWarning() << "Unable to fetch incidences:" << searchJob->errorText();
+  } else {
+    mCalendar = KCalCore::MemoryCalendar::Ptr( new KCalCore::MemoryCalendar( KSystemTimeZones::local() ) );
+    foreach( const KCalCore::Incidence::Ptr &incidence, searchJob->incidences() ) {
+      mCalendar->addIncidence( incidence );
+    }
+
+    kDebug() << "DEBUG YAY, " << searchJob->incidences().count();
+    emit update( Viewer::Delayed );
+  }
+}
+
+bool MemoryCalendarMemento::finished() const
+{
+  return mFinished;
+}
+
+KCalCore::MemoryCalendar::Ptr MemoryCalendarMemento::calendar() const
+{
+  Q_ASSERT( mFinished );
+  return mCalendar;
+}
+
+void MemoryCalendarMemento::detach()
+{
+  disconnect( this, SIGNAL(update(MessageViewer::Viewer::UpdateMode)), 0, 0 );
+}
+
+#include "memorycalendarmemento.moc"
---
+++ plugins/messageviewer/bodypartformatter/memorycalendarmemento.h
@@ -0,0 +1,65 @@
+/* Copyright (C) 2010 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.net>
+   Author: Sérgio Martins <sergio.martins@kdab.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License or (at your option) version 3 or any later version
+   accepted by the membership of KDE e.V. (or its successor approved
+   by the membership of KDE e.V.), which shall act as a proxy
+   defined in Section 14 of version 3 of the license.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef MEMORYCALENDARMEMENTO_H
+#define MEMORYCALENDARMEMENTO_H
+
+#include <messageviewer/interfaces/bodypart.h>
+
+#include <messageviewer/viewer.h>
+
+#include <KCalCore/MemoryCalendar>
+#include <KUrl>
+
+#include <QObject>
+
+class KJob;
+
+namespace CalendarSupport {
+  class IncidenceSearchJob;
+}
+
+namespace MessageViewer {
+
+class MemoryCalendarMemento : public QObject, public Interface::BodyPartMemento
+{
+  Q_OBJECT
+  public:
+    MemoryCalendarMemento();
+
+    bool finished() const;
+    KCalCore::MemoryCalendar::Ptr calendar() const;
+
+    virtual void detach();
+
+  signals:
+    // TODO: Factor our update and detach into base class
+    void update( MessageViewer::Viewer::UpdateMode );
+
+  private slots:
+    void slotSearchJobFinished( KJob *job );
+
+  private:
+    bool mFinished;
+    KCalCore::MemoryCalendar::Ptr mCalendar;
+};
+
+}
+
+#endif
