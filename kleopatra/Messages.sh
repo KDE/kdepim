7840e325c93f34524400cfc4b2522954f8858484
Pino Toscano <pino@kde.org>
2010-11-07 10:57:28 +0000

    do not extract messages from sources in 'tests' subdirectories
    svn path=/trunk/KDE/kdepim/kleopatra/; revision=1193838

--- kleopatra/Messages.sh
+++ kleopatra/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kleopatra.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kleopatra.pot
