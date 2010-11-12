32937df40aed94255fe526ef59009f351b814f00
Tobias Koenig <tokoe@kde.org>
2010-11-12 16:28:02 +0000

    Add a message handler for marking viewed messages with the \SEEN flag
    svn path=/trunk/KDE/kdepim/messageviewer/; revision=1196134

---
+++ messageviewer/markmessagereadhandler.cpp
@@ -0,0 +1,92 @@
+/*
+    Copyright (C) 2010 Klarälvdalens Datakonsult AB,
+        a KDAB Group company, info@kdab.net,
+        author Tobias Koenig <tokoe@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "markmessagereadhandler.h"
+
+#include "globalsettings.h"
+
+#include <akonadi/itemmodifyjob.h>
+#include <akonadi/kmime/messageflags.h>
+
+#include <QtCore/QQueue>
+#include <QtCore/QTimer>
+
+using namespace MessageViewer;
+
+class MarkMessageReadHandler::Private
+{
+  public:
+    Private( MarkMessageReadHandler *qq )
+      : q( qq )
+    {
+    }
+
+    void handleMessages();
+
+    MarkMessageReadHandler *q;
+    QQueue<Akonadi::Item> mItemQueue;
+    QTimer mTimer;
+};
+
+void MarkMessageReadHandler::Private::handleMessages()
+{
+  while ( !mItemQueue.isEmpty() ) {
+    Akonadi::Item item = mItemQueue.dequeue();
+    item.setFlag( Akonadi::MessageFlags::Seen );
+
+    // TODO: maybe refetch the item?!?
+    Akonadi::ItemModifyJob *job = new Akonadi::ItemModifyJob( item, q );
+    job->ignorePayload();
+  }
+}
+
+
+MarkMessageReadHandler::MarkMessageReadHandler( QObject *parent )
+  : QObject( parent ), d( new Private( this ) )
+{
+  d->mTimer.setSingleShot( true );
+  connect( &d->mTimer, SIGNAL( timeout() ), this, SLOT( handleMessages() ) );
+}
+
+MarkMessageReadHandler::~MarkMessageReadHandler()
+{
+  delete d;
+}
+
+void MarkMessageReadHandler::setItem( const Akonadi::Item &item )
+{
+  if ( item.hasFlag( Akonadi::MessageFlags::Seen ) )
+    return;
+
+  d->mTimer.stop();
+
+  d->mItemQueue.enqueue( item );
+
+  if ( MessageViewer::GlobalSettings::self()->delayedMarkAsRead() &&
+       MessageViewer::GlobalSettings::self()->delayedMarkTime() != 0 ) {
+    d->mTimer.start( MessageViewer::GlobalSettings::self()->delayedMarkTime() * 1000 );
+    return;
+  }
+
+  d->handleMessages();
+}
+
+#include "markmessagereadhandler.moc"
