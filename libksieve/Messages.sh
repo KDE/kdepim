4673e9fc35db235cf10a3f080182eeeb3e9634a2
Pino Toscano <pino@kde.org>
2010-11-15 16:57:23 +0000

    fix syntax typo and simplify sources search
    svn path=/trunk/KDE/kdepim/; revision=1197395

--- libksieve/Messages.sh
+++ libksieve/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC `find . -name '*.kcfg' >> rc.cpp || exit 11
-$XGETTEXT shared/*.cpp parser/*.cpp impl/*.h ksieve/*.h ksieveui/*.cpp -o $podir/libksieve.pot
+$EXTRACTRC `find . -name '*.kcfg'` >> rc.cpp || exit 11
+$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/libksieve.pot
 rm -f rc.cpp
