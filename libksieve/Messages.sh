6e6f702eddb8aa8d04b64af6eeeeab1ee0ae48a3
Laurent Montel <montel@kde.org>
2010-11-15 16:34:23 +0000

    Fix extract messages
    svn path=/trunk/KDE/kdepim/; revision=1197387

--- libksieve/Messages.sh
+++ libksieve/Messages.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
-$XGETTEXT shared/*.cpp parser/*.cpp impl/*.h ksieve/*.h -o $podir/libksieve.pot
+$EXTRACTRC `find . -name '*.kcfg' >> rc.cpp || exit 11
+$XGETTEXT shared/*.cpp parser/*.cpp impl/*.h ksieve/*.h ksieveui/*.cpp -o $podir/libksieve.pot
+rm -f rc.cpp
