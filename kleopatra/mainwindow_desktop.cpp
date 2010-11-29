14994d43c3cebb726af0ecb9ea18ca74aacd4076
Marc Mutz <mutz@kde.org>
2010-11-29 13:59:07 +0000

    MainWindow: KleopatraApplication::openOrRaiseConfigDialog() is now a slot
    Fixes opening the confi dialog from Settings->Configure Kleopatra... - duh
    
    svn path=/trunk/KDE/kdepim/kleopatra/; revision=1202052

--- kleopatra/mainwindow_desktop.cpp
+++ kleopatra/mainwindow_desktop.cpp
@@ -335,7 +335,7 @@ void MainWindow::Private::setupActions() {
     KStandardAction::quit( q, SLOT(closeAndQuit()), coll );
     KStandardAction::configureToolbars( q, SLOT(configureToolbars()), coll );
     KStandardAction::keyBindings( q, SLOT(editKeybindings()), coll );
-    KStandardAction::preferences( qApp, SIGNAL(openOrRaiseConfigDialog()), coll );
+    KStandardAction::preferences( qApp, SLOT(openOrRaiseConfigDialog()), coll );
 
     q->createStandardStatusBarAction();
     q->setStandardToolBarMenuEnabled( true );
