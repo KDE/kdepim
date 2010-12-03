44d27ef9e88966517ccfc15d205ca57bd1233101
Dirk Mueller <mueller@kde.org>
2010-12-03 14:56:44 +0000

    KDE 4.6 Beta2
    svn path=/tags/KDE/4.5.85/kdepim/; revision=1203301

---
+++ runtime/agents/maildispatcher/maildispatcheragent.cpp
@@ -0,0 +1,351 @@
+/*
+    Copyright 2008 Ingo Klöcker <kloecker@kde.org>
+    Copyright 2009 Constantin Berzan <exit3219@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "maildispatcheragent.h"
+
+//#include "configdialog.h"
+#include "maildispatcheragentadaptor.h"
+#include "outboxqueue.h"
+#include "sendjob.h"
+#include "settings.h"
+#include "settingsadaptor.h"
+
+#include <akonadi/agentfactory.h>
+#include <akonadi/dbusconnectionpool.h>
+#include <akonadi/itemfetchscope.h>
+
+#include <KDebug>
+#include <KIcon>
+#include <KIconLoader>
+#include <KLocalizedString>
+#include <KMime/Message>
+#include <KNotification>
+#include <KWindowSystem>
+
+#include <QtCore/QTimer>
+#include <QtDBus/QDBusConnection>
+
+#ifdef KDEPIM_STATIC_LIBS
+extern bool ___MailTransport____INIT();
+#endif
+
+#ifdef MAIL_SERIALIZER_PLUGIN_STATIC
+#include <QtPlugin>
+
+Q_IMPORT_PLUGIN(akonadi_serializer_mail)
+#endif
+
+using namespace Akonadi;
+
+class MailDispatcherAgent::Private
+{
+  public:
+    Private( MailDispatcherAgent *parent )
+      : q( parent ),
+        currentJob( 0 ),
+        aborting( false ),
+        sendingInProgress( false ),
+        sentAnything( false ),
+        errorOccurred( false )
+    {
+    }
+
+    ~Private()
+    {
+    }
+
+    MailDispatcherAgent * const q;
+
+    OutboxQueue *queue;
+    SendJob *currentJob;
+    Item currentItem;
+    bool aborting;
+    bool sendingInProgress;
+    bool sentAnything;
+    bool errorOccurred;
+    qulonglong sentItemsSize;
+
+    // slots:
+    void abort();
+    void dispatch();
+    void itemFetched( const Item &item );
+    void queueError( const QString &message );
+    void sendPercent( KJob *job, unsigned long percent );
+    void sendResult( KJob *job );
+    void emitStatusReady();
+};
+
+
+void MailDispatcherAgent::Private::abort()
+{
+  if ( !q->isOnline() ) {
+    kDebug() << "Offline. Ignoring call.";
+    return;
+  }
+
+  if ( aborting ) {
+    kDebug() << "Already aborting.";
+    return;
+  }
+
+  if ( !sendingInProgress && queue->isEmpty() ) {
+    kDebug() << "MDA is idle.";
+    Q_ASSERT( q->status() == AgentBase::Idle );
+  } else {
+    kDebug() << "Aborting...";
+    aborting = true;
+    if ( currentJob ) {
+      currentJob->abort();
+    }
+    // Further SendJobs will mark remaining items in the queue as 'aborted'.
+  }
+}
+
+void MailDispatcherAgent::Private::dispatch()
+{
+  Q_ASSERT( queue );
+
+  if ( !q->isOnline() || sendingInProgress ) {
+    kDebug() << "Offline or busy. See you later.";
+    return;
+  }
+
+  if ( !queue->isEmpty() ) {
+    if ( !sentAnything ) {
+      sentAnything = true;
+      sentItemsSize = 0;
+      emit q->percent( 0 );
+    }
+    emit q->status( AgentBase::Running,
+        i18np( "Sending messages (1 item in queue)...",
+               "Sending messages (%1 items in queue)...", queue->count() ) );
+    kDebug() << "Attempting to dispatch the next message.";
+    sendingInProgress = true;
+    queue->fetchOne(); // will trigger itemFetched
+  } else {
+    kDebug() << "Empty queue.";
+    if ( aborting ) {
+      // Finished marking messages as 'aborted'.
+      aborting = false;
+      sentAnything = false;
+      emit q->status( AgentBase::Idle, i18n( "Sending canceled." ) );
+      QTimer::singleShot( 3000, q, SLOT( emitStatusReady() ) );
+    } else {
+      if ( sentAnything ) {
+        // Finished sending messages in queue.
+        sentAnything = false;
+        emit q->percent( 100 );
+        emit q->status( AgentBase::Idle, i18n( "Finished sending messages." ) );
+
+        if ( !errorOccurred ) {
+          const QPixmap pixmap = KIcon( "mail-folder-outbox" ).pixmap( KIconLoader::SizeSmall, KIconLoader::SizeSmall );
+          KNotification *notify = new KNotification( "emailsent" );
+          notify->setComponentData( q->componentData() );
+          notify->setPixmap( pixmap );
+          notify->setText( i18nc("Notification when the email was sent", "E-mail successfully sent" ) );
+          notify->sendEvent();
+        }
+      } else {
+        // Empty queue.
+        emit q->status( AgentBase::Idle, i18n( "No items in queue." ) );
+      }
+      QTimer::singleShot( 3000, q, SLOT( emitStatusReady() ) );
+    }
+
+    errorOccurred = false;
+  }
+}
+
+
+MailDispatcherAgent::MailDispatcherAgent( const QString &id )
+  : AgentBase( id ),
+    d( new Private( this ) )
+{
+  kDebug() << "maildispatcheragent: At your service, sir!";
+
+#ifdef KDEPIM_STATIC_LIBS
+    ___MailTransport____INIT();
+#endif
+
+  new SettingsAdaptor( Settings::self() );
+  new MailDispatcherAgentAdaptor( this );
+
+  DBusConnectionPool::threadConnection().registerObject( QLatin1String( "/Settings" ),
+                                                         Settings::self(), QDBusConnection::ExportAdaptors );
+
+  DBusConnectionPool::threadConnection().registerObject( QLatin1String( "/MailDispatcherAgent" ),
+                                                         this, QDBusConnection::ExportAdaptors );
+  DBusConnectionPool::threadConnection().registerService( QLatin1String( "org.freedesktop.Akonadi.MailDispatcherAgent" ) );
+
+  d->queue = new OutboxQueue( this );
+  connect( d->queue, SIGNAL( newItems() ),
+           this, SLOT( dispatch() ) );
+  connect( d->queue, SIGNAL( itemReady( const Akonadi::Item& ) ),
+           this, SLOT( itemFetched( const Akonadi::Item& ) ) );
+  connect( d->queue, SIGNAL( error( const QString& ) ),
+           this, SLOT( queueError( const QString& ) ) );
+  connect( this, SIGNAL( itemProcessed( Akonadi::Item, bool ) ),
+           d->queue, SLOT( itemProcessed( Akonadi::Item, bool ) ) );
+  connect( this, SIGNAL( abortRequested() ),
+           this, SLOT( abort() ) );
+}
+
+MailDispatcherAgent::~MailDispatcherAgent()
+{
+  delete d;
+}
+
+void MailDispatcherAgent::configure( WId windowId )
+{
+  Q_UNUSED( windowId );
+  kDebug() << "I have no options; you can't break me.";
+}
+
+void MailDispatcherAgent::doSetOnline( bool online )
+{
+  Q_ASSERT( d->queue );
+  if ( online ) {
+    kDebug() << "Online. Dispatching messages.";
+    emit status( AgentBase::Idle, i18n( "Online, sending messages in queue." ) );
+    QTimer::singleShot( 0, this, SLOT( dispatch() ) );
+  } else {
+    kDebug() << "Offline.";
+    emit status( AgentBase::Idle, i18n( "Offline, message sending suspended." ) );
+
+    // TODO: This way, the OutboxQueue will continue to react to changes in
+    // the outbox, but the MDA will just not send anything.  Is this what we
+    // want?
+  }
+
+  AgentBase::doSetOnline( online );
+}
+
+void MailDispatcherAgent::Private::itemFetched( const Item &item )
+{
+  kDebug() << "Fetched item" << item.id() << "; creating SendJob.";
+  Q_ASSERT( sendingInProgress );
+  Q_ASSERT( !currentItem.isValid() );
+  currentItem = item;
+  Q_ASSERT( currentJob == 0 );
+  emit q->itemDispatchStarted();
+
+  currentJob = new SendJob( item, q );
+  if ( aborting ) {
+    currentJob->setMarkAborted();
+  }
+
+  q->status( AgentBase::Running, i18nc( "Message with given subject is being sent.", "Sending: %1",
+                                        item.payload<KMime::Message::Ptr>()->subject()->asUnicodeString() ) );
+
+  connect( currentJob, SIGNAL( result( KJob* ) ),
+           q, SLOT( sendResult( KJob* ) ) );
+  connect( currentJob, SIGNAL( percent( KJob*, unsigned long ) ),
+           q, SLOT( sendPercent( KJob*, unsigned long ) ) );
+
+  currentJob->start();
+}
+
+void MailDispatcherAgent::Private::queueError( const QString &message )
+{
+  emit q->error( message );
+  errorOccurred = true;
+  // FIXME figure out why this does not set the status to Broken, etc.
+}
+
+void MailDispatcherAgent::Private::sendPercent( KJob *job, unsigned long )
+{
+  Q_ASSERT( sendingInProgress );
+  Q_ASSERT( job == currentJob );
+  // The progress here is actually the TransportJob, not the entire SendJob,
+  // because the post-job doesn't report progress.  This should be fine,
+  // since the TransportJob is the lengthiest operation.
+
+  // Give the transport 80% of the weight, and move-to-sendmail 20%.
+  const double transportWeight = 0.8;
+
+  const int percent = 100 * ( sentItemsSize + job->processedAmount( KJob::Bytes ) * transportWeight )
+                          / ( sentItemsSize + currentItem.size() + queue->totalSize() );
+
+  kDebug() << "sentItemsSize" << sentItemsSize
+           << "this job processed" << job->processedAmount( KJob::Bytes )
+           << "queue totalSize" << queue->totalSize()
+           << "total total size (sent+current+queue)" << ( sentItemsSize + currentItem.size() + queue->totalSize() )
+           << "new percentage" << percent << "old percentage" << q->progress();
+
+  if ( percent != q->progress() ) {
+    // The progress can decrease too, if messages got added to the queue.
+    emit q->percent( percent );
+  }
+
+  // It is possible that the number of queued messages has changed.
+  emit q->status( AgentBase::Running,
+      i18np( "Sending messages (1 item in queue)...",
+             "Sending messages (%1 items in queue)...", 1 + queue->count() ) );
+}
+
+void MailDispatcherAgent::Private::sendResult( KJob *job )
+{
+  Q_ASSERT( sendingInProgress );
+  Q_ASSERT( job == currentJob );
+  currentJob->disconnect( q );
+  currentJob = 0;
+
+  Q_ASSERT( currentItem.isValid() );
+  sentItemsSize += currentItem.size();
+  emit q->itemProcessed( currentItem, !job->error() );
+  currentItem = Item();
+
+  if ( job->error() ) {
+    // The SendJob gave the item an ErrorAttribute, so we don't have to
+    // do anything.
+    kDebug() << "Sending failed. error:" << job->errorString();
+
+    const QPixmap pixmap = KIcon( "dialog-error" ).pixmap( KIconLoader::SizeSmall, KIconLoader::SizeSmall );
+    KNotification *notify = new KNotification( "sendingfailed" );
+    notify->setComponentData( q->componentData() );
+    notify->setPixmap( pixmap );
+    notify->setText( i18nc("Notification when email sending failed", "E-mail sending failed: %1", job->errorString() ) );
+    notify->sendEvent();
+
+    errorOccurred = true;
+  } else {
+    kDebug() << "Sending succeeded.";
+  }
+
+  // dispatch next message
+  sendingInProgress = false;
+  QTimer::singleShot( 0, q, SLOT( dispatch() ) );
+}
+
+void MailDispatcherAgent::Private::emitStatusReady()
+{
+  if ( q->status() == AgentBase::Idle ) {
+    // If still idle after aborting, clear 'aborted' status.
+    emit q->status( AgentBase::Idle, i18n( "Ready to dispatch messages." ) );
+  }
+}
+
+#ifdef KDEPIM_PLUGIN_AGENT
+AKONADI_AGENT_FACTORY( MailDispatcherAgent, akonadi_maildispatcher_agent )
+#else
+AKONADI_AGENT_MAIN( MailDispatcherAgent)
+#endif
+
+#include "maildispatcheragent.moc"
---
+++ runtime/resources/imap/subscriptiondialog.cpp
@@ -0,0 +1,397 @@
+/*
+    Copyright (c) 2010 Klarälvdalens Datakonsult AB,
+                       a KDAB Group company <info@kdab.com>
+    Author: Kevin Ottens <kevin@kdab.com>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "subscriptiondialog.h"
+
+#include <QtCore/QCoreApplication>
+#include <QtGui/QStandardItemModel>
+#include <QtGui/QBoxLayout>
+#include <QtGui/QKeyEvent>
+
+#include <klocale.h>
+#include <kdebug.h>
+#include <klineedit.h>
+
+#include <kimap/session.h>
+#include <kimap/loginjob.h>
+#include <kimap/unsubscribejob.h>
+#include <kimap/subscribejob.h>
+
+#include "imapaccount.h"
+#include "sessionuiproxy.h"
+
+#ifndef KDEPIM_MOBILE_UI
+#include <QtGui/QHeaderView>
+#include <QtGui/QCheckBox>
+#include <QtGui/QLabel>
+#include <QtGui/QTreeView>
+#else
+#include <QtGui/QListView>
+#include <QtGui/QSortFilterProxyModel>
+#include "kdescendantsproxymodel_p.h"
+
+class CheckableFilterProxyModel : public QSortFilterProxyModel
+{
+public:
+  CheckableFilterProxyModel( QObject *parent = 0 )
+    : QSortFilterProxyModel( parent ) { }
+
+protected:
+  /*reimp*/ bool filterAcceptsRow( int sourceRow, const QModelIndex &sourceParent ) const
+  {
+    QModelIndex sourceIndex = sourceModel()->index( sourceRow, 0, sourceParent );
+    return sourceModel()->flags(sourceIndex) & Qt::ItemIsUserCheckable;
+  }
+};
+
+
+#endif
+
+
+
+SubscriptionDialog::SubscriptionDialog( QWidget *parent )
+  : KDialog( parent ),
+    m_session( 0 ),
+    m_subscriptionChanged( false ),
+    m_lineEdit( 0 ),
+    m_filter( new SubscriptionFilterProxyModel( this ) ),
+    m_model( new QStandardItemModel( this ) )
+{
+  setModal( true );
+  setButtons( Ok | Cancel | User1 );
+
+  setButtonText( User1, i18n( "Reload &List" ) );
+  enableButton( User1, false );
+  connect( this, SIGNAL(user1Clicked()),
+          this, SLOT(onReloadRequested()) );
+
+  QWidget *mainWidget = new QWidget( this );
+  QVBoxLayout *mainLayout = new QVBoxLayout;
+  mainWidget->setLayout(mainLayout);
+  setMainWidget( mainWidget );
+
+  QHBoxLayout *filterBarLayout = new QHBoxLayout;
+  mainLayout->addLayout(filterBarLayout);
+
+#ifndef KDEPIM_MOBILE_UI
+  filterBarLayout->addWidget( new QLabel( i18n("Search:") ) );
+#endif
+
+  m_lineEdit = new KLineEdit( mainWidget );
+  m_lineEdit->setClearButtonShown( true );
+  connect( m_lineEdit, SIGNAL(textChanged(QString)),
+           m_filter, SLOT(setSearchPattern(QString)) );
+  filterBarLayout->addWidget( m_lineEdit );
+
+#ifndef KDEPIM_MOBILE_UI
+  QCheckBox *checkBox = new QCheckBox( i18n("Subscribed only"), mainWidget );
+  connect( checkBox, SIGNAL(stateChanged(int)),
+           m_filter, SLOT(setIncludeCheckedOnly(int)) );
+  filterBarLayout->addWidget( checkBox );
+
+  QTreeView *treeView = new QTreeView( mainWidget );
+  treeView->header()->hide();
+  m_filter->setSourceModel( m_model );
+  treeView->setModel( m_filter );
+  mainLayout->addWidget( treeView );
+#else
+  m_lineEdit->hide();
+  connect( m_lineEdit, SIGNAL(textChanged(QString)),
+           this, SLOT(onMobileLineEditChanged(QString)) );
+
+  QListView *listView = new QListView( mainWidget );
+
+  KDescendantsProxyModel *flatModel = new KDescendantsProxyModel( listView );
+  flatModel->setDisplayAncestorData( true );
+  flatModel->setAncestorSeparator( "/" );
+  flatModel->setSourceModel( m_model );
+
+  CheckableFilterProxyModel *checkableModel = new CheckableFilterProxyModel( listView );
+  checkableModel->setSourceModel( flatModel );
+
+  m_filter->setSourceModel( checkableModel );
+
+  listView->setModel( m_filter );
+  mainLayout->addWidget( listView );
+
+  // We want to get all the keyboard input all the time
+  grabKeyboard();
+#endif
+
+  connect( m_model, SIGNAL(itemChanged(QStandardItem*)),
+           this, SLOT(onItemChanged(QStandardItem*)) );
+}
+
+void SubscriptionDialog::connectAccount( const ImapAccount &account, const QString &password )
+{
+  m_session = new KIMAP::Session( account.server(), account.port(), this );
+  m_session->setUiProxy( SessionUiProxy::Ptr( new SessionUiProxy ) );
+
+  KIMAP::LoginJob *login = new KIMAP::LoginJob( m_session );
+  login->setUserName( account.userName() );
+#ifndef Q_OS_WINCE
+  login->setPassword( password );
+#else
+  login->setPassword( account.password() );
+#endif
+  login->setEncryptionMode( account.encryptionMode() );
+  login->setAuthenticationMode( account.authenticationMode() );
+
+  connect( login, SIGNAL( result( KJob* ) ),
+           this, SLOT( onLoginDone( KJob* ) ) );
+  login->start();
+}
+
+bool SubscriptionDialog::isSubscriptionChanged()
+{
+  return m_subscriptionChanged;
+}
+
+void SubscriptionDialog::onLoginDone( KJob *job )
+{
+  if ( !job->error() ) {
+    onReloadRequested();
+  }
+}
+
+void SubscriptionDialog::onReloadRequested()
+{
+  enableButton( User1, false );
+  m_itemsMap.clear();
+  m_model->clear();
+
+  // we need a connection
+  if ( !m_session
+    || m_session->state() != KIMAP::Session::Authenticated ) {
+    kWarning() <<"SubscriptionDialog - got no connection";
+    enableButton( User1, true );
+    return;
+  }
+
+  KIMAP::ListJob *list = new KIMAP::ListJob( m_session );
+  list->setIncludeUnsubscribed( true );
+  connect( list, SIGNAL( mailBoxesReceived(QList<KIMAP::MailBoxDescriptor>, QList< QList<QByteArray> >) ),
+           this, SLOT( onMailBoxesReceived(QList<KIMAP::MailBoxDescriptor>, QList< QList<QByteArray> >) ) );
+  connect( list, SIGNAL( result(KJob*) ), this, SLOT( onFullListingDone(KJob*) ) );
+  list->start();
+}
+
+void SubscriptionDialog::onMailBoxesReceived( const QList<KIMAP::MailBoxDescriptor> &mailBoxes,
+                                              const QList< QList<QByteArray> > &flags )
+{
+  for ( int i = 0; i<mailBoxes.size(); i++ ) {
+    KIMAP::MailBoxDescriptor mailBox = mailBoxes[i];
+
+    const QStringList pathParts = mailBox.name.split(mailBox.separator);
+    const QString separator = mailBox.separator;
+    Q_ASSERT( separator.size() == 1 ); // that's what the spec says
+
+    QString parentPath;
+    QString currentPath;
+
+    for ( int j = 0; j < pathParts.size(); ++j ) {
+      const bool isDummy = j != pathParts.size() - 1;
+      const bool isCheckable = !isDummy && !flags[i].contains("\\NoSelect");
+
+      const QString pathPart = pathParts.at( j );
+      currentPath += separator + pathPart;
+
+      if ( m_itemsMap.contains(currentPath) ) {
+        if ( !isDummy ) {
+          QStandardItem *item = m_itemsMap[currentPath];
+          item->setCheckable( isCheckable );
+        }
+
+      } else if ( !parentPath.isEmpty() ) {
+        Q_ASSERT( m_itemsMap.contains(parentPath) );
+
+        QStandardItem *parentItem = m_itemsMap[parentPath];
+
+        QStandardItem *item = new QStandardItem( pathPart );
+        item->setFlags( Qt::ItemIsSelectable | Qt::ItemIsEnabled );
+        item->setCheckable( isCheckable );
+        item->setData( currentPath.mid(1), PathRole );
+        parentItem->appendRow( item );
+        m_itemsMap[currentPath] = item;
+
+      } else {
+        QStandardItem *item = new QStandardItem( pathPart );
+        item->setFlags( Qt::ItemIsSelectable | Qt::ItemIsEnabled );
+        item->setCheckable( isCheckable );
+        item->setData( currentPath.mid(1), PathRole );
+        m_model->appendRow( item );
+        m_itemsMap[currentPath] = item;
+      }
+
+      parentPath = currentPath;
+    }
+  }
+}
+
+void SubscriptionDialog::onFullListingDone( KJob *job )
+{
+  if ( job->error() ) {
+    enableButton( User1, true );
+    return;
+  }
+
+  KIMAP::ListJob *list = new KIMAP::ListJob( m_session );
+  list->setIncludeUnsubscribed( false );
+  connect( list, SIGNAL( mailBoxesReceived(QList<KIMAP::MailBoxDescriptor>, QList< QList<QByteArray> >) ),
+           this, SLOT( onSubscribedMailBoxesReceived(QList<KIMAP::MailBoxDescriptor>, QList< QList<QByteArray> >) ) );
+  connect( list, SIGNAL( result(KJob*) ), this, SLOT( onReloadDone(KJob*) ) );
+  list->start();
+}
+
+void SubscriptionDialog::onSubscribedMailBoxesReceived( const QList<KIMAP::MailBoxDescriptor> &mailBoxes,
+                                                        const QList< QList<QByteArray> > &flags )
+{
+  for ( int i = 0; i<mailBoxes.size(); i++ ) {
+    KIMAP::MailBoxDescriptor mailBox = mailBoxes[i];
+    QString descriptor = mailBox.separator + mailBox.name;
+
+    if ( m_itemsMap.contains( descriptor ) ) {
+      QStandardItem *item = m_itemsMap[mailBox.separator + mailBox.name];
+      item->setCheckState( Qt::Checked );
+      item->setData( Qt::Checked, InitialStateRole );
+    }
+  }
+}
+
+void SubscriptionDialog::onReloadDone( KJob *job )
+{
+  enableButton( User1, true );
+}
+
+void SubscriptionDialog::onItemChanged( QStandardItem *item )
+{
+  QFont font = item->font();
+  font.setBold( item->checkState()!=item->data( InitialStateRole ).toInt() );
+  item->setFont( font );
+}
+
+void SubscriptionDialog::slotButtonClicked( int button )
+{
+  if ( button == KDialog::Ok ) {
+    applyChanges();
+    accept();
+  } else {
+    KDialog::slotButtonClicked( button );
+  }
+}
+
+void SubscriptionDialog::applyChanges()
+{
+  QList<QStandardItem*> items = m_itemsMap.values();
+
+  while ( !items.isEmpty() ) {
+    QStandardItem *item = items.takeFirst();
+
+    if ( item->checkState()!=item->data( InitialStateRole ).toInt() ) {
+      if ( item->checkState() == Qt::Checked ) {
+        kDebug() << "Subscribing" << item->data( PathRole );
+        KIMAP::SubscribeJob *subscribe = new KIMAP::SubscribeJob( m_session );
+        subscribe->setMailBox( item->data( PathRole ).toString() );
+        subscribe->exec();
+      } else {
+        kDebug() << "Unsubscribing" << item->data( PathRole );
+        KIMAP::UnsubscribeJob *unsubscribe = new KIMAP::UnsubscribeJob( m_session );
+        unsubscribe->setMailBox( item->data( PathRole ).toString() );
+        unsubscribe->exec();
+      }
+
+      m_subscriptionChanged = true;
+    }
+  }
+}
+
+SubscriptionFilterProxyModel::SubscriptionFilterProxyModel( QObject* parent )
+  : KRecursiveFilterProxyModel( parent ), m_checkedOnly( false )
+{
+
+}
+
+void SubscriptionFilterProxyModel::setSearchPattern( const QString &pattern )
+{
+  m_pattern = pattern;
+  invalidate();
+}
+
+void SubscriptionFilterProxyModel::setIncludeCheckedOnly( bool checkedOnly )
+{
+  m_checkedOnly = checkedOnly;
+  invalidate();
+}
+
+void SubscriptionFilterProxyModel::setIncludeCheckedOnly( int checkedOnlyState )
+{
+  m_checkedOnly = (checkedOnlyState == Qt::Checked);
+  invalidate();
+}
+
+bool SubscriptionFilterProxyModel::acceptRow(int sourceRow, const QModelIndex &sourceParent) const
+{
+  QModelIndex sourceIndex = sourceModel()->index(sourceRow, 0, sourceParent);
+
+  const bool checked = sourceIndex.data(Qt::CheckStateRole).toInt()==Qt::Checked;
+
+  if ( m_checkedOnly && !checked ) {
+    return false;
+  } else if ( !m_pattern.isEmpty() ) {
+    const QString text = sourceIndex.data(Qt::DisplayRole).toString();
+    return text.contains( m_pattern, Qt::CaseInsensitive );
+  } else {
+    return true;
+  }
+}
+
+void SubscriptionDialog::onMobileLineEditChanged( const QString &text )
+{
+    if ( !text.isEmpty() && !m_lineEdit->isVisible() ) {
+      m_lineEdit->show();
+      m_lineEdit->setFocus();
+      m_lineEdit->grabKeyboard(); // Now the line edit runs the show
+    } else if ( text.isEmpty() && m_lineEdit->isVisible() ) {
+      m_lineEdit->hide();
+      grabKeyboard(); // Line edit gone, so let's get all the events for us again
+    }
+}
+
+void SubscriptionDialog::keyPressEvent( QKeyEvent *event )
+{
+#ifndef KDEPIM_MOBILE_UI
+  KDialog::keyPressEvent( event );
+#else
+  static bool isSendingEvent = false;
+
+  if ( !isSendingEvent
+    && !event->text().isEmpty()
+    && !m_lineEdit->isVisible() ) {
+    isSendingEvent = true;
+    QCoreApplication::sendEvent( m_lineEdit, event );
+    isSendingEvent = false;
+  } else {
+    KDialog::keyPressEvent( event );
+  }
+#endif
+}
+
+#include "subscriptiondialog.moc"
