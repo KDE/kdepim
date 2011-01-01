b641433e084efba228b781c27f8827ebc3c82b05
Torgny Nyblom <kde@nyblom.org>
2011-01-01 22:19:05 +0100

    Death to ifndef Foo_NOT_Bar

--- templateparser/templateparser.cpp
+++ templateparser/templateparser.cpp
@@ -60,10 +60,10 @@
 #include <QWebElement>
 #include <QTextCodec>
 #include <QWebFrame>
-#ifndef TEMPLATEPARSER_NO_WEBKIT
-# include <QtWebKit/QWebPage>
-#else
+#ifdef TEMPLATEPARSER_NO_WEBKIT
 # include <QTextBrowser>
+#else
+# include <QtWebKit/QWebPage>
 #endif
 
 namespace TemplateParser {
@@ -1372,18 +1372,18 @@ QString TemplateParser::asPlainTextFromObjectTree( const KMime::Message::Ptr &ms
     return result;
 
   // html -> plaintext conversion, if necessary:
-#ifndef TEMPLATEPARSER_NO_WEBKIT
-  if ( isHTML /* TODO port it && mDecodeHTML*/ ) {
-    QWebPage doc;
-    doc.mainFrame()->setHtml( result );
-    result = doc.mainFrame()->toPlainText();
-  }
+#ifdef TEMPLATEPARSER_NO_WEBKIT
+    if ( isHTML /* TODO port it && mDecodeHTML*/ ) {
+      QTextDocument doc;
+      doc.setHtml( result );
+      result = doc.toPlainText();
+    }
 #else
-  if ( isHTML /* TODO port it && mDecodeHTML*/ ) {
-    QTextDocument doc;
-    doc.setHtml( result );
-    result = doc.toPlainText();
-  }
+    if ( isHTML /* TODO port it && mDecodeHTML*/ ) {
+      QWebPage doc;
+      doc.mainFrame()->setHtml( result );
+      result = doc.mainFrame()->toPlainText();
+    }
 #endif
 
   // strip the signature (footer):
