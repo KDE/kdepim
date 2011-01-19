650d05a1bdb5ea60f6bf1c9eb5ae5a601120b364
Andre Heinecke <aheinecke@intevation.de>
2011-01-19 18:28:10 +0000

    Convert filters to kde format for kfiledialog
        Instead of disabling filters when KFileDialog is used
        they are now converted to the correct format.
        The qt2KdeFilter is a copy out of KFileDialog.
    
        CCBUG: 263604

--- kleopatra/utils/filedialog.cpp
+++ kleopatra/utils/filedialog.cpp
@@ -38,6 +38,7 @@
 #ifdef QT_NO_FILEDIALOG
 #include <KFileDialog>
 #include <KUrl>
+#include <QTextStream>
 #endif
 
 #include <QMap>
@@ -62,6 +63,35 @@ static void update( const QString & fname, const QString & id ) {
         (*dir_id_2_dir_map())[ id ] = QFileInfo( fname ).absolutePath();
 }
 
+#ifdef QT_NO_FILEDIALOG
+static QString qt2KdeFilter(const QString &f)
+{
+  QString               filter;
+  QTextStream           str(&filter, QIODevice::WriteOnly);
+  QStringList           list(f.split(";;").replaceInStrings("/", "\\/"));
+  QStringList::const_iterator it(list.begin()),
+    end(list.end());
+  bool                  first=true;
+
+  for(; it!=end; ++it)
+  {
+    int ob=(*it).lastIndexOf('('),
+        cb=(*it).lastIndexOf(')');
+
+    if(-1!=cb && ob<cb)
+    {
+      if(first)
+        first=false;
+      else
+        str << '\n';
+      str << (*it).mid(ob+1, (cb-ob)-1) << '|' << (*it).mid(0, ob);
+    }
+  }
+
+  return filter;
+}
+#endif
+
 QString FileDialog::getExistingDirectory( QWidget * parent, const QString & caption, const QString & dirID ) {
 #ifndef QT_NO_FILEDIALOG
     const QString fname = QFileDialog::getExistingDirectory( parent, caption, dir( dirID ) );
@@ -76,8 +106,7 @@ QString FileDialog::getOpenFileName( QWidget * parent, const QString & caption, 
 #ifndef QT_NO_FILEDIALOG
     const QString fname = QFileDialog::getOpenFileName( parent, caption, dir( dirID ), filter );
 #else
-    Q_UNUSED( filter ); // incompatible syntax
-    const QString fname = KFileDialog::getOpenFileName( dir( dirID ), QString(), parent, caption );
+    const QString fname = KFileDialog::getOpenFileName( dir( dirID ), qt2KdeFilter( filter ), parent, caption );
 #endif
     update( fname, dirID );
     return fname;
@@ -87,8 +116,7 @@ QStringList FileDialog::getOpenFileNames( QWidget * parent, const QString & capt
 #ifndef QT_NO_FILEDIALOG
     const QStringList files = QFileDialog::getOpenFileNames( parent, caption, dir( dirID ), filter );
 #else
-    Q_UNUSED( filter ); // incompatible syntax
-    const QStringList files = KFileDialog::getOpenFileNames( dir( dirID ), QString(), parent, caption );
+    const QStringList files = KFileDialog::getOpenFileNames( dir( dirID ), qt2KdeFilter( filter ), parent, caption );
 #endif
     if ( !files.empty() )
         update( files.front(), dirID );
@@ -99,8 +127,7 @@ QString FileDialog::getSaveFileName( QWidget * parent, const QString & caption, 
 #ifndef QT_NO_FILEDIALOG
     const QString fname = QFileDialog::getSaveFileName( parent, caption, dir( dirID ), filter );
 #else
-    Q_UNUSED( filter ); // incompatible syntax
-    const QString fname = KFileDialog::getSaveFileName( dir( dirID ), QString(), parent, caption );
+    const QString fname = KFileDialog::getSaveFileName( dir( dirID ), qt2KdeFilter( filter ), parent, caption );
 #endif
     update( fname, dirID );
     return fname;
@@ -112,7 +139,7 @@ QString FileDialog::getSaveFileNameEx( QWidget * parent, const QString & caption
 #ifndef QT_NO_FILEDIALOG
     const QString fname = QFileDialog::getSaveFileName( parent, caption, QDir( dir( dirID ) ).filePath( proposedFileName ), filter );
 #else
-    const QString fname = KFileDialog::getSaveFileName( QDir( dir( dirID ) ).filePath( proposedFileName ), QString(), parent, caption );
+    const QString fname = KFileDialog::getSaveFileName( QDir( dir( dirID ) ).filePath( proposedFileName ), qt2KdeFilter( filter ), parent, caption );
 #endif
     update( fname, dirID );
     return fname;
