0339c140f75cb84e270c8e69da5b6909f488dccb
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-01-24 19:11:41 +0100

    use xgettext instead of extractqml script to extract translation strings fixes extraction of multiline strings

--- mobile/mail/Messages.sh
+++ mobile/mail/Messages.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
 $EXTRACTRC --ignore-no-input `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
-$EXTRACTQML `find . -name '*.qml'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/kmail-mobile.pot
+$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' -o -name '*.qml'` -o $podir/kmail-mobile.pot
 rm -f rc.cpp
