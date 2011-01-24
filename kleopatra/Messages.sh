0263bf02caedfd38b514af498db714701cde0487
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-01-24 16:33:46 +0100

    extract strings in qml for message translation

--- kleopatra/Messages.sh
+++ kleopatra/Messages.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
+$EXTRACTQML `find . -name '*.qml'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kleopatra.pot
