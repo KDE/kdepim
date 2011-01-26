9743c2e2e3a81fbf7d1588abfecb034ce5c9f4d8
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-01-26 10:22:35 +0100

    fix qml multiline message extraction by using xgettext with language java. Qml syntax concats strings with '+' similar to java

--- mobile/calendar/Messages.sh
+++ mobile/calendar/Messages.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 $EXTRACTRC --ignore-no-input `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' -o -name '*.qml'` -o $podir/korganizer-mobile.pot
+$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/korganizer-mobile.pot
+$XGETTEXT -ktranslate `find . -name '*.qml'` -j -L Java -o $podir/korganizer-mobile.pot
 rm -f rc.cpp
