fe2c29e48306aecce5d6534f686fdf6c3f19f1be
Bjoern Ricks <bjoern.ricks@intevation.de>
2011-01-28 09:52:36 +0100

    add calendarsupport catalog for kontact touch mail

--- mobile/mail/main.cpp
+++ mobile/mail/main.cpp
@@ -76,6 +76,7 @@ int main( int argc, char **argv )
   KGlobal::locale()->insertCatalog( "libkcalutils" ); // for invitation handling
   KGlobal::locale()->insertCatalog( "libkleopatra" ); // for Krypto format settings in identity dialog
   KGlobal::locale()->insertCatalog( "libkpimidentities" ); // for signature settings in identity dialog
+  KGlobal::locale()->insertCatalog( "calendarsupport" ); // for error messages while updating events and tasks
 
   return app.exec();
 }
