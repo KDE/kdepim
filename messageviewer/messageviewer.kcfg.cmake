62e7432f67d96f4335f20b8a732fd8a0b029340b
Tobias Koenig <tokoe@kde.org>
2011-01-31 11:20:51 +0100

    Delete invitation mail after acted on by default
    BUG: 264447

--- messageviewer/messageviewer.kcfg.cmake
+++ messageviewer/messageviewer.kcfg.cmake
@@ -208,7 +208,7 @@ xsi:schemaLocation="http://www.kde.org/standards/kcfg/1.0 http://www.kde.org/sta
       <entry name="DeleteInvitationEmailsAfterSendingReply" type="Bool">
         <label>Delete invitation emails after the reply to them has been sent</label>
         <whatsthis>When this is checked, received invitation emails that have been replied to will be moved to the Trash folder, once the reply has been successfully sent.</whatsthis>
-        <default>false</default>
+        <default>true</default>
       </entry>
 
       <entry name="AskForCommentWhenReactingToInvitation"  type="Enum">
