c43d0f4fed42826989076afc20b207446111296e
Allen Winter <allen.winter@kdab.com>
2011-02-11 10:59:36 -0500

    GIT_SILENT: Update version numbers for today's release.

--- kontact/src/main.cpp
+++ kontact/src/main.cpp
@@ -47,7 +47,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.2.9 (enterprise35 0.20101217.1207316)";
+static const char version[] = "1.2.9 (enterprise35 0.20110211.f8989f1)";
 
 class KontactApp : public KUniqueApplication {
   public:
