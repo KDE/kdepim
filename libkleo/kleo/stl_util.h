6da84b69041116e1a0f76e32ae694f9afbe50627
Andreas Holzammer <andreas.holzammer@kdab.com>
2011-02-25 16:59:19 +0100

    dont confuse copy_if and none_of for kdetools and boost

--- libkleo/kleo/stl_util.h
+++ libkleo/kleo/stl_util.h
@@ -431,7 +431,7 @@ namespace kdtools {
     template <typename O, typename I, typename P>
     O copy_if( const I & i, P p ) {
         O o;
-        copy_if( boost::begin( i ), boost::end( i ), std::back_inserter( o ), p );
+        kdtools::copy_if( boost::begin( i ), boost::end( i ), std::back_inserter( o ), p );
         return o;
     }
 
@@ -493,7 +493,7 @@ namespace kdtools {
 
     template <typename C, typename P>
     bool none_of( const C & c, P p ) {
-        return none_of( boost::begin( c ), boost::end( c ), p );
+        return kdtools::none_of( boost::begin( c ), boost::end( c ), p );
     }
 
     template <typename C, typename B>
