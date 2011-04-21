6c618ad8dcdaa4e7e574cc8ae8f907ccb553efec
Torgny Nyblom <kde@nyblom.org>
2011-04-21 20:54:16 +0200

    Update copywrite
    GIT_SILENT

--- messageviewer/htmlquotecolorer.cpp
+++ messageviewer/htmlquotecolorer.cpp
@@ -1,4 +1,5 @@
 /* Copyright 2009 Thomas McGuire <mcguire@kde.org>
+             2009 Torgny Nyblom <kde@nyblom.org>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
