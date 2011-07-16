8034495a6269fb24fdc3ce5000afe744268a4e33
Montel Laurent <montel@kde.org>
2011-07-16 19:06:01 +0200

    Fetch header too needs by mailing list (cherry picked from commit 35cf4a0b31affe46611e934ebb9312fc9fe3411a)

--- mailcommon/foldercollectionmonitor.cpp
+++ mailcommon/foldercollectionmonitor.cpp
@@ -50,6 +50,7 @@ FolderCollectionMonitor::FolderCollectionMonitor( QObject *parent )
   mMonitor->setResourceMonitored( "akonadi_nepomuktag_resource" ,  true );
 #endif
   mMonitor->itemFetchScope().fetchPayloadPart( Akonadi::MessagePart::Envelope );
+  mMonitor->itemFetchScope().fetchPayloadPart( Akonadi::MessagePart::Header );
 }
 
 FolderCollectionMonitor::~FolderCollectionMonitor()
