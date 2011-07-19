a734cdfcd6401f885bfc83b4369cf9ff11ec6b0e
Allen Winter <allen.winter@kdab.com>
2011-07-18 20:28:47 -0400

    GIT_SILENT: Update version numbers for today's release.

--- kmail/kmversion.h
+++ kmail/kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20110627.63330a1)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20110718.ce6dd46)"
 
 #endif /*kmversion_h*/
