a992aafdf64ccee345f57f039f527c7cc9b8e140
Sudhendu Kumar <sudhendu.kumar.roy@gmail.com>
2011-07-29 23:34:00 +0530

    Implemented validating HTML by QDomDocument

--- templateparser/templateparser.cpp
+++ templateparser/templateparser.cpp
@@ -59,6 +59,7 @@
 #include <QFileInfo>
 #include <QDir>
 #include <QTextCodec>
+#include <qdom.h>
 
 namespace TemplateParser {
 
@@ -1565,20 +1566,28 @@ QString TemplateParser::plainToHtml( const QString &body ) const
 
 QString TemplateParser::makeValidHtml( QString& body )
 {
-  QRegExp regEx;
-  regEx.setMinimal( true );
-  regEx.setPattern( "<html.*>" );
+  if( !body.isEmpty() ) {
+    QDomDocument doc;
 
-  if( !body.isEmpty() && !body.contains( regEx ) ) {
-    regEx.setPattern( "<body.*>" );
-    if( !body.contains( regEx ) ) {
-      body = "<body>" + body + "</body>";
+    QDomElement htmlElement = doc.createElement("html");
+    doc.appendChild( htmlElement );
+
+    if( !mHeadElement.isEmpty() ) {
+      mHeadElement = "<head>" + mHeadElement + "</head>";
+    } else {
+      mHeadElement = "<head></head>";
     }
-    regEx.setPattern( "<head.*>" );
-    if( !body.contains( regEx ) ) {
-      body = "<head>" + mHeadElement +"</head>" + body;
-    }
-    body = "<html>" + body + "</html>";
+
+    QDomDocument headDoc;
+    headDoc.setContent( mHeadElement );
+    htmlElement.appendChild( headDoc );
+    body = "<body>" + body + "<body>";
+
+    QDomDocument bodyDoc;
+    bodyDoc.setContent( body );
+    htmlElement.insertAfter( bodyDoc, headDoc );
+
+    return doc.toString();
   }
   return body;
 }
