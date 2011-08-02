b788df62f985d60ce93cdba9d3126ca6069f36f6
Montel Laurent <montel@kde.org>
2011-08-02 10:51:35 +0200

    Backport Use name and not filename otherwise when we change name in dialogbox it's never updated in treeview fix bug#Bug 279095

--- messagecomposer/attachmentmodel.cpp
+++ messagecomposer/attachmentmodel.cpp
@@ -261,7 +261,7 @@ QVariant AttachmentModel::data( const QModelIndex &index, int role ) const
   if( role == Qt::DisplayRole ) {
     switch( index.column() ) {
       case NameColumn:
-        return QVariant::fromValue( part->fileName().isEmpty() ? part->name() : part->fileName() );
+        return QVariant::fromValue( part->name().isEmpty() ? part->fileName() : part->name() );	      
       case SizeColumn:
         return QVariant::fromValue( KGlobal::locale()->formatByteSize( part->size() ) );
       case EncodingColumn:
