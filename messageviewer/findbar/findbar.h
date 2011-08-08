b3a100e72137ff09e0d60f15efbb898b66c3fa03
Torgny Nyblom <nyblom@kde.org>
2011-08-08 14:19:01 +0200

    Update email
    GIT_SILENT

--- messageviewer/findbar/findbar.h
+++ messageviewer/findbar/findbar.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2010 Torgny Nyblom <kde@nyblom.org>
+/* Copyright (C) 2010 Torgny Nyblom <nyblom@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
