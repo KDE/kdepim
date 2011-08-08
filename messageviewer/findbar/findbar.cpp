5e4e94272ed0d030fc2a3d5016df6dd5bf62750c
Torgny Nyblom <nyblom@kde.org>
2011-08-08 14:19:01 +0200

    Update email
    GIT_SILENT
    (cherry picked from commit b3a100e72137ff09e0d60f15efbb898b66c3fa03)

--- messageviewer/findbar/findbar.cpp
+++ messageviewer/findbar/findbar.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 2010 Torgny Nyblom <kde@nyblom.org>
+/* Copyright (C) 2010 Torgny Nyblom <nyblom@kde.org>
  * Copyright (C) 2010 Laurent Montel <montel@kde.org>
  *
  * This library is free software; you can redistribute it and/or
