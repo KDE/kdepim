7a505e43977164e028243095e8b081e4949c24e4
Montel Laurent <montel@kde.org>
2011-09-10 14:28:10 +0200

    Not necessary

--- messagecore/attachmentfromurlbasejob.cpp
+++ messagecore/attachmentfromurlbasejob.cpp
@@ -1,5 +1,4 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
     Copyright (C) 2011  Martin Bednár <serafean@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
