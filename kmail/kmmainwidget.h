7585e58daed226455d235d2a0a2e61de0a805d4d
Andras Mantia <amantia@kde.org>
2011-09-18 00:30:37 +0300

    Change synchornous akonadi job execution to async to prevent strange deadlocks. Remove code that doesn't make sense anymore, and try to actually make it work (fails though, too tired to debug it, but see the existing FIXME).
    BUG: 275789

--- kmail/kmmainwidget.h
+++ kmail/kmmainwidget.h
@@ -279,6 +279,7 @@ class KMAIL_EXPORT KMMainWidget : public QWidget
 
     void slotStartCheckMail();
     void slotEndCheckMail();
+    void slotEndCheckFetchCollectionsDone(KJob* job);
 
     void slotCollectionProperties();
 
@@ -529,8 +530,6 @@ class KMAIL_EXPORT KMMainWidget : public QWidget
 
     void itemsReceived(const Akonadi::Item::List &list );
     void itemsFetchDone( KJob *job );
-
-    void slotItemNotMovedByFilters( const Akonadi::Item& item );
 
     void slotCollectionPropertiesContinued( KJob* job );
 private:
