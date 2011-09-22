f5247b70b5b0a63d96842a88d79b5ef20546821f
José Millán Soto <fid@gpul.org>
2011-09-22 16:24:32 +0200

    Implementation of an accessible interface for MailWebView
    Initial version of the interface, only has some functions of
    the text interface
    
    Created MessageViewerAccessibleWidgetFactory
    
    REVIEW: 102410

---
+++ messageviewer/a11y/accessiblemailwebview.cpp
@@ -0,0 +1,130 @@
+/*
+    Copyright 2011  José Millán Soto <fid@gpul.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "accessiblemailwebview.h"
+#include <mailwebview.h>
+
+#include <QWebFrame>
+
+#if QT_VERSION >= 0x40800
+QString Q_GUI_EXPORT qTextAfterOffsetFromString( int offset, QAccessible2::BoundaryType boundaryType,
+                                         int *startOffset, int *endOffset, const QString& text );
+QString Q_GUI_EXPORT qTextBeforeOffsetFromString( int offset, QAccessible2::BoundaryType boundaryType,
+                                         int *startOffset, int *endOffset, const QString& text );
+QString Q_GUI_EXPORT qTextAtOffsetFromString( int offset, QAccessible2::BoundaryType boundaryType,
+                                         int *startOffset, int *endOffset, const QString& text );
+#endif
+
+AccessibleMailWebView::AccessibleMailWebView( MessageViewer::MailWebView* widget ):
+  QAccessibleWidgetEx( widget, QAccessible::Document )
+{
+  m_widget = widget;
+}
+
+
+int AccessibleMailWebView::characterCount()
+{
+  return m_widget->page()->mainFrame()->toPlainText().size();
+}
+
+int AccessibleMailWebView::selectionCount()
+{
+  return m_widget->hasSelection() ? 1 : 0;
+}
+
+void AccessibleMailWebView::addSelection( int startOffset, int endOffset )
+{
+}
+
+void AccessibleMailWebView::removeSelection( int selectionIndex )
+{
+  if ( selectionIndex == 0 )
+    m_widget->clearSelection();
+}
+
+void AccessibleMailWebView::setSelection( int selectionIndex, int startOffset, int endOffset )
+{
+}
+
+void AccessibleMailWebView::setCursorPosition( int position )
+{
+}
+
+QString AccessibleMailWebView::text( int startOffset, int endOffset )
+{
+  QString text = m_widget->page()->mainFrame()->toPlainText();
+  text.truncate( endOffset );
+  text.remove( 0, startOffset );
+  return text;
+}
+
+QString AccessibleMailWebView::attributes( int offset, int* startOffset, int* endOffset )
+{
+  return QString();
+}
+
+void AccessibleMailWebView::selection( int selectionIndex, int* startOffset, int* endOffset )
+{
+  *startOffset = -1;
+  *endOffset = -1;
+}
+
+QRect AccessibleMailWebView::characterRect( int offset, QAccessible2::CoordinateType coordType )
+{
+  return QRect();
+}
+
+int AccessibleMailWebView::offsetAtPoint( const QPoint& point, QAccessible2::CoordinateType coordType )
+{
+  return 0;
+}
+
+int AccessibleMailWebView::cursorPosition()
+{
+  return 0;
+}
+
+void AccessibleMailWebView::scrollToSubstring( int startIndex, int endIndex )
+{
+}
+
+QString AccessibleMailWebView::textAfterOffset( int offset, QAccessible2::BoundaryType boundaryType, int* startOffset, int* endOffset )
+{
+#if QT_VERSION >= 0x40800
+  return qTextAfterOffsetFromString( offset, boundaryType, startOffset, endOffset, m_widget->page()->mainFrame()->toPlainText() );
+#else
+  return QString();
+#endif
+}
+
+QString AccessibleMailWebView::textBeforeOffset( int offset, QAccessible2::BoundaryType boundaryType, int* startOffset, int* endOffset )
+{
+#if QT_VERSION >= 0x40800
+  return qTextBeforeOffsetFromString( offset, boundaryType, startOffset, endOffset, m_widget->page()->mainFrame()->toPlainText() );
+#else
+  return QString();
+#endif
+}
+
+QString AccessibleMailWebView::textAtOffset( int offset, QAccessible2::BoundaryType boundaryType, int* startOffset, int* endOffset )
+{
+#if QT_VERSION >= 0x40800
+  return qTextAtOffsetFromString( offset, boundaryType, startOffset, endOffset, m_widget->page()->mainFrame()->toPlainText() );
+#else
+  return QString();
+#endif
+}
---
+++ messageviewer/a11y/accessiblemailwebview.h
@@ -0,0 +1,59 @@
+/*
+    Copyright 2011  José Millán Soto <fid@gpul.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either 
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ACCESSIBLEMAILWEBVIEW_H
+#define ACCESSIBLEMAILWEBVIEW_H
+
+#include <QtGui/QAccessibleTextInterface>
+#include <QtGui/QAccessibleWidget>
+
+namespace MessageViewer {
+  class MailWebView;
+}
+
+class AccessibleMailWebView: public QAccessibleWidgetEx,
+                              public QAccessibleTextInterface
+{
+  Q_ACCESSIBLE_OBJECT
+public:
+  AccessibleMailWebView( MessageViewer::MailWebView *widget );
+
+  virtual int characterCount();
+
+  virtual int selectionCount();
+  virtual void addSelection( int startOffset, int endOffset );
+  virtual void removeSelection( int selectionIndex );
+  virtual void setSelection( int selectionIndex, int startOffset, int endOffset );
+  virtual void setCursorPosition( int );
+
+  virtual QString text( int startOffset, int endOffset );
+
+  virtual QString attributes( int offset, int* startOffset, int* endOffset );
+
+  virtual void selection( int selectionIndex, int* startOffset, int* endOffset );
+  virtual QRect characterRect( int offset, QAccessible2::CoordinateType coordType );
+  virtual int offsetAtPoint( const QPoint& point, QAccessible2::CoordinateType coordType );
+  virtual int cursorPosition();
+  virtual void scrollToSubstring( int startIndex, int endIndex );
+  virtual QString textAfterOffset( int offset, QAccessible2::BoundaryType boundaryType, int* startOffset, int* endOffset );
+  virtual QString textBeforeOffset( int offset, QAccessible2::BoundaryType boundaryType, int* startOffset, int* endOffset );
+  virtual QString textAtOffset( int offset, QAccessible2::BoundaryType boundaryType, int* startOffset, int* endOffset );
+private:
+  MessageViewer::MailWebView *m_widget;
+};
+
+#endif
