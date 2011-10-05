7dddf90e60409214ac8cc372fbe4a62459a085f9
Allen Winter <allen.winter@kdab.com>
2011-10-05 12:14:20 -0400

    GIT_SILENT: Update version numbers for today's release.

--- kmail/kmversion.h
+++ kmail/kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20110909.dc3ba70)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20111005.bf85936)"
 
 #endif /*kmversion_h*/
--- kontact/src/main.cpp
+++ kontact/src/main.cpp
@@ -47,7 +47,7 @@ using namespace std;
 static const char description[] =
     I18N_NOOP( "KDE personal information manager" );
 
-static const char version[] = "1.2.9 (enterprise35 0.20110909.dc3ba70)";
+static const char version[] = "1.2.9 (enterprise35 0.20111005.bf85936)";
 
 class KontactApp : public KUniqueApplication {
   public:
