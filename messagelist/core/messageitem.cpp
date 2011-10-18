6288e405d893dcaf88777fb40ec01fcd25820354
Montel Laurent <montel@kde.org>
2011-10-18 17:36:34 +0200

    Remove new message status which is not make sense in kmail2 as discussed with Volker

--- messagelist/core/messageitem.cpp
+++ messagelist/core/messageitem.cpp
@@ -127,17 +127,14 @@ void MessageItem::Tag::setPriority( int priority )
 }
 
 
-QColor MessageItemPrivate::mColorNewMessage;
 QColor MessageItemPrivate::mColorUnreadMessage;
 QColor MessageItemPrivate::mColorImportantMessage;
 QColor MessageItemPrivate::mColorToDoMessage;
 QFont MessageItemPrivate::mFont;
-QFont MessageItemPrivate::mFontNewMessage;
 QFont MessageItemPrivate::mFontUnreadMessage;
 QFont MessageItemPrivate::mFontImportantMessage;
 QFont MessageItemPrivate::mFontToDoMessage;
 QString MessageItemPrivate::mFontKey;
-QString MessageItemPrivate::mFontNewMessageKey;
 QString MessageItemPrivate::mFontUnreadMessageKey;
 QString MessageItemPrivate::mFontImportantMessageKey;
 QString MessageItemPrivate::mFontToDoMessageKey;
@@ -658,12 +655,6 @@ void MessageItem::subTreeToList( QList< MessageItem * > &list )
   }
 }
 
-void MessageItem::setNewMessageColor( const QColor &color )
-{
-  MessageItemPrivate::mColorNewMessage = color;
-}
-
-
 void MessageItem::setUnreadMessageColor( const QColor &color )
 {
   MessageItemPrivate::mColorUnreadMessage = color;
@@ -686,12 +677,6 @@ void MessageItem::setGeneralFont( const QFont &font )
 {
   MessageItemPrivate::mFont = font;
   MessageItemPrivate::mFontKey = font.key();
-}
-
-void MessageItem::setNewMessageFont( const QFont &font )
-{
-  MessageItemPrivate::mFontNewMessage = font;
-  MessageItemPrivate::mFontNewMessageKey = font.key();
 }
 
 void MessageItem::setUnreadMessageFont( const QFont &font )
