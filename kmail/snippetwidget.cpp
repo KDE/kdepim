c9103cda4cc5139571074ab03386c1ad09c2902a
Montel Laurent <montel@kde.org>
2011-10-18 13:52:02 +0200

    Fix 284302 - Snippet view context menu bugs
    Now we can create group all the time.
    
    FIXED-IN: 4.7.3
    BUG: 284302
    (cherry picked from commit b98e51394a3b58df018b360fe883ceed48a59e82)
    
    Conflicts:
    
    	kmail/snippetwidget.cpp

--- kmail/snippetwidget.cpp
+++ kmail/snippetwidget.cpp
@@ -37,13 +37,13 @@ SnippetWidget::SnippetWidget( KMComposerEditor *editor, KActionCollection *actio
 
   setModel( mSnippetsManager->model() );
   setSelectionModel( mSnippetsManager->selectionModel() );
-
-  connect( this, SIGNAL( activated( const QModelIndex& ) ),
-           mSnippetsManager->editSnippetAction(), SLOT( trigger() ) );
-  connect( mSnippetsManager->model(), SIGNAL( rowsInserted( const QModelIndex&, int, int ) ),
-           this, SLOT( expandAll() ) );
-  connect( mSnippetsManager->model(), SIGNAL( rowsRemoved( const QModelIndex&, int, int ) ),
-           this, SLOT( expandAll() ) );
+  
+  connect( this, SIGNAL(activated(QModelIndex)),
+           mSnippetsManager->editSnippetAction(), SLOT(trigger()) );
+  connect( mSnippetsManager->model(), SIGNAL(rowsInserted(QModelIndex,int,int)),
+           this, SLOT(expandAll()) );
+  connect( mSnippetsManager->model(), SIGNAL(rowsRemoved(QModelIndex,int,int)),
+           this, SLOT(expandAll()) );
 
   expandAll();
 }
@@ -58,12 +58,10 @@ void SnippetWidget::contextMenuEvent( QContextMenuEvent *event )
 
   const bool itemSelected = mSnippetsManager->selectionModel()->hasSelection();
 
-  bool canAddGroup = true;
   bool canAddSnippet = true;
   if ( itemSelected ) {
     popup.addTitle( mSnippetsManager->selectedName() );
     if ( mSnippetsManager->snippetGroupSelected() ) {
-      canAddGroup = false;  // subgroups are not permitted
       popup.addAction( mSnippetsManager->editSnippetGroupAction() );
       popup.addAction( mSnippetsManager->deleteSnippetGroupAction() );
     } else {
@@ -79,9 +77,7 @@ void SnippetWidget::contextMenuEvent( QContextMenuEvent *event )
   if ( canAddSnippet ) {
     popup.addAction( mSnippetsManager->addSnippetAction() );
   }
-  if ( canAddGroup ) {
-    popup.addAction( mSnippetsManager->addSnippetGroupAction() );
-  }
+  popup.addAction( mSnippetsManager->addSnippetGroupAction() );
 
   popup.exec( event->globalPos() );
 }
