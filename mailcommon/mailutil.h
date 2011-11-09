2b7ac741300e821fabd994a1700cc6999ef58040
Montel Laurent <montel@kde.org>
2011-11-09 14:49:59 +0100

    Make offline maildispacher too when we are offline

--- mailcommon/mailutil.h
+++ mailcommon/mailutil.h
@@ -77,7 +77,7 @@ namespace Util {
 
     MAILCOMMON_EXPORT void showJobErrorMessage( KJob *job );
 
-    MAILCOMMON_EXPORT Akonadi::AgentInstance::List agentInstances();
+    MAILCOMMON_EXPORT Akonadi::AgentInstance::List agentInstances( bool excludeMailTransport = true);
     MAILCOMMON_EXPORT void ensureKorganizerRunning( bool switchTo );
     MAILCOMMON_EXPORT bool createTodoFromMail( const Akonadi::Item &mailItem );
     MAILCOMMON_EXPORT bool createEventFromMail( const Akonadi::Item &mailItem );
