b974a5aee2c134c841d0ed47a1603e8fde37c543
Volker Krause <vkrause@kde.org>
2011-11-20 13:37:32 +0100

    Refactor the emulation modes, and add one for Maemo 6 as well.

--- mobile/lib/kdeclarativeapplication.h
+++ mobile/lib/kdeclarativeapplication.h
@@ -54,6 +54,10 @@ class MOBILEUI_EXPORT KDeclarativeApplicationBase : public KUniqueApplication
         KApplication constructor) if you don't use
         KDeclarativeApplication as your KApplication */
     static void postApplicationSetup();
+
+  private:
+    static void emulateMaemo5();
+    static void emulateMaemo6();
 };
 
 template <typename T>
