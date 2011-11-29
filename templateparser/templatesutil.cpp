97f33d656a373b34bc08a842be805bd0f7b9403b
Montel Laurent <montel@kde.org>
2011-11-29 12:14:46 +0100

    Remove entry in templateconfigurationrc when we remove folder

---
+++ templateparser/templatesutil.cpp
@@ -0,0 +1,32 @@
+/* -*- mode: C++; c-file-style: "gnu" -*-
+  This file is part of KMail, the KDE mail client.
+  Copyright (c) 2011 Montel Laurent <montel@kde.org>
+
+  KMail is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  KMail is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "templatesutil.h"
+#include <KSharedConfig>
+#include <KConfigGroup>
+
+void TemplateParser::Util::deleteTemplateFolder( const QString &id )
+{
+  KSharedConfig::Ptr config = KSharedConfig::openConfig( "templatesconfigurationrc", KConfig::NoGlobals );
+  const QString key = QString::fromLatin1( "Templates #%1" ).arg( id );
+  if ( config->hasGroup( key ) ) {
+    KConfigGroup group = config->group( key );
+    group.deleteGroup();
+    group.sync();
+  }
+}
---
+++ templateparser/templatesutil.h
@@ -0,0 +1,36 @@
+/* -*- mode: C++; c-file-style: "gnu" -*-
+  This file is part of KMail, the KDE mail client.
+  Copyright (c) 2011 Montel Laurent <montel@kde.org>
+
+  KMail is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  KMail is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef TEMPLATESUTIL_H
+#define TEMPLATESUTIL_H
+
+#include "templateparser_export.h"
+#include <QString>
+
+namespace TemplateParser
+{
+namespace Util {
+   TEMPLATEPARSER_EXPORT void deleteTemplateFolder( const QString &id );
+}
+
+}
+
+  
+
+#endif /* TEMPLATESUTIL_H */
+
