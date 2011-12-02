0e1a23301e4b31cf0b3e243c873acd2c3b300342
David Faure <faure@kde.org>
2011-12-02 19:31:24 +0100

    Update test, there's a vtable in these classes now

--- messagelist/tests/itemsizetest.cpp
+++ messagelist/tests/itemsizetest.cpp
@@ -34,11 +34,11 @@ class ItemSizeTest : public QObject
       qDebug() << sizeof(Item);
       QVERIFY( sizeof(Item) <= 16 );
       qDebug() << sizeof(ItemPrivate);
-      QVERIFY( sizeof(ItemPrivate) <= 88 );
+      QVERIFY( sizeof(ItemPrivate) <= 96 );
       qDebug() << sizeof(MessageItem);
       QVERIFY( sizeof(MessageItem) <= 32 );
       qDebug() << sizeof(MessageItemPrivate);
-      QVERIFY( sizeof(MessageItemPrivate) <= 144 );
+      QVERIFY( sizeof(MessageItemPrivate) <= 152 );
     }
 };
 
