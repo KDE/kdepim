8a7752b607c6878f63d060b28c9832532a0e38d9
Montel Laurent <montel@kde.org>
2011-12-19 09:41:53 +0100

    Minor optimization

--- mailcommon/foldercollection.cpp
+++ mailcommon/foldercollection.cpp
@@ -293,9 +293,9 @@ uint FolderCollection::identity() const
 QString FolderCollection::mailingListPostAddress() const
 {
   if ( mMailingList.features() & MailingList::Post ) {
-    KUrl::List::const_iterator it;
     KUrl::List post = mMailingList.postUrls();
-    for( it = post.constBegin(); it != post.constEnd(); ++it ) {
+    KUrl::List::const_iterator end( post.constEnd() );
+    for( KUrl::List::const_iterator it = post.constBegin(); it != end; ++it ) {
       // We check for isEmpty because before 3.3 postAddress was just an
       // email@kde.org and that leaves protocol() field in the kurl class
       const QString protocol = (*it).protocol();
