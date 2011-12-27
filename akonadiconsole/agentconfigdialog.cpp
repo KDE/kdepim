acf26fa518a0a9a841d707e1dbe692c869d01bc1
Allen Winter <winter@kde.org>
2011-12-27 08:10:18 -0500

    add Volker's email to his copyright, using the same address as his other files in here.

--- akonadiconsole/agentconfigdialog.cpp
+++ akonadiconsole/agentconfigdialog.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2010 Volker Krause
+    Copyright (c) 2010 Volker Krause <vkrause@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
