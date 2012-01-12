450f90f820cdd456e00bf43fab3e35a5f40e4a92
Montel Laurent <montel@kde.org>
2012-01-12 13:11:09 +0100

    continue to implement import thunderbird filter

--- mailcommon/filterimporter/filterimporterthunderbird.cpp
+++ mailcommon/filterimporter/filterimporterthunderbird.cpp
@@ -88,6 +88,33 @@ void FilterImporterThunderbird::extractConditions(const QString& line, MailCommo
 
 void FilterImporterThunderbird::splitConditions( const QString&cond, MailCommon::MailFilter* filter )
 {
+    /*
+     *    {nsMsgSearchAttrib::Subject,    "subject"},
+    {nsMsgSearchAttrib::Sender,     "from"},
+    {nsMsgSearchAttrib::Body,       "body"},
+    {nsMsgSearchAttrib::Date,       "date"},
+    {nsMsgSearchAttrib::Priority,   "priority"},
+    {nsMsgSearchAttrib::MsgStatus,  "status"},
+    {nsMsgSearchAttrib::To,         "to"},
+    {nsMsgSearchAttrib::CC,         "cc"},
+    {nsMsgSearchAttrib::ToOrCC,     "to or cc"},
+    {nsMsgSearchAttrib::AllAddresses, "all addresses"},
+    {nsMsgSearchAttrib::AgeInDays,  "age in days"},
+    {nsMsgSearchAttrib::Label,      "label"},
+    {nsMsgSearchAttrib::Keywords,   "tag"},
+    {nsMsgSearchAttrib::Size,       "size"},
+    // this used to be nsMsgSearchAttrib::SenderInAddressBook
+    // we used to have two Sender menuitems
+    // for backward compatability, we can still parse
+    // the old style.  see bug #179803
+    {nsMsgSearchAttrib::Sender,     "from in ab"},
+    {nsMsgSearchAttrib::JunkStatus, "junk status"},
+    {nsMsgSearchAttrib::JunkPercent, "junk percent"},
+    {nsMsgSearchAttrib::JunkScoreOrigin, "junk score origin"},
+    {nsMsgSearchAttrib::HasAttachmentStatus, "has attachment status"},
+
+     */
+
   QString str = cond.trimmed();
   str.remove("(");
   str.remove(str.length()-1,1); //remove last )
@@ -100,17 +127,63 @@ void FilterImporterThunderbird::splitConditions( const QString&cond, MailCommon:
   QString function = listOfCond.at( 1 );
   QString contents = listOfCond.at( 2 );
   if ( field == QLatin1String( "subject" ) ) {
-
-  } else if ( field == QLatin1String( "date" ) ) {
-
   } else if ( field == QLatin1String( "from" ) ) {
-
+  } else if ( field == QLatin1String( "body" ) ) {
+  } else if ( field == QLatin1String( "date" ) ) {
+  } else if ( field == QLatin1String( "priority" ) ) {
+  } else if ( field == QLatin1String( "status" ) ) {
+  } else if ( field == QLatin1String( "to" ) ) {
+  } else if ( field == QLatin1String( "cc" ) ) {
+  } else if ( field == QLatin1String( "to or cc" ) ) {
+  } else if ( field == QLatin1String( "all addresses" ) ) {
+  } else if ( field == QLatin1String( "age in days" ) ) {
+  } else if ( field == QLatin1String( "label" ) ) {
+  } else if ( field == QLatin1String( "tag" ) ) {
+  } else if ( field == QLatin1String( "size" ) ) {
+  } else if ( field == QLatin1String( "from in ab" ) ) {
+  } else if ( field == QLatin1String( "junk status" ) ) {
+  } else if ( field == QLatin1String( "junk percent" ) ) {
+  } else if ( field == QLatin1String( "junk score origin" ) ) {
+  } else if ( field == QLatin1String( "has attachment status" ) ) {
   }
-
+/*
+  {nsMsgSearchOp::Contains, "contains"},
+  {nsMsgSearchOp::DoesntContain,"doesn't contain"},
+  {nsMsgSearchOp::Is,"is"},
+  {nsMsgSearchOp::Isnt,  "isn't"},
+  {nsMsgSearchOp::IsEmpty, "is empty"},
+  {nsMsgSearchOp::IsntEmpty, "isn't empty"},
+  {nsMsgSearchOp::IsBefore, "is before"},
+  {nsMsgSearchOp::IsAfter, "is after"},
+  {nsMsgSearchOp::IsHigherThan, "is higher than"},
+  {nsMsgSearchOp::IsLowerThan, "is lower than"},
+  {nsMsgSearchOp::BeginsWith, "begins with"},
+  {nsMsgSearchOp::EndsWith, "ends with"},
+  {nsMsgSearchOp::IsInAB, "is in ab"},
+  {nsMsgSearchOp::IsntInAB, "isn't in ab"},
+  {nsMsgSearchOp::IsGreaterThan, "is greater than"},
+  {nsMsgSearchOp::IsLessThan, "is less than"},
+  {nsMsgSearchOp::Matches, "matches"},
+  {nsMsgSearchOp::DoesntMatch, "doesn't match"}
+*/
   if ( function == QLatin1String( "contains" ) ) {
-
+  } else if ( function == QLatin1String( "doesn't contain" ) ) {
+  } else if ( function == QLatin1String( "is" ) ) {
   } else if ( function == QLatin1String( "isn't" ) ) {
-
+  } else if ( function == QLatin1String( "is empty" ) ) {
+  } else if ( function == QLatin1String( "isn't empty" ) ) {
+  } else if ( function == QLatin1String( "is before" ) ) {
+  } else if ( function == QLatin1String( "is after" ) ) {
+  } else if ( function == QLatin1String( "is higher than" ) ) {
+  } else if ( function == QLatin1String( "is lower than" ) ) {
+  } else if ( function == QLatin1String( "begins with" ) ) {
+  } else if ( function == QLatin1String( "ends with" ) ) {
+  } else if ( function == QLatin1String( "is in ab" ) ) {
+  } else if ( function == QLatin1String( "isn't in ab" ) ) {
+  } else if ( function == QLatin1String( "is greater than" ) ) {
+  } else if ( function == QLatin1String( "is less than" ) ) {
+  } else if ( function == QLatin1String( "matches" ) ) {
+  } else if ( function == QLatin1String( "doesn't match" ) ) {
   }
 
   if ( contents == QLatin1String( "" ) )
@@ -123,6 +196,29 @@ void FilterImporterThunderbird::splitConditions( const QString&cond, MailCommon:
 
 void FilterImporterThunderbird::extractActions(const QString& line, MailCommon::MailFilter* filter)
 {
+    /*
+  { nsMsgFilterAction::MoveToFolder,            "Move to folder"},
+  { nsMsgFilterAction::CopyToFolder,            "Copy to folder"},
+  { nsMsgFilterAction::ChangePriority,          "Change priority"},
+  { nsMsgFilterAction::Delete,                  "Delete"},
+  { nsMsgFilterAction::MarkRead,                "Mark read"},
+  { nsMsgFilterAction::KillThread,              "Ignore thread"},
+  { nsMsgFilterAction::KillSubthread,           "Ignore subthread"},
+  { nsMsgFilterAction::WatchThread,             "Watch thread"},
+  { nsMsgFilterAction::MarkFlagged,             "Mark flagged"},
+  { nsMsgFilterAction::Label,                   "Label"},
+  { nsMsgFilterAction::Reply,                   "Reply"},
+  { nsMsgFilterAction::Forward,                 "Forward"},
+  { nsMsgFilterAction::StopExecution,           "Stop execution"},
+  { nsMsgFilterAction::DeleteFromPop3Server,    "Delete from Pop3 server"},
+  { nsMsgFilterAction::LeaveOnPop3Server,       "Leave on Pop3 server"},
+  { nsMsgFilterAction::JunkScore,               "JunkScore"},
+  { nsMsgFilterAction::FetchBodyFromPop3Server, "Fetch body from Pop3Server"},
+  { nsMsgFilterAction::AddTag,                  "AddTag"},
+  { nsMsgFilterAction::Custom,                  "Custom"},
+     */
+
+
   QString actionName;
   if(line == QLatin1String("Move to folder")) {
     actionName = QLatin1String( "transfer" );
@@ -136,8 +232,20 @@ void FilterImporterThunderbird::extractActions(const QString& line, MailCommon::
     actionName = QLatin1String( "add tag" );
   } else if( line == QLatin1String("Delete")) {
     actionName = QLatin1String( "delete" );
+  } else if( line == QLatin1String("Change priority")) {
+  } else if( line == QLatin1String("Ignore thread")) {
+  } else if( line == QLatin1String("Ignore subthread")) {
+  } else if( line == QLatin1String("Watch thread")) {
+  } else if( line == QLatin1String("Mark flagged")) {
+  } else if( line == QLatin1String("Label")) {
+  } else if( line == QLatin1String("Reply")) {
+  } else if( line == QLatin1String("Delete from Pop3 server")) {
+  } else if( line == QLatin1String("JunkScore")) {
+  } else if( line == QLatin1String("Fetch body from Pop3Server")) {
+  } else if( line == QLatin1String("Custom")) {
+
   } else {
-    qDebug()<<" missing convert method";
+      qDebug()<<QString::fromLatin1(" missing convert method: %1").arg(line);
   }
 }
 
