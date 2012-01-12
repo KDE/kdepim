9977da6977c75146e740e0e63773a7df86a6de9f
Allen Winter <winter@kde.org>
2012-01-12 16:15:09 -0500

    properly pluralize the "Attached business card(s)" string.
    BUG: 137773
    FIXED-IN: 4.9
    MERGE: none (due to new i18n string)

--- plugins/messageviewer/bodypartformatter/text_vcard.cpp
+++ plugins/messageviewer/bodypartformatter/text_vcard.cpp
@@ -73,19 +73,29 @@ class Formatter : public MessageViewer::Interface::BodyPartFormatter
       if ( vCard.isEmpty() ) {
         return AsIcon;
       }
+
       KABC::Addressee::List al = vcc.parseVCards( vCard.toUtf8() );
-      if ( al.empty() ) {
+
+      // Pre-count the number of non-empty addressees
+      int count = 0;
+      foreach ( const KABC::Addressee &a, al ) {
+        if ( a.isEmpty() ) {
+          continue;
+        }
+        count++;
+      }
+      if ( !count ) {
         return AsIcon;
       }
 
       writer->queue( "<div align=\"center\"><h2>" +
-                     i18n( "Attached business cards" ) +
+                     i18np( "Attached business card", "Attached business cards", count ) +
                      "</h2></div>" );
 
-      int count = 0;
+      count = 0;
       foreach ( const KABC::Addressee &a, al ) {
         if ( a.isEmpty() ) {
-          return AsIcon;
+          continue;
         }
 
         Akonadi::StandardContactFormatter formatter;
