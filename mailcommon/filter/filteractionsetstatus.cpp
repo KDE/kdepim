42da185ba919a941a95638dbe9bed6c8a3db624e
Montel Laurent <montel@kde.org>
2012-02-14 08:53:15 +0100

    Optimize++ don't modify flag when not necessary

--- mailcommon/filter/filteractionsetstatus.cpp
+++ mailcommon/filter/filteractionsetstatus.cpp
@@ -50,9 +50,10 @@ FilterAction::ReturnCode FilterActionSetStatus::process( ItemContext &context ) 
     status.setRead( false );
   else
     status.set( newStatus );
-
-  context.item().setFlags( status.statusFlags() );
-  context.setNeedsFlagStore();
+  if( newStatus!= newStatus ) {
+    context.item().setFlags( status.statusFlags() );
+    context.setNeedsFlagStore();
+  }
 
   return GoOn;
 }
