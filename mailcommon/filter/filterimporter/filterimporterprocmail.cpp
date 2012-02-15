2b4c83701528ea0fd56ad33a047d5e344df6d175
Montel Laurent <montel@kde.org>
2012-02-15 20:17:33 +0100

    Minor

--- mailcommon/filter/filterimporter/filterimporterprocmail.cpp
+++ mailcommon/filter/filterimporter/filterimporterprocmail.cpp
@@ -72,15 +72,20 @@ MailCommon::MailFilter *FilterImporterProcmail::parseLine( QTextStream &stream,
 
   } else if ( line.startsWith( QLatin1String( "* " ) ) ) {
     line.remove( 0, 2 );
-    if ( line.startsWith( QLatin1String( "^From" ) ) ) {
+    if ( line.startsWith( QLatin1String( "^From:" ) ) ) {
+      line.remove( QLatin1String( "^From:" ) );
     } else if ( line.startsWith( QLatin1String( "^Subject:" ) ) ) {
+      line.remove( QLatin1String( "^Subject:" ) );
     } else if ( line.startsWith( QLatin1String( "^Sender:" ) ) ) {
+      line.remove( QLatin1String( "^Sender:" ) );
     } else if ( line.startsWith( QLatin1String( "^Subject:" ) ) ) {
+      line.remove( QLatin1String( "^Subject:" ) );
     } else {
       qDebug()<<" line condition not parsed :"<<line;
     }
     //Condition
   } else if ( line.startsWith( QLatin1Char( '!' ) ) ) {
+    line.remove( QLatin1Char( '!' ) );
     //Redirect email
   } else if ( line.startsWith( QLatin1Char( '|' ) ) ) {
     //Shell
