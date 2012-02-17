a97be29650551c178c9569463212d970512e0615
Montel Laurent <montel@kde.org>
2012-02-17 09:04:44 +0100

    Add message when we can't find a file to import

--- kmailcvt/filter_plain.cxx
+++ kmailcvt/filter_plain.cxx
@@ -36,53 +36,54 @@ FilterPlain::~FilterPlain()
 
 void FilterPlain::import(FilterInfo *info)
 {
-    // Select directory containing plain text emails
-    const QString mailDir = KFileDialog::getExistingDirectory(QDir::homePath(),info->parent());
-    if (mailDir.isEmpty()) { // No directory selected
-        info->alert(i18n("No directory selected."));
-        return;
-    }
-    QDir dir (mailDir);
-    const QStringList files = dir.entryList(QStringList("*.[eE][mM][lL]")<<"*.[tT][xX][tT]"<<"*.[mM][sS][gG]", QDir::Files, QDir::Name);
-
-    // Count total number of files to be processed
-    info->addLog(i18n("Counting files..."));
-    int totalFiles = files.count();
-    int currentFile = 0;
-
+  // Select directory containing plain text emails
+  const QString mailDir = KFileDialog::getExistingDirectory(QDir::homePath(),info->parent());
+  if (mailDir.isEmpty()) { // No directory selected
+    info->alert(i18n("No directory selected."));
+    return;
+  }
+  QDir dir (mailDir);
+  const QStringList files = dir.entryList(QStringList("*.[eE][mM][lL]")<<"*.[tT][xX][tT]"<<"*.[mM][sS][gG]", QDir::Files, QDir::Name);
+  // Count total number of files to be processed
+  info->addLog(i18n("Counting files..."));
+  int totalFiles = files.count();
+  int currentFile = 0;
+  if ( files.isEmpty() ) {
+    info->addLog(i18n("No files found for import."));
+  } else {
     info->addLog(i18n("Importing new mail files..."));
     QStringList::ConstIterator end( files.constEnd() );
     for ( QStringList::ConstIterator mailFile = files.constBegin(); mailFile != end; ++mailFile ) {
-        info->setFrom(*mailFile);
-        info->setTo(dir.dirName());
-        info->setCurrent(0);
+      info->setFrom(*mailFile);
+      info->setTo(dir.dirName());
+      info->setCurrent(0);
 
-        /* comment by Danny Kukawka:
-         * addMessage() == old function, need more time and check for duplicates
-         * addMessage_fastImport == new function, faster and no check for duplicates
-         */
-        if(info->removeDupMsg) {
-            if(! addMessage( info, "PLAIN-" + dir.dirName(), dir.filePath(*mailFile) )) {
-                info->addLog( i18n("Could not import %1", *mailFile ) );
-            }
-        } else {
-            if( ! addMessage_fastImport( info, "PLAIN-" + dir.dirName(), dir.filePath(*mailFile) )) {
-                info->addLog( i18n("Could not import %1", *mailFile ) );
-            }
+      /* comment by Danny Kukawka:
+       * addMessage() == old function, need more time and check for duplicates
+       * addMessage_fastImport == new function, faster and no check for duplicates
+       */
+      if(info->removeDupMsg) {
+        if(! addMessage( info, "PLAIN-" + dir.dirName(), dir.filePath(*mailFile) )) {
+          info->addLog( i18n("Could not import %1", *mailFile ) );
         }
+      } else {
+        if( ! addMessage_fastImport( info, "PLAIN-" + dir.dirName(), dir.filePath(*mailFile) )) {
+          info->addLog( i18n("Could not import %1", *mailFile ) );
+        }
+      }
 
-        info->setCurrent(100);
-        info->setOverall(100 * ++currentFile/ totalFiles);
-        if ( info->shouldTerminate() ) break;
+      info->setCurrent(100);
+      info->setOverall(100 * ++currentFile/ totalFiles);
+      if ( info->shouldTerminate() ) break;
     }
 
     info->addLog( i18n("Finished importing emails from %1", mailDir ));
     if (count_duplicates > 0) {
-        info->addLog( i18np("1 duplicate message not imported", "%1 duplicate messages not imported", count_duplicates));
+      info->addLog( i18np("1 duplicate message not imported", "%1 duplicate messages not imported", count_duplicates));
     }
     if (info->shouldTerminate())
       info->addLog( i18n("Finished import, canceled by user."));
-
-    count_duplicates = 0;
+  }
+  count_duplicates = 0;
 }
 
