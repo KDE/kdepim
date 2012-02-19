a87084e49abd77854b9880a1f23ca76d06794087
Montel Laurent <montel@kde.org>
2012-02-19 14:28:38 +0100

    Use specific file for filterinfo class

--- kmailcvt/filters.cpp
+++ kmailcvt/filters.cpp
@@ -19,6 +19,7 @@
 // Local Includes
 #include "filters.h"
 #include "kmailcvt.h"
+#include "filterinfo.h"
 
 // KDEPIM Includes
 #include <KPIMUtils/KFileIO>
@@ -39,112 +40,6 @@
 #include <KDebug>
 #include <KMessageBox>
 
-// Qt Includes
-
-//////////////////////////////////////////////////////////////////////////////////
-//
-// The API to the kmailcvt dialog --> Gives the import filter access to
-// put information on the dialog.
-//
-//////////////////////////////////////////////////////////////////////////////////
-
-bool FilterInfo::s_terminateASAP = false;
-
-FilterInfo::FilterInfo( KImportPageDlg* dlg, QWidget* parent)
-  : m_dlg( dlg ),
-    m_parent( parent ),
-    m_removeDupMsg( false )
-{
-  s_terminateASAP = false;
-}
-
-FilterInfo::~FilterInfo()
-{
-}
-
-void FilterInfo::setStatusMsg( const QString& status )
-{
-  m_dlg->_textStatus->setText( status );
-}
-
-void FilterInfo::setFrom( const QString& from )
-{
-  m_dlg->_from->setText( from );
-}
-
-void FilterInfo::setTo( const QString& to )
-{
-  m_dlg->_to->setText( to );
-}
-
-void FilterInfo::setCurrent( const QString& current )
-{
-  m_dlg->_current->setText( current );
-  kapp->processEvents();
-}
-
-void  FilterInfo::setCurrent( int percent )
-{
-  m_dlg->_done_current->setValue( percent );
-  kapp->processEvents(); // Be careful - back & finish buttons disabled, so only user event that can happen is cancel/close button
-}
-
-void  FilterInfo::setOverall( int percent )
-{
-  m_dlg->_done_overall->setValue( percent );
-}
-
-void FilterInfo::addLog( const QString& log )
-{
-  m_dlg->_log->addItem( log );
-  m_dlg->_log->setCurrentItem( m_dlg->_log->item(m_dlg->_log->count() - 1 ));
-  kapp->processEvents();
-}
-
-void FilterInfo::clear()
-{
-  m_dlg->_log->clear();
-  setCurrent();
-  setOverall();
-  setCurrent( QString() );
-  setFrom( QString() );
-  setTo( QString() );
-}
-
-void FilterInfo::alert( const QString& message )
-{
-  KMessageBox::information( m_parent, message );
-}
-
-void FilterInfo::terminateASAP()
-{
-  s_terminateASAP = true;
-}
-
-bool FilterInfo::shouldTerminate() const
-{
-  return s_terminateASAP;
-}
-
-Akonadi::Collection FilterInfo::rootCollection() const
-{
-  return m_rootCollection;
-}
-
-void FilterInfo::setRootCollection( const Akonadi::Collection &collection )
-{
-  m_rootCollection = collection;
-}
-
-void FilterInfo::setRemoveDupMessage( bool removeDupMessage )
-{
-  m_removeDupMsg = removeDupMessage;
-}
-
-bool FilterInfo::removeDupMessage() const
-{
-  return m_removeDupMsg;
-}
 
 
 
