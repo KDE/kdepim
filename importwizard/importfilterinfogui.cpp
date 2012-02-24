6cd2ab3591c25b9d05a40fb29e7c683a1a8d5ab8
Montel Laurent <montel@kde.org>
2012-02-24 13:53:56 +0100

    Add class for the future when we will implement import filter

---
+++ importwizard/importfilterinfogui.cpp
@@ -0,0 +1,86 @@
+/*
+  Copyright (c) 2012 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "importfilterinfogui.h"
+#include <KMessageBox>
+#include <KApplication>
+
+ImportFilterInfoGui::ImportFilterInfoGui(QWidget* parent)
+  : MailImporter::FilterInfoGui(),
+    m_parent( parent )
+{
+}
+
+ImportFilterInfoGui::~ImportFilterInfoGui()
+{
+}
+
+void ImportFilterInfoGui::setStatusMessage( const QString& status )
+{
+}
+
+void ImportFilterInfoGui::setFrom( const QString& from )
+{
+}
+
+void ImportFilterInfoGui::setTo( const QString& to )
+{
+}
+
+void ImportFilterInfoGui::setCurrent( const QString& current )
+{
+  kapp->processEvents();
+}
+
+void  ImportFilterInfoGui::setCurrent( int percent )
+{
+  kapp->processEvents(); // Be careful - back & finish buttons disabled, so only user event that can happen is cancel/close button
+}
+
+void  ImportFilterInfoGui::setOverall( int percent )
+{
+}
+
+void ImportFilterInfoGui::addInfoLogEntry( const QString& log )
+{
+  kapp->processEvents();
+}
+
+void ImportFilterInfoGui::addErrorLogEntry( const QString& log )
+{
+  kapp->processEvents();
+}
+
+
+void ImportFilterInfoGui::clear()
+{
+  setCurrent();
+  setOverall();
+  setCurrent( QString() );
+  setFrom( QString() );
+  setTo( QString() );
+}
+
+void ImportFilterInfoGui::alert( const QString& message )
+{
+}
+
+QWidget *ImportFilterInfoGui::parent()
+{
+  return m_parent;
+}
+  
---
+++ importwizard/importfilterinfogui.h
@@ -0,0 +1,45 @@
+/*
+  Copyright (c) 2012 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef IMPORTFILTERINFOGUI_H
+#define IMPORTFILTERINFOGUI_H
+
+#include "filterinfogui.h"
+
+class ImportFilterInfoGui : public MailImporter::FilterInfoGui
+{
+public:
+  explicit ImportFilterInfoGui( QWidget* parent);
+  ~ImportFilterInfoGui();
+  void setStatusMessage( const QString& status );
+  void setFrom( const QString& from );
+  void setTo( const QString& to );
+  void setCurrent( const QString& current );
+  void setCurrent( int percent = 0 );
+  void setOverall( int percent = 0 );
+  void addErrorLogEntry( const QString& log );
+  void addInfoLogEntry( const QString& log );
+  void clear();
+  void alert( const QString& message );
+  QWidget *parent();
+private:
+  QWidget *m_parent;
+};
+
+
+#endif /* IMPORTFILTERINFOGUI_H */
+
