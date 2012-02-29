67443bedee7cd584bacc01df1fb7f28356c91f89
Allen Winter <winter@kde.org>
2012-02-29 10:37:29 -0500

    GIT_SILENT: Update version numbers for today's release.

--- kmail/kmversion.h
+++ kmail/kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20120224.7746379)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20120229.d68f619)"
 
 #endif /*kmversion_h*/
--- korganizer/version.h
+++ korganizer/version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise35 0.20120224.7746379)";
+static const char korgVersion[] = "3.5.9 (enterprise35 0.20120229.d68f619)";
 
 #endif
