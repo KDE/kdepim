765f1498ec23926926b4fd4f3fc0ac681d6ba9d6
Montel Laurent <montel@kde.org>
2012-03-06 10:03:40 +0100

    Fix mem leak Akonadi::CollectionCreateJob was not deleted during import (cherry picked from commit 20091f41683f291e9235539cc7c8a6670b10d3a7)

--- kmailcvt/filters.cxx
+++ kmailcvt/filters.cxx
@@ -237,13 +237,17 @@ Akonadi::Collection Filter::addSubCollection( FilterInfo* info,
   newSubCollection.setName( newCollectionPathName );
 
   Akonadi::CollectionCreateJob * job = new Akonadi::CollectionCreateJob( newSubCollection );
+  job->setAutoDelete( false );
   if( !job->exec() ) {
     info->alert( i18n("<b>Error:</b> Could not create folder. Reason: %1",
 		 job->errorString() ) );
+    delete job;
     return Akonadi::Collection();
   }
   // Return the newly created collection
-  return job->collection();
+  Akonadi::Collection collection = job->collection();
+  delete job;
+  return collection;
 }
 
 bool Filter::checkForDuplicates ( FilterInfo* info, const QString& msgID,
