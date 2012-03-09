722395bb9a3799f88a5cb65e0f11ed70e247679e
Montel Laurent <montel@kde.org>
2012-03-09 12:25:28 +0100

    Fix Bug 295558 - cursor is misplaced when replying
    FIXED-IN: 4.8.2
    BUG: 295558
    (cherry picked from commit 73fc2af37cac079b41c173797687103df499d73e)
    
    Conflicts:
    
    	templateparser/templateparser.cpp

--- templateparser/templateparser.cpp
+++ templateparser/templateparser.cpp
@@ -1094,7 +1094,9 @@ void TemplateParser::processWithTemplate( const QString &tmpl )
         // turn on debug
         kDebug() << "Command: CURSOR";
         i += strlen( "CURSOR" );
-        KMime::Headers::Generic *header = new KMime::Headers::Generic( "X-KMail-CursorPos", mMsg.get(), QString::number( plainBody.length()-1 ), "utf-8" );
+        KMime::Headers::Generic *header =
+          new KMime::Headers::Generic( "X-KMail-CursorPos", mMsg.get(),
+                                       QString::number( plainBody.length() ), "utf-8" );
         mMsg->setHeader( header );
         //FIXME HTML part for header remaining
       } else if ( cmd.startsWith( QLatin1String( "SIGNATURE" ) ) ) {
