e5a1c35f55c467ba75aa0b281b3224a12698f9ef
Montel Laurent <montel@kde.org>
2012-07-06 18:22:59 +0200

    Fix Bug 303086 - UI formatting problems in KMail Quick Search Bar
    Limit maximize size of combobox
    
    BUG: 303086
    FIXED-IN: 4.8.5

--- messagelist/core/widgetbase.cpp
+++ messagelist/core/widgetbase.cpp
@@ -169,6 +169,7 @@ Widget::Widget( QWidget *pParent )
   // The status filter button. Will be populated later, as populateStatusFilterCombo() is virtual
   d->mStatusFilterCombo = new KComboBox( this ) ;
   d->mStatusFilterCombo->setVisible( Settings::self()->showQuickSearch() );
+  d->mStatusFilterCombo->setMaximumWidth(300);
   g->addWidget( d->mStatusFilterCombo, 0, 2 );
 
   // The "Open Full Search" button
