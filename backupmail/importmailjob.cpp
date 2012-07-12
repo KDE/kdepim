cac4c6da0c46c9ca49198b0716d5ac592bdaf063
Montel Laurent <montel@kde.org>
2012-07-12 21:02:50 +0200

    Remove qDebug

--- backupmail/importmailjob.cpp
+++ backupmail/importmailjob.cpp
@@ -679,7 +679,6 @@ void ImportMailJob::restoreIdentity()
   MessageViewer::KCursorSaver busy( MessageViewer::KBusyPtr::busy() );
   const KArchiveEntry* identity = mArchiveDirectory->entry(path);
   if(identity && identity->isFile()) {
-      qDebug()<<" SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS";
     const KArchiveFile* fileIdentity = static_cast<const KArchiveFile*>(identity);
     fileIdentity->copyTo(mTempDirName);
 
@@ -689,7 +688,6 @@ void ImportMailJob::restoreIdentity()
 
     const QStringList identityList = identityConfig->groupList().filter( QRegExp( "Identity #\\d+" ) );
     Q_FOREACH(const QString&identityStr, identityList) {
-        qDebug()<<" 55555555555555555555555555555555";
       KConfigGroup group = identityConfig->group(identityStr);
       int oldUid = -1;
       const QString uidStr("uoid");
