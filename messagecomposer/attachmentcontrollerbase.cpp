69baff2918fdd3a450b39a61877b06a63133cf94
Montel Laurent <montel@kde.org>
2012-07-16 13:35:55 +0200

    Add icon

--- messagecomposer/attachmentcontrollerbase.cpp
+++ messagecomposer/attachmentcontrollerbase.cpp
@@ -376,8 +376,8 @@ void AttachmentControllerBase::createActions()
   connect( d->addAction, SIGNAL(triggered(bool)), this, SLOT(showAddAttachmentDialog()) );
   connect( d->addContextAction, SIGNAL(triggered(bool)), this, SLOT(showAddAttachmentDialog()) );
 
-  d->removeAction = new KAction( i18n( "&Remove Attachment" ), this );
-  d->removeContextAction = new KAction( i18n( "Remove" ), this ); // FIXME need two texts. is there a better way?
+  d->removeAction = new KAction( KIcon(QLatin1String("edit-delete")), i18n( "&Remove Attachment" ), this );
+  d->removeContextAction = new KAction( KIcon(QLatin1String("edit-delete")), i18n( "Remove" ), this ); // FIXME need two texts. is there a better way?
   connect( d->removeAction, SIGNAL(triggered(bool)), this, SLOT(removeSelectedAttachments()) );
   connect( d->removeContextAction, SIGNAL(triggered(bool)), this, SLOT(removeSelectedAttachments()) );
 
