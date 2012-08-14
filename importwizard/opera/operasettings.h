98ac50484978e183ffccd6996efbf4c235f7ef46
Montel Laurent <montel@kde.org>
2012-08-14 10:02:29 +0200

    Add settings too

---
+++ importwizard/opera/operasettings.h
@@ -0,0 +1,32 @@
+/*
+  Copyright (c) 2012 Montel Laurent <montel@kde.org>
+  
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+  
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef OPERASETTINGS_H
+#define OPERASETTINGS_H
+
+#include "abstractsettings.h"
+
+class ImportWizard;
+
+class OperaSettings : public AbstractSettings
+{
+public:
+  explicit OperaSettings(const QString& filename, ImportWizard *parent);
+  ~OperaSettings();
+};
+
+#endif // OPERASETTINGS_H
