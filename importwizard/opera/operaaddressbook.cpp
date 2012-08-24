3180874879dd6e769ff760bc9a9bd99c7fc52b4b
Montel Laurent <montel@kde.org>
2012-08-24 19:43:14 +0200

    Continue to import addressbook

--- importwizard/opera/operaaddressbook.cpp
+++ importwizard/opera/operaaddressbook.cpp
@@ -53,21 +53,23 @@ OperaAddressBook::OperaAddressBook(const QString &filename, ImportWizard *parent
         if(line.startsWith(QLatin1String("ID"))) {
             //Nothing
         } else if(line.startsWith(QLatin1String("NAME"))) {
-
+            contact->setName(line.remove(QLatin1String("NAME=")));
         } else if(line.startsWith(QLatin1String("URL"))) {
-
+            contact->setUrl(KUrl(line.remove(QLatin1String("URL="))));
         } else if(line.startsWith(QLatin1String("DESCRIPTION"))) {
             contact->setNote(line.remove(QLatin1String("DESCRIPTION=")));
         } else if(line.startsWith(QLatin1String("PHONE"))) {
-
+            contact->insertPhoneNumber( KABC::PhoneNumber( line.remove(QLatin1String("PHONE=")), KABC::PhoneNumber::Home ) );
         } else if(line.startsWith(QLatin1String("FAX"))) {
-
+            contact->insertPhoneNumber( KABC::PhoneNumber( line.remove(QLatin1String("FAX=")), KABC::PhoneNumber::Fax ) );
         } else if(line.startsWith(QLatin1String("POSTALADDRESS"))) {
-
+            //TODO
         } else if(line.startsWith(QLatin1String("PICTUREURL"))) {
-
+            //TODO
         } else if(line.startsWith(QLatin1String("ICON"))) {
-
+            //TODO
+        } else if(line.startsWith(QLatin1String("SHORT NAME"))) {
+            contact->setNickName(line.remove(QLatin1String("SHORT NAME=")));
         }
     }
   }
