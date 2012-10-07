22b4d21e9252aba5332309e994c74f940bfefb22
Montel Laurent <montel@kde.org>
2012-10-07 18:26:00 +0200

    add akregator2 plugins

---
+++ kontact/plugins/akregator2/akregator_plugin.cpp
@@ -0,0 +1,160 @@
+/*
+  This file is part of Akregator.
+
+  Copyright (C) 2004 Sashmit Bhaduri <smt@vfemail.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
+*/
+
+#include "akregator_plugin.h"
+#include "akregator2_part.h"
+#include "akregator2_options.h"
+#include "partinterface.h"
+
+#include <KontactInterface/Core>
+
+#include <KAction>
+#include <KActionCollection>
+#include <KLocale>
+
+EXPORT_KONTACT_PLUGIN( AkregatorPlugin, akregator )
+
+AkregatorPlugin::AkregatorPlugin( KontactInterface::Core *core, const QVariantList & )
+  : KontactInterface::Plugin( core, core, "akregator2" ), m_interface( 0 )
+{
+
+  setComponentData( KontactPluginFactory::componentData() );
+
+  KAction *action =
+    new KAction( KIcon( "bookmark-new" ),
+                 i18nc( "@action:inmenu", "New Feed..." ), this );
+  actionCollection()->addAction( "feed_new", action );
+  action->setShortcut( QKeySequence( Qt::CTRL + Qt::SHIFT + Qt::Key_F ) );
+  action->setHelpText(
+    i18nc( "@info:status", "Create a new feed" ) );
+  action->setWhatsThis(
+    i18nc( "@info:whatsthis",
+           "You will be presented with a dialog where you can add a new feed." ) );
+  connect( action, SIGNAL(triggered(bool)), SLOT(addFeed()) );
+  insertNewAction( action );
+
+  mUniqueAppWatcher = new KontactInterface::UniqueAppWatcher(
+    new KontactInterface::UniqueAppHandlerFactory<AkregatorUniqueAppHandler>(), this );
+}
+
+AkregatorPlugin::~AkregatorPlugin()
+{
+  delete m_interface;
+  m_interface = 0;
+}
+
+bool AkregatorPlugin::isRunningStandalone() const
+{
+  return mUniqueAppWatcher->isRunningStandalone();
+}
+
+QStringList AkregatorPlugin::invisibleToolbarActions() const
+{
+  return QStringList( "file_new_contact" );
+}
+
+OrgKdeAkregator2PartInterface *AkregatorPlugin::interface()
+{
+  if ( !m_interface ) {
+    part();
+  }
+  Q_ASSERT( m_interface );
+  return m_interface;
+
+}
+
+QString AkregatorPlugin::tipFile() const
+{
+  // TODO: tips file
+  //QString file = KStandardDirs::locate("data", "akregator/tips");
+  QString file;
+  return file;
+}
+
+KParts::ReadOnlyPart *AkregatorPlugin::createPart()
+{
+  KParts::ReadOnlyPart *part = loadPart();
+  if ( !part ) {
+    return 0;
+  }
+
+  m_interface = new OrgKdeAkregator2PartInterface(
+    "org.kde.akregator2", "/Akregator2", QDBusConnection::sessionBus() );
+  m_interface->openStandardFeedList();
+
+  return part;
+}
+
+void AkregatorPlugin::addFeed()
+{
+  // Ensure part is loaded
+  (void)part();
+
+  org::kde::akregator2::part akregator(
+    "org.kde.akregator2", "/Akregator2", QDBusConnection::sessionBus() );
+  akregator.addFeed();
+}
+
+QStringList AkregatorPlugin::configModules() const
+{
+  QStringList modules;
+  modules << "PIM/akregator2.desktop";
+  return modules;
+}
+
+void AkregatorPlugin::readProperties( const KConfigGroup &config )
+{
+  if ( part() ) {
+    Akregator2::Part *myPart = static_cast<Akregator2::Part*>( part() );
+    myPart->readProperties( config );
+  }
+}
+
+void AkregatorPlugin::saveProperties( KConfigGroup &config )
+{
+  if ( part() ) {
+    Akregator2::Part *myPart = static_cast<Akregator2::Part*>( part() );
+    myPart->saveProperties( config );
+  }
+}
+
+void AkregatorUniqueAppHandler::loadCommandLineOptions()
+{
+  KCmdLineArgs::addCmdLineOptions( Akregator2::akregator2_options() );
+}
+
+int AkregatorUniqueAppHandler::newInstance()
+{
+  // Ensure part is loaded
+  (void)plugin()->part();
+
+  org::kde::akregator2::part akregator(
+    "org.kde.akregator2", "/Akregator2", QDBusConnection::sessionBus() );
+  akregator.openStandardFeedList();
+  //PORTING ! akregator.handleCommandLine();
+
+  return KontactInterface::UniqueAppHandler::newInstance();
+}
+
+#include "akregator_plugin.moc"
---
+++ kontact/plugins/akregator2/akregator_plugin.h
@@ -0,0 +1,73 @@
+/*
+  This file is part of Akregator.
+
+  Copyright (C) 2004 Sashmit Bhaduri <smt@vfemail.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public Licensea along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+  As a special exception, permission is given to link this program
+  with any edition of Qt, and distribute the resulting executable,
+  without including the source code for Qt in the source distribution.
+*/
+
+#ifndef AKREGATOR_PLUGIN_H
+#define AKREGATOR_PLUGIN_H
+
+#include <KontactInterface/UniqueAppHandler>
+
+namespace KontactInterface {
+  class Plugin;
+}
+
+class OrgKdeAkregator2PartInterface;
+
+class AkregatorUniqueAppHandler : public KontactInterface::UniqueAppHandler
+{
+  public:
+    AkregatorUniqueAppHandler( KontactInterface::Plugin *plugin )
+      : KontactInterface::UniqueAppHandler( plugin ) {}
+    virtual void loadCommandLineOptions();
+    virtual int newInstance();
+};
+
+class AkregatorPlugin : public KontactInterface::Plugin
+{
+  Q_OBJECT
+
+  public:
+    AkregatorPlugin( KontactInterface::Core *core, const QVariantList & );
+    ~AkregatorPlugin();
+
+    virtual QString tipFile() const;
+    int weight() const { return 475; }
+
+    OrgKdeAkregator2PartInterface *interface();
+
+    virtual QStringList configModules() const;
+    virtual QStringList invisibleToolbarActions() const;
+    virtual bool isRunningStandalone() const;
+    virtual void readProperties( const KConfigGroup &config );
+    virtual void saveProperties( KConfigGroup &config );
+
+  private slots:
+    void addFeed();
+
+  protected:
+    KParts::ReadOnlyPart *createPart();
+    KontactInterface::UniqueAppWatcher *mUniqueAppWatcher;
+    OrgKdeAkregator2PartInterface *m_interface;
+};
+
+#endif
