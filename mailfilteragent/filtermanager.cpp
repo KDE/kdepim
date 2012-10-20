4262753be3ffe58fad0799dbf479139329bc7c5b
Montel Laurent <montel@kde.org>
2012-10-20 14:22:23 +0200

    Fix compile

--- mailfilteragent/filtermanager.cpp
+++ mailfilteragent/filtermanager.cpp
@@ -455,8 +455,6 @@ bool FilterManager::processContextItem( ItemContext context )
             Akonadi::ItemMoveJob *moveJob = new Akonadi::ItemMoveJob( context.item(), context.moveTargetCollection(), this );
             connect( moveJob, SIGNAL(result(KJob*)), SLOT(moveJobResult(KJob*)) );
           } else {
-            if ( emitSignal )
-              emit itemNotMoved( context.item() );
             return false;
           }
       }
