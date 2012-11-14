b8a156ae07054cc01c2f0c1d86c48c7aed611118
Montel Laurent <montel@kde.org>
2012-11-14 09:12:50 +0100

    Remove duplicate code

---
+++ kaddressbook/startup.cpp
@@ -0,0 +1,25 @@
+#include "startup.h"
+#include <KLocale>
+#include <KGlobal>
+
+namespace KAddressBook {
+void insertLibraryCatalogues()
+{
+    static const char * const catalogs[] = {
+        "libkdepim",
+        "kabc",
+        "libakonadi",
+        "kabcakonadi",
+        "akonadicontact",
+        "libpimcommon"
+    };
+
+    KLocale * l = KGlobal::locale();
+    for ( unsigned int i = 0 ; i < sizeof catalogs / sizeof *catalogs ; ++i ) {
+      l->insertCatalog( catalogs[i] );
+    }
+}
+
+}
+
+
