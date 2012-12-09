2d6a8bec0ad5d5bda5e92eb2ed051f66468efd4e
Montel Laurent <montel@kde.org>
2012-12-09 22:56:03 +0100

    Change cursor

--- composereditor-ng/composerimageresizewidget.cpp
+++ composereditor-ng/composerimageresizewidget.cpp
@@ -21,7 +21,7 @@
 #include "composerimageresizewidget.h"
 
 #include <QMouseEvent>
-#include <QPaintEvent>
+#include <QPainter>
 
 namespace ComposerEditorNG
 {
@@ -29,17 +29,71 @@ namespace ComposerEditorNG
 class ComposerImageResizeWidgetPrivate
 {
 public:
+    enum ResizeDirection {
+        None,
+        Top,
+        Bottom,
+        Left,
+        Right,
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight
+    };
+
     ComposerImageResizeWidgetPrivate(ComposerImageResizeWidget *qq, const QWebElement& element)
-        : q(qq), imageElement(element)
+        : q(qq),
+          imageElement(element),
+          direction(None),
+          mousePressed(false)
     {
     }
+
+    void setResizeDirectionCursor(const QPoint& pos);
+    ResizeDirection resizeDirection(const QPoint& pos);
+
     ComposerImageResizeWidget *q;
     QWebElement imageElement;
+    ResizeDirection direction;
+    bool mousePressed;
 };
+
+void ComposerImageResizeWidgetPrivate::setResizeDirectionCursor(const QPoint& pos)
+{
+    ResizeDirection dir = resizeDirection(pos);
+    switch(dir) {
+    case None:
+        q->setCursor(Qt::ArrowCursor);
+        break;
+    case Top:
+    case Bottom:
+        q->setCursor(Qt::SizeVerCursor);
+        break;
+    case Left:
+    case Right:
+        q->setCursor(Qt::SizeHorCursor);
+        break;
+    case TopLeft:
+    case BottomRight:
+        q->setCursor(Qt::SizeFDiagCursor);
+        break;
+    case TopRight:
+    case BottomLeft:
+        q->setCursor(Qt::SizeBDiagCursor);
+        break;
+    }
+}
+
+ComposerImageResizeWidgetPrivate::ResizeDirection ComposerImageResizeWidgetPrivate::resizeDirection(const QPoint& pos)
+{
+    //TODO
+    return ComposerImageResizeWidgetPrivate::None;
+}
 
 ComposerImageResizeWidget::ComposerImageResizeWidget(const QWebElement &element, QWidget *parent)
     : QWidget(parent), d(new ComposerImageResizeWidgetPrivate(this,element))
 {
+    setMouseTracking(true);
 }
 
 ComposerImageResizeWidget::~ComposerImageResizeWidget()
@@ -49,21 +103,29 @@ ComposerImageResizeWidget::~ComposerImageResizeWidget()
 
 void ComposerImageResizeWidget::mouseMoveEvent( QMouseEvent * event )
 {
-
+    if(!d->mousePressed) {
+        d->setResizeDirectionCursor(event->pos());
+    }
 }
 
 void ComposerImageResizeWidget::mousePressEvent( QMouseEvent * event )
 {
-
+    d->mousePressed = true;
 }
 
 void ComposerImageResizeWidget::mouseReleaseEvent( QMouseEvent * event )
 {
+    d->mousePressed = false;
 
 }
+
 void ComposerImageResizeWidget::paintEvent( QPaintEvent * )
 {
+    if(d->imageElement.isNull())
+        return;
 
+    QPainter painter(this);
+    painter.drawRect(rect());
 }
 
 
