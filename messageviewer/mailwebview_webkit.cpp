5ccd74105d53765e8bd510324611ee2bb6f38ec3
Montel Laurent <montel@kde.org>
2012-12-10 08:23:25 +0100

    Remove not necessary includes

--- messageviewer/mailwebview_webkit.cpp
+++ messageviewer/mailwebview_webkit.cpp
@@ -16,8 +16,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <config-messageviewer.h>
-
 #include "mailwebview.h"
 
 #include <KDebug>
