79f71af6fb4a324a9aebffad1019772bad29a9a1
Montel Laurent <montel@kde.org>
2012-12-13 09:30:22 +0100

    We can't reuse same tabledialog when we edit table

---
+++ composereditor-ng/composertableformatdialog.cpp
@@ -0,0 +1,106 @@
+/*
+  Copyright (c) 2012 Montel Laurent <montel@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+
+*/
+
+#include "composertableformatdialog.h"
+
+#include <KPIMTextEdit/InsertTableWidget>
+
+#include <KLocale>
+
+#include <QWebElement>
+
+namespace ComposerEditorNG
+{
+class ComposerTableFormatDialogPrivate
+{
+public:
+    ComposerTableFormatDialogPrivate(ComposerTableFormatDialog *qq)
+        :q(qq)
+    {
+    }
+
+    void initialize( const QWebElement& element);
+
+    void updateTableHtml();
+
+    QWebElement webElement;
+    KPIMTextEdit::InsertTableWidget *insertTableWidget;
+    ComposerTableFormatDialog *q;
+};
+
+void ComposerTableFormatDialogPrivate::updateTableHtml()
+{
+    if(!webElement.isNull()) {
+        webElement.setAttribute(QLatin1String("border"),QString::number(insertTableWidget->border()));
+        const QString width = QString::fromLatin1("%1%2").arg(insertTableWidget->length()).arg(insertTableWidget->typeOfLength() == QTextLength::PercentageLength ? QLatin1String("%") : QString());
+        webElement.setAttribute(QLatin1String("width"),width);
+        //TODO update column/row
+    }
+}
+
+void ComposerTableFormatDialogPrivate::initialize(const QWebElement &element)
+{
+    webElement = element;
+    q->setCaption( i18n( "Table Format" ) );
+    q->setButtons( KDialog::Ok|KDialog::Cancel );
+    q->setButtonText( KDialog::Ok, i18n( "Edit" ) );
+    insertTableWidget = new KPIMTextEdit::InsertTableWidget( q );
+    q->setMainWidget( insertTableWidget );
+    q->connect(q,SIGNAL(okClicked()),q,SLOT(slotOkClicked()));
+    if(!webElement.isNull()) {
+        if(webElement.hasAttribute(QLatin1String("border"))) {
+            insertTableWidget->setBorder(webElement.attribute(QLatin1String("border")).toInt());
+        }
+        if(webElement.hasAttribute(QLatin1String("width"))) {
+            const QString width = webElement.attribute(QLatin1String("border"));
+            if(width.endsWith(QLatin1Char('%'))) {
+                //insertTableWidget->setTypeOfLength(QTextLength::PercentageLength);
+            } else {
+                //insertTableWidget->setTypeOfLength(QTextLength::FixedLength);
+                //TODO
+                //insertTableWidget->set(QTextLength::FixedLength);
+            }
+        }
+    }
+}
+
+ComposerTableFormatDialog::ComposerTableFormatDialog(const QWebElement& element, QWidget *parent)
+    : KDialog(parent), d(new ComposerTableFormatDialogPrivate(this))
+{
+    d->initialize(element);
+}
+
+ComposerTableFormatDialog::~ComposerTableFormatDialog()
+{
+    delete d;
+}
+
+
+void ComposerTableFormatDialog::slotOkClicked()
+{
+    if(!d->webElement.isNull()) {
+        d->updateTableHtml();
+    }
+    accept();
+}
+
+}
+
+#include "composertableformatdialog.moc"
