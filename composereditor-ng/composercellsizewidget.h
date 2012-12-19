ca668d0be2e55a5be7737b0037d0889d265c495b
Montel Laurent <montel@kde.org>
2012-12-19 09:44:52 +0100

    Create a widget to config cell size

---
+++ composereditor-ng/composercellsizewidget.h
@@ -0,0 +1,43 @@
+/*
+  Copyright (c) 2012 Montel Laurent <montel@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+
+*/
+
+#ifndef COMPOSERCELLSIZEWIDGET_H
+#define COMPOSERCELLSIZEWIDGET_H
+
+#include <QWidget>
+
+namespace ComposerEditorNG
+{
+class ComposerCellSizeWidgetPrivate;
+
+class ComposerCellSizeWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ComposerCellSizeWidget(QWidget *parent);
+    ~ComposerCellSizeWidget();
+
+private:
+    friend class ComposerCellSizeWidgetPrivate;
+    ComposerCellSizeWidgetPrivate * const d;
+};
+}
+
+#endif // COMPOSERCELLSIZEWIDGET_H
