4f2c736c0ab91d7d7ba4748e0a38d94bc052239b
Pino Toscano <pino@kde.org>
2012-12-30 20:38:20 +0100

    Messages.sh: fix to actually work

--- composereditor-ng/Messages.sh
+++ composereditor-ng/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC `find . -name '*.ui' -or -name '*.kcfg' >> rc.cpp || exit 11
-$XGETTEXT *.h *.cpp -o | grep -v "/tests" $podir/libcomposereditorng.pot
+$EXTRACTRC `find . -name '*.ui' -o -name '*.kcfg'` >> rc.cpp || exit 11
+$XGETTEXT *.h *.cpp -o $podir/libcomposereditorng.pot
 rm -f rc.cpp
