8f330bb2fe781149c43e262ef5da7bb79a0a49ec
Montel Laurent <montel@kde.org>
2012-12-30 20:09:25 +0100

    Excluse tests

--- composereditor-ng/Messages.sh
+++ composereditor-ng/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name '*.ui' -or -name '*.kcfg' >> rc.cpp || exit 11
-$XGETTEXT *.h *.cpp -o $podir/libcomposereditorng.pot
+$XGETTEXT *.h *.cpp -o | grep -v "/tests" $podir/libcomposereditorng.pot
 rm -f rc.cpp
