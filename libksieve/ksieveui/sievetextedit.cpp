30883c8a47f1083d7c57b796ccd43d81db51a94a
Montel Laurent <montel@kde.org>
2013-01-09 09:44:56 +0100

    Add "find" icon

--- libksieve/ksieveui/sievetextedit.cpp
+++ libksieve/ksieveui/sievetextedit.cpp
@@ -58,7 +58,7 @@ void SieveTextEdit::contextMenuEvent( QContextMenuEvent *event )
   QMenu *popup = createStandardContextMenu();
   if (popup) {
     popup->addSeparator();
-    popup->addAction( KStandardGuiItem::find().text(),this,SIGNAL(findText()) , Qt::Key_F+Qt::CTRL);
+    popup->addAction( KStandardGuiItem::find().icon(), KStandardGuiItem::find().text(),this,SIGNAL(findText()) , Qt::Key_F+Qt::CTRL);
     //Code from KTextBrowser
     KIconTheme::assignIconsToContextMenu( isReadOnly() ? KIconTheme::ReadOnlyText
                                           : KIconTheme::TextEditor,
