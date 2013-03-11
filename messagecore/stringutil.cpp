e30fcdf1b1feebf375c66a1e7d2bc2d8d21ffbe6
David Faure <faure@kde.org>
2013-03-11 09:54:49 +0100

    Use KPIMUtils::quoteNameIfNecessary instead of duplicating it

--- messagecore/stringutil.cpp
+++ messagecore/stringutil.cpp
@@ -835,11 +835,7 @@ QString guessEmailAddressFromLoginName( const QString &loginName )
   const KUser user( loginName );
   if ( user.isValid() ) {
     QString fullName = user.property( KUser::FullName ).toString();
-    if ( fullName.contains( QRegExp( "[^ 0-9A-Za-z\\x0080-\\xFFFF]" ) ) )
-      address = '"' + fullName.replace( '\\', "\\" ).replace( '"', "\\" )
-          + "\" <" + address + '>';
-    else
-      address = fullName + " <" + address + '>';
+    address = KPIMUtils::quoteNameIfNecessary(fullName) + " <" + address + '>';
   }
 
   return address;
@@ -1069,7 +1065,7 @@ QString stripOffPrefixes( const QString &subject )
     QString tmp = subject;
     if ( regExp.indexIn( tmp ) == 0 ) {
       return tmp.remove( 0, regExp.matchedLength() );
-    } 
+    }
   } else {
    kWarning() << "bigRegExp = \""
                << bigRegExp << "\"\n"
@@ -1111,7 +1107,7 @@ KMime::Types::Mailbox mailboxFromUnicodeString( const QString &address )
 
   if( mailboxes.size() > 1 ) {
     kDebug()<<" mailboxes size > 1 ";
-  }       
+  }
   return mailboxes.first();
 }
 
@@ -1130,7 +1126,7 @@ KMime::Types::Mailbox mailboxFrom7BitString( const QByteArray &address )
   const KMime::Types::Mailbox::List mailboxes = header.mailboxes();
   if( mailboxes.isEmpty() ) {
     return KMime::Types::Mailbox();
-  } 
+  }
   if( mailboxes.size() > 1 ) {
     kDebug()<<" mailboxes size > 1 ";
   }
