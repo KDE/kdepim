8fd48d8b9061cd54ea6e42d45223789cd93f7f31
Montel Laurent <montel@kde.org>
2013-03-14 13:44:53 +0100

    Add a theme manager

---
+++ messageviewer/header/grantleethememanager.cpp
@@ -0,0 +1,47 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "grantleethememanager.h"
+
+#include <QMap>
+
+using namespace MessageViewer;
+
+class GrantleeThemeManager::Private
+{
+public:
+    Private(const QString &path, GrantleeThemeManager *qq)
+        : themePath(path),
+          q(qq)
+    {
+
+    }
+    QMap<QString, QString> themes;
+    QString themePath;
+    GrantleeThemeManager *q;
+};
+
+GrantleeThemeManager::GrantleeThemeManager(const QString &path, QObject *parent)
+    : QObject(parent), d(new Private(path,this))
+{
+}
+
+GrantleeThemeManager::~GrantleeThemeManager()
+{
+    delete d;
+}
+
