4d189a7469cb583a50abfed3588817890f6114d1
Montel Laurent <montel@kde.org>
2013-03-23 07:04:09 +0100

    Create a html report

--- messageviewer/scamdetection/scamdetection.cpp
+++ messageviewer/scamdetection/scamdetection.cpp
@@ -35,9 +35,9 @@ ScamDetection::~ScamDetection()
 
 void ScamDetection::scanPage(const QWebElement &rootElement)
 {
-    mDetails.clear();
     if (GlobalSettings::self()->scamDetectionEnabled()) {
-        mDetails = i18n("Details:");
+        mDetails.clear();
+        mDetails = QLatin1String("<b>") + i18n("Details:") + QLatin1String("</b><ul>");
         bool foundScam = false;
         QRegExp ip4regExp;
         ip4regExp.setPattern(IPv4_PATTERN);
@@ -50,29 +50,29 @@ void ScamDetection::scanPage(const QWebElement &rootElement)
                 if (title.startsWith(QLatin1String("http:")) || title.startsWith(QLatin1String("https:"))) {
                     if (href != title) {
                         foundScam = true;
-                        mDetails += '\n' + i18n("title definition in anchor '%1' is different from url definition in href '%2'", title, href);
+                        mDetails += QLatin1String("<li>") + i18n("title definition in anchor '%1' is different from url definition in href '%2'", title, href) + QLatin1String("</li>");
                     }
                 }
             }
             //2) detect if url href has ip and not server name.
             const QUrl url(href);
             if (url.host().contains(ip4regExp)) {
-                mDetails += '\n' + i18n("Hostname from href defines ip '%1'", url.host());
+                mDetails += QLatin1String("<li>") + i18n("Hostname from href defines ip '%1'", url.host())+QLatin1String("</li>");
                 foundScam = true;
             } else if (url.path().contains(QLatin1String("url?q="))) { //4) redirect url.
-                mDetails += '\n' + i18n("Href '%1' has a redirection", url.path());
+                mDetails += QLatin1String("<li>") + i18n("Href '%1' has a redirection", url.path()) +QLatin1String("</li>");
                 foundScam = true;
             } else if (url.path().count(QLatin1String("http://"))>1) { //5) more that 1 http in url.
-                mDetails += '\n' + i18n("Href '%1' contains multiple http://", url.path());
+                mDetails += QLatin1String("<li>") + i18n("Href '%1' contains multiple http://", url.path()) + QLatin1String("</li>");
                 foundScam = true;
             }
         }
         //3) has form
         if (rootElement.findAll(QLatin1String("form")).count() > 0) {
-            mDetails += '\n' + i18n("Message contains form element");
+            mDetails += QLatin1String("<li>") + i18n("Message contains form element") + QLatin1String("</li>");
             foundScam = true;
         }
-
+        mDetails += QLatin1String("</ul>");
         if (foundScam)
             Q_EMIT messageMayBeAScam();
     }
