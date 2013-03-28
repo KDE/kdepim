fa2e5fa87224bdcaf9df2d4661c7dd5d8ed234ed
Pino Toscano <pino@kde.org>
2013-03-28 08:51:42 +0100

    fix Messages.sh

--- mailcommon/Messages.sh
+++ mailcommon/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name '*.ui'` >> rc.cpp || exit 11
-$XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/' -o $podir/libmailcommon.pot
+$XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/'` -o $podir/libmailcommon.pot
 rm -f rc.cpp
