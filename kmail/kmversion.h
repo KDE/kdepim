4de3580fe0f3db1b3c4fbf2b7d3e813ef1ca77e0
Allen Winter <winter@kde.org>
2013-04-10 17:55:40 -0400

    GIT_SILENT: Update version numbers for today's release.

--- kmail/kmversion.h
+++ kmail/kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20121116.8809f57)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20130410.6a3ce30)"
 
 #endif /*kmversion_h*/
