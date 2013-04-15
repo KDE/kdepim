2f6f9b1d41fbc2987776b80bdb096a37a79646aa
Montel Laurent <montel@kde.org>
2013-04-15 13:59:12 +0200

    Look at to create grammar link plugins

---
+++ grammar/plugins/linkgrammar/grammarlinkclient.h
@@ -0,0 +1,37 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef GRAMMARLINKCLIENT_H
+#define GRAMMARLINKCLIENT_H
+
+#include "grammar/grammarclient.h"
+#include <QtCore/QVariantList>
+
+class GrammarLinkClient : public GrammarLinkClient::Client
+{
+    Q_OBJECT
+public:
+    GrammarLinkClient(QObject *parent, const QVariantList &);
+    ~GrammarLinkClient();
+
+    QStringList languages() const;
+
+    QString name() const;
+};
+
+#endif // GRAMMARLINKCLIENT_H
