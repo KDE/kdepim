00cf2819da501bcdec2862c8185b4479866a2d33
Christian Mollekopf <chrigi_1@fastmail.fm>
2013-04-18 11:29:36 +0200

    Instead of playing throw catch, we can also just check if a suitable payload is available.
    That is especially useful when asserting in the throw block in Akonadi::Item::payload for development.

--- calendarsupport/utils.cpp
+++ calendarsupport/utils.cpp
@@ -73,22 +73,16 @@ using namespace KCalCore;
 
 KCalCore::Incidence::Ptr CalendarSupport::incidence( const Akonadi::Item &item )
 {
-  try {
+  if (item.hasPayload<KCalCore::Incidence::Ptr>()) {
     return item.payload<KCalCore::Incidence::Ptr>();
-  } catch( Akonadi::PayloadException ) {
-    return KCalCore::Incidence::Ptr();
   }
+  return KCalCore::Incidence::Ptr();
 }
 
 KCalCore::Event::Ptr CalendarSupport::event( const Akonadi::Item &item )
 {
-  try {
-    KCalCore::Incidence::Ptr incidence = item.payload<KCalCore::Incidence::Ptr>();
-    if ( incidence && incidence->type() == KCalCore::Incidence::TypeEvent ) {
-      return item.payload<KCalCore::Event::Ptr>();
-    }
-  } catch( Akonadi::PayloadException ) {
-    return KCalCore::Event::Ptr();
+  if (item.hasPayload<KCalCore::Event::Ptr>()) {
+    return item.payload<KCalCore::Event::Ptr>();
   }
   return KCalCore::Event::Ptr();
 }
