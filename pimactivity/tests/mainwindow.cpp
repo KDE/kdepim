b478aa33a689bb04b6687329c9af476be15a3b7f
Montel Laurent <montel@kde.org>
2013-04-23 19:23:57 +0200

    Move to own directory

--- pimactivity/tests/mainwindow.cpp
+++ pimactivity/tests/mainwindow.cpp
@@ -16,7 +16,7 @@
 */
 
 #include "mainwindow.h"
-#include "pimactivity/pimwidgets/transportcomboboxactivity.h"
+#include "pimactivity/pimwidgets/mailtransport/transportcomboboxactivity.h"
 
 #include <QVBoxLayout>
 
