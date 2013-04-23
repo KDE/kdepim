26e324433850654d4e45eccb7dc37611568c3722
Montel Laurent <montel@kde.org>
2013-04-23 13:23:25 +0200

    fix coding style

--- libksieve/ksieveui/editor/sievesyntaxhighlighter.cpp
+++ libksieve/ksieveui/editor/sievesyntaxhighlighter.cpp
@@ -126,7 +126,7 @@ void SieveSyntaxHighlighter::init()
                <<QLatin1String( "\\bremoveflag\\b" )
                <<QLatin1String( "\\bdeleteheader\\b" )
                <<QLatin1String( "\\baddheader\\b" )
-               <<QLatin1String("\\bvacation\\b");
+               <<QLatin1String( "\\bvacation\\b" );
     Q_FOREACH ( const QString & s, actionType ) {
         const QRegExp regex( s, Qt::CaseInsensitive );
         m_rules.append( Rule( regex, actionFormat ) );
@@ -137,14 +137,14 @@ void SieveSyntaxHighlighter::init()
     testFormat.setForeground( Qt::gray );
     testFormat.setFontWeight( QFont::Bold );
     QStringList testType;
-    testType << QLatin1String( "\\benvelope\\b" )
-             << QLatin1String( "\\baddress\\b" )
+    testType <<QLatin1String( "\\benvelope\\b" )
+             <<QLatin1String( "\\baddress\\b" )
              <<QLatin1String( "\\ballof\\b" )
              <<QLatin1String( "\\banyof\\b" )
              <<QLatin1String( "\\bexists\\b" )
              <<QLatin1String( "\\bfalse\\b" )
              <<QLatin1String( "\\bheader\\b" )
-             <<QLatin1String("\\bnot\\b" )
+             <<QLatin1String( "\\bnot\\b" )
              <<QLatin1String( "\\bsize\\b" )
              <<QLatin1String( "\\btrue\\b" );
     Q_FOREACH ( const QString & s, testType ) {
