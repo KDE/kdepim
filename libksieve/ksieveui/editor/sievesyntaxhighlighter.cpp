371342e541c739b1abf1c093efeb2fc52faf3dad
Montel Laurent <montel@kde.org>
2013-05-12 12:35:44 +0200

    Add missing keywords

--- libksieve/ksieveui/editor/sievesyntaxhighlighter.cpp
+++ libksieve/ksieveui/editor/sievesyntaxhighlighter.cpp
@@ -95,7 +95,9 @@ void SieveSyntaxHighlighter::init()
               <<QLatin1String( "\\s:all\\b" )
               <<QLatin1String( "\\s:copy\\b" )
               <<QLatin1String( "\\s:message\\b" )
-              <<QLatin1String( "\\s:importance\\b" );
+              <<QLatin1String( "\\s:importance\\b" )
+              <<QLatin1String( "\\s:seconds\\b" )
+              <<QLatin1String( "\\s:days\\b" );
     Q_FOREACH ( const QString & s, matchType ) {
         const QRegExp regex( s, Qt::CaseInsensitive );
         m_rules.append( Rule( regex, matchFormat ) );
