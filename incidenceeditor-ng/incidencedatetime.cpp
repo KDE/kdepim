bf2a557982ea8803ce6340cfd5b0186252df1196
Sergio Martins <iamsergio@gmail.com>
2013-05-19 01:30:33 +0100

    Cleanup code that doesn't make sense anymore.

--- incidenceeditor-ng/incidencedatetime.cpp
+++ incidenceeditor-ng/incidencedatetime.cpp
@@ -545,24 +545,6 @@ void IncidenceDateTime::load( const KCalCore::Event::Ptr &event )
   if ( !isTemplate ) {
     KDateTime startDT = event->dtStart();
     KDateTime endDT = event->dtEnd();
-    /*
-    if ( event->recurs() && mActiveDate.isValid() ) {
-      // Consider the active date when editing recurring Events.
-      KDateTime kdt( mActiveDate, QTime( 0, 0, 0 ), KSystemTimeZones::local() );
-      const int eventLength = startDT.daysTo( endDT );
-      kdt = kdt.addSecs( -1 );
-      startDT.setDate( event->recurrence()->getNextDateTime( kdt ).date() );
-      if ( event->hasEndDate() ) {
-        endDT.setDate( startDT.addDays( eventLength ).date() );
-      } else {
-        if ( event->hasDuration() ) {
-          endDT = startDT.addSecs( event->duration().asSeconds() );
-        } else {
-          endDT = startDT;
-        }
-      }
-    }
-    */
 
     setDateTimes( startDT, endDT );
   } else {
@@ -610,14 +592,6 @@ void IncidenceDateTime::load( const KCalCore::Journal::Ptr &journal )
   if ( !isTemplate ) {
     KDateTime startDT = journal->dtStart();
 
-    /*
-    if ( journal->recurs() && mActiveDate.isValid() ) {
-      // Consider the active date when editing recurring journals
-      KDateTime kdt( mActiveDate, QTime( 0, 0, 0 ), KSystemTimeZones::local() );
-      kdt = kdt.addSecs( -1 );
-      startDT.setDate( journal->recurrence()->getNextDateTime( kdt ).date() );
-    }
-    */
     // Convert UTC to local timezone, if needed (i.e. for kolab #204059)
     if ( startDT.isUtc() ) {
       startDT = startDT.toLocalZone();
