9c49a16583aa140c045539ccbf6ea0833dabc92b
Montel Laurent <montel@kde.org>
2013-05-21 16:40:50 +0200

    Fix enable/disable remove button

--- headerthemeeditor/managethemes.cpp
+++ headerthemeeditor/managethemes.cpp
@@ -101,13 +101,15 @@ void ManageThemes::initialize()
     const QString localDirectory = KStandardDirs::locateLocal("data",QLatin1String("messageviewer/themes/"));
     QDir dir(localDirectory);
     if (dir.exists()) {
+        bool hasSubDir = false;
         QDirIterator dirIt( localDirectory, QStringList(), QDir::AllDirs | QDir::NoDotAndDotDot );
         while ( dirIt.hasNext() ) {
             dirIt.next();
             const QString dirName = dirIt.fileName();
             new QListWidgetItem(dirName, mListThemes);
+            hasSubDir = true;
         }
-        enableButtonOk(true);
+        enableButtonOk(hasSubDir);
     } else {
         enableButtonOk(false);
     }
