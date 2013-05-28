1c9945a665af05612cb4956a1ff0999d3b2c9208
Montel Laurent <montel@kde.org>
2013-05-28 07:50:02 +0200

    Add TODO

--- libksieve/ksieveui/autocreatescripts/sieveconditions/sieveconditionmetadata.cpp
+++ libksieve/ksieveui/autocreatescripts/sieveconditions/sieveconditionmetadata.cpp
@@ -24,6 +24,7 @@
 #include <QHBoxLayout>
 #include <QDebug>
 
+//TODO implement it
 using namespace KSieveUi;
 SieveConditionMetaData::SieveConditionMetaData(QObject *parent)
     : SieveCondition(QLatin1String("metadata"), i18n("Meta Data"), parent)
