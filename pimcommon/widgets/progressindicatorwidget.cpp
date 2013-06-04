5a863291675653ae84ae1b03ac97b052397682af
Montel Laurent <montel@kde.org>
2013-06-04 07:54:16 +0200

    Start to add new class progressindicatorwidget

---
+++ pimcommon/widgets/progressindicatorwidget.cpp
@@ -0,0 +1,50 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "progressindicatorwidget.h"
+
+namespace PimCommon {
+
+class ProgressIndicatorWidgetPrivate
+{
+public:
+    ProgressIndicatorWidgetPrivate(ProgressIndicatorWidget *qq)
+        : q(qq)
+    {
+
+    }
+    ProgressIndicatorWidget *q;
+};
+
+ProgressIndicatorWidget::ProgressIndicatorWidget(QWidget *parent)
+    : QLabel(parent),
+      d(new ProgressIndicatorWidgetPrivate(this))
+{
+}
+
+ProgressIndicatorWidget::~ProgressIndicatorWidget()
+{
+    delete d;
+}
+
+void ProgressIndicatorWidget::start()
+{
+    //TODO
+}
+}
+
+#include "progressindicatorwidget.moc"
