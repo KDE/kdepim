e1e9a3ecd3d11dc25d5792785c3b7f6dd9ff5591
Montel Laurent <montel@kde.org>
2013-06-12 09:27:00 +0200

    For the future

---
+++ pimsettingexporter/alarm/exportalarmjob.cpp
@@ -0,0 +1,52 @@
+/*
+  Copyright (c) 2012-2013 Montel Laurent <montel@kde.org>
+  
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+  
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "exportalarmjob.h"
+
+#include "messageviewer/utils/kcursorsaver.h"
+
+
+#include <KLocale>
+
+#include <QWidget>
+
+
+ExportAlarmJob::ExportAlarmJob(QWidget *parent, ArchiveStorage *archiveStorage)
+    :AbstractImportExportJob(parent,archiveStorage,/*typeSelected,numberOfStep*/0,0 /*TODO fix it*/)
+{
+}
+
+ExportAlarmJob::~ExportAlarmJob()
+{
+
+}
+
+void ExportAlarmJob::start()
+{
+    //TODO
+}
+
+
+void ExportAlarmJob::backupResources()
+{
+    showInfo(i18n("Backing up resources..."));
+    MessageViewer::KCursorSaver busy( MessageViewer::KBusyPtr::busy() );
+    //TODO backup calendar
+    Q_EMIT info(i18n("Resources backup done."));
+}
+
+#include "exportalarmjob.moc"
