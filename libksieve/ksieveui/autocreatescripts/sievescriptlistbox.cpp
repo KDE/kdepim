8e453746dfc57e89d83a1c6e2db99387314fc9eb
Montel Laurent <montel@kde.org>
2013-08-02 08:09:36 +0200

    Was implemented

--- libksieve/ksieveui/autocreatescripts/sievescriptlistbox.cpp
+++ libksieve/ksieveui/autocreatescripts/sievescriptlistbox.cpp
@@ -416,7 +416,6 @@ void SieveScriptListBox::loadScript(const QDomDocument &doc)
                         currentPage->globalVariableWidget()->loadScript(e);
                     } else if (actionName == QLatin1String("set") && (typeBlock == TypeBlockGlobal)) {
                         currentPage->globalVariableWidget()->loadSetVariable(e);
-                        //TODO implement load set global variable.
                     } else {
                         if (typeBlock != TypeBlockAction) {
                             currentPage = createNewScript(scriptName.isEmpty() ? createUniqName() : scriptName, comment);
