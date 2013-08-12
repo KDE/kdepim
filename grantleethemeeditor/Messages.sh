6d43a9dd8ff1455d49bd8d63f499aa49ba73fafd
Montel Laurent <montel@kde.org>
2013-08-12 22:09:42 +0200

    Fix extract messages

--- grantleethemeeditor/Messages.sh
+++ grantleethemeeditor/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/'` -o $podir/libgrantleethemeeditor.pot
+$XGETTEXT `find . -name '*.cpp' -o -name '*.h'` -o $podir/libgrantleethemeeditor.pot
 rm -f rc.cpp
