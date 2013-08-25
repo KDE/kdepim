6d99e48d4fb481d078ca4f579d6469ac9fa037a5
Montel Laurent <montel@kde.org>
2013-08-25 14:15:50 +0200

    Fix extract messages

--- calendarviews/Messages.sh
+++ calendarviews/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-$EXTRACTRC *.ui *.kcfg >> rc.cpp
+$EXTRACTRC `find . -name '*.ui' -or -name '*.kcfg'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp"` -o $podir/libeventviews.pot
 rm -f rc.cpp
