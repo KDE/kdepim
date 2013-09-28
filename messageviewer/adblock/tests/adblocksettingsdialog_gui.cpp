d1fef70a8dbc6be865be20bf3067bd21051f399c
Montel Laurent <montel@kde.org>
2013-09-28 08:11:13 +0200

    Add dialog test

---
+++ messageviewer/adblock/tests/adblocksettingsdialog_gui.cpp
@@ -0,0 +1,39 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <kdebug.h>
+#include <kcmdlineargs.h>
+#include <kapplication.h>
+#include <KDialog>
+#include "messageviewer/adblock/adblocksettingwidget.h"
+
+int main (int argc, char **argv)
+{
+    KCmdLineArgs::init(argc, argv, "adblocksettings_gui", 0, ki18n("adblocksettingstest_Gui"),
+                       "1.0", ki18n("Test for adblocksettings"));
+    KApplication app;
+
+    MessageViewer::AdBlockSettingWidget *w = new MessageViewer::AdBlockSettingWidget;
+    KDialog dlg;
+    dlg.setMainWidget(w);
+    w->doLoadFromGlobalSettings();
+    if (dlg.exec()) {
+        w->save();
+    }
+    return 0;
+}
+
