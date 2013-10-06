4e37ae73878bf197cecd28acaa1eb1e952ba7902
Montel Laurent <montel@kde.org>
2013-10-06 08:15:57 +0200

    Add template for unittest

---
+++ mailcommon/filter/filterimporter/tests/filterimportevolutiontest.h
@@ -0,0 +1,29 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef FILTERIMPORTEVOLUTIONTEST_H
+#define FILTERIMPORTEVOLUTIONTEST_H
+#include <QObject>
+class FilterImportEvolutiontest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void initTestCase();
+    void testImportFilters();
+};
+
+#endif // FILTERIMPORTEVOLUTIONTEST_H
