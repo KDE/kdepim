05ff200a55aaf6b9d781e95f2d097e87025bf432
Montel Laurent <montel@kde.org>
2013-10-12 09:50:42 +0200

    Add templates for import/export notes

---
+++ pimsettingexporter/notes/importnotesjob.cpp
@@ -0,0 +1,60 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+  
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+  
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "importnotesjob.h"
+#include "archivestorage.h"
+
+#include "pimcommon/util/createresource.h"
+
+#include <KTempDir>
+#include <KStandardDirs>
+#include <KArchive>
+#include <KLocale>
+#include <KConfigGroup>
+#include <KZip>
+
+#include <QFile>
+#include <QDir>
+
+static const QString storeJot = QLatin1String("backupjot/");
+
+ImportNotesJob::ImportNotesJob(QWidget *parent, Utils::StoredTypes typeSelected, ArchiveStorage *archiveStorage, int numberOfStep)
+    : AbstractImportExportJob(parent, archiveStorage, typeSelected, numberOfStep)
+{
+    initializeImportJob();
+}
+
+ImportNotesJob::~ImportNotesJob()
+{
+}
+
+void ImportNotesJob::start()
+{
+    mArchiveDirectory = archive()->directory();
+    if (mTypeSelected & Utils::Config)
+        restoreConfig();
+}
+
+
+
+void ImportNotesJob::restoreConfig()
+{
+    //TODO
+    Q_EMIT info(i18n("Config restored."));
+}
+
+#include "importnotesjob.moc"
