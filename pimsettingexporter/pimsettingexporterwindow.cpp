ac41b8d1239b411e0bf399cc2ef4814122169ad0
Montel Laurent <montel@kde.org>
2013-11-10 09:56:49 +0100

    Move to own directory

--- pimsettingexporter/pimsettingexporterwindow.cpp
+++ pimsettingexporter/pimsettingexporterwindow.cpp
@@ -43,11 +43,11 @@
 #include "blogilo/importblogilojob.h"
 
 #include "pimsettingexporterkernel.h"
-#include "selectiontypedialog.h"
+#include "dialog/selectiontypedialog.h"
 #include "utils.h"
 #include "archivestorage.h"
 
-#include "backupfilestructureinfodialog.h"
+#include "dialog/backupfilestructureinfodialog.h"
 
 #include <mailcommon/kernel/mailkernel.h>
 #include <mailcommon/filter/filtermanager.h>
