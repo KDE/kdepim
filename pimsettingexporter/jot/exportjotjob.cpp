367398f6219ae64fcaa48bfde6d9c51af6508206
Montel Laurent <montel@kde.org>
2013-11-14 20:08:28 +0100

    Fix import/export kjots

--- pimsettingexporter/jot/exportjotjob.cpp
+++ pimsettingexporter/jot/exportjotjob.cpp
@@ -71,7 +71,25 @@ void ExportJotJob::backupResources()
     foreach( const Akonadi::AgentInstance &agent, list ) {
         const QString identifier = agent.identifier();
         if (identifier.contains(QLatin1String("akonadi_akonotes_resource_"))) {
-            backupResourceDirectory(agent, Utils::jotPath());
+            const QString archivePath = Utils::jotPath() + identifier + QDir::separator();
+            KUrl url = Utils::resourcePath(agent);
+            if (!url.isEmpty()) {
+                const bool fileAdded = backupFullDirectory(url, archivePath, QLatin1String("job.zip"));
+                if (fileAdded) {
+                    const QString errorStr = Utils::storeResources(archive(), identifier, archivePath);
+                    if (!errorStr.isEmpty())
+                        Q_EMIT error(errorStr);
+                    url = Utils::akonadiAgentConfigPath(identifier);
+                    if (!url.isEmpty()) {
+                        const QString filename = url.fileName();
+                        const bool fileAdded  = archive()->addLocalFile(url.path(), archivePath + filename);
+                        if (fileAdded)
+                            Q_EMIT info(i18n("\"%1\" was backuped.",filename));
+                        else
+                            Q_EMIT error(i18n("\"%1\" file cannot be added to backup file.",filename));
+                    }
+                }
+            }
         }
     }
 
