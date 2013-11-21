b09cb7edebf3bbe2d58b36227070644f110a991e
Montel Laurent <montel@kde.org>
2013-11-21 21:31:14 +0100

    Add test apps. Fix copyright. Minor fix

--- pimcommon/nepomukdebug/sparqlsyntaxhighlighter.cpp
+++ pimcommon/nepomukdebug/sparqlsyntaxhighlighter.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2010 Vishesh Handa <handa.vish@gmail.com>
+    Copyright (C) 2013 Laurent Montel <montel@kde.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
