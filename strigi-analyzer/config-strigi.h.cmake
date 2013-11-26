2bfffcbc068ad8b5065261a30003dd216401f537
Volker Krause <vkrause@kde.org>
2013-11-26 19:19:57 +0100

    Remove Strigi analyzers and static Strigi daemon.
    The analyzers are disabled since almost a year, and not even used by the
    Nepomuk indexers anymore. The static Strigi daemon was only used for
    Windows CE 5.

--- strigi-analyzer/config-strigi.h.cmake
+++
@@ -1,8 +0,0 @@
-#cmakedefine STRIGI_NEEDS_SIGNED_CHAR_01 1
-#ifdef STRIGI_NEEDS_SIGNED_CHAR_01
-#define STRIGI_ENDANALYZER_RETVAL signed char
-#else
-#define STRIGI_ENDANALYZER_RETVAL char
-#endif
-
-#cmakedefine HAVE_KMPSEARCHER_H 1
