31de2092aac407277b045f831296257a3e91baa8
Volker Krause <vkrause@kde.org>
2013-11-28 21:28:19 +0100

    Remove usage of KPIMUtils::NetworkAccessHelper.
    It's non-functional on anything but Windows CE 5, and is going to be
    removed in KF5.

--- mobile/mail/mainview.h
+++ mobile/mail/mainview.h
@@ -29,7 +29,6 @@
 #include <akonadi/kmime/specialmailcollections.h>
 #include <akonadi/selectionproxymodel.h>
 #include <messagecomposer/helper/messagefactory.h>
-#include <kpimutils/networkaccesshelper.h>
 
 namespace Akonadi {
   class EntityMimeTypeFilterModel;
@@ -235,7 +234,6 @@ class MainView : public KDeclarativeMainView
     AclEditor *mAclEditor;
     MessageListSettingsController *mMessageListSettingsController;
     Akonadi::Collection mCurrentCollection;
-    KPIMUtils::NetworkAccessHelper *mNetworkAccessHelper;
 };
 
 Q_DECLARE_METATYPE( MainView::ForwardMode )
