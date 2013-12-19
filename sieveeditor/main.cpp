980c13f7b4558f0eae6f1191bf47f4cb5a0b531a
Montel Laurent <montel@kde.org>
2013-12-19 13:34:00 +0100

    Create a new application: SieveEditor
    It's necessary because some guys doesn't want to install all imap resource
    in akonadi to edit server script.

---
+++ sieveeditor/main.cpp
@@ -0,0 +1,44 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+
+*/
+
+
+#include "kdepim-version.h"
+#include "contacteditormainwindow.h"
+#include <kapplication.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
+
+int main( int argc, char **argv )
+{
+    KAboutData aboutData( "sieveeditor", 0, ki18n("Sieve Editor"),
+      KDEPIM_VERSION, ki18n("Sieve Editor"), KAboutData::License_GPL_V2,
+      ki18n("Copyright © 2013 sieveeditor authors"));
+    aboutData.addAuthor(ki18n("Laurent Montel"), ki18n("Maintainer"), "montel@kde.org");
+    aboutData.setProgramIconName(QLatin1String("kmail"));
+    KCmdLineArgs::init( argc, argv, &aboutData );
+
+    KCmdLineOptions options;
+    KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
+
+    KApplication app;
+    //ContactEditorMainWindow *mw = new ContactEditorMainWindow();
+    mw->show();
+    app.exec();
+}
