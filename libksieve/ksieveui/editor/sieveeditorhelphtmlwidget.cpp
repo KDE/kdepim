880d9f8b7f7385e1affc45245a083160048ae9e0
Montel Laurent <montel@kde.org>
2013-12-19 08:02:36 +0100

    I want to add a help for sieve

---
+++ libksieve/ksieveui/editor/sieveeditorhelphtmlwidget.cpp
@@ -0,0 +1,38 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "sieveeditorhelphtmlwidget.h"
+
+#include <QWebView>
+#include <QVBoxLayout>
+
+using namespace KSieveUi;
+
+SieveEditorHelpHtmlWidget::SieveEditorHelpHtmlWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    mWebView = new QWebView;
+    QVBoxLayout *lay = new QVBoxLayout;
+    lay->addWidget(mWebView);
+    setLayout(lay);
+}
+
+SieveEditorHelpHtmlWidget::~SieveEditorHelpHtmlWidget()
+{
+
+}
+
---
+++ libksieve/ksieveui/editor/sieveeditorhelphtmlwidget.h
@@ -0,0 +1,37 @@
+/*
+  Copyright (c) 2013 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef SIEVEEDITORHELPHTMLWIDGET_H
+#define SIEVEEDITORHELPHTMLWIDGET_H
+
+#include <QWidget>
+class QWebView;
+
+namespace KSieveUi {
+class SieveEditorHelpHtmlWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SieveEditorHelpHtmlWidget(QWidget *parent=0);
+    ~SieveEditorHelpHtmlWidget();
+
+private:
+    QWebView *mWebView;
+};
+}
+
+#endif // SIEVEEDITORHELPHTMLWIDGET_H
