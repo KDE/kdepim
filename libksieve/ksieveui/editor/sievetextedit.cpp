e8ef16b2a05b309291ae8ed80d7e57d5c0113c15
Montel Laurent <montel@kde.org>
2013-12-21 10:58:44 +0100

    Add quote

--- libksieve/ksieveui/editor/sievetextedit.cpp
+++ libksieve/ksieveui/editor/sievetextedit.cpp
@@ -247,7 +247,7 @@ void SieveTextEdit::addExtraMenuEntry(QMenu *menu)
             separator->setSeparator(true);
             menu->insertAction(menu->actions().at(0), separator);
 
-            KAction *searchAction = new KAction(i18n("Help about: %1",word), menu);
+            KAction *searchAction = new KAction(i18n("Help about: \'%1\'",word), menu);
             searchAction->setShortcut(Qt::Key_F1);
             searchAction->setIcon(KIcon(QLatin1String("help-hint")));
             searchAction->setData(word);
