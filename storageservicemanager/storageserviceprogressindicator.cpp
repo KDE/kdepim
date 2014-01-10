06c152246d9f9b3e04603996ab24dd77bf2595bc
Montel Laurent <montel@kde.org>
2014-01-10 07:49:51 +0100

    Add progress indicator

---
+++ storageservicemanager/storageserviceprogressindicator.cpp
@@ -0,0 +1,58 @@
+/*
+  Copyright (c) 2014 Montel Laurent <montel@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+
+*/
+
+#include "storageserviceprogressindicator.h"
+#include <QTimer>
+
+StorageServiceProgressIndicator::StorageServiceProgressIndicator(QObject *parent)
+    : QObject(parent),
+      mProgressCount(0)
+{
+    mProgressPix = KPixmapSequence(QLatin1String("process-working"), KIconLoader::SizeSmallMedium);
+    mProgressTimer = new QTimer(this);
+    connect(mProgressTimer, SIGNAL(timeout()), this, SLOT(slotTimerDone()));
+}
+
+StorageServiceProgressIndicator::~StorageServiceProgressIndicator()
+{
+}
+
+void StorageServiceProgressIndicator::slotTimerDone()
+{
+    //mItem->setProgressAnimation(mProgressPix.frameAt(mProgressCount));
+    ++mProgressCount;
+    if (mProgressCount == 8)
+        mProgressCount = 0;
+
+    mProgressTimer->start(300);
+}
+
+void StorageServiceProgressIndicator::startAnimation()
+{
+    mProgressCount = 0;
+    mProgressTimer->start(300);
+}
+
+void StorageServiceProgressIndicator::stopAnimation()
+{
+    if (mProgressTimer->isActive())
+        mProgressTimer->stop();
+    //mItem->setDefaultIcon();
+}
