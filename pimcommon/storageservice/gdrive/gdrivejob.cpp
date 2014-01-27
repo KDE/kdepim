e5d2544cf8d8d02f70986f3ea8d850aa602d8f85
Montel Laurent <montel@kde.org>
2014-01-27 13:10:20 +0100

    Look at to implement login

--- pimcommon/storageservice/gdrive/gdrivejob.cpp
+++ pimcommon/storageservice/gdrive/gdrivejob.cpp
@@ -16,20 +16,31 @@
 */
 
 #include "gdrivejob.h"
-#include "pimcommon/storageservice/storageserviceabstract.h"
+#include "storageservice/authdialog/storageauthviewdialog.h"
 #include "pimcommon/storageservice/storageservicejobconfig.h"
 
+#include <KLocalizedString>
 
 #include <qjson/parser.h>
 
+#include <QNetworkAccessManager>
+#include <QDebug>
+#include <QNetworkReply>
+#include <QPointer>
+
 #include <QDebug>
 #include <QFile>
+#include <QPointer>
+#include <QDateTime>
 
 using namespace PimCommon;
 
 GDriveJob::GDriveJob(QObject *parent)
-    : PimCommon::OAuth2Job(parent)
+    : PimCommon::StorageServiceAbstractJob(parent),
+      mExpireInTime(0),
+      mNeedRefreshToken(false)
 {
+    mRedirectUri = PimCommon::StorageServiceJobConfig::self()->oauth2RedirectUrl();
     mClientId = PimCommon::StorageServiceJobConfig::self()->gdriveClientId();
     mClientSecret = PimCommon::StorageServiceJobConfig::self()->gdriveClientSecret();
     mRedirectUri = QLatin1String("urn:ietf:wg:oauth:2.0:oob");
@@ -41,6 +52,8 @@ GDriveJob::GDriveJob(QObject *parent)
     mFileInfoPath = QLatin1String("/2.0/files/");
     mCurrentAccountInfoPath = QLatin1String("/2.0/users/me");
     mScope = QLatin1String("https://www.googleapis.com/auth/drive");
+
+    connect(mNetworkAccessManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(slotSendDataFinished(QNetworkReply*)));
 }
 
 GDriveJob::~GDriveJob()
@@ -48,22 +61,32 @@ GDriveJob::~GDriveJob()
 
 }
 
-void GDriveJob::parseAccountInfo(const QString &data)
+void GDriveJob::initializeToken(const QString &refreshToken, const QString &token, const QDateTime &expireDateTime)
 {
-    QJson::Parser parser;
-    bool ok;
+    mError = false;
+    mRefreshToken = refreshToken;
+    mToken = token;
+    mNeedRefreshToken = (QDateTime::currentDateTime() >= expireDateTime);
+}
 
-    const QMap<QString, QVariant> info = parser.parse(data.toUtf8(), &ok).toMap();
-    //qDebug()<<" info"<<info;
-    PimCommon::AccountInfo accountInfo;
-    if (info.contains(QLatin1String("space_used"))) {
-        accountInfo.shared = info.value(QLatin1String("space_used")).toLongLong();
-    }
-    if (info.contains(QLatin1String("space_amount"))) {
-        accountInfo.quota = info.value(QLatin1String("space_amount")).toLongLong();
-    }
-    Q_EMIT accountInfoDone(accountInfo);
+void GDriveJob::createServiceFolder()
+{
+    mActionType = PimCommon::StorageServiceAbstract::CreateServiceFolder;
+    mError = false;
+    //TODO
+    Q_EMIT actionFailed(QLatin1String("Not Implemented"));
+    qDebug()<<" not implemented";
     deleteLater();
+}
+
+QNetworkReply * GDriveJob::downloadFile(const QString &name, const QString &fileId, const QString &destination)
+{
+    mActionType = PimCommon::StorageServiceAbstract::DownLoadFile;
+    mError = false;
+    Q_EMIT actionFailed(QLatin1String("Not Implemented"));
+    qDebug()<<" not implemented";
+    deleteLater();
+    return 0;
 }
 
 void GDriveJob::deleteFile(const QString &filename)
@@ -153,30 +176,98 @@ void GDriveJob::copyFolder(const QString &source, const QString &destination)
     deleteLater();
 }
 
-QNetworkReply *GDriveJob::uploadFile(const QString &filename, const QString &destination)
+void GDriveJob::requestTokenAccess()
 {
-    QFile *file = new QFile(filename);
-    if (file->exists()) {
-        mActionType = PimCommon::StorageServiceAbstract::UploadFile;
-        mError = false;
-        if (file->open(QIODevice::ReadOnly)) {
-            QUrl url;
-            url.setUrl(mApiUrl + mFileInfoPath + QLatin1String("content"));
-            QNetworkRequest request(url);
-            request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("application/x-www-form-urlencoded"));
-            request.setRawHeader("Authorization", "Bearer "+ mToken.toLatin1());
-            qDebug()<<" request "<<request.rawHeaderList()<<" reqyest "<<request.url();
-            QUrl postData;
-            //TODO
-            QNetworkReply *reply = mNetworkAccessManager->post(request, postData.encodedQuery());
-            file->setParent(reply);
-            connect(reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(slotError(QNetworkReply::NetworkError)));
-            return reply;
-        } else {
-            delete file;
+    mError = false;
+    mActionType = PimCommon::StorageServiceAbstract::RequestToken;
+    QUrl url(mServiceUrl + mAuthorizePath );
+    url.addQueryItem(QLatin1String("response_type"), QLatin1String("code"));
+    url.addQueryItem(QLatin1String("client_id"), mClientId);
+    url.addQueryItem(QLatin1String("redirect_uri"), mRedirectUri);
+    if (!mScope.isEmpty())
+        url.addQueryItem(QLatin1String("scope"),mScope);
+    mAuthUrl = url;
+    qDebug()<<" url"<<url;
+    delete mAuthDialog;
+    mAuthDialog = new PimCommon::StorageAuthViewDialog;
+    //connect(mAuthDialog, SIGNAL(urlChanged(QUrl)), this, SLOT(slotRedirect(QUrl)));
+    mAuthDialog->setUrl(url);
+    if (mAuthDialog->exec()) {
+        delete mAuthDialog;
+    } else {
+        Q_EMIT authorizationFailed(i18n("Authorization canceled."));
+        delete mAuthDialog;
+        deleteLater();
+    }
+}
+
+void GDriveJob::slotRedirect(const QUrl &url)
+{
+    if (url != mAuthUrl) {
+        qDebug()<<" Redirect !"<<url;
+        mAuthDialog->accept();
+        parseRedirectUrl(url);
+    }
+}
+
+void GDriveJob::parseRedirectUrl(const QUrl &url)
+{
+    const QList<QPair<QString, QString> > listQuery = url.queryItems();
+    //qDebug()<< "listQuery "<<listQuery;
+
+    QString authorizeCode;
+    QString errorStr;
+    QString errorDescription;
+    for (int i = 0; i < listQuery.size(); ++i) {
+        const QPair<QString, QString> item = listQuery.at(i);
+        if (item.first == QLatin1String("code")) {
+            authorizeCode = item.second;
+            break;
+        } else if (item.first == QLatin1String("error")) {
+            errorStr = item.second;
+        } else if (item.first == QLatin1String("error_description")) {
+            errorDescription = item.second;
         }
     }
-    return 0;
+    if (!authorizeCode.isEmpty()) {
+        getTokenAccess(authorizeCode);
+    } else {
+        Q_EMIT authorizationFailed(errorStr + QLatin1Char(' ') + errorDescription);
+        deleteLater();
+    }
+}
+
+void GDriveJob::getTokenAccess(const QString &authorizeCode)
+{
+    mActionType = PimCommon::StorageServiceAbstract::AccessToken;
+    mError = false;
+    QNetworkRequest request(QUrl(mServiceUrl + mPathToken));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("application/x-www-form-urlencoded"));
+    QUrl postData;
+    postData.addQueryItem(QLatin1String("code"), authorizeCode);
+    postData.addQueryItem(QLatin1String("redirect_uri"), mRedirectUri);
+    postData.addQueryItem(QLatin1String("grant_type"), QLatin1String("authorization_code"));
+    postData.addQueryItem(QLatin1String("client_id"), mClientId);
+    postData.addQueryItem(QLatin1String("client_secret"), mClientSecret);
+    QNetworkReply *reply = mNetworkAccessManager->post(request, postData.encodedQuery());
+    connect(reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(slotError(QNetworkReply::NetworkError)));
+}
+
+QNetworkReply *GDriveJob::uploadFile(const QString &filename, const QString &destination)
+{
+    mActionType = PimCommon::StorageServiceAbstract::UploadFile;
+    mError = false;
+    QUrl url;
+    url.setUrl(mApiUrl + mFileInfoPath + QLatin1String("content"));
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("application/x-www-form-urlencoded"));
+    request.setRawHeader("Authorization", "Bearer "+ mToken.toLatin1());
+    qDebug()<<" request "<<request.rawHeaderList()<<" reqyest "<<request.url();
+    QUrl postData;
+    //TODO
+    QNetworkReply *reply = mNetworkAccessManager->post(request, postData.encodedQuery());
+    connect(reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(slotError(QNetworkReply::NetworkError)));
+    return reply;
 }
 
 void GDriveJob::listFolder(const QString &folder)
@@ -250,6 +341,149 @@ void GDriveJob::shareLink(const QString &root, const QString &path)
     connect(reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(slotError(QNetworkReply::NetworkError)));
 }
 
+
+void GDriveJob::slotSendDataFinished(QNetworkReply *reply)
+{
+    const QString data = QString::fromUtf8(reply->readAll());
+    reply->deleteLater();
+    if (mError) {
+        qDebug()<<" error type "<<data;
+        QJson::Parser parser;
+        bool ok;
+
+        QMap<QString, QVariant> error = parser.parse(data.toUtf8(), &ok).toMap();
+        qDebug()<<" error "<<error;
+        if (error.contains(QLatin1String("message"))) {
+            const QString errorStr = error.value(QLatin1String("message")).toString();
+            switch(mActionType) {
+            case PimCommon::StorageServiceAbstract::NoneAction:
+                deleteLater();
+                break;
+            case PimCommon::StorageServiceAbstract::RequestToken:
+                Q_EMIT authorizationFailed(errorStr);
+                deleteLater();
+                break;
+            case PimCommon::StorageServiceAbstract::AccessToken:
+                Q_EMIT authorizationFailed(errorStr);
+                deleteLater();
+                break;
+            case PimCommon::StorageServiceAbstract::UploadFile:
+                Q_EMIT uploadFileFailed(errorStr);
+                errorMessage(mActionType, errorStr);
+                deleteLater();
+                break;
+            case PimCommon::StorageServiceAbstract::DownLoadFile:
+                Q_EMIT downLoadFileFailed(errorStr);
+                errorMessage(mActionType, errorStr);
+                deleteLater();
+                break;
+            case PimCommon::StorageServiceAbstract::DeleteFile:
+            case PimCommon::StorageServiceAbstract::CreateFolder:
+            case PimCommon::StorageServiceAbstract::AccountInfo:
+            case PimCommon::StorageServiceAbstract::ListFolder:
+            case PimCommon::StorageServiceAbstract::CreateServiceFolder:
+            case PimCommon::StorageServiceAbstract::DeleteFolder:
+            case PimCommon::StorageServiceAbstract::RenameFolder:
+            case PimCommon::StorageServiceAbstract::RenameFile:
+            case PimCommon::StorageServiceAbstract::MoveFolder:
+            case PimCommon::StorageServiceAbstract::MoveFile:
+            case PimCommon::StorageServiceAbstract::CopyFile:
+            case PimCommon::StorageServiceAbstract::CopyFolder:
+            case PimCommon::StorageServiceAbstract::ShareLink:
+                errorMessage(mActionType, errorStr);
+                deleteLater();
+                break;
+            }
+        } else {
+            if (!mErrorMsg.isEmpty()) {
+                errorMessage(mActionType, mErrorMsg);
+            } else {
+                errorMessage(mActionType, i18n("Unknown Error \"%1\"", data));
+            }
+            deleteLater();
+        }
+        return;
+    }
+    qDebug()<<" data: "<<data;
+    switch(mActionType) {
+    case PimCommon::StorageServiceAbstract::NoneAction:
+        deleteLater();
+        break;
+    case PimCommon::StorageServiceAbstract::RequestToken:
+        deleteLater();
+        break;
+    case PimCommon::StorageServiceAbstract::AccessToken:
+        parseAccessToken(data);
+        break;
+    case PimCommon::StorageServiceAbstract::UploadFile:
+        parseUploadFile(data);
+        break;
+    case PimCommon::StorageServiceAbstract::CreateFolder:
+        parseCreateFolder(data);
+        break;
+    case PimCommon::StorageServiceAbstract::AccountInfo:
+        parseAccountInfo(data);
+        break;
+    case PimCommon::StorageServiceAbstract::ListFolder:
+        parseListFolder(data);
+        break;
+    case PimCommon::StorageServiceAbstract::CreateServiceFolder:
+        parseCreateServiceFolder(data);
+        break;
+    case PimCommon::StorageServiceAbstract::DeleteFile:
+        parseDeleteFile(data);
+        break;
+    case PimCommon::StorageServiceAbstract::DeleteFolder:
+        parseDeleteFolder(data);
+        break;
+    case PimCommon::StorageServiceAbstract::CopyFile:
+        parseCopyFile(data);
+        break;
+    case PimCommon::StorageServiceAbstract::CopyFolder:
+        parseCopyFolder(data);
+        break;
+    case PimCommon::StorageServiceAbstract::RenameFile:
+        parseRenameFile(data);
+        break;
+    case PimCommon::StorageServiceAbstract::RenameFolder:
+        parseRenameFolder(data);
+        break;
+    case PimCommon::StorageServiceAbstract::MoveFolder:
+        parseMoveFolder(data);
+        break;
+    case PimCommon::StorageServiceAbstract::MoveFile:
+        parseMoveFile(data);
+        break;
+    case PimCommon::StorageServiceAbstract::ShareLink:
+        parseShareLink(data);
+        break;
+    case PimCommon::StorageServiceAbstract::DownLoadFile:
+        Q_EMIT actionFailed(QLatin1String("Not Implemented"));
+        deleteLater();
+        break;
+    }
+}
+
+void GDriveJob::parseRenameFile(const QString &data)
+{
+    Q_EMIT renameFileDone(QString());
+}
+
+void GDriveJob::parseRenameFolder(const QString &data)
+{
+    Q_EMIT renameFolderDone(QString());
+}
+
+void GDriveJob::parseCopyFile(const QString &data)
+{
+    Q_EMIT copyFileDone(QString());
+}
+
+void GDriveJob::parseCopyFolder(const QString &data)
+{
+    Q_EMIT copyFolderDone(QString());
+}
+
 void GDriveJob::parseDeleteFolder(const QString &data)
 {
     QJson::Parser parser;
@@ -287,6 +521,32 @@ void GDriveJob::parseListFolder(const QString &data)
     deleteLater();
 }
 
+void GDriveJob::parseMoveFolder(const QString &data)
+{
+    Q_EMIT moveFolderDone(data);
+    deleteLater();
+}
+
+void GDriveJob::parseMoveFile(const QString &data)
+{
+    Q_EMIT moveFileDone(data);
+    deleteLater();
+}
+
+void GDriveJob::parseShareLink(const QString &data)
+{
+    //TODO reimplement in derivated function
+    Q_EMIT actionFailed(QLatin1String("Not Implemented"));
+    deleteLater();
+}
+
+void GDriveJob::parseAccountInfo(const QString &)
+{
+    //TODO reimplement in derivated function
+    Q_EMIT actionFailed(QLatin1String("Not Implemented"));
+    deleteLater();
+}
+
 void GDriveJob::parseCreateFolder(const QString &data)
 {
     //TODO
@@ -301,4 +561,46 @@ void GDriveJob::parseUploadFile(const QString &data)
     qDebug()<<" data "<<data;
     Q_EMIT uploadFileDone(QString());
     deleteLater();
+}
+
+
+void GDriveJob::parseAccessToken(const QString &data)
+{
+    QJson::Parser parser;
+    bool ok;
+
+    const QMap<QString, QVariant> info = parser.parse(data.toUtf8(), &ok).toMap();
+    qDebug()<<" info"<<info;
+    if (info.contains(QLatin1String("refresh_token"))) {
+        mRefreshToken = info.value(QLatin1String("refresh_token")).toString();
+    }
+    if (info.contains(QLatin1String("access_token"))) {
+        mToken = info.value(QLatin1String("access_token")).toString();
+    }
+    if (info.contains(QLatin1String("expires_in"))) {
+        mExpireInTime = info.value(QLatin1String("expires_in")).toLongLong();
+    }
+    qDebug()<<" parseAccessToken";
+    Q_EMIT authorizationDone(mRefreshToken, mToken, mExpireInTime);
+    //TODO save it.
+    deleteLater();
+}
+
+
+void GDriveJob::refreshToken()
+{
+    mActionType = PimCommon::StorageServiceAbstract::AccessToken;
+    QNetworkRequest request(QUrl(mServiceUrl + QLatin1String("/oauth/token")));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, QLatin1String("application/x-www-form-urlencoded"));
+
+    QUrl postData;
+
+    postData.addQueryItem(QLatin1String("refresh_token"), mRefreshToken);
+    postData.addQueryItem(QLatin1String("grant_type"), QLatin1String("refresh_token"));
+    postData.addQueryItem(QLatin1String("client_id"), mClientId);
+    postData.addQueryItem(QLatin1String("client_secret"), mClientSecret);
+    qDebug()<<"refreshToken postData: "<<postData;
+
+    QNetworkReply *reply = mNetworkAccessManager->post(request, postData.encodedQuery());
+    connect(reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(slotError(QNetworkReply::NetworkError)));
 }
