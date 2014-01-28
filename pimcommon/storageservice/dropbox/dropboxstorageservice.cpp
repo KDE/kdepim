c09545c32fc43d1d580c9d8480d43df3f0ae137a
Montel Laurent <montel@kde.org>
2014-01-28 13:12:04 +0100

    Fix display folder/file

--- pimcommon/storageservice/dropbox/dropboxstorageservice.cpp
+++ pimcommon/storageservice/dropbox/dropboxstorageservice.cpp
@@ -380,6 +380,9 @@ void DropBoxStorageService::storageServiceCopyFolder(const QString &source, cons
 
 QString DropBoxStorageService::fileIdentifier(const QVariantMap &variantMap)
 {
+    if (variantMap.contains(QLatin1String("path"))) {
+        return variantMap.value(QLatin1String("path")).toString();
+    }
     return QString();
 }
 
@@ -429,15 +432,19 @@ QString DropBoxStorageService::fillListWidget(StorageServiceTreeWidget *listWidg
             if (qwer.contains(QLatin1String("is_dir"))) {
                 const bool isDir = qwer.value(QLatin1String("is_dir")).toBool();
                 const QString name = qwer.value(QLatin1String("path")).toString();
+
+                const QString itemName = name.right((name.length() - name.lastIndexOf(QLatin1Char('/'))) - 1);
+
+
                 StorageServiceTreeWidgetItem *item;
                 if (isDir) {
-                    item = listWidget->addFolder(name, name);
+                    item = listWidget->addFolder(itemName, name);
                 } else {
                     QString mimetype;
                     if (qwer.contains(QLatin1String("mime_type"))) {
                         mimetype = qwer.value(QLatin1String("mime_type")).toString();
                     }
-                    item = listWidget->addFile(name, name, mimetype);
+                    item = listWidget->addFile(itemName, name, mimetype);
                     if (qwer.contains(QLatin1String("bytes"))) {
                         item->setSize(qwer.value(QLatin1String("bytes")).toULongLong());
                     }
