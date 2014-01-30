a7fb58455b70a668c4f3e44988a3269984e9aad9
Montel Laurent <montel@kde.org>
2014-01-30 13:35:42 +0100

    Fix focus

--- pimcommon/storageservice/authdialog/logindialog.cpp
+++ pimcommon/storageservice/authdialog/logindialog.cpp
@@ -52,7 +52,6 @@ LoginDialog::LoginDialog(QWidget *parent)
     connect(mUsername, SIGNAL(textChanged(QString)), this, SLOT(slotUserNameChanged(QString)));
     enableButtonOk(false);
     resize(300,100);
-    mUsername->setFocus();
 }
 
 LoginDialog::~LoginDialog()
