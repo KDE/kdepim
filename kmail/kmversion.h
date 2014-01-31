49e39ef4160723a3a81e0af785e64cfed18f730d
Andre Heinecke <aheinecke@intevation.de>
2014-01-31 15:28:49 +0100

    GIT_SILENT: Update version numbers for today's release.

--- kmail/kmversion.h
+++ kmail/kmversion.h
@@ -3,6 +3,6 @@
 #ifndef kmversion_h
 #define kmversion_h
 
-#define KMAIL_VERSION "1.9.10 (enterprise35 0.20131030.0f75d05)"
+#define KMAIL_VERSION "1.9.10 (enterprise35 0.20140131.7b9655b)"
 
 #endif /*kmversion_h*/
--- korganizer/version.h
+++ korganizer/version.h
@@ -53,6 +53,6 @@
     3.2 alpha1
 */
 
-static const char korgVersion[] = "3.5.9 (enterprise35 0.20131030.0f75d05)";
+static const char korgVersion[] = "3.5.9 (enterprise35 0.20140131.7b9655b)";
 
 #endif
