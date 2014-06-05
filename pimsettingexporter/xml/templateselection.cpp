89dc91f111c31f1e0d2ff4f7deaf44821e1570e1
Montel Laurent <montel@kde.org>
2014-06-05 09:24:44 +0200

    Make it compile

--- pimsettingexporter/xml/templateselection.cpp
+++ pimsettingexporter/xml/templateselection.cpp
@@ -102,8 +102,6 @@ QHash<Utils::AppsType, Utils::StoredTypes> TemplateSelection::loadTemplate(const
                 type = Utils::Akregator;
             else if (tagName == QLatin1String("blogilo"))
                 type = Utils::Blogilo;
-            else if (tagName == QLatin1String("knode"))
-                type = Utils::KNode;
             if (type != Utils::Unknown) {
                 Utils::StoredTypes storedType = loadStoredTypes(e);
                 if (storedType != Utils::None)
