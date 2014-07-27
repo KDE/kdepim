b54b78a6c45f15e8e1b358a8fe427cc9e1f869f1
Sandro Knauß <knauss@kolabsys.com>
2014-07-27 11:04:15 +0200

    individual mail dialog for incidenceeditor.
    REVIEW: 119462
    
    Conflicts:
    	incidenceeditor-ng/CMakeLists.txt
    	incidenceeditor-ng/autotests/CMakeLists.txt
    
    (cherry picked from commit 1ecf3e0e1737967a34c0b68d47ee6212e1ece90e)

---
+++ incidenceeditor-ng/autotests/testindividualmaildialog.cpp
@@ -0,0 +1,70 @@
+/*
+Copyright (c) 2014 Sandro Knauß <knauss@kolabsys.com>
+
+This library is free software; you can redistribute it and/or modify it
+under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this library; see the file COPYING.LIB.  If not, write to the
+Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+ */
+
+#include "individualmaildialog.h"
+
+#include <QObject>
+#include <qtest.h>
+
+using namespace IncidenceEditorNG;
+
+class TestIndividualMailDialog : public QObject
+{
+    Q_OBJECT
+private slots:
+    void testDialog() {
+        KCalCore::Attendee::List attendees;
+        KGuiItem buttonYes = KGuiItem(QLatin1String("Send Email"));
+        KGuiItem buttonNo = KGuiItem(QLatin1String("Do not send"));
+
+        KCalCore::Attendee::Ptr attendee1(new KCalCore::Attendee(QLatin1String("test1"), QLatin1String("test1@example.com")));
+        KCalCore::Attendee::Ptr attendee2(new KCalCore::Attendee(QLatin1String("test2"), QLatin1String("test2@example.com")));
+        KCalCore::Attendee::Ptr attendee3(new KCalCore::Attendee(QLatin1String("test3"), QLatin1String("test3@example.com")));
+
+        attendees << attendee1 << attendee2 << attendee3;
+
+        IndividualMailDialog dialog(QLatin1String("title"), attendees, buttonYes, buttonNo, 0);
+
+        QCOMPARE(dialog.editAttendees().count(), 0);
+        QCOMPARE(dialog.updateAttendees().count(), 3);
+
+        // Just make sure, that the QCombobox is sorted like we think
+        QComboBox *first = dialog.mAttendeeDecision[attendees[0]];
+        QCOMPARE((IndividualMailDialog::Decisions)first->itemData(0, Qt::UserRole).toInt(), IndividualMailDialog::Update);
+        QCOMPARE((IndividualMailDialog::Decisions)first->itemData(1, Qt::UserRole).toInt(), IndividualMailDialog::NoUpdate);
+        QCOMPARE((IndividualMailDialog::Decisions)first->itemData(2, Qt::UserRole).toInt(), IndividualMailDialog::Edit);
+
+        // No update for first attendee, other default
+        first->setCurrentIndex(1);
+        QCOMPARE(dialog.editAttendees().count(), 0);
+        QCOMPARE(dialog.updateAttendees().count(), 2);
+        QVERIFY(dialog.updateAttendees().contains(attendee2));
+        QVERIFY(dialog.updateAttendees().contains(attendee3));
+
+        // edit for frist attende, other default
+        first->setCurrentIndex(2);
+        QCOMPARE(dialog.editAttendees().count(), 1);
+        QCOMPARE(dialog.updateAttendees().count(), 2);
+        QCOMPARE(dialog.editAttendees()[0], attendee1);
+   }
+ };
+
+QTEST_MAIN( TestIndividualMailDialog )
+
+#include "testindividualmaildialog.moc"
---
+++ incidenceeditor-ng/individualmaildialog.cpp
@@ -0,0 +1,92 @@
+/*
+  Copyright (c) 2014 Sandro Knauß <knauss@kolabsys.com>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+#include "individualmaildialog.h"
+
+#include <KLocalizedString>
+
+#include <QGridLayout>
+#include <QComboBox>
+#include <QLabel>
+
+using namespace IncidenceEditorNG;
+
+IndividualMailDialog::IndividualMailDialog(const QString &question, const KCalCore::Attendee::List  &attendees,
+                                           const KGuiItem &buttonYes, const KGuiItem &buttonNo, QWidget *parent)
+    : KDialog(parent)
+{
+    setCaption(i18nc("@title:window", "Group Scheduling Email"));
+    setButtons(KDialog::Yes | KDialog::No | KDialog::Details);
+    setButtonText(KDialog::Details, i18nc("@action:button show list of attendees", "Individual mailsettings"));
+    setButtonGuiItem(KDialog::Yes, buttonYes);
+    setButtonGuiItem(KDialog::No, buttonNo);
+
+    QWidget *widget = new QWidget();
+    QGridLayout *layout = new QGridLayout(widget);
+    int row = 0;
+    foreach (const KCalCore::Attendee::Ptr & attendee, attendees) {
+        QComboBox *options = new QComboBox();
+        options->addItem(i18nc("@item:inlistbox ITIP Messages for one attendee", "Send update"), QVariant(Update));
+        options->addItem(i18nc("@item:inlistbox ITIP Messages for one attendee", "Send no update"), QVariant(NoUpdate));
+        options->addItem(i18nc("@item:inlistbox ITIP Messages for one attendee", "Edit mail"), QVariant(Edit));
+        mAttendeeDecision[attendee] = options;
+
+        layout->addWidget(new QLabel(attendee->fullName()), row, 0);
+        layout->addWidget(options, row, 1);
+        ++row;
+    }
+    widget->sizePolicy().setHorizontalStretch(1);
+    widget->sizePolicy().setVerticalStretch(1);
+
+    QWidget *mW = new QLabel(question);
+
+    setMainWidget(mW);
+    setDetailsWidget(widget);
+}
+
+IndividualMailDialog::~IndividualMailDialog()
+{
+
+}
+
+KCalCore::Attendee::List IndividualMailDialog::editAttendees() const
+{
+    KCalCore::Attendee::List edit;
+    QList<KCalCore::Attendee::Ptr> attendees = mAttendeeDecision.keys();
+    foreach (const KCalCore::Attendee::Ptr & attendee, attendees) {
+        int index = mAttendeeDecision[attendee]->currentIndex();
+        if (mAttendeeDecision[attendee]->itemData(index, Qt::UserRole) == Edit) {
+            edit.append(attendee);
+        }
+    }
+    return edit;
+}
+
+KCalCore::Attendee::List IndividualMailDialog::updateAttendees() const
+{
+    KCalCore::Attendee::List update;
+    QList<KCalCore::Attendee::Ptr> attendees = mAttendeeDecision.keys();
+    foreach (const KCalCore::Attendee::Ptr & attendee, attendees) {
+        int index = mAttendeeDecision[attendee]->currentIndex();
+        if (mAttendeeDecision[attendee]->itemData(index, Qt::UserRole) == Update) {
+            update.append(attendee);
+        }
+    }
+    return update;
+}
