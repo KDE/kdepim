1a9b5a9d0532fd76775f1acba516f0f98fc238f7
Albert Astals Cid <aacid@kde.org>
2014-08-17 22:46:31 +0200

    Fix find
    CCMAIL: knauss@kolabsys.com

--- incidenceeditor-ng/Messages.sh
+++ incidenceeditor-ng/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.ui -o -name \*.kcfg`  >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" -name "*.h"`  -o $podir/libincidenceeditors.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h"`  -o $podir/libincidenceeditors.pot
 rm -f rc.cpp
