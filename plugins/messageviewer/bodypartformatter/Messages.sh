9b2ce1b402b9744f27f7a61b30448a389564dddc
Montel Laurent <montel@kde.org>
2014-08-18 14:00:13 +0200

    Extract from updatecontactjob.cpp too

--- plugins/messageviewer/bodypartformatter/Messages.sh
+++ plugins/messageviewer/bodypartformatter/Messages.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT text_vcard.cpp rc.cpp -o $podir/messageviewer_text_vcard_plugin.pot
+$XGETTEXT text_vcard.cpp updatecontactjob.cpp rc.cpp -o $podir/messageviewer_text_vcard_plugin.pot
 $XGETTEXT text_calendar.cpp attendeeselector.cpp delegateselector.cpp rc.cpp -o $podir/messageviewer_text_calendar_plugin.pot
 $XGETTEXT text_xdiff.cpp rc.cpp -o $podir/messageviewer_text_xdiff_plugin.pot
 $XGETTEXT application_ms-tnef.cpp -o $podir/messageviewer_application_mstnef_plugin.pot
