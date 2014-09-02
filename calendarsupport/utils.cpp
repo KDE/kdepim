8417d9af6a38a586749f0399c9145ac2057c8670
Sandro Knauß <knauss@kolabsys.com>
2014-09-02 16:34:41 +0200

    CalendarSupport: show displayName if available for collections

--- calendarsupport/utils.cpp
+++ calendarsupport/utils.cpp
@@ -622,7 +622,9 @@ QString CalendarSupport::displayName( Akonadi::ETMCalendar *calendar, const Akon
   const QString dName = fullCollection.displayName();
 
   if ( !dName.isEmpty() ) {
-    return fullCollection.name().startsWith( QLatin1String( "akonadi_" ) ) ? i18n( "My %1", dName ) : fullCollection.name();
+    return fullCollection.name().startsWith( QLatin1String( "akonadi_" ) ) ? i18n( "My %1", dName ) : dName;
+  } else if ( !fullCollection.name().isEmpty() ) {
+    return fullCollection.name();
   } else {
     return i18nc( "unknown resource", "Unknown" );
   }
