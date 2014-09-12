a0a048675913db34be248d3402b8a5f8a50ce244
Montel Laurent <montel@kde.org>
2014-09-12 15:25:57 +0200

    Fix extract messages

--- kleopatra/Messages.sh
+++ kleopatra/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kleopatra.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/' | grep -v '/kwatchgnupg/'` -o $podir/kleopatra.pot
 $XGETTEXT -ktranslate `find . -name '*.qml'` -j -L Java -o $podir/kleopatra.pot
