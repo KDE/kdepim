f5ce0a278065ffb62e56a0593e8f3b716da00d76
Montel Laurent <montel@kde.org>
2014-09-21 18:42:11 +0200

    Start to create config widget

---
+++ pimcommon/texttospeech/autotests/texttospeechconfigwidgettest.h
@@ -0,0 +1,35 @@
+/*
+  Copyright (c) 2014 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef TEXTTOSPEECHCONFIGWIDGETTEST_H
+#define TEXTTOSPEECHCONFIGWIDGETTEST_H
+
+#include <QObject>
+
+class TextToSpeechConfigWidgetTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TextToSpeechConfigWidgetTest(QObject *parent=0);
+    ~TextToSpeechConfigWidgetTest();
+
+private Q_SLOTS:
+    void shouldHaveDefaultValue();
+};
+
+#endif // TEXTTOSPEECHCONFIGWIDGETTEST_H
