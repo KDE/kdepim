421e254924c0bd4f60571fc57ecc133664751b6b
Montel Laurent <montel@kde.org>
2014-10-01 13:52:51 +0200

    Allow to wrap text

--- messageviewer/widgets/todoedit.cpp
+++ messageviewer/widgets/todoedit.cpp
@@ -53,6 +53,7 @@ TodoEdit::TodoEdit(QWidget *parent)
     mMsgWidget->setCloseButtonVisible(true);
     mMsgWidget->setMessageType(KMessageWidget::Positive);
     mMsgWidget->setObjectName(QLatin1String("msgwidget"));
+    mMsgWidget->setWordWrap(true);
     mMsgWidget->setVisible(false);
     vbox->addWidget(mMsgWidget);
 
