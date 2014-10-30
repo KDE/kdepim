33f6f4afc44198f3728277db2fcba9fc257afb5e
Montel Laurent <montel@kde.org>
2014-10-30 07:57:56 +0100

    Add test apps for config dialog

---
+++ pimcommon/texttospeech/tests/texttospeechconfigdialoggui.cpp
@@ -0,0 +1,48 @@
+/*
+  Copyright (c) 2014 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "pimcommon/texttospeech/texttospeechconfigdialog.h"
+#include "pimcommon/texttospeech/texttospeech.h"
+#include <KLocalizedString>
+#include <QApplication>
+#include <KAboutData>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QCommandLineParser>
+#include <QMainWindow>
+#include <QDebug>
+
+int main(int argc, char **argv)
+{
+    KAboutData aboutData(QLatin1String("texttospeechconfigdialog_gui"), i18n("texttospeechconfigdialog_Gui"), QLatin1String("1.0"));
+    aboutData.setShortDescription(i18n("Test for config dialog text to speech"));
+    QApplication app(argc, argv);
+    QCommandLineParser parser;
+    KAboutData::setApplicationData(aboutData);
+    parser.addVersionOption();
+    parser.addHelpOption();
+    aboutData.setupCommandLine(&parser);
+    parser.process(app);
+    aboutData.processCommandLine(&parser);
+
+    PimCommon::TextToSpeechConfigDialog *w = new PimCommon::TextToSpeechConfigDialog;
+
+    w->show();
+    app.exec();
+    delete w;
+    return 0;
+}
