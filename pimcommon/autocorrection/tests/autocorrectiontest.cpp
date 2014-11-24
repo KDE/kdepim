a1ca3102b2dea4c3d9f3ed09f75fac29e4551404
Montel Laurent <montel@kde.org>
2014-11-24 16:25:36 +0100

    Rename function. Test change format

--- pimcommon/autocorrection/tests/autocorrectiontest.cpp
+++ pimcommon/autocorrection/tests/autocorrectiontest.cpp
@@ -230,7 +230,7 @@ void AutoCorrectionTest::shouldNotReplaceUppercaseLetter()
     QCOMPARE(doc.toPlainText(), text);
 }
 
-void AutoCorrectionTest::shouldReplaceToBold()
+void AutoCorrectionTest::shouldReplaceToTextFormat()
 {
     PimCommon::AutoCorrection autocorrection;
     autocorrection.setEnabledAutoCorrection(true);
@@ -255,12 +255,24 @@ void AutoCorrectionTest::shouldReplaceToBold()
     position = text.length();
     autocorrection.autocorrect(true, doc, position);
     QCOMPARE(doc.toPlainText(), result);
+    QTextCursor cursor(&doc);
+    cursor.setPosition(2);
+    QTextCharFormat charFormat = cursor.charFormat();
+    QCOMPARE(charFormat.font().underline(), true);
+    QCOMPARE(charFormat.font().bold(), false);
+    QCOMPARE(charFormat.font().strikeOut(), false);
 
     text = QLatin1String("-foo-");
     doc.setHtml(text);
     position = text.length();
     autocorrection.autocorrect(true, doc, position);
     QCOMPARE(doc.toPlainText(), result);
+    cursor = QTextCursor(&doc);
+    cursor.setPosition(2);
+    charFormat = cursor.charFormat();
+    QCOMPARE(charFormat.font().underline(), false);
+    QCOMPARE(charFormat.font().bold(), false);
+    QCOMPARE(charFormat.font().strikeOut(), true);
 
     //Don't convert it.
     text = QLatin1String("-foo1");
