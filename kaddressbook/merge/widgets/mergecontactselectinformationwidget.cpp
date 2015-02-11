e2a859fe07b44616db05c2ec7f705b95ea19e3a9
Montel Laurent <montel@kde.org>
2015-02-11 21:59:22 +0100

    Move to own directory

--- kaddressbook/merge/widgets/mergecontactselectinformationwidget.cpp
+++ kaddressbook/merge/widgets/mergecontactselectinformationwidget.cpp
@@ -16,7 +16,7 @@
 */
 
 #include "mergecontactselectinformationwidget.h"
-#include "mergecontactselectlistwidget.h"
+#include "merge/widgets/mergecontactselectlistwidget.h"
 #include <KLocalizedString>
 #include <QVBoxLayout>
 #include <QLabel>
