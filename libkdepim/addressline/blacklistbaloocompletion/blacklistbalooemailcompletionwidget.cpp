12cdf915191596af179838e3a74e4b18f05ecf7d
Montel Laurent <montel@kde.org>
2015-02-12 08:38:58 +0100

    Add text

--- libkdepim/addressline/blacklistbaloocompletion/blacklistbalooemailcompletionwidget.cpp
+++ libkdepim/addressline/blacklistbaloocompletion/blacklistbalooemailcompletionwidget.cpp
@@ -45,6 +45,8 @@ BlackListBalooEmailCompletionWidget::BlackListBalooEmailCompletionWidget(QWidget
     searchLayout->addWidget(lab);
 
     mSearchLineEdit = new KLineEdit;
+    // KF5 add i18n
+    mSearchLineEdit->setClickMessage(QLatin1String("Research is done from 3 characters"));
     mSearchLineEdit->setFocus();
     mSearchLineEdit->setClearButtonShown(true);
     mSearchLineEdit->setTrapReturnKey(true);
