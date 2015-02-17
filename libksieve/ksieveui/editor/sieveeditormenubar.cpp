8e28ceecc3dd193a6ae2cad3a99077d1838a7966
Montel Laurent <montel@kde.org>
2015-02-17 22:58:51 +0100

    We need a menubar here

---
+++ libksieve/ksieveui/editor/sieveeditormenubar.cpp
@@ -0,0 +1,99 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "sieveeditormenubar.h"
+#include "sieveeditortextmodewidget.h"
+#include <KStandardAction>
+#include <KLocalizedString>
+using namespace KSieveUi;
+
+SieveEditorMenuBar::SieveEditorMenuBar(QWidget *parent)
+    : QMenuBar(parent)
+{
+
+}
+
+SieveEditorMenuBar::~SieveEditorMenuBar()
+{
+
+}
+
+void SieveEditorMenuBar::initActions()
+{
+#if 0
+    mGoToLine = ac->addAction(QLatin1String("gotoline"), mMainWidget->sieveEditorMainWidget(), SLOT(slotGoToLine()));
+    mGoToLine->setText(i18n("Go to Line"));
+    mGoToLine->setIcon(KIcon(QLatin1String("go-jump")));
+    mGoToLine->setShortcut(QKeySequence( Qt::CTRL + Qt::Key_G ));
+    mGoToLine->setEnabled(false);
+
+    mFindAction = KStandardAction::find(this, SIGNAL(find()), this);
+    mReplaceAction = KStandardAction::replace(this, SIGNAL(replace()), this);
+    mUndoAction = KStandardAction::undo(this, SIGNAL(undo()), this);
+    mRedoAction = KStandardAction::redo(this, SIGNAL(redo()), this);
+    mCopyAction = KStandardAction::copy(this, SIGNAL(copy()), this);
+    mPasteAction = KStandardAction::paste(this, SIGNAL(paste()), this);
+    mCutAction = KStandardAction::cut(this, SIGNAL(cut()), this);
+    mSelectAllAction = KStandardAction::selectAll(this, SIGNAL(selectAll()), this);
+#endif
+}
+KAction *SieveEditorMenuBar::selectAllAction() const
+{
+    return mSelectAllAction;
+}
+
+KAction *SieveEditorMenuBar::cutAction() const
+{
+    return mCutAction;
+}
+
+KAction *SieveEditorMenuBar::pasteAction() const
+{
+    return mPasteAction;
+}
+
+KAction *SieveEditorMenuBar::copyAction() const
+{
+    return mCopyAction;
+}
+
+KAction *SieveEditorMenuBar::redoAction() const
+{
+    return mRedoAction;
+}
+
+KAction *SieveEditorMenuBar::undoAction() const
+{
+    return mUndoAction;
+}
+
+KAction *SieveEditorMenuBar::replaceAction() const
+{
+    return mReplaceAction;
+}
+
+KAction *SieveEditorMenuBar::findAction() const
+{
+    return mFindAction;
+}
+
+KAction *SieveEditorMenuBar::goToLine() const
+{
+    return mGoToLine;
+}
+
---
+++ libksieve/ksieveui/editor/sieveeditormenubar.h
@@ -0,0 +1,66 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef SIEVEEDITORMENUBAR_H
+#define SIEVEEDITORMENUBAR_H
+
+#include <QMenuBar>
+class KAction;
+namespace KSieveUi {
+class SieveEditorMenuBar : public QMenuBar
+{
+    Q_OBJECT
+public:
+    explicit SieveEditorMenuBar(QWidget *parent = 0);
+    ~SieveEditorMenuBar();
+
+    KAction *goToLine() const;
+    KAction *findAction() const;
+    KAction *replaceAction() const;
+    KAction *undoAction() const;
+    KAction *redoAction() const;
+    KAction *copyAction() const;
+    KAction *pasteAction() const;
+    KAction *cutAction() const;
+    KAction *selectAllAction() const;
+
+Q_SIGNALS:
+    void gotoLine();
+    void find();
+    void replace();
+    void undo();
+    void redo();
+    void copy();
+    void paste();
+    void cut();
+    void selectAll();
+
+private:
+    void initActions();
+    KAction *mGoToLine;
+    KAction *mFindAction;
+    KAction *mReplaceAction;
+    KAction *mUndoAction;
+    KAction *mRedoAction;
+    KAction *mCopyAction;
+    KAction *mPasteAction;
+    KAction *mCutAction;
+    KAction *mSelectAllAction;
+};
+}
+
+#endif // SIEVEEDITORMENUBAR_H
