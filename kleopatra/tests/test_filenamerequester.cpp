8e36eeec070090c65d5148f7beb92813a32bf897
Montel Laurent <montel@kde.org>
2015-02-20 08:45:27 +0100

    Remove not necessary

--- kleopatra/tests/test_filenamerequester.cpp
+++ kleopatra/tests/test_filenamerequester.cpp
@@ -35,11 +35,9 @@
 #include "../utils/filenamerequester.h"
 
 #include <QApplication>
-#include <KComponentData>
 
 int main(int argc, char *argv[])
 {
-    KComponentData cd("test_filenamerequester");
     QApplication app(argc, argv);
 
     Kleo::FileNameRequester requester;
