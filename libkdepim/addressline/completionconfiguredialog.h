ced801ea3d78d7792f93839f19515454e0f332b4
Montel Laurent <montel@kde.org>
2015-02-26 08:30:48 +0100

    Create a completionconfiguredialog which has all widget to configure it

---
+++ libkdepim/addressline/completionconfiguredialog.h
@@ -0,0 +1,35 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+
+*/
+
+#ifndef COMPLETIONCONFIGUREDIALOG_H
+#define COMPLETIONCONFIGUREDIALOG_H
+
+#include <KDialog>
+namespace KPIM {
+class CompletionConfigureDialog : public KDialog
+{
+    Q_OBJECT
+public:
+    explicit CompletionConfigureDialog(QWidget *parent=0);
+    ~CompletionConfigureDialog();
+};
+}
+
+#endif // COMPLETIONCONFIGUREDIALOG_H
