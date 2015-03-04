716ad5a08c391cb871ccbbe560905e5e4f4c7d14
Montel Laurent <montel@kde.org>
2015-03-04 20:34:07 +0100

    Add more unittest

---
+++ sieveeditor/autotests/sieveeditorconfigureserverpagetest.cpp
@@ -0,0 +1,51 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "sieveeditorconfigureserverpagetest.h"
+#include "../sieveeditorconfigureserverpage.h"
+#include <QPushButton>
+#include <qtest_kde.h>
+#include <QSignalSpy>
+#include <qtestmouse.h>
+SieveEditorConfigureServerPageTest::SieveEditorConfigureServerPageTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+SieveEditorConfigureServerPageTest::~SieveEditorConfigureServerPageTest()
+{
+
+}
+
+void SieveEditorConfigureServerPageTest::shouldHaveDefaultValue()
+{
+    SieveEditorConfigureServerPage w;
+    QPushButton *button = qFindChild<QPushButton*>(&w, QLatin1String("configure_button"));
+    QVERIFY(button);
+}
+
+void SieveEditorConfigureServerPageTest::shouldEmitSignalWhenClickOnButton()
+{
+    SieveEditorConfigureServerPage w;
+    QPushButton *button = qFindChild<QPushButton*>(&w, QLatin1String("configure_button"));
+    QSignalSpy spy(&w, SIGNAL(configureClicked()));
+    QTest::mouseClick(button, Qt::LeftButton);
+    QCOMPARE(spy.count(), 1);
+}
+
+QTEST_KDEMAIN(SieveEditorConfigureServerPageTest, GUI)
---
+++ sieveeditor/autotests/sieveeditorconfigureserverpagetest.h
@@ -0,0 +1,35 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef SIEVEEDITORCONFIGURESERVERPAGETEST_H
+#define SIEVEEDITORCONFIGURESERVERPAGETEST_H
+
+#include <QObject>
+
+class SieveEditorConfigureServerPageTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SieveEditorConfigureServerPageTest(QObject *parent = 0);
+    ~SieveEditorConfigureServerPageTest();
+
+private Q_SLOTS:
+    void shouldEmitSignalWhenClickOnButton();
+    void shouldHaveDefaultValue();
+};
+
+#endif // SIEVEEDITORCONFIGURESERVERPAGETEST_H
