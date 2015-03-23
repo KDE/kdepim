fd6b29a0172c0aec3e86968166261eef8043ae5d
Montel Laurent <montel@kde.org>
2015-03-23 23:13:18 +0100

    Add more unittest

---
+++ mailcommon/filter/autotests/filteractionremoveheadertest.cpp
@@ -0,0 +1,48 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "filteractionremoveheadertest.h"
+#include "../filteractions/filteractionremoveheader.h"
+#include <qtest_kde.h>
+#include <widgets/minimumcombobox.h>
+
+FilterActionRemoveHeaderTest::FilterActionRemoveHeaderTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+FilterActionRemoveHeaderTest::~FilterActionRemoveHeaderTest()
+{
+
+}
+
+void FilterActionRemoveHeaderTest::shouldHaveDefaultValue()
+{
+    MailCommon::FilterActionRemoveHeader filter;
+    QWidget *w = filter.createParamWidget(0);
+    PimCommon::MinimumComboBox *comboBox = dynamic_cast<PimCommon::MinimumComboBox *>(w);
+    QVERIFY(comboBox);
+}
+
+void FilterActionRemoveHeaderTest::shouldHaveSieveRequires()
+{
+    MailCommon::FilterActionRemoveHeader filter;
+    QCOMPARE(filter.sieveRequires(), QStringList() << QLatin1String("editheader"));
+}
+
+QTEST_KDEMAIN(FilterActionRemoveHeaderTest, GUI)
