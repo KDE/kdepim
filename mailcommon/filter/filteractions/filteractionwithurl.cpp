38bdd1d500c7e98546d5551fc2d4dcc8c49b9599
Montel Laurent <montel@kde.org>
2015-03-23 13:22:21 +0100

    Fix crash reported by hussam from irc.
    Add unittest too.

--- mailcommon/filter/filteractions/filteractionwithurl.cpp
+++ mailcommon/filter/filteractions/filteractionwithurl.cpp
@@ -60,9 +60,11 @@ QWidget* FilterActionWithUrl::createParamWidget( QWidget *parent ) const
     QHBoxLayout *layout = new QHBoxLayout;
     widget->setLayout(layout);
     KUrlRequester *requester = new KUrlRequester( parent );
+    requester->setObjectName(QLatin1String("requester"));
     requester->setUrl( KUrl( mParameter ) );
     layout->addWidget(requester);
     mHelpButton = new FilterActionWithUrlHelpButton(parent);
+    mHelpButton->setObjectName(QLatin1String("helpbutton"));
     connect(mHelpButton, SIGNAL(clicked()), this, SLOT(slotHelp()));
     layout->addWidget(mHelpButton);
 
@@ -80,19 +82,27 @@ void FilterActionWithUrl::slotHelp()
 
 void FilterActionWithUrl::applyParamWidgetValue( QWidget *paramWidget )
 {
-    const KUrl url = static_cast<KUrlRequester*>( paramWidget )->url();
+    KUrlRequester *requester = paramWidget->findChild<KUrlRequester*>( QLatin1String("requester") );
+    Q_ASSERT( requester );
+
+    const KUrl url = requester->url();
 
     mParameter = (url.isLocalFile() ? url.toLocalFile() : url.path());
 }
 
 void FilterActionWithUrl::setParamWidgetValue( QWidget *paramWidget ) const
 {
-    static_cast<KUrlRequester*>( paramWidget )->setUrl( KUrl( mParameter ) );
+    KUrlRequester *requester = paramWidget->findChild<KUrlRequester*>( QLatin1String("requester") );
+    Q_ASSERT( requester );
+
+    requester->setUrl( KUrl( mParameter ) );
 }
 
 void FilterActionWithUrl::clearParamWidget( QWidget *paramWidget ) const
 {
-    static_cast<KUrlRequester*>( paramWidget )->clear();
+    KUrlRequester *requester = paramWidget->findChild<KUrlRequester*>( QLatin1String("requester") );
+    Q_ASSERT( requester );
+    requester->clear();
 }
 
 void FilterActionWithUrl::argsFromString( const QString &argsStr )
