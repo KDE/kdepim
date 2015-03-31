5059f9f3e37e3cfba635995989b7c1e4a638ae9a
Montel Laurent <montel@kde.org>
2015-03-31 23:01:30 +0200

    Add more unittest

--- mailcommon/filter/filteractions/filteractionreplyto.cpp
+++ mailcommon/filter/filteractions/filteractionreplyto.cpp
@@ -31,7 +31,6 @@ FilterAction* FilterActionReplyTo::newAction()
 FilterActionReplyTo::FilterActionReplyTo( QObject *parent )
     : FilterActionWithAddress( QLatin1String("set Reply-To"), i18n( "Set Reply-To To" ), parent )
 {
-    mParameter.clear();
 }
 
 FilterAction::ReturnCode FilterActionReplyTo::process(ItemContext &context , bool) const
