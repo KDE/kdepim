757b72f65fbfdbe669d58bc16582b3cc3bc7fb95
Montel Laurent <montel@kde.org>
2015-04-08 08:11:01 +0200

    Adapt to qtest

--- mailcommon/search/autotests/searchpatterntest.cpp
+++ mailcommon/search/autotests/searchpatterntest.cpp
@@ -17,7 +17,7 @@
 
 #include "searchpatterntest.h"
 #include "../searchpattern.h"
-#include <qtest_kde.h>
+#include <qtest.h>
 
 SearchPatternTest::SearchPatternTest(QObject *parent)
     : QObject(parent)
@@ -81,4 +81,4 @@ void SearchPatternTest::shouldRuleRequirePart()
     QCOMPARE( rule->requiredPart(), part );
 }
 
-QTEST_KDEMAIN(SearchPatternTest, NoGUI)
+QTEST_MAIN(SearchPatternTest)
