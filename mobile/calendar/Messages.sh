64edf73790a9984376b23f93e670faf719c50e51
Montel Laurent <montel@kde.org>
2015-04-11 19:34:58 +0200

    Remove mobile.

--- mobile/calendar/Messages.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-$EXTRACTRC --ignore-no-input `find . \( -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake' \) -and -not -name '*-mobile.rc'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h'` -o $podir/korganizer-mobile.pot
-$XGETTEXT -ktranslate `find . -name '*.qml'` -j -L Java -o $podir/korganizer-mobile.pot
-rm -f rc.cpp
--- mobile/contacts/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-$EXTRACTRC --ignore-no-input `find . \( -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake' \) -and -not -name '*-mobile.rc'` >> rc.cpp || exit 11
-$XGETTEXT -ktranslate `find -name '*.cpp' -o -name '*.h' -o -name '*.qml'` -o $podir/kaddressbook-mobile.pot
-rm -f rc.cpp
