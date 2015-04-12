13525a4f31c0d8ff9c036ad4a86ca08e9b576108
Montel Laurent <montel@kde.org>
2015-04-12 17:01:23 +0200

    Add more unittest

---
+++ mailcommon/search/autotests/searchrulestatustest.h
@@ -0,0 +1,17 @@
+#ifndef SEARCHRULESTATUSTEST_H
+#define SEARCHRULESTATUSTEST_H
+
+#include <QObject>
+
+class SearchRuleStatusTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit SearchRuleStatusTest(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // SEARCHRULESTATUSTEST_H
