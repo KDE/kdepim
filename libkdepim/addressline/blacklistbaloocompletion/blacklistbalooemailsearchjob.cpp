cba6c5a31cfa0daa746d04e6f23ebb1b76ae5f89
Dan Vrátil <dvratil@redhat.com>
2015-04-20 11:31:38 +0200

    Adapt to API changes in AkonadiSearch

--- libkdepim/addressline/blacklistbaloocompletion/blacklistbalooemailsearchjob.cpp
+++ libkdepim/addressline/blacklistbaloocompletion/blacklistbalooemailsearchjob.cpp
@@ -20,7 +20,7 @@
 
 #include "blacklistbalooemailsearchjob.h"
 
-#include <baloo/pim/contactcompleter.h>
+#include <Akonadi/Search/contactcompleter.h>
 #include <QStringList>
 using namespace KPIM;
 
@@ -44,7 +44,7 @@ bool BlackListBalooEmailSearchJob::start()
         return false;
     }
 
-    Baloo::PIM::ContactCompleter com(trimmedString, mLimit);
+    Akonadi::Search::ContactCompleter com(trimmedString, mLimit);
     Q_EMIT emailsFound(com.complete());
     deleteLater();
     return true;
--- messagelist/core/filter.cpp
+++ messagelist/core/filter.cpp
@@ -21,8 +21,8 @@
 #include "core/filter.h"
 #include "core/messageitem.h"
 
-#include <baloo/pim/emailquery.h>
-#include <baloo/pim/resultiterator.h>
+#include <Akonadi/Search/emailquery.h>
+#include <Akonadi/Search/resultiterator.h>
 
 using namespace MessageList::Core;
 
@@ -154,7 +154,7 @@ void Filter::setSearchString(const QString &search, QuickSearchLine::SearchOptio
         needToSplitString = true;
     }
     if (!newStr.trimmed().isEmpty()) {
-        Baloo::PIM::EmailQuery query;
+        Akonadi::Search::EmailQuery query;
         if (options & QuickSearchLine::SearchEveryWhere) {
             query.matches(newStr);
             query.setSplitSearchMatchString(needToSplitString);
@@ -175,7 +175,7 @@ void Filter::setSearchString(const QString &search, QuickSearchLine::SearchOptio
             query.addCollection(mCurrentFolder.id());
         }
 
-        Baloo::PIM::ResultIterator it = query.exec();
+        Akonadi::Search::ResultIterator it = query.exec();
         while (it.next()) {
             mMatchingItemIds << it.id();
         }
