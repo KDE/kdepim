8b05013a56fce3a6f20d306596db7436e0ea0cfc
Luigi Toscano <luigi.toscano@tiscali.it>
2015-04-22 00:51:15 +0200

    Fix spelling (vCard)

--- plugins/messageviewer/bodypartformatter/vcard/updatecontactjob.cpp
+++ plugins/messageviewer/bodypartformatter/vcard/updatecontactjob.cpp
@@ -44,7 +44,7 @@ public:
         const KContacts::Addressee::List contacts = searchJob->contacts();
 
         if (contacts.isEmpty()) {
-            const QString text = i18n( "The VCard's primary email address is not in addressbook." );
+            const QString text = i18n( "The vCard's primary email address is not in addressbook." );
             KMessageBox::information(mParentWidget, text);
             q->setError(UserDefinedError);
             q->emitResult();
