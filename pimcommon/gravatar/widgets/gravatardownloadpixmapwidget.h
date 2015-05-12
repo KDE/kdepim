8d45769374fd6e34220e505bdc9f908c9724ee67
Montel Laurent <montel@kde.org>
2015-05-12 07:58:10 +0200

    Add widget

---
+++ pimcommon/gravatar/widgets/gravatardownloadpixmapwidget.h
@@ -0,0 +1,35 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  based on code from Sune Vuorela <sune@vuorela.dk> (Rawatar source code)
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef GRAVATARDOWNLOADPIXMAPWIDGET_H
+#define GRAVATARDOWNLOADPIXMAPWIDGET_H
+
+#include <QWidget>
+#include "pimcommon_export.h"
+namespace PimCommon {
+class PIMCOMMON_EXPORT GravatarDownloadPixmapWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GravatarDownloadPixmapWidget(QWidget *parent = 0);
+    ~GravatarDownloadPixmapWidget();
+};
+}
+
+#endif // GRAVATARDOWNLOADPIXMAPWIDGET_H
