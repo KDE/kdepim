caaeaf4782a0143c8ddd6d4e2da847aad23d9a81
Montel Laurent <montel@kde.org>
2015-05-21 23:17:19 +0200

    kdelibs4support--

--- mailcommon/search/searchrule/searchrule.cpp
+++ mailcommon/search/searchrule/searchrule.cpp
@@ -23,10 +23,7 @@
 
 #include <KMime/KMimeMessage>
 
-#include <KDebug>
 #include <KConfigGroup>
-#include <KLocale>
-#include <KGlobal>
 
 #include <QDataStream>
 #include <QRegExp>
@@ -118,9 +115,9 @@ SearchRule::Ptr SearchRule::createInstanceFromConfig(const KConfigGroup &config,
 {
     const char cIdx = char(int('A') + aIdx);
 
-    static const QString &field = KGlobal::staticQString("field");
-    static const QString &func = KGlobal::staticQString("func");
-    static const QString &contents = KGlobal::staticQString("contents");
+    static const QString field = QStringLiteral("field");
+    static const QString func = QStringLiteral("func");
+    static const QString contents = QStringLiteral("contents");
 
     const QByteArray &field2 = config.readEntry(field + cIdx, QString()).toLatin1();
     Function func2 = configValueToFunc(config.readEntry(func + cIdx, QString()).toLatin1());
@@ -176,9 +173,9 @@ QString SearchRule::functionToString(Function function)
 void SearchRule::writeConfig(KConfigGroup &config, int aIdx) const
 {
     const char cIdx = char('A' + aIdx);
-    static const QString &field = KGlobal::staticQString("field");
-    static const QString &func = KGlobal::staticQString("func");
-    static const QString &contents = KGlobal::staticQString("contents");
+    static const QString field = QStringLiteral("field");
+    static const QString func = QStringLiteral("func");
+    static const QString contents = QStringLiteral("contents");
 
     config.writeEntry(field + cIdx, QString(mField));
     config.writeEntry(func + cIdx, functionToString(mFunction));
