9e2261ec80bb80a715d26e2bc8c02e66c56c2748
Montel Laurent <montel@kde.org>
2015-05-24 08:14:37 +0200

    Add missing Q_DECL_OVERRIde

--- accountwizard/transport.h
+++ accountwizard/transport.h
@@ -28,8 +28,8 @@ class Transport : public SetupObject
     Q_OBJECT
 public:
     explicit Transport(const QString &type, QObject *parent = Q_NULLPTR);
-    void create();
-    void destroy();
+    void create() Q_DECL_OVERRIDE;
+    void destroy() Q_DECL_OVERRIDE;
     void edit();
 
     int transportId() const;
