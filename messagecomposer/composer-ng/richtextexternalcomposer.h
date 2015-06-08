d78d111ed7011a797b7096e31fd0a2b2e3ae6d14
Montel Laurent <montel@kde.org>
2015-06-08 13:35:49 +0200

    Add richtextexternal features

---
+++ messagecomposer/composer-ng/richtextexternalcomposer.h
@@ -0,0 +1,40 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef RICHTEXTEXTERNALCOMPOSER_H
+#define RICHTEXTEXTERNALCOMPOSER_H
+
+#include "messagecomposer_export.h"
+#include <QObject>
+
+namespace MessageComposer
+{
+class MESSAGECOMPOSER_EXPORT RichTextExternalComposer : public QObject
+{
+public:
+    explicit RichTextExternalComposer(QObject *parent = Q_NULLPTR);
+    ~RichTextExternalComposer();
+    bool useExtEditor() const;
+    void setUseExtEditor(bool value);
+
+private:
+    class RichTextExternalComposerPrivate;
+    RichTextExternalComposerPrivate *const d;
+};
+}
+
+#endif // RICHTEXTEXTERNALCOMPOSER_H
