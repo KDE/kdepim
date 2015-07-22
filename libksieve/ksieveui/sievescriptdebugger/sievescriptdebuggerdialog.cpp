566cafa320e76e04bd95dd5b05fed62a96068f35
Montel Laurent <montel@kde.org>
2015-07-22 13:27:46 +0200

    Start a sieve script debugger.

---
+++ libksieve/ksieveui/sievescriptdebugger/sievescriptdebuggerdialog.cpp
@@ -0,0 +1,55 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "sievescriptdebuggerdialog.h"
+
+#include <KLocalizedString>
+#include <KConfigGroup>
+#include <KSharedConfig>
+
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+
+using namespace KSieveUi;
+
+SieveScriptDebuggerDialog::SieveScriptDebuggerDialog(QWidget *parent)
+    : QDialog(parent)
+{
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    setLayout(mainLayout);
+    readConfig();
+}
+
+SieveScriptDebuggerDialog::~SieveScriptDebuggerDialog()
+{
+    writeConfig();
+}
+
+void SieveScriptDebuggerDialog::writeConfig()
+{
+    KConfigGroup group(KSharedConfig::openConfig(), "SieveScriptDebuggerDialog");
+    group.writeEntry("Size", size());
+}
+
+void SieveScriptDebuggerDialog::readConfig()
+{
+    KConfigGroup group(KSharedConfig::openConfig(), "SieveScriptDebuggerDialog");
+    const QSize sizeDialog = group.readEntry("Size", QSize(800, 600));
+    if (sizeDialog.isValid()) {
+        resize(sizeDialog);
+    }
+}
