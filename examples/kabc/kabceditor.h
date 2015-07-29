3bfc30120115c528cb643ea747a0d07135b94b31
Montel Laurent <montel@kde.org>
2015-07-29 21:46:18 +0200

    Add explicit

--- examples/kabc/kabceditor.h
+++ examples/kabc/kabceditor.h
@@ -36,7 +36,7 @@ class Dialog : public QDialog
     Q_OBJECT
 
 public:
-    Dialog(QWidget *parent = Q_NULLPTR);
+    explicit Dialog(QWidget *parent = Q_NULLPTR);
     ~Dialog();
 
 private Q_SLOTS:
