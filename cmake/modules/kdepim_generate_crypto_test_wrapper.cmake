bcec71d5dc499df66f5a2f61ee180134b4b58522
Volker Krause <vkrause@kde.org>
2015-08-16 14:58:59 +0200

    Don't attempt to kill my system gpg-agent when running cryto tests.

--- cmake/modules/kdepim_generate_crypto_test_wrapper.cmake
+++ cmake/modules/kdepim_generate_crypto_test_wrapper.cmake
@@ -11,6 +11,9 @@ file(WRITE "${_filename}"
 "#!/bin/sh
 # created by cmake, don't edit, changes will be lost
 
+# don't mess with a gpg-agent already running on the system
+unset GPG_AGENT_INFO
+
 ${_library_path_variable}=${_ld_library_path}\${${_library_path_variable}:+:\$${_library_path_variable}} GNUPGHOME=${_gnupghome} gpg-agent --daemon \"${_executable}\" \"$@\"
 _result=$?
 _pid=`echo GETINFO pid | GNUPGHOME=${_gnupghome} gpg-connect-agent | grep 'D' | cut -d' ' -f2`
