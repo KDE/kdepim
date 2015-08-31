e0e7fe31a61f038b9651a7be1925a5e9f48f2663
Montel Laurent <montel@kde.org>
2015-08-31 21:38:03 +0200

    Don't export this symbol too

--- pimsettingexporter/core/utils.h
+++ pimsettingexporter/core/utils.h
@@ -94,7 +94,7 @@ PIMSETTINGEXPORTER_EXPORT QString jotPath();
 PIMSETTINGEXPORTER_EXPORT QString infoPath();
 PIMSETTINGEXPORTER_EXPORT QString notePath();
 PIMSETTINGEXPORTER_EXPORT QString prefixAkonadiConfigFile();
-PIMSETTINGEXPORTER_EXPORT QString akonadiAgentName(const QString &configPath);
+QString akonadiAgentName(const QString &configPath);
 
 void convertCollectionListToRealPath(KConfigGroup &group, const QString &currentKey);
 void convertCollectionToRealPath(KConfigGroup &group, const QString &currentKey);
