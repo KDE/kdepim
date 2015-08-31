5ae458024659e64471ccffc13398129fcf221c3f
Montel Laurent <montel@kde.org>
2015-08-31 08:48:34 +0200

    Remove old comment

--- blogilo/src/composer/bilbobrowser.cpp
+++ blogilo/src/composer/bilbobrowser.cpp
@@ -20,7 +20,6 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, see http://www.gnu.org/licenses/
 */
-//krazy:excludeall=qmethods due to use of KStatusBar::showMessage()
 
 #include "bilbobrowser.h"
 
