7d680b459b9e1725854b321bcc8e667976124012
Montel Laurent <montel@kde.org>
2015-09-07 21:58:01 +0200

    Add interface class

---
+++ messageviewer/header/headerstyleinterface.h
@@ -0,0 +1,41 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef HEADERSTYLEINTERFACE_H
+#define HEADERSTYLEINTERFACE_H
+
+#include <QObject>
+#include "messageviewer_export.h"
+class KToggleAction;
+namespace MessageViewer
+{
+class HeaderStyle;
+class HeaderStrategy;
+class MESSAGEVIEWER_EXPORT HeaderStyleInterface : public QObject
+{
+    Q_OBJECT
+public:
+    explicit HeaderStyleInterface(MessageViewer::HeaderStyle *headerStyle, MessageViewer::HeaderStrategy *headerStrategy, QObject *parent = Q_NULLPTR);
+    virtual ~HeaderStyleInterface();
+    virtual KToggleAction *action() const = 0;
+
+protected:
+    HeaderStyle *mHeaderStyle;
+    HeaderStrategy *mHeaderStrategy;
+};
+}
+#endif // HEADERSTYLEINTERFACE_H
