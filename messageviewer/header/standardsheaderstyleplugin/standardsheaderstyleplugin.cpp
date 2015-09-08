64537097a65e5a8131acf893ab32ed373146d7de
Montel Laurent <montel@kde.org>
2015-09-08 13:54:32 +0200

    Add standard header plugin

---
+++ messageviewer/header/standardsheaderstyleplugin/standardsheaderstyleplugin.cpp
@@ -0,0 +1,55 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "standardsheaderstyleplugin.h"
+#include "header/fancyheaderstyle.h"
+//Temporary
+#include "header/headerstrategy_p.h"
+#include <kpluginfactory.h>
+using namespace MessageViewer;
+
+K_PLUGIN_FACTORY_WITH_JSON(MessageViewerFancyHeaderStylePluginFactory, "messageviewer_standardsheaderstyleplugin.json", registerPlugin<StandardsHeaderStylePlugin>();)
+
+StandardsHeaderStylePlugin::StandardsHeaderStylePlugin(QObject *parent, const QList<QVariant> &)
+    : MessageViewer::HeaderStylePlugin(parent),
+      mHeaderStyle(new FancyHeaderStyle),
+      mHeaderStrategy(new RichHeaderStrategy)
+{
+}
+
+StandardsHeaderStylePlugin::~StandardsHeaderStylePlugin()
+{
+
+}
+
+HeaderStyle *StandardsHeaderStylePlugin::headerStyle() const
+{
+    return mHeaderStyle;
+}
+
+HeaderStrategy *StandardsHeaderStylePlugin::headerStrategy() const
+{
+    return mHeaderStrategy;
+}
+
+HeaderStyleInterface *StandardsHeaderStylePlugin::createView(KActionCollection *ac, QObject *parent)
+{
+    //TODO
+    return Q_NULLPTR;
+}
+
+#include "standardsheaderstyleplugin.moc"
