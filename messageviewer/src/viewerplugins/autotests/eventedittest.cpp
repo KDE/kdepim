KDE/kdepim

commit c988586fca929ec14ae1543f95ea722e04ec748a

Fix Bug 352168 - kMail 5.0.0: Creating an event based on a mail prepends "Reply to" to the event title
FIXED-IN: 5.1
BUG: 352168

messageviewer/src/viewerplugins/createeventplugin/eventedit.cpp
@@ -230,7 +230,7 @@ void EventEdit::setMessage(const KMime::Message::Ptr &value)
         mMessage = value;
         const KMime::Headers::Subject *const subject = mMessage ? mMessage->subject(false) : 0;
         if (subject) {
-            mEventEdit->setText(i18n("Reply to \"%1\"", subject->asUnicodeString()));
+            mEventEdit->setText(i18n("Event about to \"%1\"", subject->asUnicodeString()));
             mEventEdit->selectAll();
             mEventEdit->setFocus();
         } else {

commit e06da7273102fc49f5b9a238194cde0d4ec12981

Fix wording of subject of events created from emails.
"Event about to <subject>" sounds weird, let's just use the subject
directly, which is also more in line with bug 352168.

This also fixes the corresponding unit tests that regressed in c988586.

messageviewer/src/viewerplugins/autotests/eventedittest.cpp
@@ -260,7 +260,7 @@ void EventEditTest::shouldEventHasCorrectSubject()
     QCOMPARE(spy.count(), 1);
     KCalCore::Event::Ptr eventPtr = spy.at(0).at(0).value<KCalCore::Event::Ptr>();
     QVERIFY(eventPtr);
-    QCOMPARE(eventPtr->summary(), QStringLiteral("Reply to \"%1\"").arg(subject));
+    QCOMPARE(eventPtr->summary(), subject);
 }
 
 void EventEditTest::shouldSelectLineWhenPutMessage()
@@ -274,7 +274,7 @@ void EventEditTest::shouldSelectLineWhenPutMessage()
     QVERIFY(noteedit);
     QVERIFY(noteedit->hasSelectedText());
     const QString selectedText = noteedit->selectedText();
-    QCOMPARE(selectedText, QStringLiteral("Reply to \"%1\"").arg(subject));
+    QCOMPARE(selectedText, subject);
 }
 
 void EventEditTest::shouldHaveCorrectStartEndDateTime()

messageviewer/src/viewerplugins/createeventplugin/eventedit.cpp
@@ -230,7 +230,7 @@ void EventEdit::setMessage(const KMime::Message::Ptr &value)
         mMessage = value;
         const KMime::Headers::Subject *const subject = mMessage ? mMessage->subject(false) : 0;
         if (subject) {
-            mEventEdit->setText(i18n("Event about to \"%1\"", subject->asUnicodeString()));
+            mEventEdit->setText(subject->asUnicodeString());
             mEventEdit->selectAll();
             mEventEdit->setFocus();
         } else {