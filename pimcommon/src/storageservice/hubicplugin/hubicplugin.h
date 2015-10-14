e3cc36228922d8f60d1358d974c83dee24b2cf14
Montel Laurent <montel@kde.org>
2015-10-14 08:17:08 +0200

    Add hubic

---
+++ pimcommon/src/storageservice/hubicplugin/hubicplugin.h
@@ -0,0 +1,38 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef HUBICPLUGIN_H
+#define HUBICPLUGIN_H
+
+#include <storageservice/storageserviceplugin.h>
+
+#include <QVariant>
+
+
+namespace PimCommon
+{
+class HubicPlugin : public PimCommon::StorageServicePlugin
+{
+    Q_OBJECT
+public:
+    explicit HubicPlugin(QObject *parent = Q_NULLPTR, const QList<QVariant> & = QList<QVariant>());
+    ~HubicPlugin();
+
+    QString storageServiceName() const Q_DECL_OVERRIDE;
+};
+}
+#endif // HUBICPLUGIN_H
