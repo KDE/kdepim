4770e741d791b97a1b71725f5f4390f72a5db0a2
Daniel Vrátil <dvratil@kde.org>
2015-10-31 17:50:16 +0100

    Fix typo

--- messageviewer/src/viewerplugins/autotests/eventedittest.cpp
+++ messageviewer/src/viewerplugins/autotests/eventedittest.cpp
@@ -398,7 +398,7 @@ void EventEditTest::shouldUpdateStartEndDateWhenReopenIt()
     QCOMPARE(startDateTime->dateTime().time().minute(), newDateTime.time().minute());
     QCOMPARE(startDateTime->dateTime().date(), newDateTime.date());
 
-    newDateTime = newDateTime.addMSecs(3600);
+    newDateTime = newDateTime.addSecs(3600);
     QCOMPARE(endDateTime->dateTime().time().hour(), newDateTime.time().hour());
     QCOMPARE(endDateTime->dateTime().time().minute(), newDateTime.time().minute());
     QCOMPARE(endDateTime->dateTime().date(), newDateTime.date());
