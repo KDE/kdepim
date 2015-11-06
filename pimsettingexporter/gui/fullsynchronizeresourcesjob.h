f0c07b5f853e3240435f8b3f82ee85706d7088cc
Montel Laurent <montel@kde.org>
2015-11-06 07:45:05 +0100

    Add job to sync resource

---
+++ pimsettingexporter/gui/fullsynchronizeresourcesjob.h
@@ -0,0 +1,45 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef FULLSYNCHRONIZERESOURCESJOB_H
+#define FULLSYNCHRONIZERESOURCESJOB_H
+
+#include <QObject>
+#include <QStringList>
+
+class QWidget;
+class FullSynchronizeResourcesJob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FullSynchronizeResourcesJob(QObject *parent = Q_NULLPTR);
+    ~FullSynchronizeResourcesJob();
+
+    void setResources(const QStringList &lst);
+
+    void setWindowParent(QWidget *parent);
+
+    void start();
+Q_SIGNALS:
+    void synchronizeFinished();
+
+private:
+    QStringList mResources;
+    QWidget *mWindowParent;
+};
+
+#endif // FULLSYNCHRONIZERESOURCESJOB_H
