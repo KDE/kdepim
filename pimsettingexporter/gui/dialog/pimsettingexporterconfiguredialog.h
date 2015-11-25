b29f19b3bdbb0f885a2f79812e06b8d23dbe94e0
Montel Laurent <montel@kde.org>
2015-11-25 21:12:57 +0100

    Start to create settings dialog

---
+++ pimsettingexporter/gui/dialog/pimsettingexporterconfiguredialog.h
@@ -0,0 +1,31 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef PIMSETTINGEXPORTERCONFIGUREDIALOG_H
+#define PIMSETTINGEXPORTERCONFIGUREDIALOG_H
+
+#include <QDialog>
+
+class PimSettingExporterConfigureDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit PimSettingExporterConfigureDialog(QWidget *parent = Q_NULLPTR);
+    ~PimSettingExporterConfigureDialog();
+};
+
+#endif // PIMSETTINGEXPORTERCONFIGUREDIALOG_H
