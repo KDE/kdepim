06b153fde9d0bece08468d2025b6c9ed62695d01
Volker Krause <vkrause@kde.org>
2015-12-05 13:46:28 +0100

    Adapt crypto tests to the changed directory layout.

--- cmake/modules/kdepim_add_gpg_crypto_test.cmake
+++ cmake/modules/kdepim_add_gpg_crypto_test.cmake
@@ -3,7 +3,7 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-set( GNUPGHOME ${CMAKE_BINARY_DIR}/messagecore/autotests/gnupg_home )
+set( GNUPGHOME ${CMAKE_BINARY_DIR}/messagelib/messagecore/autotests/gnupg_home )
 add_definitions( -DGNUPGHOME="\\"${GNUPGHOME}\\"" )
 
 macro (ADD_GPG_CRYPTO_TEST _target _testname)
