e63e79fd1b1520b785fab07091ae43c53e9268aa
Montel Laurent <montel@kde.org>
2015-12-07 21:40:52 +0100

    Start to implement plugin for kaddressbook

---
+++ kaddressbook/plugininterface/plugininterface.cpp
@@ -0,0 +1,79 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "plugininterface.h"
+#include "pimcommon/genericpluginmanager.h"
+#include "kaddressbook_debug.h"
+
+#include <KActionCollection>
+
+#include <pimcommon/genericplugin.h>
+
+PluginInterface::PluginInterface(KActionCollection *ac, QObject *parent)
+    : QObject(parent),
+      mParentWidget(Q_NULLPTR),
+      mActionCollection(ac)
+{
+    PimCommon::GenericPluginManager::self()->setPluginName(QStringLiteral("kaddressbook"));
+    PimCommon::GenericPluginManager::self()->setServiceTypeName(QStringLiteral("KAddressBook/MainViewPlugin"));
+    if (!PimCommon::GenericPluginManager::self()->initializePlugins()) {
+        qCDebug(KADDRESSBOOK_LOG()) << " Impossible to initialize plugins";
+    }
+}
+
+PluginInterface::~PluginInterface()
+{
+
+}
+
+void PluginInterface::createPluginInterface()
+{
+    Q_FOREACH(PimCommon::GenericPlugin *plugin, PimCommon::GenericPluginManager::self()->pluginsList()) {
+        PimCommon::GenericPluginInterface *interface = plugin->createInterface(mActionCollection, mParentWidget);
+        connect(interface, &PimCommon::GenericPluginInterface::emitPluginActivated, this, &PluginInterface::slotPluginActivated);
+        mListGenericInterface.append(interface);
+    }
+}
+
+void PluginInterface::slotPluginActivated(PimCommon::GenericPluginInterface *interface)
+{
+    if (interface) {
+        interface->exec();
+    }
+}
+
+void PluginInterface::setParentWidget(QWidget *widget)
+{
+    mParentWidget = widget;
+}
+
+QHash<PimCommon::ActionType::Type, QList<QAction *> > PluginInterface::actionsType() const
+{
+    QHash<PimCommon::ActionType::Type, QList<QAction *> > listType;
+    Q_FOREACH(PimCommon::GenericPluginInterface *interface, mListGenericInterface) {
+        PimCommon::ActionType actionType = interface->actionType();
+        if (listType.contains(actionType.type())) {
+            QList<QAction *> lst = listType.value(actionType.type());
+            lst << actionType.action();
+            listType.insert(actionType.type(), lst);
+        } else {
+            listType.insert(actionType.type(), QList<QAction *>() << actionType.action());
+        }
+    }
+
+    return listType;
+}
---
+++ kaddressbook/plugininterface/plugininterface.h
@@ -0,0 +1,47 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef PLUGININTERFACE_H
+#define PLUGININTERFACE_H
+
+#include <QObject>
+#include <QVector>
+
+#include <pimcommon/genericplugininterface.h>
+class KActionCollection;
+class QAction;
+class PluginInterface : public QObject
+{
+    Q_OBJECT
+public:
+    explicit PluginInterface(KActionCollection *ac, QObject *parent = Q_NULLPTR);
+    ~PluginInterface();
+
+    void setParentWidget(QWidget *widget);
+    QHash<PimCommon::ActionType::Type, QList<QAction *> > actionsType() const;
+    void createPluginInterface();
+
+private Q_SLOTS:
+    void slotPluginActivated(PimCommon::GenericPluginInterface *interface);
+
+private:
+    QWidget *mParentWidget;
+    KActionCollection *mActionCollection;
+    QVector<PimCommon::GenericPluginInterface *> mListGenericInterface;
+};
+
+#endif // PLUGININTERFACE_H
