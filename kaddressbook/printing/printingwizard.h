c8c83bcb26feced697800c914cbd1e1c5b073703
Montel Laurent <montel@kde.org>
2016-01-27 21:37:56 +0100

    Fix copyright

--- kaddressbook/printing/printingwizard.h
+++ kaddressbook/printing/printingwizard.h
@@ -3,7 +3,7 @@
   Copyright (c) 1996-2002 Mirko Boehm <mirko@kde.org>
                           Tobias Koenig <tokoe@kde.org>
 
-  Copyright (c) 2009-2015 Laurent Montel <montel@kde.org>
+  Copyright (c) 2009-2016 Laurent Montel <montel@kde.org>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
--- plugins/messageviewer/bodypartformatter/calendar/delegateselector.h
+++ plugins/messageviewer/bodypartformatter/calendar/delegateselector.h
@@ -1,6 +1,6 @@
 /*
     Copyright (c) 2007 Volker Krause <vkrause@kde.org>
-    Copyright (C) 2012-2015 Laurent Montel <montel@kde.org>
+    Copyright (C) 2012-2016 Laurent Montel <montel@kde.org>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
