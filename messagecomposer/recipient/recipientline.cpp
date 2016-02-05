021e3c7fe16d93bc946dadf7a3109a8ef8dfb7b1
Montel Laurent <laurent.montel@kdab.com>
2016-02-05 11:12:56 +0000

    Remove clear button

--- messagecomposer/recipient/recipientline.cpp
+++ messagecomposer/recipient/recipientline.cpp
@@ -84,7 +84,7 @@ RecipientLineNG::RecipientLineNG( QWidget* parent )
     mCombo->setToolTip( i18nc("@label:listbox","Select type of recipient") );
     mEdit = new RecipientLineEdit( this );
     mEdit->setToolTip( i18n( "Set the list of email addresses to receive this message" ) );
-    mEdit->setClearButtonShown( true );
+    mEdit->setClearButtonShown( false );
     topLayout->addWidget( mEdit );
 
     connect( mEdit, SIGNAL(returnPressed()), SLOT(slotReturnPressed()) );
