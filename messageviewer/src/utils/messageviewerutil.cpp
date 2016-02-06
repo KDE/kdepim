43c23ea9a360f1aafe3229975774464569d27ed2
Montel Laurent <montel@kde.org>
2016-02-06 15:01:17 +0100

    Fix [kmail2] [Bug 357557] New: Save attachment: Dialog does not show correct folder
    FIXED-IN: 5.1.2
    BUG: 357557

--- messageviewer/src/utils/messageviewerutil.cpp
+++ messageviewer/src/utils/messageviewerutil.cpp
@@ -52,7 +52,7 @@
 #include <kmbox/mbox.h>
 
 #include <KMime/Message>
-
+#include <KFileWidget>
 #include <kcharsets.h>
 #include <KLocalizedString>
 #include <kmessagebox.h>
@@ -61,7 +61,7 @@
 #include <KJobWidgets>
 #include <KIO/StatJob>
 #include <KIO/FileCopyJob>
-
+#include <KRecentDirs>
 #include <QAction>
 #include <QIcon>
 #include <QTemporaryFile>
@@ -462,26 +462,18 @@ bool Util::saveMessageInMbox(const Akonadi::Item::List &retrievedMsgs, QWidget *
     }
 
     const QString filter = i18n("email messages (*.mbox);;all files (*)");
-    QPointer<QFileDialog> dlg = new QFileDialog(parent, i18np("Save Message", "Save Messages", retrievedMsgs.count()), fileName, filter);
-    dlg->setFileMode(QFileDialog::AnyFile);
-    dlg->setAcceptMode(QFileDialog::AcceptSave);
 
-    if (appendMessages) {
-        dlg->setOption(QFileDialog::DontConfirmOverwrite);
-    }
-    if (dlg->exec()) {
-        QList<QUrl> url = dlg->selectedUrls();
-        if (url.isEmpty()) {
-            delete dlg;
-            return true;
-        }
+    QString fileClass;
+    const QUrl startUrl = KFileWidget::getStartUrl(QUrl(QStringLiteral("kfiledialog:///savemessage")), fileClass);
+    QFileDialog::Options opt;
+    if (appendMessages)
+        opt |= QFileDialog::DontConfirmOverwrite;
+    QString localFile = startUrl.toLocalFile() + QLatin1Char('/') + fileName;
+    QString saveFileName = QFileDialog::getSaveFileName(parent, i18np("Save Message", "Save Messages", retrievedMsgs.count()), localFile, filter, Q_NULLPTR, opt);
 
-        const QString localFileName = url.at(0).toLocalFile();
-        if (localFileName.isEmpty()) {
-            delete dlg;
-            return true;
-        }
 
+    if(!saveFileName.isEmpty()) {
+        const QString localFileName = saveFileName;
         if (!appendMessages) {
             QFile::remove(localFileName);
         }
@@ -493,7 +485,6 @@ bool Util::saveMessageInMbox(const Akonadi::Item::List &retrievedMsgs, QWidget *
             } else {
                 KMessageBox::error(parent, i18n("File %1 could not be created.", localFileName), i18n("Error saving message"));
             }
-            delete dlg;
             return false;
         }
         foreach (const Akonadi::Item &item, retrievedMsgs) {
@@ -504,11 +495,13 @@ bool Util::saveMessageInMbox(const Akonadi::Item::List &retrievedMsgs, QWidget *
 
         if (!mbox.save()) {
             KMessageBox::error(parent, i18n("We cannot save message."), i18n("Error saving message"));
-            delete dlg;
             return false;
         }
+        QUrl url = QUrl::fromLocalFile(saveFileName);
+        if(url.isLocalFile()) {
+            KRecentDirs::add(fileClass, url.adjusted(QUrl::RemoveFilename|QUrl::StripTrailingSlash).path());
+        }
     }
-    delete dlg;
     return true;
 }
 
