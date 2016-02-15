89bf5866ba61fe953570b601c3301425da7a888c
Andre Heinecke <aheinecke@intevation.de>
2016-02-15 11:12:08 +0100

    Remove unused windows defines
    kde_file_win.h no longer exists and causes an error. It's also
    not necessary. KPATH_SEPERATOR is also not used anymore.

--- kleopatra/config-kleopatra.h.cmake
+++ kleopatra/config-kleopatra.h.cmake
@@ -21,17 +21,5 @@
 /* Define to 1 if you build libkleopatraclient */
 #cmakedefine HAVE_KLEOPATRACLIENT_LIBRARY 1
 
-#if defined _WIN32 || defined _WIN64
-#include <kde_file_win.h>
-#endif
-
-#if !defined(KPATH_SEPARATOR)
-#if defined _WIN32 || defined _WIN64
-#define KPATH_SEPARATOR ';'
-#else
-#define KPATH_SEPARATOR ':'
-#endif
-#endif
-
 /* Mirrored from cmake option */
 #cmakedefine KLEO_STATIC_KCMODULES 1
