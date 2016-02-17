ada45f6bb32820c28c04e2f66a8adf4995e2cb16
Andre Heinecke <aheinecke@intevation.de>
2016-02-17 14:56:52 +0100

    Fix formatting in selftest details
    xi18nc escapes richtext in arguments so the messages are
    not formated. This bypasses this by using QString argument
    extension, while not modifying the original string.
    
    REVIEW: 127099

--- kleopatra/selftest/libkleopatrarccheck.cpp
+++ kleopatra/selftest/libkleopatrarccheck.cpp
@@ -75,10 +75,15 @@ public:
         }
         m_error = i18n("Errors found");
 
+        // The building of the following string is a bit of a hack to avoid
+        // that xi18nc does not escape the html tags while not breaking
+        // the historic string.
         m_explaination
             = xi18nc("@info",
                      "<para>Kleopatra detected the following errors in the libkleopatrarc configuration:</para>"
-                     "%1", QStringLiteral("<ol><li>") + errors.join(QStringLiteral("</li><li>")) + QLatin1String("</li></ol>"));
+                     "%1", QStringLiteral("%1")).arg(QStringLiteral("<ol><li>") +
+                                                     errors.join(QStringLiteral("</li><li>")) +
+                                                     QStringLiteral("</li></ol>"));
     }
 
     ///* reimp */ bool canFixAutomatically() const { return false; }
