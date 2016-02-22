fa0e4ccd36b4a5691f3bc05b26580bd8c6677f39
Montel Laurent <montel@kde.org>
2016-02-22 20:08:41 +0100

    Use qtpaths directly

--- akonadiconsole/kconf_update/akonadiconsole-15.08-kickoff.sh
+++ akonadiconsole/kconf_update/akonadiconsole-15.08-kickoff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kickoffrcname=`kf5-config --path config --locate kickoffrc`
+kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
 if [ -f "$kickoffrcname" ]; then
    sed -i "s/\/akonadiconsole.desktop/\/org.kde.akonadiconsole.desktop/" $kickoffrcname
 fi
--- importwizard/kconf_update/importwizard-15.08-kickoff.sh
+++ importwizard/kconf_update/importwizard-15.08-kickoff.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kickoffrcname=`kf5-config --path config --locate kickoffrc`
+kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
 if [ -f "$kickoffrcname" ]; then
    sed -i "s/\/importwizard.desktop/\/org.kde.importwizard.desktop/" $kickoffrcname
 fi
